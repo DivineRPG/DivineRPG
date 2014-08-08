8e53bf14090be452f308f8af46b3202dc83bf197
Eternaldoom <mezack@icloud.com>
2014-08-08 06:31:06 -0700

    Update TwilightRecipeHelper.java

--- java/net/divinerpg/utils/recipes/TwilightRecipeHelper.java
+++ java/net/divinerpg/utils/recipes/TwilightRecipeHelper.java
@@ -137,7 +137,7 @@ public class TwilightRecipeHelper extends RecipeUtil {
 		addRecipe(TwilightItemsTools.skythernShickaxe, TwilightItemsTools.skythernPickaxe, TwilightItemsTools.skythernShovel, TwilightItemsTools.skythernAxe);
 		
 		addRecipe(TwilightItemsTools.mortumPickaxe, "III", " S ", " S ", TwilightItemsOther.mortumChunk, 'S', Items.stick);
-		addRecipe(TwilightItemsTools.mortumShove, " I ", " S ", " S ", TwilightItemsOther.mortumChunk, 'S', Items.stick);
+		addRecipe(TwilightItemsTools.mortumShovel, " I ", " S ", " S ", TwilightItemsOther.mortumChunk, 'S', Items.stick);
 		addRecipe(TwilightItemsTools.mortumAxe, "II ", "IS ", " S ", TwilightItemsOther.mortumChunk, 'S', Items.stick);
 		addShapelessRecipe(TwilightItemsTools.mortumShickaxe, TwilightItemsTools.mortumPickaxe, TwilightItemsTools.mortumShovel, TwilightItemsTools.mortumAxe);
 
