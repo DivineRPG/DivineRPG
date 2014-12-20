5221c0d26803c14c7b82187eeab3a82d651e74b1
Eternaldoom <mezack@icloud.com>
2014-12-19 17:34:31 -0800

    fix bad

--- java/net/divinerpg/client/render/gui/GUIOverlay.java
+++ java/net/divinerpg/client/render/gui/GUIOverlay.java
@@ -21,6 +21,7 @@ public class GUIOverlay {
 	private ScaledResolution res;
 
 	public void drawOverlay() {
+		res = new ScaledResolution(Minecraft.getMinecraft(), Minecraft.getMinecraft().displayWidth, Minecraft.getMinecraft().displayHeight);
 		if(!seen) {
 			try {
 				text = Reference.MOD_NAME + " " + Util.GREEN + Reference.MOD_VERSION;
@@ -45,29 +46,32 @@ public class GUIOverlay {
 		GL11.glDisable(GL11.GL_BLEND);
 		
 		if(Minecraft.getMinecraft().gameSettings.showDebugInfo) {
-			Minecraft.getMinecraft().fontRenderer.drawStringWithShadow(EnumChatFormatting.DARK_BLUE + text, 2, res.getScaledHeight()-117, 4210752);
+			Minecraft.getMinecraft().fontRenderer.drawStringWithShadow(EnumChatFormatting.DARK_BLUE + text, 2, res.getScaledHeight()-10, 4210752);
 		} else if(!Minecraft.getMinecraft().gameSettings.showDebugInfo) {
 			Minecraft.getMinecraft().fontRenderer.drawString(text2, 2, 2, 4210752);
 		}
 	}
 	public void drawArmor() {
-		ResourceLocation r = new ResourceLocation(Reference.PREFIX + "textures/gui/armorBar.png");
-		res = new ScaledResolution(Minecraft.getMinecraft(), Minecraft.getMinecraft().displayWidth, Minecraft.getMinecraft().displayHeight);
-		int x = (res.getScaledWidth() / 2) - 11;
-		int y = res.getScaledHeight() - 49;
-		GL11.glPushMatrix();
-		GL11.glEnable(GL11.GL_BLEND);
-		switch(EventArmorTick.size) {
-		case 1:
-			Minecraft.getMinecraft().getTextureManager().bindTexture(r);
-			Util.drawTexturedModalRect(x, y, 0, 0, 9, 9);
-			break;
-		case 2: //Not used. Here if we need it.
-			Minecraft.getMinecraft().getTextureManager().bindTexture(r);
-			Util.drawTexturedModalRect(x, y, 9, 0, 18, 9);
-			break;
+		if(!Minecraft.getMinecraft().thePlayer.capabilities.isCreativeMode) {
+			GL11.glDisable(GL11.GL_BLEND);
+			GL11.glColor4f(1, 1, 1, 1);
+			ResourceLocation r = new ResourceLocation(Reference.PREFIX + "textures/gui/armorBar.png");
+			res = new ScaledResolution(Minecraft.getMinecraft(), Minecraft.getMinecraft().displayWidth, Minecraft.getMinecraft().displayHeight);
+			int x = (res.getScaledWidth() / 2) - 11;
+			int y = res.getScaledHeight() - 49;
+			GL11.glPushMatrix();
+			switch(EventArmorTick.size) {
+			case 1:
+				Minecraft.getMinecraft().getTextureManager().bindTexture(r);
+				Util.drawTexturedModalRect(x, y, 0, 0, 9, 9);
+				break;
+			case 2: //Not used. Here if we need it.
+				Minecraft.getMinecraft().getTextureManager().bindTexture(r);
+				Util.drawTexturedModalRect(x, y, 9, 0, 18, 9);
+				break;
+			}
+			GL11.glPopMatrix();
+			GL11.glDisable(GL11.GL_BLEND);
 		}
-		GL11.glPopMatrix();
-		GL11.glDisable(GL11.GL_BLEND);
 	}
 }
