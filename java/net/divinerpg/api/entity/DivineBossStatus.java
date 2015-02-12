ad7074fdc64e60e46fb922881b26bda9669ed5ed
krwminer <krwshop@hotmail.com>
2015-02-11 22:02:47 -0500

    Check to make sure the rendered boss is real before setting the boss bar. Fixes #163

--- java/net/divinerpg/api/entity/DivineBossStatus.java
+++ java/net/divinerpg/api/entity/DivineBossStatus.java
@@ -11,8 +11,11 @@ public final class DivineBossStatus {
     public static String bossName;
 
     public static void setBossStatus(EntityDivineRPGBoss b, int boss) {
-        healthScale = b.getHealth() / b.getMaxHealth();
-        statusBarTime = 100;
-        bossNumber = boss;
+    	// Make sure that the rendered Boss is real. (Fixes glitch caused by bosses rendered in NEI spawners).
+        if (b.posX != 0 || b.posY != 0 || b.posZ != 0) {
+        	healthScale = b.getHealth() / b.getMaxHealth();
+            statusBarTime = 100;
+            bossNumber = boss;
+        }
     }
 }
