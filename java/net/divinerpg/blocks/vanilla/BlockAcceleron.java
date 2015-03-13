5260c3db91bff01397ffb8dd7d1a8fd6c048afb4
RadioactiveStud <caleb@divinerpg.net>
2015-03-13 13:51:28 -0400

    Added acceleron directional placement. Closes #221

--- java/net/divinerpg/blocks/vanilla/BlockAcceleron.java
+++ java/net/divinerpg/blocks/vanilla/BlockAcceleron.java
@@ -14,10 +14,13 @@ import net.minecraft.world.World;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class BlockAcceleron extends BlockMod{
+public class BlockAcceleron extends BlockMod {
 
-	private IIcon top;
 	private IIcon side;
+	private IIcon top_n;
+	private IIcon top_s;
+	private IIcon top_e;
+	private IIcon top_w;
 	
 	public BlockAcceleron() {
 		super(EnumBlockType.ROCK, "acceleron", 3.0F);
@@ -28,12 +31,25 @@ public class BlockAcceleron extends BlockMod{
     @SideOnly(Side.CLIENT)
     public void registerIcons(IIconRegister icon) {
         side = icon.registerIcon(Reference.PREFIX + "acceleron" + "_side");
-        top = icon.registerIcon(Reference.PREFIX + "acceleron" + "_top");
+        top_n = icon.registerIcon(Reference.PREFIX + "acceleron" + "_north");
+        top_s = icon.registerIcon(Reference.PREFIX + "acceleron" + "_south");
+        top_e = icon.registerIcon(Reference.PREFIX + "acceleron" + "_east");
+        top_w = icon.registerIcon(Reference.PREFIX + "acceleron" + "_west");
     }
 	
     @Override
-    public IIcon getIcon(int par1, int par2) {
-        int var3 = par2 & 12;
-        return var3 == 0 && (par1 == 1 || par1 == 0) ? this.top : (var3 == 4 && (par1 == 5 || par1 == 4) ? this.top  : (var3 == 8 && (par1 == 2 || par1 == 3) ? top  : this.side ));
+    public IIcon getIcon(int side, int meta) {
+    	if(side == 0 || side == 1)
+    		return meta == 2 ? top_n : (meta == 3 ? top_s : (meta == 4 ? top_w : top_e));
+    	return this.side;
     }
+    
+    @Override
+	public void onBlockPlacedBy(World w, int x, int y, int z, EntityLivingBase living, ItemStack item) {
+		int meta = MathHelper.floor_double((double)(living.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+		if(meta == 0) w.setBlockMetadataWithNotify(x, y, z, 2, 2);
+		if(meta == 1) w.setBlockMetadataWithNotify(x, y, z, 5, 2);
+		if(meta == 2) w.setBlockMetadataWithNotify(x, y, z, 3, 2);
+		if(meta == 3) w.setBlockMetadataWithNotify(x, y, z, 4, 2);
+	}
 }
