91fc28c71bdda4e427f3f36faf64e51ba52b6b98
Eternaldoom <mezack@icloud.com>
2015-09-20 21:44:46 -0700

    sky flower tooltip

--- java/net/divinerpg/items/vanilla/ItemSkyFlower.java
+++ java/net/divinerpg/items/vanilla/ItemSkyFlower.java
@@ -1,5 +1,7 @@
 package net.divinerpg.items.vanilla;
 
+import java.util.List;
+
 import net.divinerpg.items.base.ItemFastFood;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -21,5 +23,10 @@ public class ItemSkyFlower extends ItemFastFood {
         player.addPotionEffect(new PotionEffect(Potion.moveSpeed.id, 600, 0, true));
         return stack;
     }
-
+    
+    @Override
+    public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean par4) {
+        super.addInformation(stack, player, list, par4);
+        list.add("Gives Speed and Water Breathing for 30 seconds.");
+    }
 }
