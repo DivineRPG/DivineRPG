627c5a54de12db515f13a3b2b759492d6a7fa395
NicosaurusRex99 <pusspot1999@gmail.com>
2018-01-13 15:35:10 +1030

    Added new furnace recipes

--- src/main/java/naturix/divinerpg/utils/ModRecipes.java
+++ src/main/java/naturix/divinerpg/utils/ModRecipes.java
@@ -8,6 +8,13 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class ModRecipes {
 	public static void init() {
 		GameRegistry.addSmelting(ModBlocks.oreArlemite, new ItemStack(ModItems.ingotArlemite), 0.7f);
+		GameRegistry.addSmelting(ModBlocks.oreapalachia, new ItemStack(ModItems.gemApalachia), 0.2f);
+		GameRegistry.addSmelting(ModBlocks.oreEden, new ItemStack(ModItems.gemEden), 0.2f);
+		GameRegistry.addSmelting(ModBlocks.oreNetherite, new ItemStack(ModItems.ingotNetherite), 0.2f);
+		GameRegistry.addSmelting(ModBlocks.oreRealmite, new ItemStack(ModItems.ingotRealmite), 0.2f);
+		GameRegistry.addSmelting(ModBlocks.oreRupee, new ItemStack(ModItems.ingotRupee), 0.2f);
+		GameRegistry.addSmelting(ModBlocks.oreWildwood, new ItemStack(ModItems.gemWildwood), 0.2f);
+		GameRegistry.addSmelting(ModBlocks.oreSkythern, new ItemStack(ModItems.gemSkythern), 0.2f);
 
 	}
 }
