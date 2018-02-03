fe516fa30fe32a7eb600aa9f24196a4363e10cf4
NicosaurusRex99 <pusspot1999@gmail.com>
2018-02-03 19:14:51 +1030

    Quite cheating. turns out im gonna write recipes all myself *sigh*

--- src/main/java/naturix/divinerpg/registry/ModRecipes.java
+++ src/main/java/naturix/divinerpg/registry/ModRecipes.java
@@ -1,7 +1,5 @@
 package naturix.divinerpg.registry;
 
-import naturix.divinerpg.utils.DivineRecipeCheats;
-import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -16,22 +14,5 @@ public class ModRecipes {
 		GameRegistry.addSmelting(ModBlocks.oreWildwood, new ItemStack(ModItems.gemWildwood), 0.2f);
 		GameRegistry.addSmelting(ModBlocks.oreSkythern, new ItemStack(ModItems.gemSkythern), 0.2f);
 		}
-	static void registerShaped(ItemStack output, Object... inputs) {
-		DivineRecipeCheats.addShapedOreRecipe(output, inputs);
-	}
 
-	static void registerShapeless(ItemStack output, Object... inputs) {
-		DivineRecipeCheats.addShapelessOreRecipe(output, inputs);
-	}
-	static void addToolAndArmourRecipes(ItemStack sword, ItemStack pickaxe, ItemStack axe, ItemStack hoe, ItemStack spade, ItemStack helmet, ItemStack chestplate, ItemStack leggings, ItemStack boots, String material) {
-		registerShaped(sword, "G", "G", "S", 'S', Items.STICK, 'G', material);
-		registerShaped(pickaxe, "GGG", " S ", " S ", 'S', Items.STICK, 'G', material);
-		registerShaped(axe, "GG", "GS", " S", 'S', Items.STICK, 'G', material);
-		registerShaped(hoe, "GG", " S", " S", 'S', Items.STICK, 'G', material);
-		registerShaped(spade, "G", "S", "S", 'S', Items.STICK, 'G', material);
-		registerShaped(helmet, "GGG", "G G", 'G', material);
-		registerShaped(chestplate, "G G", "GGG", "GGG", 'G', material);
-		registerShaped(leggings, "GGG", "G G", "G G", 'G', material);
-		registerShaped(boots, "G G", "G G", 'G', material);
-}
 }
