f3b9336aa54246607f36f7170670c0fc6542da32
NicosaurusRex99 <pusspot1999@gmail.com>
2018-07-07 10:50:43 +0930

    added float customization for the statues

--- src/main/java/naturix/divinerpg/bases/blocks/statues/WatcherStatue.java
+++ src/main/java/naturix/divinerpg/bases/blocks/statues/WatcherStatue.java
@@ -7,15 +7,29 @@ import naturix.divinerpg.bases.blocks.statues.tiles.TileWatcher;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class WatcherStatue extends BaseStatue {
-
+	private static float x1;
+	private static float y1;
+	private static float z1;
+	private static float x2;
+	private static float y2;
+	private static float z2;
+	public WatcherStatue(String name, float x1, float y1, float z1, float x2, float y2, float z2) {
+		super(Material.ROCK, name);
+		this.x1 = x1;
+		this.x2 = x2;
+		this.y1 = y1;
+		this.y2 = y2;
+		this.z1 = z1;
+		this.z2 = z2;
+	}
 	public WatcherStatue(String name) {
 		super(Material.ROCK, name);
 	}
@@ -25,7 +39,11 @@ public class WatcherStatue extends BaseStatue {
 		return false;
 
 	}
-	
+	public static final AxisAlignedBB BLOCK_AABB = new AxisAlignedBB(x1, y1, z1, x2, y2, z2);
+	 @Override
+	    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+	        return BLOCK_AABB;
+	    }
 	@Override
 	public Class<TileWatcher> getTileEntityClass() {
 		return TileWatcher.class;
