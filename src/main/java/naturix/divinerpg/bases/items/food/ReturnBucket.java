4ce74f7ffa9014f1790a5e924dabfd1e420f8c1d
NicosaurusRex99 <pusspot1999@gmail.com>
2018-08-07 00:58:47 +0930

    Fixed stack return

--- src/main/java/naturix/divinerpg/bases/items/food/ReturnBucket.java
+++ src/main/java/naturix/divinerpg/bases/items/food/ReturnBucket.java
@@ -2,16 +2,20 @@ package naturix.divinerpg.bases.items.food;
 
 import naturix.divinerpg.DivineRPG;
 import naturix.divinerpg.bases.items.FoodBase;
+import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
+import net.minecraft.stats.StatList;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.world.World;
 
 public class ReturnBucket extends FoodBase{
 	
-	private EntityPlayer entityplayer;
-
+	
 	public ReturnBucket(String name, int healAmount, boolean alwaysEdible) {
 		super(name, healAmount, alwaysEdible);
 		this.setMaxStackSize(1);
@@ -22,8 +26,17 @@ public class ReturnBucket extends FoodBase{
     {
         if (entityLiving instanceof EntityPlayer && !((EntityPlayer)entityLiving).capabilities.isCreativeMode)
         {
-
+            EntityPlayer entityplayer = (EntityPlayer)entityLiving;
             entityplayer.getFoodStats().addStats(this, stack);
+            worldIn.playSound((EntityPlayer)null, entityplayer.posX, entityplayer.posY, entityplayer.posZ, SoundEvents.ENTITY_PLAYER_BURP, SoundCategory.PLAYERS, 0.5F, worldIn.rand.nextFloat() * 0.1F + 0.9F);
+            this.onFoodEaten(stack, worldIn, entityplayer);
+            entityplayer.addStat(StatList.getObjectUseStats(this));
+
+            if (entityplayer instanceof EntityPlayerMP)
+            {
+                CriteriaTriggers.CONSUME_ITEM.trigger((EntityPlayerMP)entityplayer, stack);
+            }
+
             stack.shrink(1);
         }
 				
--- src/main/java/naturix/divinerpg/bases/items/food/ReturnsBowl.java
+++ src/main/java/naturix/divinerpg/bases/items/food/ReturnsBowl.java
@@ -1,10 +1,15 @@
 package naturix.divinerpg.bases.items.food;
 
 import naturix.divinerpg.bases.items.FoodBase;
+import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
+import net.minecraft.stats.StatList;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.world.World;
 
 public class ReturnsBowl extends FoodBase{
@@ -22,8 +27,16 @@ public class ReturnsBowl extends FoodBase{
 		
         if (entityLiving instanceof EntityPlayer && !((EntityPlayer)entityLiving).capabilities.isCreativeMode)
         {
-
+            EntityPlayer entityplayer = (EntityPlayer)entityLiving;
             entityplayer.getFoodStats().addStats(this, stack);
+            worldIn.playSound((EntityPlayer)null, entityplayer.posX, entityplayer.posY, entityplayer.posZ, SoundEvents.ENTITY_PLAYER_BURP, SoundCategory.PLAYERS, 0.5F, worldIn.rand.nextFloat() * 0.1F + 0.9F);
+            this.onFoodEaten(stack, worldIn, entityplayer);
+            entityplayer.addStat(StatList.getObjectUseStats(this));
+
+            if (entityplayer instanceof EntityPlayerMP)
+            {
+                CriteriaTriggers.CONSUME_ITEM.trigger((EntityPlayerMP)entityplayer, stack);
+            }
             stack.shrink(1);
         }
 				
