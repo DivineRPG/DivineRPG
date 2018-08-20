821700ed81bb56aef04a2bdde540759835582390
NicosaurusRex99 <pusspot1999@gmail.com>
2018-08-20 12:42:21 +0930

    edited render

--- src/main/java/naturix/divinerpg/entities/hostile/render/RenderDramcryx.java
+++ src/main/java/naturix/divinerpg/entities/hostile/render/RenderDramcryx.java
@@ -8,16 +8,16 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.util.ResourceLocation;
 
-public class RenderDramcryx extends RenderLiving {
+public class RenderDramcryx extends RenderLiving<JungleStegosaurus> {
 	static RenderManager renderManager;
 	ResourceLocation texture = new ResourceLocation("divinerpg:textures/entity/dramcryx_jungle.png");
 	public RenderDramcryx(float size) {
 		super(renderManager, new ModelDramcryx(), size);
 	}
 
-	public void doRender(JungleStegosaurus p_76986_1_, double p_76986_2_, double p_76986_4_, double p_76986_6_, float p_76986_8_, float p_76986_9_)
-    {
-        super.doRender(p_76986_1_, p_76986_2_, p_76986_4_, p_76986_6_, p_76986_8_, p_76986_9_);
+	@Override
+    public void doRender(JungleStegosaurus entity, double x, double y, double z, float entityYaw, float partialTicks) {
+        super.doRender(entity, x, y, z, entityYaw, partialTicks);
     }
 
     /**
@@ -28,42 +28,5 @@ public class RenderDramcryx extends RenderLiving {
         return texture;
     }
 
-    /*
-     * Defines what float the third param in setRotationAngles of ModelBase is
-     */
-    /*protected float handleRotationFloat(JungleStegosaurus p_77044_1_, float p_77044_2_)
-    {
-        float f1 = p_77044_1_.field_70888_h + (p_77044_1_.field_70886_e - p_77044_1_.field_70888_h) * p_77044_2_;
-        float f2 = p_77044_1_.field_70884_g + (p_77044_1_.destPos - p_77044_1_.field_70884_g) * p_77044_2_;
-        return (MathHelper.sin(f1) + 1.0F) * f2;
-    }*/
-
-    /**
-     * Actually renders the given argument. This is a synthetic bridge method, always casting down its argument and then
-     * handing it off to a worker function which does the actual work. In all probabilty, the class Render is generic
-     * (Render<T extends Entity) and this method has signature public void func_76986_a(T entity, double d, double d1,
-     * double d2, float f, float f1). But JAD is pre 1.5 so doesn't do that.
-     */
-    @Override
-    public void doRender(EntityLiving p_76986_1_, double p_76986_2_, double p_76986_4_, double p_76986_6_, float p_76986_8_, float p_76986_9_)
-    {
-        this.doRender((JungleStegosaurus)p_76986_1_, p_76986_2_, p_76986_4_, p_76986_6_, p_76986_8_, p_76986_9_);
-    }
-
-    /**
-     * Actually renders the given argument. This is a synthetic bridge method, always casting down its argument and then
-     * handing it off to a worker function which does the actual work. In all probabilty, the class Render is generic
-     * (Render<T extends Entity) and this method has signature public void func_76986_a(T entity, double d, double d1,
-     * double d2, float f, float f1). But JAD is pre 1.5 so doesn't do that.
-     */
-
-    /**
-     * Returns the location of an entity's texture. Doesn't seem to be called unless you call Render.bindEntityTexture.
-     */
-    @Override
-    protected ResourceLocation getEntityTexture(Entity p_110775_1_)
-    {
-        return this.getEntityTexture((JungleStegosaurus)p_110775_1_);
-    }
 
 }
