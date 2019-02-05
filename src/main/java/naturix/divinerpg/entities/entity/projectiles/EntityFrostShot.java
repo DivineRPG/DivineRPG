5796a3711db5209704d0bb0d614dc92dececbc26
krwminer <krwshop@hotmail.com>
2019-02-04 22:02:16 -0500

    Update EntityFrostShot.java

--- src/main/java/naturix/divinerpg/entities/entity/projectiles/EntityFrostShot.java
+++ src/main/java/naturix/divinerpg/entities/entity/projectiles/EntityFrostShot.java
@@ -1,64 +1,92 @@
 package naturix.divinerpg.entities.entity.projectiles;
 
-import net.minecraft.entity.EntityLiving;
+import java.util.List;
+
+import net.minecraft.entity.EntityAreaEffectCloud;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.projectile.EntityFireball;
-import net.minecraft.init.Blocks;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.datafix.DataFixer;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class EntityFrostShot extends EntityFireball {
-
-    public EntityFrostShot(World w) {
-        super(w);
-        setSize(0.3125F, 0.3125F);
+    public EntityFrostShot(World worldIn) {
+        super(worldIn);
+        this.setSize(1.0F, 1.0F);
     }
 
-    public EntityFrostShot(World w, EntityLivingBase shooter, double i, double j, double k) {
-        this(w);
-        this.setLocationAndAngles(shooter.posX, shooter.posY, shooter.posZ, shooter.rotationYaw, shooter.rotationPitch);
-        this.setPosition(shooter.posX, shooter.posY, shooter.posZ);
-        double d = (double) MathHelper.sqrt(i * i + j * j + k * k);
-        this.accelerationX = i / d * 0.1D;
-        this.accelerationY = j / d * 0.1D;
-        this.accelerationZ = k / d * 0.1D;
-        this.shootingEntity = shooter;
+    @SideOnly(Side.CLIENT)
+    public EntityFrostShot(World worldIn, double x, double y, double z, double accelX, double accelY, double accelZ) {
+        super(worldIn, x, y, z, accelX, accelY, accelZ);
+        this.setSize(1.0F, 1.0F);
     }
 
-    @Override
+    public EntityFrostShot(World worldIn, EntityLivingBase shooter, double accelX, double accelY, double accelZ) {
+        super(worldIn, shooter, accelX, accelY, accelZ);
+        this.setSize(1.0F, 1.0F);
+    }
+
+    public static void registerFixesDragonFireball(DataFixer fixer) {
+        EntityFireball.registerFixesFireball(fixer, "FrostShot");
+    }
+
+    /**
+     * Called when this EntityFireball hits a block or entity.
+     */
     protected void onImpact(RayTraceResult result) {
-        if (!this.world.isRemote) {
-            if (result.entityHit != null) {
-                if (!result.entityHit.isImmuneToFire()) {
-                    boolean flag = result.entityHit
-                            .attackEntityFrom(DamageSource.causeFireballDamage(this, this.shootingEntity), 6.0F);
+        if (result.entityHit == null || !result.entityHit.isEntityEqual(this.shootingEntity)) {
+            if (!this.world.isRemote) {
+                List<EntityLivingBase> list = this.world.<EntityLivingBase>getEntitiesWithinAABB(EntityLivingBase.class,
+                        this.getEntityBoundingBox().grow(4.0D, 2.0D, 4.0D));
+                EntityAreaEffectCloud entityareaeffectcloud = new EntityAreaEffectCloud(this.world, this.posX,
+                        this.posY, this.posZ);
+                entityareaeffectcloud.setOwner(this.shootingEntity);
+                entityareaeffectcloud.setParticle(EnumParticleTypes.SNOWBALL);
+                entityareaeffectcloud.setWaitTime(0);
+                entityareaeffectcloud.setRadius(3.0F);
+                entityareaeffectcloud.setDuration(50);
+                entityareaeffectcloud.setRadiusPerTick(
+                        (0.0F - entityareaeffectcloud.getRadius()) / (float) entityareaeffectcloud.getDuration());
+                // entityareaeffectcloud.addEffect(new PotionEffect(MobEffects.INSTANT_DAMAGE,
+                // 1, 1));
 
-                    if (flag) {
-                        this.applyEnchantments(this.shootingEntity, result.entityHit);
-                        result.entityHit.setFire(5);
+                if (!list.isEmpty()) {
+                    for (EntityLivingBase entitylivingbase : list) {
+                        double d0 = this.getDistanceSq(entitylivingbase);
+
+                        if (d0 < 16.0D) {
+                            entityareaeffectcloud.setPosition(entitylivingbase.posX, entitylivingbase.posY,
+                                    entitylivingbase.posZ);
+                            break;
+                        }
                     }
                 }
-            } else {
-                boolean flag1 = true;
 
-                if (this.shootingEntity != null && this.shootingEntity instanceof EntityLiving) {
-                    flag1 = net.minecraftforge.event.ForgeEventFactory.getMobGriefingEvent(this.world,
-                            this.shootingEntity);
-                }
-
-                if (flag1) {
-                    BlockPos blockpos = result.getBlockPos().offset(result.sideHit);
-
-                    if (this.world.isAirBlock(blockpos)) {
-                        this.world.setBlockState(blockpos, Blocks.FIRE.getDefaultState());
-                    }
-                }
+                this.world.playEvent(2006, new BlockPos(this.posX, this.posY, this.posZ), 0);
+                this.world.spawnEntity(entityareaeffectcloud);
+                this.setDead();
             }
-
-            this.setDead();
         }
+    }
+
+    public boolean canBeCollidedWith() {
+        return false;
+    }
+
+    public boolean attackEntityFrom(DamageSource source, float amount) {
+        return false;
+    }
+
+    protected EnumParticleTypes getParticleType() {
+        return EnumParticleTypes.DRIP_WATER;
+    }
+
+    protected boolean isFireballFiery() {
+        return false;
     }
 }
