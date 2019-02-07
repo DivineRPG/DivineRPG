9c09843fa65c05b30b94328383c43253e9260920
NicosaurusRex99 <pusspot1999@gmail.com>
2019-02-08 03:01:44 +1030

    .

--- src/main/java/naturix/divinerpg/objects/entities/entity/EntityFrostCloud.java
+++ src/main/java/naturix/divinerpg/objects/entities/entity/EntityFrostCloud.java
@@ -11,9 +11,11 @@ import javax.annotation.Nullable;
 import com.google.common.collect.Maps;
 
 import naturix.divinerpg.DivineRPG;
+import naturix.divinerpg.utils.log.Logging;
 import net.minecraft.block.material.EnumPushReaction;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
@@ -25,182 +27,189 @@ import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 
 public class EntityFrostCloud extends Entity {
-    private static final DataParameter<Float> RADIUS = EntityDataManager.<Float>createKey(EntityFrostCloud.class,
-            DataSerializers.FLOAT);
-    private final Map<Entity, Integer> reapplicationDelayMap;
-    private int duration;
-    private int reapplicationDelay;
-    private boolean colorSet;
-    private float radiusPerTick;
-    private EntityLivingBase owner;
-    private UUID ownerUniqueId;
+	private static final DataParameter<Float> RADIUS = EntityDataManager.<Float>createKey(EntityFrostCloud.class,
+	        DataSerializers.FLOAT);
+	private final Map<Entity, Integer> reapplicationDelayMap;
+	private int duration;
+	private int reapplicationDelay;
+	private float radiusPerTick;
+	private EntityLivingBase owner;
+	private UUID ownerUniqueId;
+	private EntityPlayer player;
 
-    public EntityFrostCloud(World worldIn) {
-        super(worldIn);
-        this.reapplicationDelayMap = Maps.<Entity, Integer>newHashMap();
-        this.duration = 50;
-        this.reapplicationDelay = 20;
-        this.noClip = true;
-        this.isImmuneToFire = true;
-        this.setRadius(3.0F);
-    }
+	public EntityFrostCloud(World worldIn) {
+		super(worldIn);
+		this.reapplicationDelayMap = Maps.<Entity, Integer>newHashMap();
+		this.duration = 50;
+		this.reapplicationDelay = 20;
+		this.noClip = true;
+		this.isImmuneToFire = true;
+		this.setRadius(3.0F);
+	}
 
-    public EntityFrostCloud(World worldIn, double x, double y, double z) {
-        this(worldIn);
-        this.setPosition(x, y, z);
-    }
+	public EntityFrostCloud(World worldIn, double x, double y, double z) {
+		this(worldIn);
+		this.setPosition(x, y, z);
+	}
 
-    protected void entityInit() {
-        this.getDataManager().register(RADIUS, Float.valueOf(3.0F));
-    }
+	@Override
+	protected void entityInit() {
+		this.getDataManager().register(RADIUS, Float.valueOf(3.0F));
+	}
 
-    public void setRadius(float radiusIn) {
-        double d0 = this.posX;
-        double d1 = this.posY;
-        double d2 = this.posZ;
-        this.setSize(radiusIn * 2.0F, 0.5F);
-        this.setPosition(d0, d1, d2);
+	public int getDuration() {
+		return this.duration;
+	}
 
-        if (!this.world.isRemote) {
-            this.getDataManager().set(RADIUS, Float.valueOf(radiusIn));
-        }
-    }
+	@Nullable
+	public EntityLivingBase getOwner() {
+		if (this.owner == null && this.ownerUniqueId != null && this.world instanceof WorldServer) {
+			Entity entity = ((WorldServer) this.world).getEntityFromUuid(this.ownerUniqueId);
+			if (entity instanceof EntityLivingBase) {
+				this.owner = (EntityLivingBase) entity;
+			}
+		}
+		return this.owner;
+	}
 
-    public float getRadius() {
-        return ((Float) this.getDataManager().get(RADIUS)).floatValue();
-    }
+	@Override
+	public EnumPushReaction getPushReaction() {
+		return EnumPushReaction.IGNORE;
+	}
 
-    public int getDuration() {
-        return this.duration;
-    }
+	public float getRadius() {
+		return this.getDataManager().get(RADIUS).floatValue();
+	}
 
-    public void setDuration(int durationIn) {
-        this.duration = durationIn;
-    }
+	@Override
+	public void notifyDataManagerChange(DataParameter<?> key) {
+		if (RADIUS.equals(key)) {
+			this.setRadius(this.getRadius());
+		}
+		super.notifyDataManagerChange(key);
+	}
 
-    public void onUpdate() {
-        super.onUpdate();
-        float f = this.getRadius();
+	@Override
+	public void onUpdate() {
+		super.onUpdate();
+		float f = this.getRadius();
 
-        if (this.world.isRemote) {
-            float f5 = (float) Math.PI * f * f;
+		if (this.world.isRemote) {
+			float f5 = (float) Math.PI * f * f;
 
-            for (int k1 = 0; (float) k1 < f5; ++k1) {
-                float f6 = this.rand.nextFloat() * ((float) Math.PI * 2F);
-                float f7 = MathHelper.sqrt(this.rand.nextFloat()) * f;
-                float f8 = MathHelper.cos(f6) * f7;
-                float f9 = MathHelper.sin(f6) * f7;
+			for (int k1 = 0; k1 < f5; ++k1) {
+				float f6 = this.rand.nextFloat() * ((float) Math.PI * 2F);
+				float f7 = MathHelper.sqrt(this.rand.nextFloat()) * f;
+				float f8 = MathHelper.cos(f6) * f7;
+				float f9 = MathHelper.sin(f6) * f7;
 
-                DivineRPG.logger.info("FrostCloud spawned particle at "
-                        + new BlockPos(this.posX + (double) f8, this.posY, this.posZ + (double) f9));
-                this.world.spawnAlwaysVisibleParticle(EnumParticleTypes.SNOWBALL.getParticleID(),
-                        this.posX + (double) f8, this.posY, this.posZ + (double) f9,
-                        (0.5D - this.rand.nextDouble()) * 0.15D, 0.009999999776482582D,
-                        (0.5D - this.rand.nextDouble()) * 0.15D);
-            }
-        } else {
-            if (this.ticksExisted >= this.duration) {
-                this.setDead();
-                return;
-            }
+				DivineRPG.logger.info(
+				        "FrostCloud spawned particle at " + new BlockPos(this.posX + f8, this.posY, this.posZ + f9));
+				Logging.message(player,
+				        "FrostCloud spawned particle at " + new BlockPos(this.posX + f8, this.posY, this.posZ + f9));
+				this.world.spawnAlwaysVisibleParticle(EnumParticleTypes.SNOWBALL.getParticleID(), this.posX + f8,
+				        this.posY, this.posZ + f9, (0.5D - this.rand.nextDouble()) * 0.15D, 0.009999999776482582D,
+				        (0.5D - this.rand.nextDouble()) * 0.15D);
+			}
+		} else {
+			if (this.ticksExisted >= this.duration) {
+				this.setDead();
+				return;
+			}
 
-            if (this.radiusPerTick != 0.0F) {
-                f += this.radiusPerTick;
+			if (this.radiusPerTick != 0.0F) {
+				f += this.radiusPerTick;
 
-                if (f < 0.5F) {
-                    this.setDead();
-                    return;
-                }
+				if (f < 0.5F) {
+					this.setDead();
+					return;
+				}
 
-                this.setRadius(f);
-            }
+				this.setRadius(f);
+			}
 
-            if (this.ticksExisted % 5 == 0) {
-                Iterator<Entry<Entity, Integer>> iterator = this.reapplicationDelayMap.entrySet().iterator();
+			if (this.ticksExisted % 5 == 0) {
+				Iterator<Entry<Entity, Integer>> iterator = this.reapplicationDelayMap.entrySet().iterator();
 
-                while (iterator.hasNext()) {
-                    Entry<Entity, Integer> entry = (Entry) iterator.next();
+				while (iterator.hasNext()) {
+					Entry<Entity, Integer> entry = iterator.next();
 
-                    if (this.ticksExisted >= ((Integer) entry.getValue()).intValue()) {
-                        iterator.remove();
-                    }
-                }
+					if (this.ticksExisted >= entry.getValue().intValue()) {
+						iterator.remove();
+					}
+				}
 
-                List<EntityLivingBase> list = this.world.<EntityLivingBase>getEntitiesWithinAABB(EntityLivingBase.class,
-                        this.getEntityBoundingBox());
+				List<EntityLivingBase> list = this.world.<EntityLivingBase>getEntitiesWithinAABB(EntityLivingBase.class,
+				        this.getEntityBoundingBox());
 
-                if (!list.isEmpty()) {
-                    for (EntityLivingBase entitylivingbase : list) {
-                        if (!this.reapplicationDelayMap.containsKey(entitylivingbase)
-                                && entitylivingbase.canBeHitWithPotion()) {
-                            double d0 = entitylivingbase.posX - this.posX;
-                            double d1 = entitylivingbase.posZ - this.posZ;
-                            double d2 = d0 * d0 + d1 * d1;
+				if (!list.isEmpty()) {
+					for (EntityLivingBase entitylivingbase : list) {
+						if (!this.reapplicationDelayMap.containsKey(entitylivingbase)
+						        && entitylivingbase.canBeHitWithPotion()) {
+							double d0 = entitylivingbase.posX - this.posX;
+							double d1 = entitylivingbase.posZ - this.posZ;
+							double d2 = d0 * d0 + d1 * d1;
 
-                            if (d2 <= (double) (f * f)) {
-                                this.reapplicationDelayMap.put(entitylivingbase,
-                                        Integer.valueOf(this.ticksExisted + this.reapplicationDelay));
+							if (d2 <= f * f) {
+								this.reapplicationDelayMap.put(entitylivingbase,
+								        Integer.valueOf(this.ticksExisted + this.reapplicationDelay));
 
-                                if (!entitylivingbase.isEntityUndead()) {
-                                    // entitylivingbase.attackEntityFrom(DamageSource.MAGIC, 1.0F);
-                                }
-                            }
-                        }
+								if (!entitylivingbase.isEntityUndead()) {
+									// entitylivingbase.attackEntityFrom(DamageSource.MAGIC, 1.0F);
+								}
+							}
+						}
 
-                    }
-                }
-            }
-        }
-    }
+					}
+				}
+			}
+		}
+	}
 
-    public void setRadiusPerTick(float radiusPerTickIn) {
-        this.radiusPerTick = radiusPerTickIn;
-    }
+	@Override
+	protected void readEntityFromNBT(NBTTagCompound compound) {
+		this.ticksExisted = compound.getInteger("Age");
+		this.duration = compound.getInteger("Duration");
+		this.reapplicationDelay = compound.getInteger("ReapplicationDelay");
+		this.radiusPerTick = compound.getFloat("RadiusPerTick");
+		this.setRadius(compound.getFloat("Radius"));
+		this.ownerUniqueId = compound.getUniqueId("OwnerUUID");
+	}
 
-    public void setOwner(@Nullable EntityLivingBase ownerIn) {
-        this.owner = ownerIn;
-        this.ownerUniqueId = ownerIn == null ? null : ownerIn.getUniqueID();
-    }
+	public void setDuration(int durationIn) {
+		this.duration = durationIn;
+	}
 
-    @Nullable
-    public EntityLivingBase getOwner() {
-        if (this.owner == null && this.ownerUniqueId != null && this.world instanceof WorldServer) {
-            Entity entity = ((WorldServer) this.world).getEntityFromUuid(this.ownerUniqueId);
-            if (entity instanceof EntityLivingBase) {
-                this.owner = (EntityLivingBase) entity;
-            }
-        }
-        return this.owner;
-    }
+	public void setOwner(@Nullable EntityLivingBase ownerIn) {
+		this.owner = ownerIn;
+		this.ownerUniqueId = ownerIn == null ? null : ownerIn.getUniqueID();
+	}
 
-    protected void readEntityFromNBT(NBTTagCompound compound) {
-        this.ticksExisted = compound.getInteger("Age");
-        this.duration = compound.getInteger("Duration");
-        this.reapplicationDelay = compound.getInteger("ReapplicationDelay");
-        this.radiusPerTick = compound.getFloat("RadiusPerTick");
-        this.setRadius(compound.getFloat("Radius"));
-        this.ownerUniqueId = compound.getUniqueId("OwnerUUID");
-    }
+	public void setRadius(float radiusIn) {
+		double d0 = this.posX;
+		double d1 = this.posY;
+		double d2 = this.posZ;
+		this.setSize(radiusIn * 2.0F, 0.5F);
+		this.setPosition(d0, d1, d2);
 
-    protected void writeEntityToNBT(NBTTagCompound compound) {
-        compound.setInteger("Age", this.ticksExisted);
-        compound.setInteger("Duration", this.duration);
-        compound.setInteger("ReapplicationDelay", this.reapplicationDelay);
-        compound.setFloat("RadiusPerTick", this.radiusPerTick);
-        compound.setFloat("Radius", this.getRadius());
-        if (this.ownerUniqueId != null) {
-            compound.setUniqueId("OwnerUUID", this.ownerUniqueId);
-        }
-    }
+		if (!this.world.isRemote) {
+			this.getDataManager().set(RADIUS, Float.valueOf(radiusIn));
+		}
+	}
 
-    public void notifyDataManagerChange(DataParameter<?> key) {
-        if (RADIUS.equals(key)) {
-            this.setRadius(this.getRadius());
-        }
-        super.notifyDataManagerChange(key);
-    }
+	public void setRadiusPerTick(float radiusPerTickIn) {
+		this.radiusPerTick = radiusPerTickIn;
+	}
 
-    public EnumPushReaction getPushReaction() {
-        return EnumPushReaction.IGNORE;
-    }
+	@Override
+	protected void writeEntityToNBT(NBTTagCompound compound) {
+		compound.setInteger("Age", this.ticksExisted);
+		compound.setInteger("Duration", this.duration);
+		compound.setInteger("ReapplicationDelay", this.reapplicationDelay);
+		compound.setFloat("RadiusPerTick", this.radiusPerTick);
+		compound.setFloat("Radius", this.getRadius());
+		if (this.ownerUniqueId != null) {
+			compound.setUniqueId("OwnerUUID", this.ownerUniqueId);
+		}
+	}
 }
--- src/main/java/naturix/divinerpg/utils/log/Logging.java
+++ src/main/java/naturix/divinerpg/utils/log/Logging.java
@@ -16,79 +16,81 @@ import net.minecraft.world.World;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 
 public class Logging {
-    private static Logging instance = null;
+	private static Logging instance = null;
 
-    public static long prevTicks = -1;
-    private Logger logger;
+	public static long prevTicks = -1;
+	public static boolean debugMode = false;
 
-    public static boolean debugMode = false;
-    public static boolean doLogging = false;
+	public static boolean doLogging = false;
 
-    private Logging() {
-        logger = LogManager.getLogger();
-        instance = this;
-    }
+	public static void broadcast(String message) {
+		FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList()
+		        .sendMessage(new TextComponentString(message), false);
+	}
 
-    private static Logging getInstance() {
-        if (instance == null) {
-            instance = new Logging();
-        }
-        return instance;
-    }
+	public static void broadcast(TextComponentBase base) {
+		FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().sendMessage(base, false);
+	}
 
-    public static void logError(String msg) {
-        getLogger().log(Level.ERROR, msg);
-    }
+	private static Logging getInstance() {
+		if (instance == null) {
+			instance = new Logging();
+		}
+		return instance;
+	}
 
-    public static void log(World world, TileEntity te, String message) {
-        if (doLogging) {
-            long ticks = world.getTotalWorldTime();
-            if (ticks != prevTicks) {
-                prevTicks = ticks;
-                getInstance().logger.log(Level.INFO, "=== Time " + ticks + " ===");
-            }
-            String id = te.getPos().getX() + "," + te.getPos().getY() + "," + te.getPos().getZ() + ": ";
-            getInstance().logger.log(Level.INFO, id + message);
-        }
-    }
+	public static Logger getLogger() {
+		return getInstance().logger;
+	}
 
-    public static Logger getLogger() {
-        return getInstance().logger;
-    }
+	public static void log(String message) {
+		getInstance().logger.log(Level.INFO, message);
+	}
 
-    public static void logError(String msg, Throwable e) {
-        getLogger().error(msg, e);
-    }
+	public static void log(World world, TileEntity te, String message) {
+		if (doLogging) {
+			long ticks = world.getTotalWorldTime();
+			if (ticks != prevTicks) {
+				prevTicks = ticks;
+				getInstance().logger.log(Level.INFO, "=== Time " + ticks + " ===");
+			}
+			String id = te.getPos().getX() + "," + te.getPos().getY() + "," + te.getPos().getZ() + ": ";
+			getInstance().logger.log(Level.INFO, id + message);
+		}
+	}
 
-    public static void log(String message) {
-        getInstance().logger.log(Level.INFO, message);
-    }
+	public static void logDebug(String message) {
+		if (debugMode) {
+			getInstance().logger.log(Level.INFO, message);
+		}
+	}
 
-    public static void logDebug(String message) {
-        if (debugMode) {
-            getInstance().logger.log(Level.INFO, message);
-        }
-    }
+	public static void logError(String msg) {
+		getLogger().log(Level.ERROR, msg);
+	}
 
-    public static void message(@Nonnull EntityPlayer player, String message) {
-        player.sendStatusMessage(new TextComponentString(message), false);
-    }
+	public static void logError(String msg, Throwable e) {
+		getLogger().error(msg, e);
+	}
 
-    public static void message(@Nonnull EntityPlayer player, TextComponentBase base) {
-        player.sendStatusMessage(base, false);
-    }
+	public static void message(@Nonnull EntityPlayer player, String message) {
+		player.sendStatusMessage(new TextComponentString(message), false);
+	}
 
-    public static void broadcast(String message) {
-        FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList()
-                .sendMessage(new TextComponentString(message), false);
-    }
+	public static void message(@Nonnull EntityPlayer player, TextComponentBase base) {
+		player.sendStatusMessage(base, false);
+	}
 
-    public static void broadcast(TextComponentBase base) {
-        FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().sendMessage(base, false);
-    }
+	public static void warn(@Nonnull EntityPlayer player, String message) {
+		player.sendStatusMessage(new TextComponentString(message).setStyle(new Style().setColor(TextFormatting.RED)),
+		        false);
 
-    public static void warn(@Nonnull EntityPlayer player, String message) {
-        player.sendStatusMessage(new TextComponentString(message).setStyle(new Style().setColor(TextFormatting.RED)),
-                false);
-    }
+	}
+
+	private Logger logger;
+
+	private Logging() {
+		logger = LogManager.getLogger();
+		instance = this;
+	}
 }
