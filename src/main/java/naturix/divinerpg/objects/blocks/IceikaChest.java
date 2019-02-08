628f8fde80a1262ce80a4930e4eb37a58c3c9c64
NicosaurusRex99 <pusspot1999@gmail.com>
2019-02-08 15:48:54 +1030

    Update IceikaChest.java

--- src/main/java/naturix/divinerpg/objects/blocks/IceikaChest.java
+++ src/main/java/naturix/divinerpg/objects/blocks/IceikaChest.java
@@ -1,5 +1,9 @@
 package naturix.divinerpg.objects.blocks;
 
+import java.util.Random;
+
+import javax.annotation.Nullable;
+
 import naturix.divinerpg.DivineRPG;
 import naturix.divinerpg.objects.blocks.tile.block.TileEntityIceikaChest;
 import naturix.divinerpg.particle.ParticleFrost;
@@ -24,131 +28,123 @@ import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-import javax.annotation.Nullable;
-import java.util.Random;
-
 /**
- * Created by LiteWolf101 on Feb
- * /01/2019
+ * Created by LiteWolf101 on Feb /01/2019
  */
 public class IceikaChest extends BlockContainer {
-    protected String name;
-    public IceikaChest(String name) {
-        super(Material.WOOD);
-        setUnlocalizedName(name);
-        setRegistryName(name);
-        setCreativeTab(DivineRPG.BlocksTab);
-        setSoundType(SoundType.GLASS);
-    }
+	public String name;
 
-    @SideOnly(Side.CLIENT)
-    public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand)
-    {
-        Random random = worldIn.rand;
-        double d0 = 0.0625D;
+	public IceikaChest(String name) {
+		super(Material.WOOD);
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(DivineRPG.BlocksTab);
+		setSoundType(SoundType.GLASS);
+		this.name = name;
+	}
 
-        for (int i = 0; i < 6; ++i)
-        {
-            double d1 = (double)((float)pos.getX() + random.nextFloat());
-            double d2 = (double)((float)pos.getY() + random.nextFloat());
-            double d3 = (double)((float)pos.getZ() + random.nextFloat());
+	@Override
+	public void breakBlock(World worldIn, BlockPos pos, IBlockState state) {
+		TileEntityIceikaChest tileEntity = (TileEntityIceikaChest) worldIn.getTileEntity(pos);
+		InventoryHelper.dropInventoryItems(worldIn, pos, tileEntity);
+		super.breakBlock(worldIn, pos, state);
+	}
 
-            if (i == 0 && !worldIn.getBlockState(pos.up()).isOpaqueCube())
-            {
-                d2 = (double)pos.getY() + 0.0625D + 1.0D;
-            }
+	public Item createItemBlock() {
+		return new ItemBlock(this).setRegistryName(getRegistryName());
+	}
 
-            if (i == 1 && !worldIn.getBlockState(pos.down()).isOpaqueCube())
-            {
-                d2 = (double)pos.getY() - 0.0625D;
-            }
+	@Nullable
+	@Override
+	public TileEntity createNewTileEntity(World worldIn, int meta) {
+		return new TileEntityIceikaChest();
+	}
 
-            if (i == 2 && !worldIn.getBlockState(pos.south()).isOpaqueCube())
-            {
-                d3 = (double)pos.getZ() + 0.0625D + 1.0D;
-            }
+	@Override
+	public EnumBlockRenderType getRenderType(IBlockState state) {
+		return EnumBlockRenderType.ENTITYBLOCK_ANIMATED;
+	}
 
-            if (i == 3 && !worldIn.getBlockState(pos.north()).isOpaqueCube())
-            {
-                d3 = (double)pos.getZ() - 0.0625D;
-            }
+	@Override
+	public boolean isFullBlock(IBlockState state) {
+		return false;
+	}
 
-            if (i == 4 && !worldIn.getBlockState(pos.east()).isOpaqueCube())
-            {
-                d1 = (double)pos.getX() + 0.0625D + 1.0D;
-            }
+	@Override
+	public boolean isFullCube(IBlockState state) {
+		return false;
+	}
 
-            if (i == 5 && !worldIn.getBlockState(pos.west()).isOpaqueCube())
-            {
-                d1 = (double)pos.getX() - 0.0625D;
-            }
+	@Override
+	public boolean isOpaqueCube(IBlockState state) {
+		return false;
+	}
 
-            if (d1 < (double)pos.getX() || d1 > (double)(pos.getX() + 1) || d2 < 0.0D || d2 > (double)(pos.getY() + 1) || d3 < (double)pos.getZ() || d3 > (double)(pos.getZ() + 1))
-            {
-                if (rand.nextInt(10) <= 5) {
-                    ParticleFrost var20 = new ParticleFrost(worldIn, d1, d2, d3, 0.0D, 0.5D, 0.0D);
-                    FMLClientHandler.instance().getClient().effectRenderer.addEffect(var20);
-                }
-            }
-        }
-    }
+	@Override
+	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn,
+	        EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+		if (!worldIn.isRemote) {
+			playerIn.openGui(DivineRPG.instance, GUIHandler.ICEIKA_CHEST_GUI_ID, worldIn, pos.getX(), pos.getY(),
+			        pos.getZ());
+		}
+		return true;
+	}
 
-    @Override
-    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-        if (!worldIn.isRemote) {
-            playerIn.openGui(DivineRPG.instance, GUIHandler.ICEIKA_CHEST_GUI_ID, worldIn, pos.getX(), pos.getY(), pos.getZ());
-        }
-        return true;
-    }
+	@Override
+	public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer,
+	        ItemStack stack) {
+		if (stack.hasDisplayName()) {
+			TileEntity tileEntity = worldIn.getTileEntity(pos);
+			if (tileEntity instanceof TileEntityIceikaChest) {
+				((TileEntityIceikaChest) tileEntity).setCustomName(stack.getDisplayName());
+			}
+		}
+	}
 
-    @Override
-    public void breakBlock(World worldIn, BlockPos pos, IBlockState state) {
-        TileEntityIceikaChest tileEntity = (TileEntityIceikaChest)worldIn.getTileEntity(pos);
-        InventoryHelper.dropInventoryItems(worldIn, pos, tileEntity);
-        super.breakBlock(worldIn, pos, state);
-    }
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+		Random random = worldIn.rand;
+		for (int i = 0; i < 6; ++i) {
+			double d1 = pos.getX() + random.nextFloat();
+			double d2 = pos.getY() + random.nextFloat();
+			double d3 = pos.getZ() + random.nextFloat();
 
-    @Override
-    public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
-        if (stack.hasDisplayName()) {
-            TileEntity tileEntity = worldIn.getTileEntity(pos);
-            if (tileEntity instanceof TileEntityIceikaChest) {
-                ((TileEntityIceikaChest)tileEntity).setCustomName(stack.getDisplayName());
-            }
-        }
-    }
+			if (i == 0 && !worldIn.getBlockState(pos.up()).isOpaqueCube()) {
+				d2 = pos.getY() + 0.0625D + 1.0D;
+			}
 
-    @Nullable
-    @Override
-    public TileEntity createNewTileEntity(World worldIn, int meta) {
-        return new TileEntityIceikaChest();
-    }
+			if (i == 1 && !worldIn.getBlockState(pos.down()).isOpaqueCube()) {
+				d2 = pos.getY() - 0.0625D;
+			}
 
-    @Override
-    public EnumBlockRenderType getRenderType(IBlockState state) {
-        return EnumBlockRenderType.ENTITYBLOCK_ANIMATED;
-    }
+			if (i == 2 && !worldIn.getBlockState(pos.south()).isOpaqueCube()) {
+				d3 = pos.getZ() + 0.0625D + 1.0D;
+			}
 
-    @Override
-    public boolean isFullBlock(IBlockState state) {
-        return false;
-    }
+			if (i == 3 && !worldIn.getBlockState(pos.north()).isOpaqueCube()) {
+				d3 = pos.getZ() - 0.0625D;
+			}
 
-    @Override
-    public boolean isFullCube(IBlockState state) {
-        return false;
-    }
+			if (i == 4 && !worldIn.getBlockState(pos.east()).isOpaqueCube()) {
+				d1 = pos.getX() + 0.0625D + 1.0D;
+			}
 
-    @Override
-    public boolean isOpaqueCube(IBlockState state) {
-        return false;
-    }
+			if (i == 5 && !worldIn.getBlockState(pos.west()).isOpaqueCube()) {
+				d1 = pos.getX() - 0.0625D;
+			}
 
-    public void registerItemModel(Item itemBlock) {
-        DivineRPG.proxy.registerItemRenderer(itemBlock, 0, name);
-    }
+			if (d1 < pos.getX() || d1 > pos.getX() + 1 || d2 < 0.0D || d2 > pos.getY() + 1 || d3 < pos.getZ()
+			        || d3 > pos.getZ() + 1) {
+				if (rand.nextInt(10) <= 5) {
+					ParticleFrost var20 = new ParticleFrost(worldIn, d1, d2, d3, 0.0D, 0.5D, 0.0D);
+					FMLClientHandler.instance().getClient().effectRenderer.addEffect(var20);
+				}
+			}
+		}
+	}
 
-    public Item createItemBlock() {
-        return new ItemBlock(this).setRegistryName(getRegistryName());
-    }
+	public void registerItemModel(Item itemBlock) {
+		DivineRPG.proxy.registerItemRenderer(itemBlock, 0, name);
+	}
 }
