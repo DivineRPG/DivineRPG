fdcba26e058a4c8756143d71572980d58c61ff7f
krwminer <krwshop@hotmail.com>
2019-02-08 01:17:23 -0500

    I do not get it

--- src/main/java/naturix/divinerpg/objects/entities/entity/EntityFrostCloud.java
+++ src/main/java/naturix/divinerpg/objects/entities/entity/EntityFrostCloud.java
@@ -11,9 +11,7 @@ import javax.annotation.Nullable;
 import com.google.common.collect.Maps;
 
 import naturix.divinerpg.DivineRPG;
-import naturix.divinerpg.particle.ParticleFrost;
 import naturix.divinerpg.utils.DRPGParticleTypes;
-import naturix.divinerpg.utils.log.Logging;
 import net.minecraft.block.material.EnumPushReaction;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
@@ -22,196 +20,191 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
-import net.minecraft.util.EnumParticleTypes;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 
 public class EntityFrostCloud extends Entity {
-	private static final DataParameter<Float> RADIUS = EntityDataManager.<Float>createKey(EntityFrostCloud.class,
-	        DataSerializers.FLOAT);
-	private final Map<Entity, Integer> reapplicationDelayMap;
-	private int duration;
-	private int reapplicationDelay;
-	private float radiusPerTick;
-	private EntityLivingBase owner;
-	private UUID ownerUniqueId;
-	private EntityPlayer player;
+    private static final DataParameter<Float> RADIUS = EntityDataManager.<Float>createKey(EntityFrostCloud.class,
+            DataSerializers.FLOAT);
+    private final Map<Entity, Integer> reapplicationDelayMap;
+    private int duration;
+    private int reapplicationDelay;
+    private float radiusPerTick;
+    private EntityLivingBase owner;
+    private UUID ownerUniqueId;
+    private EntityPlayer player;
 
-	public EntityFrostCloud(World worldIn) {
-		super(worldIn);
-		this.reapplicationDelayMap = Maps.<Entity, Integer>newHashMap();
-		this.duration = 50;
-		this.reapplicationDelay = 20;
-		this.noClip = true;
-		this.isImmuneToFire = true;
-		this.setRadius(3.0F);
-	}
+    public EntityFrostCloud(World worldIn) {
+        super(worldIn);
+        this.reapplicationDelayMap = Maps.<Entity, Integer>newHashMap();
+        this.duration = 600;
+        this.reapplicationDelay = 20;
+        this.noClip = true;
+        this.isImmuneToFire = true;
+        this.setRadius(3.0F);
+    }
 
-	public EntityFrostCloud(World worldIn, double x, double y, double z) {
-		this(worldIn);
-		this.setPosition(x, y, z);
-	}
+    public EntityFrostCloud(World worldIn, double x, double y, double z) {
+        this(worldIn);
+        this.setPosition(x, y, z);
+    }
 
-	@Override
-	protected void entityInit() {
-		this.getDataManager().register(RADIUS, Float.valueOf(3.0F));
-	}
+    @Override
+    protected void entityInit() {
+        this.getDataManager().register(RADIUS, Float.valueOf(3.0F));
+    }
 
-	public int getDuration() {
-		return this.duration;
-	}
+    public int getDuration() {
+        return this.duration;
+    }
 
-	@Nullable
-	public EntityLivingBase getOwner() {
-		if (this.owner == null && this.ownerUniqueId != null && this.world instanceof WorldServer) {
-			Entity entity = ((WorldServer) this.world).getEntityFromUuid(this.ownerUniqueId);
-			if (entity instanceof EntityLivingBase) {
-				this.owner = (EntityLivingBase) entity;
-			}
-		}
-		return this.owner;
-	}
+    @Nullable
+    public EntityLivingBase getOwner() {
+        if (this.owner == null && this.ownerUniqueId != null && this.world instanceof WorldServer) {
+            Entity entity = ((WorldServer) this.world).getEntityFromUuid(this.ownerUniqueId);
+            if (entity instanceof EntityLivingBase) {
+                this.owner = (EntityLivingBase) entity;
+            }
+        }
+        return this.owner;
+    }
 
-	@Override
-	public EnumPushReaction getPushReaction() {
-		return EnumPushReaction.IGNORE;
-	}
+    @Override
+    public EnumPushReaction getPushReaction() {
+        return EnumPushReaction.IGNORE;
+    }
 
-	public float getRadius() {
-		return this.getDataManager().get(RADIUS).floatValue();
-	}
+    public float getRadius() {
+        return this.getDataManager().get(RADIUS).floatValue();
+    }
 
-	@Override
-	public void notifyDataManagerChange(DataParameter<?> key) {
-		if (RADIUS.equals(key)) {
-			this.setRadius(this.getRadius());
-		}
-		super.notifyDataManagerChange(key);
-	}
+    @Override
+    public void notifyDataManagerChange(DataParameter<?> key) {
+        if (RADIUS.equals(key)) {
+            this.setRadius(this.getRadius());
+        }
+        super.notifyDataManagerChange(key);
+    }
 
-	@Override
-	public void onUpdate() {
-		super.onUpdate();
-		float f = this.getRadius();
+    @Override
+    public void onUpdate() {
+        super.onUpdate();
+        float f = this.getRadius();
 
-		if (this.world.isRemote) {
-			float f5 = (float) Math.PI * f * f;
+        DivineRPG.logger.info("FC " + this.getPosition() + " t=" + this.ticksExisted + " r=" + f);
+        if (this.world.isRemote) {
+            float f5 = (float) Math.PI * f * f;
 
-			for (int k1 = 0; k1 < f5; ++k1) {
-				float f6 = this.rand.nextFloat() * ((float) Math.PI * 2F);
-				float f7 = MathHelper.sqrt(this.rand.nextFloat()) * f;
-				float f8 = MathHelper.cos(f6) * f7;
-				float f9 = MathHelper.sin(f6) * f7;
+            for (int k1 = 0; k1 < f5; ++k1) {
+                float f6 = this.rand.nextFloat() * ((float) Math.PI * 2F);
+                float f7 = MathHelper.sqrt(this.rand.nextFloat()) * f;
+                float f8 = MathHelper.cos(f6) * f7;
+                float f9 = MathHelper.sin(f6) * f7;
 
-				DivineRPG.logger.info(
-				        "FrostCloud spawned particle at " + new BlockPos(this.posX + f8, this.posY, this.posZ + f9));
-				Logging.message(player,
-				        "FrostCloud spawned particle at " + new BlockPos(this.posX + f8, this.posY, this.posZ + f9));
-				DivineRPG.proxy.spawnParticle(world, DRPGParticleTypes.FROST, this.posX + f8,
-				        this.posY, this.posZ + f9, (0.5D - this.rand.nextDouble()) * 0.15D, 0.009999999776482582D,
-				        (0.5D - this.rand.nextDouble()) * 0.15D);
-			}
-		} else {
-			if (this.ticksExisted >= this.duration) {
-				this.setDead();
-				return;
-			}
+                DivineRPG.proxy.spawnParticle(world, DRPGParticleTypes.FROST, this.posX + f8, this.posY, this.posZ + f9,
+                        (0.5D - this.rand.nextDouble()) * 0.15D, 0.009999999776482582D,
+                        (0.5D - this.rand.nextDouble()) * 0.15D);
+            }
+        } else {
+            if (this.ticksExisted >= this.duration) {
+                this.setDead();
+                return;
+            }
 
-			if (this.radiusPerTick != 0.0F) {
-				f += this.radiusPerTick;
+            if (this.radiusPerTick != 0.0F) {
+                f += this.radiusPerTick;
 
-				if (f < 0.5F) {
-					this.setDead();
-					return;
-				}
+                if (f < 0.5F) {
+                    this.setDead();
+                    return;
+                }
 
-				this.setRadius(f);
-			}
+                this.setRadius(f);
+            }
 
-			if (this.ticksExisted % 5 == 0) {
-				Iterator<Entry<Entity, Integer>> iterator = this.reapplicationDelayMap.entrySet().iterator();
+            if (this.ticksExisted % 5 == 0) {
+                Iterator<Entry<Entity, Integer>> iterator = this.reapplicationDelayMap.entrySet().iterator();
 
-				while (iterator.hasNext()) {
-					Entry<Entity, Integer> entry = iterator.next();
+                while (iterator.hasNext()) {
+                    Entry<Entity, Integer> entry = iterator.next();
 
-					if (this.ticksExisted >= entry.getValue().intValue()) {
-						iterator.remove();
-					}
-				}
+                    if (this.ticksExisted >= entry.getValue().intValue()) {
+                        iterator.remove();
+                    }
+                }
 
-				List<EntityLivingBase> list = this.world.<EntityLivingBase>getEntitiesWithinAABB(EntityLivingBase.class,
-				        this.getEntityBoundingBox());
+                List<EntityLivingBase> list = this.world.<EntityLivingBase>getEntitiesWithinAABB(EntityLivingBase.class,
+                        this.getEntityBoundingBox());
 
-				if (!list.isEmpty()) {
-					for (EntityLivingBase entitylivingbase : list) {
-						if (!this.reapplicationDelayMap.containsKey(entitylivingbase)
-						        && entitylivingbase.canBeHitWithPotion()) {
-							double d0 = entitylivingbase.posX - this.posX;
-							double d1 = entitylivingbase.posZ - this.posZ;
-							double d2 = d0 * d0 + d1 * d1;
+                if (!list.isEmpty()) {
+                    for (EntityLivingBase entitylivingbase : list) {
+                        if (!this.reapplicationDelayMap.containsKey(entitylivingbase)
+                                && entitylivingbase.canBeHitWithPotion()) {
+                            double d0 = entitylivingbase.posX - this.posX;
+                            double d1 = entitylivingbase.posZ - this.posZ;
+                            double d2 = d0 * d0 + d1 * d1;
 
-							if (d2 <= f * f) {
-								this.reapplicationDelayMap.put(entitylivingbase,
-								        Integer.valueOf(this.ticksExisted + this.reapplicationDelay));
+                            if (d2 <= f * f) {
+                                this.reapplicationDelayMap.put(entitylivingbase,
+                                        Integer.valueOf(this.ticksExisted + this.reapplicationDelay));
 
-								if (!entitylivingbase.isEntityUndead()) {
-									// entitylivingbase.attackEntityFrom(DamageSource.MAGIC, 1.0F);
-								}
-							}
-						}
+                                if (!entitylivingbase.isEntityUndead()) {
+                                    // entitylivingbase.attackEntityFrom(DamageSource.MAGIC, 1.0F);
+                                }
+                            }
+                        }
 
-					}
-				}
-			}
-		}
-	}
+                    }
+                }
+            }
+        }
+    }
 
-	@Override
-	protected void readEntityFromNBT(NBTTagCompound compound) {
-		this.ticksExisted = compound.getInteger("Age");
-		this.duration = compound.getInteger("Duration");
-		this.reapplicationDelay = compound.getInteger("ReapplicationDelay");
-		this.radiusPerTick = compound.getFloat("RadiusPerTick");
-		this.setRadius(compound.getFloat("Radius"));
-		this.ownerUniqueId = compound.getUniqueId("OwnerUUID");
-	}
+    @Override
+    protected void readEntityFromNBT(NBTTagCompound compound) {
+        this.ticksExisted = compound.getInteger("Age");
+        this.duration = compound.getInteger("Duration");
+        this.reapplicationDelay = compound.getInteger("ReapplicationDelay");
+        this.radiusPerTick = compound.getFloat("RadiusPerTick");
+        this.setRadius(compound.getFloat("Radius"));
+        this.ownerUniqueId = compound.getUniqueId("OwnerUUID");
+    }
 
-	public void setDuration(int durationIn) {
-		this.duration = durationIn;
-	}
+    public void setDuration(int durationIn) {
+        this.duration = durationIn;
+    }
 
-	public void setOwner(@Nullable EntityLivingBase ownerIn) {
-		this.owner = ownerIn;
-		this.ownerUniqueId = ownerIn == null ? null : ownerIn.getUniqueID();
-	}
+    public void setOwner(@Nullable EntityLivingBase ownerIn) {
+        this.owner = ownerIn;
+        this.ownerUniqueId = ownerIn == null ? null : ownerIn.getUniqueID();
+    }
 
-	public void setRadius(float radiusIn) {
-		double d0 = this.posX;
-		double d1 = this.posY;
-		double d2 = this.posZ;
-		this.setSize(radiusIn * 2.0F, 0.5F);
-		this.setPosition(d0, d1, d2);
+    public void setRadius(float radiusIn) {
+        double d0 = this.posX;
+        double d1 = this.posY;
+        double d2 = this.posZ;
+        this.setSize(radiusIn * 2.0F, 0.5F);
+        this.setPosition(d0, d1, d2);
 
-		if (!this.world.isRemote) {
-			this.getDataManager().set(RADIUS, Float.valueOf(radiusIn));
-		}
-	}
+        if (!this.world.isRemote) {
+            this.getDataManager().set(RADIUS, Float.valueOf(radiusIn));
+        }
+    }
 
-	public void setRadiusPerTick(float radiusPerTickIn) {
-		this.radiusPerTick = radiusPerTickIn;
-	}
+    public void setRadiusPerTick(float radiusPerTickIn) {
+        this.radiusPerTick = radiusPerTickIn;
+    }
 
-	@Override
-	protected void writeEntityToNBT(NBTTagCompound compound) {
-		compound.setInteger("Age", this.ticksExisted);
-		compound.setInteger("Duration", this.duration);
-		compound.setInteger("ReapplicationDelay", this.reapplicationDelay);
-		compound.setFloat("RadiusPerTick", this.radiusPerTick);
-		compound.setFloat("Radius", this.getRadius());
-		if (this.ownerUniqueId != null) {
-			compound.setUniqueId("OwnerUUID", this.ownerUniqueId);
-		}
-	}
+    @Override
+    protected void writeEntityToNBT(NBTTagCompound compound) {
+        compound.setInteger("Age", this.ticksExisted);
+        compound.setInteger("Duration", this.duration);
+        compound.setInteger("ReapplicationDelay", this.reapplicationDelay);
+        compound.setFloat("RadiusPerTick", this.radiusPerTick);
+        compound.setFloat("Radius", this.getRadius());
+        if (this.ownerUniqueId != null) {
+            compound.setUniqueId("OwnerUUID", this.ownerUniqueId);
+        }
+    }
 }
--- src/main/java/naturix/divinerpg/objects/entities/entity/projectiles/EntityFrostShot.java
+++ src/main/java/naturix/divinerpg/objects/entities/entity/projectiles/EntityFrostShot.java
@@ -6,7 +6,6 @@ import naturix.divinerpg.objects.entities.entity.EntityFrostCloud;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.projectile.EntityFireball;
 import net.minecraft.util.DamageSource;
-import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.datafix.DataFixer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
@@ -15,78 +14,75 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class EntityFrostShot extends EntityFireball {
-	public static void registerFixesDragonFireball(DataFixer fixer) {
-		EntityFireball.registerFixesFireball(fixer, "FrostShot");
-	}
+    public static void registerFixesDragonFireball(DataFixer fixer) {
+        EntityFireball.registerFixesFireball(fixer, "FrostShot");
+    }
 
-	public EntityFrostShot(World worldIn) {
-		super(worldIn);
-		this.setSize(1.0F, 1.0F);
-	}
+    public EntityFrostShot(World worldIn) {
+        super(worldIn);
+        this.setSize(1.0F, 1.0F);
+    }
 
-	@SideOnly(Side.CLIENT)
-	public EntityFrostShot(World worldIn, double x, double y, double z, double accelX, double accelY, double accelZ) {
-		super(worldIn, x, y, z, accelX, accelY, accelZ);
-		this.setSize(1.0F, 1.0F);
-	}
+    @SideOnly(Side.CLIENT)
+    public EntityFrostShot(World worldIn, double x, double y, double z, double accelX, double accelY, double accelZ) {
+        super(worldIn, x, y, z, accelX, accelY, accelZ);
+        this.setSize(1.0F, 1.0F);
+    }
 
-	public EntityFrostShot(World worldIn, EntityLivingBase shooter, double accelX, double accelY, double accelZ) {
-		super(worldIn, shooter, accelX, accelY, accelZ);
-		this.setSize(1.0F, 1.0F);
-	}
+    public EntityFrostShot(World worldIn, EntityLivingBase shooter, double accelX, double accelY, double accelZ) {
+        super(worldIn, shooter, accelX, accelY, accelZ);
+        this.setSize(1.0F, 1.0F);
+    }
 
-	@Override
-	public boolean attackEntityFrom(DamageSource source, float amount) {
-		return false;
-	}
+    @Override
+    public boolean attackEntityFrom(DamageSource source, float amount) {
+        return false;
+    }
 
-	@Override
-	public boolean canBeCollidedWith() {
-		return false;
-	}
+    @Override
+    public boolean canBeCollidedWith() {
+        return false;
+    }
 
-	@Override
-	protected boolean isFireballFiery() {
-		return false;
-	}
+    @Override
+    protected boolean isFireballFiery() {
+        return false;
+    }
 
-	/**
-	 * Called when this EntityFireball hits a block or entity.
-	 */
-	@Override
-	protected void onImpact(RayTraceResult result) {
-		if (result.entityHit == null || !result.entityHit.isEntityEqual(this.shootingEntity)) {
-			if (!this.world.isRemote) {
-				List<EntityLivingBase> list = this.world.<EntityLivingBase>getEntitiesWithinAABB(EntityLivingBase.class,
-				        this.getEntityBoundingBox().grow(4.0D, 2.0D, 4.0D));
-				EntityFrostCloud frostCloud = new EntityFrostCloud(this.world, this.posX, this.posY, this.posZ);
+    /**
+     * Called when this EntityFireball hits a block or entity.
+     */
+    @Override
+    protected void onImpact(RayTraceResult result) {
+        if (result.entityHit == null || !result.entityHit.isEntityEqual(this.shootingEntity)) {
+            if (!this.world.isRemote) {
+                List<EntityLivingBase> list = this.world.<EntityLivingBase>getEntitiesWithinAABB(EntityLivingBase.class,
+                        this.getEntityBoundingBox().grow(4.0D, 2.0D, 4.0D));
+                EntityFrostCloud frostCloud = new EntityFrostCloud(this.world, this.posX, this.posY, this.posZ);
 
-				// frostCloud.setParticle(EnumParticleTypes.SNOWBALL);
-				// frostCloud.setWaitTime(0);
+                // frostCloud.setParticle(EnumParticleTypes.SNOWBALL);
+                // frostCloud.setWaitTime(0);
 
-				BlockPos pos = this.getPosition();
-				world.spawnParticle(EnumParticleTypes.SNOWBALL, pos.getX(), pos.getY(), pos.getZ(), 0, 1, 0);
+                frostCloud.setOwner(this.shootingEntity);
+                frostCloud.setRadius(3.0F);
+                frostCloud.setDuration(50);
+                frostCloud.setRadiusPerTick((0.0F - frostCloud.getRadius()) / frostCloud.getDuration());
 
-				frostCloud.setOwner(this.shootingEntity);
-				frostCloud.setRadius(3.0F);
-				frostCloud.setDuration(50);
-				frostCloud.setRadiusPerTick((0.0F - frostCloud.getRadius()) / frostCloud.getDuration());
+                if (!list.isEmpty()) {
+                    for (EntityLivingBase entitylivingbase : list) {
+                        double d0 = this.getDistanceSq(entitylivingbase);
 
-				if (!list.isEmpty()) {
-					for (EntityLivingBase entitylivingbase : list) {
-						double d0 = this.getDistanceSq(entitylivingbase);
+                        if (d0 < 16.0D) {
+                            frostCloud.setPosition(entitylivingbase.posX, entitylivingbase.posY, entitylivingbase.posZ);
+                            break;
+                        }
+                    }
+                }
 
-						if (d0 < 16.0D) {
-							frostCloud.setPosition(entitylivingbase.posX, entitylivingbase.posY, entitylivingbase.posZ);
-							break;
-						}
-					}
-				}
-
-				this.world.playEvent(2006, new BlockPos(this.posX, this.posY, this.posZ), 0);
-				this.world.spawnEntity(frostCloud);
-				this.setDead();
-			}
-		}
-	}
+                this.world.playEvent(2006, new BlockPos(this.posX, this.posY, this.posZ), 0);
+                this.world.spawnEntity(frostCloud);
+                this.setDead();
+            }
+        }
+    }
 }
