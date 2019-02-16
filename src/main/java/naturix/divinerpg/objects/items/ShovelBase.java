3b6a4bcc43db07a472870a44e61ce42452644f2c
NicosaurusRex99 <pusspot1999@gmail.com>
2019-02-16 19:05:33 +1030

    Update ShovelBase.java

--- src/main/java/naturix/divinerpg/objects/items/ShovelBase.java
+++ src/main/java/naturix/divinerpg/objects/items/ShovelBase.java
@@ -1,22 +1,42 @@
 package naturix.divinerpg.objects.items;
 
+import java.util.List;
+
+import javax.annotation.Nullable;
+
 import naturix.divinerpg.DivineRPG;
+import naturix.divinerpg.utils.TooltipLocalizer;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.ItemSpade;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ShovelBase extends ItemSpade {
 
 	private String name;
 
-	public ShovelBase(ToolMaterial material, String name, float damage, float speed) {
+	public ShovelBase(ToolMaterial material, String name) {
 		super(material);
 		setRegistryName(name);
 		setUnlocalizedName(name);
 		setCreativeTab(DivineRPG.ToolsTab);
 		this.name = name;
 	}
-	
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> infoList, ITooltipFlag flagIn) {
+		infoList.add(TooltipLocalizer.efficiency(toolMaterial.getEfficiency()));
+		if (stack.getMaxDamage() != 0) {
+			infoList.add(TooltipLocalizer.usesRemaining(stack.getMaxDamage() - stack.getItemDamage()));
+		} else {
+			infoList.add(TooltipLocalizer.infiniteUses());
+		}
+	}
+
 	public void registerItemModel() {
 		DivineRPG.proxy.registerItemRenderer(this, 0, name);
 	}
-
 }
