8f2ab16d7eb11f1a24520a1e3d74d710cc3bdc14
krwminer <krwshop@hotmail.com>
2019-02-20 17:18:53 -0500

    Updated Alicanto and Iceika Loots

--- src/main/java/naturix/divinerpg/objects/entities/entity/EntityDivineRPGMob.java
+++ src/main/java/naturix/divinerpg/objects/entities/entity/EntityDivineRPGMob.java
@@ -1,7 +1,13 @@
 package naturix.divinerpg.objects.entities.entity;
 
 import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.ai.*;
+import net.minecraft.entity.ai.EntityAIAttackMelee;
+import net.minecraft.entity.ai.EntityAIHurtByTarget;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAIWanderAvoidWater;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.EnumDifficulty;
@@ -9,9 +15,9 @@ import net.minecraft.world.World;
 
 public abstract class EntityDivineRPGMob extends EntityMob {
 
-	public EntityDivineRPGMob(World par1World) {
-		super(par1World);
-	}
+    public EntityDivineRPGMob(World par1World) {
+        super(par1World);
+    }
 
     @Override
     protected void applyEntityAttributes() {
@@ -20,32 +26,32 @@ public abstract class EntityDivineRPGMob extends EntityMob {
         this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.27D);
     }
 
-	@Override
-	protected void initEntityAI() {
-		if (!needsSpecialAI()) {
-			addBasicAI();
-		}
-	}
-	
+    @Override
+    protected void initEntityAI() {
+        if (!needsSpecialAI()) {
+            addBasicAI();
+        }
+    }
+
     public boolean needsSpecialAI() {
-		return false;
-	}
-    
-	protected void addBasicAI() {
-		this.tasks.addTask(0, new EntityAISwimming(this));
-		this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
-	    this.tasks.addTask(7, new EntityAIWanderAvoidWater(this, 1.0D));
-	    this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
-	    this.tasks.addTask(6, new EntityAILookIdle(this));
-	}
+        return false;
+    }
 
-	protected void addAttackingAI() {
-		this.tasks.addTask(1, new EntityAIAttackMelee(this, 1, true));
-		this.targetTasks.addTask(1, new EntityAINearestAttackableTarget(this, EntityPlayer.class, true));
-	}
+    protected void addBasicAI() {
+        this.tasks.addTask(0, new EntityAISwimming(this));
+        this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
+        this.tasks.addTask(6, new EntityAILookIdle(this));
+        this.tasks.addTask(7, new EntityAIWanderAvoidWater(this, 1.0D));
+        this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+    }
 
-	@Override
-	public boolean getCanSpawnHere() {
-		return (this.world.getDifficulty() != EnumDifficulty.PEACEFUL) && this.isValidLightLevel();
-	}
+    protected void addAttackingAI() {
+        this.tasks.addTask(1, new EntityAIAttackMelee(this, 1, true));
+        this.targetTasks.addTask(1, new EntityAINearestAttackableTarget(this, EntityPlayer.class, true));
+    }
+
+    @Override
+    public boolean getCanSpawnHere() {
+        return (this.world.getDifficulty() != EnumDifficulty.PEACEFUL) && this.isValidLightLevel();
+    }
 }
--- src/main/java/naturix/divinerpg/objects/entities/entity/iceika/Alicanto.java
+++ src/main/java/naturix/divinerpg/objects/entities/entity/iceika/Alicanto.java
@@ -1,73 +1,88 @@
 package naturix.divinerpg.objects.entities.entity.iceika;
 
 import naturix.divinerpg.DivineRPG;
+import naturix.divinerpg.objects.entities.entity.EntityDivineRPGMob;
 import naturix.divinerpg.registry.ModDimensions;
-import net.minecraft.block.Block;
-import net.minecraft.entity.EntityLivingBase;
+import naturix.divinerpg.registry.ModSounds;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.ai.*;
-import net.minecraft.entity.monster.EntityMob;
-import net.minecraft.entity.monster.EntityPigZombie;
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.EnumDifficulty;
 import net.minecraft.world.World;
 
-import javax.annotation.Nullable;
-
-public class Alicanto extends EntityMob {
+public class Alicanto extends EntityDivineRPGMob {
+    public static final ResourceLocation LOOT = new ResourceLocation(DivineRPG.modId, "entities/alicanto");
+    private BlockPos currentFlightTarget;
+    private int flyTimer;
 
     public Alicanto(World worldIn) {
-		super(worldIn);
-		this.setSize(1.2F, 1.2f);
-		this.setHealth(this.getMaxHealth());
-	}
-    public static final ResourceLocation LOOT = new ResourceLocation(DivineRPG.modId, "entities/alicanto");
-
-
-    protected boolean isMaster() {
-        return false;
+        super(worldIn);
+        this.setHealth(this.getMaxHealth());
+        this.flyTimer = 0;
     }
-
-    @Override
-    protected boolean canDespawn() {
-        return true;
-    }
-
-    private ResourceLocation deathLootTable = LOOT;
 
     @Override
     protected void applyEntityAttributes() {
         super.applyEntityAttributes();
-        this.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(35.0D);
-        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.32D);
-        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(40.0D);
-        if (isMaster()) {
-            this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(8.0D);
-            this.getEntityAttribute(SharedMonsterAttributes.ARMOR).setBaseValue(6.0D);
-        } else {
-            this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(4.0D);
-            this.getEntityAttribute(SharedMonsterAttributes.ARMOR).setBaseValue(4.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(75.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(10.0D);
+    }
+
+    @Override
+    protected void initEntityAI() {
+        super.initEntityAI();
+        addAttackingAI();
+    }
+
+    public void onUpdate() {
+        super.onUpdate();
+        this.motionY *= 0.6000000238418579D;
+    }
+
+    protected void updateAITasks() {
+        super.updateAITasks();
+
+        if (this.getAttackTarget() != null) {
+            this.currentFlightTarget = this.getAttackTarget().getPosition();
+        } else if (this.flyTimer != 0) {
+            this.flyTimer = 120;
+            this.currentFlightTarget = new BlockPos((int) (this.posX + this.rand.nextInt(16)) - 8,
+                    (int) (this.posY + this.rand.nextInt(32)) - 16, (int) (this.posZ + this.rand.nextInt(16)) - 8);
+        }
+
+        if (this.currentFlightTarget != null) {
+            double deltaX = (double) this.currentFlightTarget.getX() - this.posX;
+            double deltaY = (double) this.currentFlightTarget.getY() - this.posY;
+            double deltaZ = (double) this.currentFlightTarget.getZ() - this.posZ;
+
+            if (Math.signum(deltaX) != 0 || Math.signum(deltaY) != 0 || Math.signum(deltaZ) != 0) {
+                this.motionX += (Math.signum(deltaX) * 0.15D - this.motionX) * 0.10000000149011612D;
+                this.motionY += (Math.signum(deltaY) * 1.699999988079071D - this.motionY) * 0.10000000149011612D;
+                this.motionZ += (Math.signum(deltaZ) * 0.15D - this.motionZ) * 0.10000000149011612D;
+                float var7 = (float) (Math.atan2(this.motionZ, this.motionX) * 180.0D / Math.PI) - 90.0F;
+                float var8 = MathHelper.wrapDegrees(var7 - this.rotationYaw);
+                this.moveForward = 0.5F;
+                this.rotationYaw += var8;
             }
+            flyTimer--;
+        }
     }
 
-    protected void initEntityAI()
-    {
-        this.tasks.addTask(0, new EntityAISwimming(this));
-        this.tasks.addTask(5, new EntityAIMoveTowardsRestriction(this, 1.0D));
-        this.tasks.addTask(7, new EntityAIWanderAvoidWater(this, 1.0D));
-        this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
-        this.tasks.addTask(8, new EntityAILookIdle(this));
-        this.tasks.addTask(8, new EntityAIAttackMelee(this, 1, true));
-        this.tasks.addTask(8, new EntityAIFollow(this, 1, 1, 1));
-        this.applyEntityAI();
+    @Override
+    public void fall(float distance, float damageMultiplier) {
     }
 
-    private void applyEntityAI() {
-        this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true, new Class[]{EntityPigZombie.class}));
-        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, true));
+    @Override
+    protected void updateFallState(double y, boolean onGroundIn, IBlockState state, BlockPos pos) {
+    }
+
+    @Override
+    public boolean doesEntityNotTriggerPressurePlate() {
+        return true;
     }
 
     @Override
@@ -76,37 +91,33 @@ public class Alicanto extends EntityMob {
     }
 
     @Override
-    public int getMaxSpawnedInChunk() {
-        return 3;
+    protected float getSoundPitch() {
+        return super.getSoundPitch() * 0.95F;
     }
 
-    @Override
-    public void setAttackTarget(@Nullable EntityLivingBase entitylivingbaseIn) {
-        super.setAttackTarget(entitylivingbaseIn);
-        if (entitylivingbaseIn instanceof EntityPlayer) {
-            
-        }
-    }
-
-    @Override
-    protected void playStepSound(BlockPos pos, Block blockIn) {
-        super.playStepSound(pos, blockIn);
-    }
-
-    @Nullable
     @Override
     protected SoundEvent getAmbientSound() {
-        return super.getAmbientSound();
+        return this.rand.nextInt(4) != 0 ? null : ModSounds.ALICANTO;
     }
-    @Override
-	protected ResourceLocation getLootTable()
-	{
-		return this.LOOT;
 
-	}
     @Override
-    public boolean getCanSpawnHere()
-    {
-        return this.world.getDifficulty() != EnumDifficulty.PEACEFUL && world.provider.getDimension() == ModDimensions.iceikaDimension.getId();
+    protected SoundEvent getHurtSound(DamageSource source) {
+        return ModSounds.ALICANTO_HURT;
+    }
+
+    @Override
+    protected SoundEvent getDeathSound() {
+        return ModSounds.ALICANTO_HURT;
+    }
+
+    @Override
+    protected ResourceLocation getLootTable() {
+        return this.LOOT;
+    }
+
+    @Override
+    public boolean getCanSpawnHere() {
+        return this.world.getDifficulty() != EnumDifficulty.PEACEFUL
+                && world.provider.getDimension() == ModDimensions.iceikaDimension.getId();
     }
 }
