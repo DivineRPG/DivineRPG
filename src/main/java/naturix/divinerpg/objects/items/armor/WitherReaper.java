de3b6bcabdd0a60346ec43396ad7277016538c0e
NicosaurusRex99 <pusspot1999@gmail.com>
2019-02-27 00:23:35 +1030

    im so confused with why torches are broken
    Armor can all be done via ArmorBase tho

--- src/main/java/naturix/divinerpg/objects/items/armor/WitherReaper.java
+++ src/main/java/naturix/divinerpg/objects/items/armor/WitherReaper.java
@@ -1,15 +1,13 @@
 package naturix.divinerpg.objects.items.armor;
-import naturix.divinerpg.DivineRPG;
+
 import naturix.divinerpg.objects.items.ArmorBase;
 import net.minecraft.inventory.EntityEquipmentSlot;
 
 public class WitherReaper extends ArmorBase {
 
-	
-	private String textureName;
 	public WitherReaper(ArmorMaterial material, EntityEquipmentSlot slot, String name) {
 		super(material, slot, name);
-		//this.setCreativeTab(DivineRPG.ArmorTab);
+		// this.setCreativeTab(DivineRPG.ArmorTab);
 	}
 
 }
