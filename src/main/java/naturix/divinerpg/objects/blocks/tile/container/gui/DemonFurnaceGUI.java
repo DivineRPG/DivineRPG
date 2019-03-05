58e3cd138a163ec87a914eb8585dcc9105e91044
krwminer <krwshop@hotmail.com>
2019-03-04 22:17:02 -0500

    Ooops

--- src/main/java/naturix/divinerpg/objects/blocks/tile/container/gui/DemonFurnaceGUI.java
+++ src/main/java/naturix/divinerpg/objects/blocks/tile/container/gui/DemonFurnaceGUI.java
@@ -7,7 +7,7 @@ import net.minecraft.util.ResourceLocation;
 
 public class DemonFurnaceGUI extends InfiniteFurnaceGUI {
     private static final ResourceLocation TEXTURES = new ResourceLocation(
-            Reference.MODID + ":textures/gui/oceanfire_furnace.png");
+            Reference.MODID + ":textures/gui/demon_furnace.png");
 
     public DemonFurnaceGUI(InventoryPlayer player, TileEntityInfiniteFurnace tileEntity) {
         super(player, tileEntity);
--- src/main/java/naturix/divinerpg/objects/blocks/tile/container/gui/MoltenFurnaceGUI.java
+++ src/main/java/naturix/divinerpg/objects/blocks/tile/container/gui/MoltenFurnaceGUI.java
@@ -7,7 +7,7 @@ import net.minecraft.util.ResourceLocation;
 
 public class MoltenFurnaceGUI extends InfiniteFurnaceGUI {
     private static final ResourceLocation TEXTURES = new ResourceLocation(
-            Reference.MODID + ":textures/gui/demon_furnace.png");
+            Reference.MODID + ":textures/gui/molten_furnace.png");
 
     public MoltenFurnaceGUI(InventoryPlayer player, TileEntityInfiniteFurnace tileEntity) {
         super(player, tileEntity);
