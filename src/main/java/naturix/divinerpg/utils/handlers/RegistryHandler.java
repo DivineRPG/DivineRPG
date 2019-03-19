dfd480c0e9b95a53bcfdf9db92d65c2542f23183
NicosaurusRex99 <pusspot1999@gmail.com>
2019-03-19 11:25:30 +1030

    made JSON gen 100% configurable

--- src/main/java/naturix/divinerpg/utils/handlers/RegistryHandler.java
+++ src/main/java/naturix/divinerpg/utils/handlers/RegistryHandler.java
@@ -1,5 +1,6 @@
 package naturix.divinerpg.utils.handlers;
 
+import naturix.divinerpg.Config;
 import naturix.divinerpg.registry.ModBlocks;
 import naturix.divinerpg.registry.ModItems;
 import naturix.divinerpg.registry.ModSpawns;
@@ -17,38 +18,42 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
  */
 @Mod.EventBusSubscriber
 public class RegistryHandler {
-    @SubscribeEvent
-    public static void onLivingSpawn(LivingSpawnEvent event) {
-        ModSpawns.init(event);
-    }
+	@SubscribeEvent
+	public static void onBlockRegister(RegistryEvent.Register<Block> event) {
+		event.getRegistry().registerAll(ModBlocks.BLOCKS.toArray(new Block[0]));
+		if (Config.genJSON) {
+			ModBlocks.CreateJSONs();
+		}
+	}
 
-    @SubscribeEvent
-    public static void onItemRegister(RegistryEvent.Register<Item> event) {
-        event.getRegistry().registerAll(ModItems.ITEMS.toArray(new Item[0]));
-        ModItems.CreateJSONs();
-    }
+	@SubscribeEvent
+	public static void onItemRegister(RegistryEvent.Register<Item> event) {
+		event.getRegistry().registerAll(ModItems.ITEMS.toArray(new Item[0]));
+		if (Config.genJSON) {
+			ModItems.CreateJSONs();
+		}
+	}
 
-    @SubscribeEvent
-    public static void onBlockRegister(RegistryEvent.Register<Block> event) {
-        event.getRegistry().registerAll(ModBlocks.BLOCKS.toArray(new Block[0]));
-        ModBlocks.CreateJSONs();
-    }
+	@SubscribeEvent
+	public static void onLivingSpawn(LivingSpawnEvent event) {
+		ModSpawns.init(event);
+	}
 
-    @SubscribeEvent
-    public static void onModelRegister(ModelRegistryEvent event) {
-        for (Item item : ModItems.ITEMS) {
-            if (item instanceof IHasModel) {
-                ((IHasModel) item).registerModels();
-            }
-        }
+	@SubscribeEvent
+	public static void onModelRegister(ModelRegistryEvent event) {
+		for (Item item : ModItems.ITEMS) {
+			if (item instanceof IHasModel) {
+				((IHasModel) item).registerModels();
+			}
+		}
 
-        for (Block block : ModBlocks.BLOCKS) {
-            if (block instanceof IHasModel) {
-                ((IHasModel) block).registerModels();
-            }
-        }
-    }
+		for (Block block : ModBlocks.BLOCKS) {
+			if (block instanceof IHasModel) {
+				((IHasModel) block).registerModels();
+			}
+		}
+	}
 
-    public static void otherRegistries() {
-    }
+	public static void otherRegistries() {
+	}
 }
