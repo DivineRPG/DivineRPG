3bc1d7bd7341b5e2da3451f637e29c806c575a83
krwminer <krwshop@hotmail.com>
2019-03-23 18:09:43 -0400

    Fix merge

--- src/main/java/naturix/divinerpg/objects/blocks/iceika/BlockWinterberryBush.java
+++ src/main/java/naturix/divinerpg/objects/blocks/iceika/BlockWinterberryBush.java
@@ -22,67 +22,65 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockWinterberryBush extends BlockMod implements IShearable {
 
-	protected String stateChangeName;
-	public boolean isGrown;
+    protected String stateChangeName;
+    public boolean isGrown;
 
-	World w;
+    World w;
 
-	public BlockWinterberryBush(boolean grown, String name) {
-		super(EnumBlockType.LEAVES, name, 0.3F);
-		this.isGrown = grown;
-		setTickRandomly(true);
-	}
+    public BlockWinterberryBush(boolean grown, String name) {
+        super(EnumBlockType.LEAVES, name, 0.3F);
+        this.isGrown = grown;
+        setTickRandomly(true);
+    }
 
-	@Override
-	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
-		if (this == ModBlocks.winterberryBushGrown) {
-			return ModItems.winterberry;
-		}
-		return null;
-	}
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+        if (this == ModBlocks.winterberryBushGrown) {
+            return ModItems.winterberry;
+        }
+        return null;
+    }
 
-	public boolean isOpaqueCube() {
-		return false;
-	}
+    public boolean isOpaqueCube() {
+        return false;
+    }
 
-	@Override
-	public boolean isShearable(ItemStack item, IBlockAccess world, BlockPos pos) {
-		return true;
-	}
+    @Override
+    public boolean isShearable(ItemStack item, IBlockAccess world, BlockPos pos) {
+        return true;
+    }
 
-	@Override
-	public void onBlockDestroyedByPlayer(World w, BlockPos pos, IBlockState state) {
-		if (isGrown) {
-			w.setBlockState(pos, ModBlocks.winterberryBush.getDefaultState());
-		}
-	}
+    @Override
+    public void onBlockDestroyedByPlayer(World w, BlockPos pos, IBlockState state) {
+        if (isGrown) {
+            w.setBlockState(pos, ModBlocks.winterberryBush.getDefaultState());
+        }
+    }
 
-	@Override
-	public List<ItemStack> onSheared(ItemStack item, IBlockAccess world, BlockPos pos, int fortune) {
-		ArrayList<ItemStack> ret = new ArrayList<ItemStack>();
-		if (this == ModBlocks.winterberryBush) {
-			ret.add(new ItemStack(this, 1));
-		}
-		return ret;
-	}
+    @Override
+    public List<ItemStack> onSheared(ItemStack item, IBlockAccess world, BlockPos pos, int fortune) {
+        ArrayList<ItemStack> ret = new ArrayList<ItemStack>();
+        if (this == ModBlocks.winterberryBush) {
+            ret.add(new ItemStack(this, 1));
+        }
+        return ret;
+    }
 
-	public boolean renderAsNormalBlock() {
-		return false;
-	}
+    public boolean renderAsNormalBlock() {
+        return false;
+    }
 
-	@Override
-	@SideOnly(Side.CLIENT)
-	public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos,
-	        EnumFacing side) {
-		return !Minecraft.getMinecraft().gameSettings.fancyGraphics && blockState == this ? false
-		        : super.shouldSideBeRendered(blockState, blockAccess, pos, side);
-	}
+    @Override
+    @SideOnly(Side.CLIENT)
+    public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side) {
+        return !Minecraft.getMinecraft().gameSettings.fancyGraphics && blockState == this ? false : super.shouldSideBeRendered(blockState, blockAccess, pos, side);
+    }
 
-	@Override
-	public void updateTick(World w, BlockPos pos, IBlockState state, Random r) {
-		if (r.nextInt(2) == 0 && w.getBlockState(pos) == ModBlocks.winterberryBush) {
-			w.setBlockState(pos, ModBlocks.winterberryBushGrown.getDefaultState());
-		}
-	}
+    @Override
+    public void updateTick(World w, BlockPos pos, IBlockState state, Random r) {
+        if (r.nextInt(2) == 0 && w.getBlockState(pos) == ModBlocks.winterberryBush) {
+            w.setBlockState(pos, ModBlocks.winterberryBushGrown.getDefaultState());
+        }
+    }
 
 }
