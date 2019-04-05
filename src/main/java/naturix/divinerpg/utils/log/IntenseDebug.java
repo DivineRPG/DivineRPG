b8dcb4cf78d4e3fa492c45ef80a5f0ba2d9979c2
NicosaurusRex99 <pusspot1999@gmail.com>
2019-04-05 17:20:33 +1030

    .

--- src/main/java/naturix/divinerpg/utils/log/IntenseDebug.java
+++ src/main/java/naturix/divinerpg/utils/log/IntenseDebug.java
@@ -1,5 +1,6 @@
 package naturix.divinerpg.utils.log;
 
+import naturix.divinerpg.Config;
 import naturix.divinerpg.DivineRPG;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
@@ -9,19 +10,22 @@ import net.minecraftforge.fml.common.registry.EntityEntry;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 public class IntenseDebug {
-	public static void init() {
-		for(Biome biome : ForgeRegistries.BIOMES.getValues()) {
-			DivineRPG.logger.info("Biome: "+biome.getBiomeName());
-		}
-		for(Item item : ForgeRegistries.ITEMS.getValues()) {
-			DivineRPG.logger.info("Item: "+new ItemStack(item).getDisplayName());
-		}
-		for(Block block: ForgeRegistries.BLOCKS.getValues()) {
-			DivineRPG.logger.info("Block: "+new ItemStack(Item.getItemFromBlock(block)).getDisplayName());
-		}
-		for(EntityEntry entities : ForgeRegistries.ENTITIES.getValues()) {
-			DivineRPG.logger.info("Entity: "+entities.getName());
-		}
+public static void init() {
 
+	if (Config.debug) {
+		for (Biome biome : ForgeRegistries.BIOMES.getValues()) {
+			DivineRPG.logger.info("Biome: " + biome.getBiomeName());
+		}
+		for (Item item : ForgeRegistries.ITEMS.getValues()) {
+			DivineRPG.logger.info("Item: " + new ItemStack(item).getDisplayName());
+		}
+		for (Block block : ForgeRegistries.BLOCKS.getValues()) {
+			DivineRPG.logger.info("Block: " + new ItemStack(Item.getItemFromBlock(block)).getDisplayName());
+		}
+		for (EntityEntry entities : ForgeRegistries.ENTITIES.getValues()) {
+			DivineRPG.logger.info("Entity: " + entities.getName());
+		}
 	}
+
+}
 }
