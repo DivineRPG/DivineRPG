1a46a6b47202bed10a3fc512ff2fbb46c4bebc50
NicosaurusRex99 <pusspot1999@gmail.com>
2019-04-14 19:07:26 +0930

    Fixed?!? arrow to the knee

--- src/main/java/naturix/divinerpg/registry/ModEvents.java
+++ src/main/java/naturix/divinerpg/registry/ModEvents.java
@@ -8,6 +8,7 @@ import java.util.concurrent.TimeUnit;
 import naturix.divinerpg.Config;
 import naturix.divinerpg.DivineRPG;
 import naturix.divinerpg.events.EventArmorSet;
+import naturix.divinerpg.objects.entities.entity.twilight.TwilightArcher;
 import naturix.divinerpg.utils.Reference;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
@@ -19,38 +20,42 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @EventBusSubscriber(modid = Reference.MODID)
 public class ModEvents {
-    public static final ExecutorService THREAD_POOL = new ThreadPoolExecutor(0, 2, 1, TimeUnit.MINUTES,
-            new LinkedBlockingQueue<Runnable>());
+public static final ExecutorService THREAD_POOL = new ThreadPoolExecutor(0, 2, 1, TimeUnit.MINUTES,
+        new LinkedBlockingQueue<Runnable>());
 
-    @SubscribeEvent(priority = EventPriority.HIGHEST, receiveCanceled = true)
-    public static void onEvent(RightClickBlock event) {
-        EntityPlayer thePlayer = event.getEntityPlayer();
-        if (thePlayer instanceof EntityPlayerMP) {
-            EntityPlayerMP thePlayerMP = (EntityPlayerMP) thePlayer;
+@SubscribeEvent(priority = EventPriority.HIGHEST, receiveCanceled = true)
+public static void onEvent(RightClickBlock event) {
+	EntityPlayer thePlayer = event.getEntityPlayer();
+	if (thePlayer instanceof EntityPlayerMP) {
+		EntityPlayerMP thePlayerMP = (EntityPlayerMP) thePlayer;
 
-            if (Config.debug) {
-                DivineRPG.logger.info("Right clicking block with " + thePlayer.getHeldItem(event.getHand()));
-            }
-            // FIXME - this is not as easy for the Forge Spawn Eggs
-            //if (thePlayer.getHeldItem(event.getHand()).getItem() instanceof ItemPetSpawnEgg) {
-            //	ModTriggers.SPAWN_PET.trigger(thePlayerMP);
-            //}
-            if (thePlayer.getHeldItem(event.getHand()).getItem() == ModItems.densosCrystal
-                    && thePlayer.world.provider.getDimensionType().getId() == Config.mortumDimensionId) {
-                ModTriggers.TEN_HEADS.trigger(thePlayerMP);
-            }
-            if (thePlayer.getHeldItem(event.getHand()).getItem() == ModItems.eternalArcherCrystal
-                    && thePlayer.world.provider.getDimensionType().getId() == Config.mortumDimensionId) {
-                ModTriggers.SIX_ONE.trigger(thePlayerMP);
-            }
-        }
-    }
+		if (Config.debug) {
+			DivineRPG.logger.info("Right clicking block with " + thePlayer.getHeldItem(event.getHand()));
+		}
+		// FIXME - this is not as easy for the Forge Spawn Eggs
+		// if (thePlayer.getHeldItem(event.getHand()).getItem() instanceof
+		// ItemPetSpawnEgg) {
+		// ModTriggers.SPAWN_PET.trigger(thePlayerMP);
+		// }
+		if (thePlayer.getHeldItem(event.getHand()).getItem() == ModItems.densosCrystal
+		        && thePlayer.world.provider.getDimensionType().getId() == Config.mortumDimensionId) {
+			ModTriggers.TEN_HEADS.trigger(thePlayerMP);
+		}
+		if (thePlayer.getHeldItem(event.getHand()).getItem() == ModItems.eternalArcherCrystal
+		        && thePlayer.world.provider.getDimensionType().getId() == Config.mortumDimensionId) {
+			ModTriggers.SIX_ONE.trigger(thePlayerMP);
+		}
+		if (thePlayer.getAttackingEntity() instanceof TwilightArcher) {
+			ModTriggers.ARROW_KNEE.trigger(thePlayerMP);
+		}
+	}
+}
 
-    public static void postForgeEvent(Object o) {
-        MinecraftForge.EVENT_BUS.register(o);
-    }
+public static void postForgeEvent(Object o) {
+	MinecraftForge.EVENT_BUS.register(o);
+}
 
-    public static void preInit() {
-        postForgeEvent(new EventArmorSet());
-    }
+public static void preInit() {
+	postForgeEvent(new EventArmorSet());
+}
 }
--- src/main/java/naturix/divinerpg/registry/ModTriggers.java
+++ src/main/java/naturix/divinerpg/registry/ModTriggers.java
@@ -3,6 +3,7 @@ package naturix.divinerpg.registry;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import naturix.divinerpg.events.advancementtriggers.AdvancementArrowKnee;
 import naturix.divinerpg.events.advancementtriggers.AdvancementSpawnPet;
 import naturix.divinerpg.events.advancementtriggers.AdvancementTenHeads;
 import net.minecraft.advancements.CriteriaTriggers;
@@ -12,44 +13,52 @@ import net.minecraftforge.fml.relauncher.ReflectionHelper;
 public class ModTriggers
 
 {
-	public static final AdvancementSpawnPet SPAWN_PET = new AdvancementSpawnPet("spawn_pet");
-	public static final AdvancementTenHeads TEN_HEADS = new AdvancementTenHeads("ten_heads");
-	public static final AdvancementTenHeads SIX_ONE = new AdvancementTenHeads("six_one");
+public static final AdvancementSpawnPet SPAWN_PET = new AdvancementSpawnPet("spawn_pet");
+public static final AdvancementTenHeads TEN_HEADS = new AdvancementTenHeads("ten_heads");
+public static final AdvancementTenHeads SIX_ONE = new AdvancementTenHeads("six_one");
+public static final AdvancementArrowKnee ARROW_KNEE = new AdvancementArrowKnee("arrow_knee");
 
-	public static final AdvancementSpawnPet[] TRIGGER_ARRAY = new AdvancementSpawnPet[] { SPAWN_PET };
-	public static final AdvancementTenHeads[] TRIGGER_ARRAY1 = new AdvancementTenHeads[] { TEN_HEADS };
-	public static final AdvancementTenHeads[] TRIGGER_ARRAY2 = new AdvancementTenHeads[] { SIX_ONE };
+public static final AdvancementSpawnPet[] TRIGGER_ARRAY = new AdvancementSpawnPet[] { SPAWN_PET };
+public static final AdvancementTenHeads[] TRIGGER_ARRAY1 = new AdvancementTenHeads[] { TEN_HEADS };
+public static final AdvancementTenHeads[] TRIGGER_ARRAY2 = new AdvancementTenHeads[] { SIX_ONE };
+public static final AdvancementArrowKnee[] TRIGGER_ARRAY3 = new AdvancementArrowKnee[] { ARROW_KNEE };
 
-	public static void registerTriggers() {
-		System.out.println("Registering custom triggers");
+public static void registerTriggers() {
+	System.out.println("Registering custom triggers");
 
-		Method method;
+	Method method;
 
-		method = ReflectionHelper.findMethod(CriteriaTriggers.class, "register", "func_192118_a",
-		        ICriterionTrigger.class);
+	method = ReflectionHelper.findMethod(CriteriaTriggers.class, "register", "func_192118_a", ICriterionTrigger.class);
 
-		method.setAccessible(true);
+	method.setAccessible(true);
 
-		for (int i = 0; i < TRIGGER_ARRAY.length; i++) {
-			try {
-				method.invoke(null, TRIGGER_ARRAY[i]);
-			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-				e.printStackTrace();
-			}
-		}
-		for (int i = 0; i < TRIGGER_ARRAY1.length; i++) {
-			try {
-				method.invoke(null, TRIGGER_ARRAY1[i]);
-			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-				e.printStackTrace();
-			}
-		}
-		for (int i = 0; i < TRIGGER_ARRAY2.length; i++) {
-			try {
-				method.invoke(null, TRIGGER_ARRAY2[i]);
-			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-				e.printStackTrace();
-			}
+	for (int i = 0; i < TRIGGER_ARRAY.length; i++) {
+		try {
+			method.invoke(null, TRIGGER_ARRAY[i]);
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			e.printStackTrace();
 		}
 	}
+	for (int i = 0; i < TRIGGER_ARRAY1.length; i++) {
+		try {
+			method.invoke(null, TRIGGER_ARRAY1[i]);
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			e.printStackTrace();
+		}
+	}
+	for (int i = 0; i < TRIGGER_ARRAY2.length; i++) {
+		try {
+			method.invoke(null, TRIGGER_ARRAY2[i]);
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			e.printStackTrace();
+		}
+	}
+	for (int i = 0; i < TRIGGER_ARRAY3.length; i++) {
+		try {
+			method.invoke(null, TRIGGER_ARRAY2[i]);
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			e.printStackTrace();
+		}
+	}
+}
 }
