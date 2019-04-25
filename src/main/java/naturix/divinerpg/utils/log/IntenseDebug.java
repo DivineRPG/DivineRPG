dfa6f1d1a0fc7673578709d4d1a1971fa41310ef
NicosaurusRex99 <pusspot1999@gmail.com>
2019-04-26 02:05:18 +0930

    tweaked logging

--- src/main/java/naturix/divinerpg/utils/log/IntenseDebug.java
+++ src/main/java/naturix/divinerpg/utils/log/IntenseDebug.java
@@ -2,22 +2,28 @@ package naturix.divinerpg.utils.log;
 
 import naturix.divinerpg.Config;
 import naturix.divinerpg.DivineRPG;
+import naturix.divinerpg.objects.items.ItemNotReady;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.biome.Biome;
 import net.minecraftforge.fml.common.registry.EntityEntry;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class IntenseDebug {
-public static void init() {
 
+    @SideOnly(Side.CLIENT)
+public static void init() {
 	if (Config.debug) {
 		for (Biome biome : ForgeRegistries.BIOMES.getValues()) {
 			DivineRPG.logger.info("Biome: " + biome.getBiomeName());
 		}
 		for (Item item : ForgeRegistries.ITEMS.getValues()) {
 			DivineRPG.logger.info("Item: " + new ItemStack(item).getDisplayName());
+			if(item instanceof ItemNotReady) {
+			DivineRPG.logger.info("Item: " +new ItemStack(item).getDisplayName() + " still needs porting");}
 		}
 		for (Block block : ForgeRegistries.BLOCKS.getValues()) {
 			DivineRPG.logger.info("Block: " + new ItemStack(Item.getItemFromBlock(block)).getDisplayName());
