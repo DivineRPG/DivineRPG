e1d7894d9e5513d8768ad307307f6cefcf075862
NicosaurusRex99 <pusspot1999@gmail.com>
2019-05-01 16:35:14 +0930

    Update ModBiomes.java

--- src/main/java/naturix/divinerpg/registry/ModBiomes.java
+++ src/main/java/naturix/divinerpg/registry/ModBiomes.java
@@ -11,10 +11,14 @@ import naturix.divinerpg.world.biomes.BiomeSkythern;
 import naturix.divinerpg.world.biomes.BiomeWildWood;
 import net.minecraft.world.biome.Biome;
 import net.minecraftforge.common.BiomeDictionary;
+import net.minecraftforge.common.BiomeDictionary.Type;
 import net.minecraftforge.common.BiomeManager;
+import net.minecraftforge.common.BiomeManager.BiomeEntry;
+import net.minecraftforge.common.BiomeManager.BiomeType;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.registries.IForgeRegistry;
 
 @Mod.EventBusSubscriber(modid = Reference.MODID)
@@ -38,21 +42,25 @@ public class ModBiomes {
 	public static void registerBiomes(RegistryEvent.Register<Biome> event) {
 		IForgeRegistry<Biome> registry = event.getRegistry();
 
-		registerBiome(registry, Eden, "biome_eden", BiomeManager.BiomeType.WARM, 20, BiomeDictionary.Type.FOREST, BiomeDictionary.Type.SPOOKY);
-		registerBiome(registry, WildWood, "biome_wildwood", BiomeManager.BiomeType.WARM, 20, BiomeDictionary.Type.FOREST, BiomeDictionary.Type.SPOOKY);
-		registerBiome(registry, Apalachia, "biome_apalachia", BiomeManager.BiomeType.WARM, 20, BiomeDictionary.Type.FOREST, BiomeDictionary.Type.SPOOKY);
-		registerBiome(registry, Skythern, "biome_skythern", BiomeManager.BiomeType.WARM, 20, BiomeDictionary.Type.FOREST, BiomeDictionary.Type.SPOOKY);
-		registerBiome(registry, Mortum, "biome_mortum", BiomeManager.BiomeType.WARM, 20, BiomeDictionary.Type.FOREST, BiomeDictionary.Type.SPOOKY);
-		registerBiome(registry, Iceika, "biome_iceika", BiomeManager.BiomeType.WARM, 20, BiomeDictionary.Type.FOREST, BiomeDictionary.Type.SPOOKY);
-
+        initBiome(Eden, "biome_eden", BiomeType.WARM, Type.VOID, Type.MAGICAL, Type.SPOOKY);
+        initBiome(WildWood, "biome_wildwood", BiomeType.WARM, Type.VOID, Type.MAGICAL, Type.SPOOKY);
+        initBiome(Apalachia, "biome_apalachia", BiomeType.WARM, Type.VOID, Type.MAGICAL, Type.SPOOKY);
+        initBiome(Skythern, "biome_skythern", BiomeType.WARM, Type.VOID, Type.MAGICAL, Type.SPOOKY);
+        initBiome(Mortum, "biome_mortum", BiomeType.WARM, Type.VOID, Type.MAGICAL, Type.SPOOKY);
+        initBiome(Iceika, "biome_iceika", BiomeType.WARM, Type.VOID, Type.MAGICAL, Type.SPOOKY);
+        
 
 	}
 
-	private static <T extends Biome> void registerBiome(final IForgeRegistry<Biome> registry, final T biome, final String biomeName, final BiomeManager.BiomeType biomeType, final int weight, final BiomeDictionary.Type... types) {
-		registry.register(biome.setRegistryName(Reference.MODID, biomeName));
-		BiomeDictionary.addTypes(biome, types);
-		//BiomeManager.addBiome(biomeType, new BiomeManager.BiomeEntry(biome, weight));
-		for(int x = 0; x < weight; x++)
-			BIOME_LIST.add((Biome) biome); //temp hack for now
-	}
+	private static Biome initBiome(Biome biome, String name, BiomeType biomeType, Type... types)
+    {
+        biome.setRegistryName(name);
+        ForgeRegistries.BIOMES.register(biome);
+        BiomeDictionary.addTypes(biome, types);
+        BiomeManager.addBiome(biomeType, new BiomeEntry(biome, 1024));          //set to large spread
+//      BiomeManager.addBiome(biomeType, new BiomeEntry(biome, 10));            //set to general biome spread
+        
+        
+        return biome;
+    }
 }
