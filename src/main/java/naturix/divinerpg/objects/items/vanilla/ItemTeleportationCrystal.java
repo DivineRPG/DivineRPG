d83503244623f5a07d893559806d32d7c8f62dc6
krwminer <krwshop@hotmail.com>
2019-05-18 15:20:27 -0400

    Update ItemTeleportationCrystal.java

--- src/main/java/naturix/divinerpg/objects/items/vanilla/ItemTeleportationCrystal.java
+++ src/main/java/naturix/divinerpg/objects/items/vanilla/ItemTeleportationCrystal.java
@@ -7,7 +7,6 @@ import javax.annotation.Nullable;
 import naturix.divinerpg.objects.items.base.ItemMod;
 import naturix.divinerpg.registry.DRPGCreativeTabs;
 import naturix.divinerpg.utils.SecondaryTeleporter;
-import naturix.divinerpg.utils.TeleportationHandler;
 import naturix.divinerpg.utils.TooltipLocalizer;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.EntityPlayer;
@@ -41,16 +40,17 @@ public class ItemTeleportationCrystal extends ItemMod {
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer player, EnumHand hand) {
         ItemStack stack = player.getHeldItem(hand);
-        if (worldIn.provider.getDimension() != 0) {
-            if (!worldIn.isRemote) {
-                //                teleportPlayer(worldIn, entityIn);
-                teleportToDimension(player, 0, worldIn.getSpawnPoint().getX(),
-                        getGroundPos(worldIn, worldIn.getSpawnPoint().getX(), worldIn.getSpawnPoint().getZ()).getY(),
-                        worldIn.getSpawnPoint().getZ());
+        if (!worldIn.isRemote) {
+            BlockPos teleportPos = player.getBedLocation(0);
+            if (teleportPos == null) {
+                teleportPos = player.getEntityWorld().getMinecraftServer().getWorld(0).getSpawnPoint();
             }
-        } else {
-            player.setPosition(player.getBedLocation().getX(), player.getBedLocation().getY(),
-                    player.getBedLocation().getZ());
+            if (worldIn.provider.getDimension() != 0) {
+                teleportToDimension(player, 0, teleportPos);
+            } else {
+                teleportPos = worldIn.getTopSolidOrLiquidBlock(teleportPos);
+                player.setPositionAndUpdate(teleportPos.getX(), teleportPos.getY(), teleportPos.getZ());
+            }
         }
         if (!player.isCreative()) {
             stack.damageItem(1, player);
@@ -58,37 +58,19 @@ public class ItemTeleportationCrystal extends ItemMod {
         return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, stack);
     }
 
-    @Nullable
-    public static BlockPos getGroundPos(World world, int x, int z) {
-        final BlockPos topPos = world.getHeight(new BlockPos(x, 0, z));
-
-        return world.getTopSolidOrLiquidBlock(topPos);
-    }
-
-    private void teleportPlayer(World world, EntityPlayer player) {
-        EntityPlayerMP playerMP = (EntityPlayerMP) player;
-        BlockPos coords;
-
-        coords = TeleportationHandler.getDimPos(playerMP, 0, world.getSpawnPoint());
-        TeleportationHandler.updateDimPos(playerMP, world.provider.getDimension(), player.getPosition());
-        TeleportationHandler.teleport(playerMP, 0, coords.getX(), coords.getY(), coords.getZ(),
-                playerMP.mcServer.getPlayerList());
-    }
-
-    public static void teleportToDimension(EntityPlayer player, int dimension, double x, double y, double z) {
+    public static void teleportToDimension(EntityPlayer player, int dimension, BlockPos pos) {
         int oldDimension = player.getEntityWorld().provider.getDimension();
         EntityPlayerMP entityPlayerMP = (EntityPlayerMP) player;
         MinecraftServer server = player.getEntityWorld().getMinecraftServer();
         WorldServer worldServer = server.getWorld(dimension);
         player.addExperienceLevel(0);
 
+        BlockPos teleportPos = worldServer.getTopSolidOrLiquidBlock(pos);
+        double x = teleportPos.getX();
+        double y = teleportPos.getY();
+        double z = teleportPos.getZ();
         worldServer.getMinecraftServer().getPlayerList().transferPlayerToDimension(entityPlayerMP, dimension,
                 new SecondaryTeleporter(worldServer, x, y, z));
         player.setPositionAndUpdate(x, y, z);
-        if (oldDimension == 1) {
-            player.setPositionAndUpdate(x, y, z);
-            worldServer.spawnEntity(player);
-            worldServer.updateEntityWithOptionalForce(player, false);
-        }
     }
 }
