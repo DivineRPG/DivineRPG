4d5267786320b770ce83b751f6a986c6fe4264f5
NicosaurusRex99 <pusspot1999@gmail.com>
2019-05-20 14:53:18 +0930

    added skythern decoration

--- src/main/java/naturix/divinerpg/world/biomes/BiomeSkythern.java
+++ src/main/java/naturix/divinerpg/world/biomes/BiomeSkythern.java
@@ -3,13 +3,15 @@ package naturix.divinerpg.world.biomes;
 import java.awt.Color;
 import java.util.Random;
 
+import naturix.divinerpg.dimensions.eden.worldgen.features.WorldGenConeUp;
+import naturix.divinerpg.dimensions.skythern.worldgen.WorldGenSkythernPlants;
 import naturix.divinerpg.dimensions.skythern.worldgen.trees.SkythernTree;
 import naturix.divinerpg.dimensions.skythern.worldgen.trees.SkythernTreeLarge;
 import naturix.divinerpg.registry.ModBlocks;
+import naturix.divinerpg.world.features.WorldGenTwilightDoublePlants;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.gen.feature.WorldGenAbstractTree;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -38,13 +40,54 @@ public class BiomeSkythern extends Biome {
     public int getSkyColorByTemp(float par1) {
         return Color.getHSBColor(0.1361F, 0.95F, 1.0F).getRGB();
     }
-
+    
     @Override
-    public WorldGenAbstractTree getRandomTreeFeature(Random random) {
-        if (random.nextInt(5) == 0) {
-            return new SkythernTree(false, 5);
-        } else {
-            return new SkythernTreeLarge(true, 5);
+    public void decorate(World worldIn, Random rand, BlockPos pos) {
+        SkythernTree genTree = new SkythernTree(false, 5);
+        SkythernTreeLarge genLargeTree = new SkythernTreeLarge(true, 5);
+        WorldGenConeUp genConeUp = new WorldGenConeUp(ModBlocks.divineMossystone);
+        WorldGenSkythernPlants brush = new WorldGenSkythernPlants(ModBlocks.skythernBrush);
+        WorldGenSkythernPlants bloom = new WorldGenSkythernPlants(ModBlocks.dustLily);
+        WorldGenTwilightDoublePlants blossom = new WorldGenTwilightDoublePlants(ModBlocks.dustBrambles, ModBlocks.skythernGrass);
+
+        BlockPos.MutableBlockPos mutPos = new BlockPos.MutableBlockPos(0, 0, 0);
+        for (int i = 0; i < 40; i++) {
+            int rx = pos.getX() + rand.nextInt(16) + 8;
+            int ry = 13 + rand.nextInt(80);
+            int rz = pos.getZ() + rand.nextInt(16) + 8;
+            mutPos.setPos(rx, ry, rz);
+            bloom.generate(worldIn, rand, mutPos);
+            genLargeTree.generate(worldIn, rand, mutPos);
         }
+        for (int i = 0; i < 100; i++) {
+            int rx = pos.getX() + rand.nextInt(16) + 8;
+            int ry = 13 + rand.nextInt(80);
+            int rz = pos.getZ() + rand.nextInt(16) + 8;
+            mutPos.setPos(rx, ry, rz);
+            brush.generate(worldIn, rand, mutPos);
+            blossom.generate(worldIn, rand, mutPos);
+        }
+        for (int i = 0; i < 1000; i++) {
+            int rx = pos.getX() + rand.nextInt(16) + 8;
+            int ry = 13 + rand.nextInt(80);
+            int rz = pos.getZ() + rand.nextInt(16) + 8;
+            mutPos.setPos(rx, ry, rz);
+            brush.generate(worldIn, rand, mutPos);
+        }
+        for (int i = 0; i < 10; i++) {
+            int rx = pos.getX() + rand.nextInt(16) + 8;
+            int ry = 55 + rand.nextInt(20);
+            int rz = pos.getZ() + rand.nextInt(16) + 8;
+            mutPos.setPos(rx, ry, rz);
+            genTree.generate(worldIn, rand, mutPos);
+        }
+        for (int i = 0; i < 1; i++) {
+            int rx = pos.getX() + rand.nextInt(16) + 8;
+            int ry = 55 + rand.nextInt(20);
+            int rz = pos.getZ() + rand.nextInt(16) + 8;
+            mutPos.setPos(rx, ry, rz);
+            genConeUp.generate(worldIn, rand, mutPos);
+        }
+        
     }
 }
