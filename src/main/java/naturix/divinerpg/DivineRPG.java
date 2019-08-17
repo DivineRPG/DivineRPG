0e54535d504b7aae760014d8f3479ac080486d49
oshi4 <sheglovaoe@gmail.com>
2019-08-17 14:29:42 +0300

    Dev hat render

--- src/main/java/naturix/divinerpg/DivineRPG.java
+++ src/main/java/naturix/divinerpg/DivineRPG.java
@@ -9,6 +9,7 @@ import naturix.divinerpg.compat.ProjectECompat;
 import naturix.divinerpg.events.ArcanaTickHandler;
 import naturix.divinerpg.events.EventArmorSet;
 import naturix.divinerpg.events.EventArmorTick;
+import naturix.divinerpg.events.EventDevHat;
 import naturix.divinerpg.proxy.CommonProxy;
 import naturix.divinerpg.registry.ModMessages;
 import naturix.divinerpg.registry.ModOreDict;
@@ -80,6 +81,7 @@ public class DivineRPG {
         MinecraftForge.EVENT_BUS.register(new EventArmorSet());
         MinecraftForge.EVENT_BUS.register(new EventArmorTick());
         MinecraftForge.EVENT_BUS.register(new ArcanaTickHandler());
+        MinecraftForge.EVENT_BUS.register(new EventDevHat());
 
         ModMessages.init();
     }
