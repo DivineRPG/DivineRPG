8a2915e1bf9fcd794d66ba1a64b5856a93a82861
Аркадий <sheglovaoe@gmail.com>
2019-08-22 12:26:50 +0300

    Made async update checking

--- src/main/java/naturix/divinerpg/events/EventClientLogin.java
+++ src/main/java/naturix/divinerpg/events/EventClientLogin.java
@@ -1,7 +1,5 @@
 package naturix.divinerpg.events;
 
-import java.io.IOException;
-
 import naturix.divinerpg.Config;
 import naturix.divinerpg.DivineRPG;
 import naturix.divinerpg.utils.MessageLocalizer;
@@ -15,39 +13,101 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
+import java.util.concurrent.CompletableFuture;
+
 @Mod.EventBusSubscriber(modid = Reference.MODID, value = Side.CLIENT)
 public class EventClientLogin {
 
     @SubscribeEvent
     public void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent evt) {
-        EntityPlayer p = evt.player;
-        
-        if (!p.world.isRemote) {
+        EntityPlayer player = evt.player;
 
-        	if (Config.UpdateChecker) {
-                if (!UpdateChecker.isOnline() && !Config.canShowOverlay) {
-                	Logging.message(p, TextFormatting.LIGHT_PURPLE + MessageLocalizer.normal("message.version.internet"));
-                			
-                } else if (UpdateChecker.isOnline() && UpdateChecker.isUpdateAvailable() && !Config.canShowOverlay) {
-                	Logging.message(p, TextFormatting.RED + MessageLocalizer.normal("message.version.update"));
-                    try {
-                    Logging.message(p, TextFormatting.WHITE + MessageLocalizer.version(UpdateChecker.getCurrentVersion()));
-                    } catch (IOException e) {
-                    Logging.message(p, TextFormatting.LIGHT_PURPLE + MessageLocalizer.normal("message.version.internet"));
-                    	}
-                }
-            }else {Logging.message(p, TextFormatting.AQUA + p.getDisplayName().getFormattedText() + " has the most recent version of DivineRPG installed!");}
-            if(Utils.DEV_LIST.contains(p.getName())) {
-    			Logging.message(p, TextFormatting.WHITE +	MessageLocalizer.normal("Welcome DivineRPG developer! "+p.getName()+" is on the DivineRPG dev team"));
-                Logging.message(p, "Welcome " + p.getDisplayName().getFormattedText());
+        if (!player.world.isRemote) {
+
+            // Greetings
+            if (Utils.DEV_LIST.contains(player.getName())) {
+                Logging.message(player, TextFormatting.WHITE + MessageLocalizer.normal("Welcome DivineRPG developer! " + player.getName() + " is on the DivineRPG dev team"));
+            }
+
+            Logging.message(player, "Welcome " + player.getDisplayName().getFormattedText());
+
+            printGeneralInformation(player);
+        }
+    }
+
+    /**
+     * If UpdateChecking enabled, download version asynchronously and writes to chat up-to-date info
+     * All methods are using overlay config
+     *
+     * @param player
+     */
+    private void printGeneralInformation(EntityPlayer player) {
+        if (Config.UpdateChecker) {
+            // check online status
+            CompletableFuture<Boolean> onlineStatus = CompletableFuture.supplyAsync(() -> checkPlayerOnlineStatus(player));
+            // get version
+            CompletableFuture<String> version = onlineStatus.thenApply(isOnline -> isOnline ? UpdateChecker.getCurrentVersion() : null);
+
+            // after all tasks print version detect
+            version.thenCombineAsync(onlineStatus, (currentVersion, isOnline) -> {
+                printVersionStatus(player, currentVersion, isOnline);
+                // need to return value
+                return true;
+            });
+        }
+    }
+
+    /**
+     * Checking online status and writing log if detecting offline
+     *
+     * @param player
+     * @return
+     */
+    private boolean checkPlayerOnlineStatus(EntityPlayer player) {
+        // Writing mod version
+        DivineRPG.logger.info("Current Version : " + Reference.VERSION);
+
+        // trying ping Google
+        boolean isOnline = UpdateChecker.pingGoogle();
+        // Log message with is offline
+        if (!isOnline && Config.canShowOverlay) {
+            Logging.message(player, TextFormatting.LIGHT_PURPLE + MessageLocalizer.normal("message.version.internet"));
+        }
+
+        // logging online status
+        DivineRPG.logger.info("Is Online: " + isOnline);
+        return isOnline;
+    }
+
+    /**
+     * Retreiving actual DivineRPG mod version and Log
+     *
+     * @param player
+     * @param newVersion
+     * @param isOnline
+     * @return
+     */
+    private void printVersionStatus(EntityPlayer player, String newVersion, boolean isOnline) {
+
+        boolean githubAccessError = isOnline && newVersion == null;
+        boolean haveUpdates = !githubAccessError && newVersion.contains(Reference.MODID);
+
+        if (Config.canShowOverlay && !githubAccessError) {
+            if (haveUpdates) {
+                Logging.message(player, TextFormatting.RED + MessageLocalizer.normal("message.version.update"));
+                Logging.message(player, TextFormatting.WHITE + MessageLocalizer.version(newVersion));
+
             } else {
-                Logging.message(p, "Welcome " + p.getDisplayName().getFormattedText());
+                Logging.message(player, TextFormatting.AQUA + player.getDisplayName().getFormattedText() + " has the most recent version of DivineRPG installed!");
             }
         }
-        DivineRPG.logger.info("Current Version : " + Reference.VERSION);
-		DivineRPG.logger.info("Is Online : " + UpdateChecker.isOnline());
-		if(UpdateChecker.isOnline()) {
-		DivineRPG.logger.info("Update Available : " + UpdateChecker.isUpdateAvailable());
-		}
-    }  		
+
+        if (isOnline) {
+            if (githubAccessError) {
+                DivineRPG.logger.info("Can't retrieve current version from GitHub");
+            } else {
+                DivineRPG.logger.info("Update Available : " + haveUpdates);
+            }
+        }
+    }
 }
--- src/main/java/naturix/divinerpg/events/UpdateChecker.java
+++ src/main/java/naturix/divinerpg/events/UpdateChecker.java
@@ -3,71 +3,31 @@ package naturix.divinerpg.events;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.Inet4Address;
 import java.net.InetAddress;
-import java.net.InterfaceAddress;
-import java.net.NetworkInterface;
 import java.net.URL;
-import java.util.Enumeration;
-import java.util.Iterator;
-import java.util.List;
-
-import naturix.divinerpg.DivineRPG;
-import naturix.divinerpg.utils.Reference;
 
 
 public class UpdateChecker {
 
-    public static boolean isUpdateAvailable() {
+    public static boolean pingGoogle() {
         try {
-            if (!getCurrentVersion().contains(Reference.VERSION))
-                return true;
+            return InetAddress.getByName("google.com").isReachable(500);
         } catch (IOException e) {
             e.printStackTrace();
             return false;
         }
-        return false;
     }
 
-    public static boolean isOnline() {
+
+    public static String getCurrentVersion() {
         try {
-            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
-            while (interfaces.hasMoreElements()) {
-                NetworkInterface interf = interfaces.nextElement();
-                if (interf.isUp() && !interf.isLoopback()) {
-                    List<InterfaceAddress> adrs = interf.getInterfaceAddresses();
-                    for (Iterator<InterfaceAddress> iter = adrs.iterator(); iter.hasNext();) {
-                        InterfaceAddress adr = iter.next();
-                        InetAddress inadr = adr.getAddress();
-                        if (inadr instanceof Inet4Address) {
-                            DivineRPG.logger.debug("Internet connection found");
-                            return true;
-                        }
-                    }
-                }
-            }
-        } catch (Exception e) {
-        	DivineRPG.logger.warn("Something is probably wrong with your network configuration. DivineRPG can continue loading but joining worlds will be slightly slower!");
-            try {
-                URL url = new URL("https://github.com");
-                HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection();
-                Object data = httpConnection.getContent();
-            } catch (Exception ex) {
-            	DivineRPG.logger.debug("Internet connection not found");
-                return false;
-            }
-            DivineRPG.logger.debug("Internet connection found");
-            return true;
+            BufferedReader versionFile = new BufferedReader(new InputStreamReader(new URL("https://raw.githubusercontent.com/NicosaurusRex99/DivineRPG/1.12.2/version.txt").openStream()));
+            String curVersion = versionFile.readLine();
+            versionFile.close();
+            return curVersion;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
         }
-        DivineRPG.logger.debug("Internet connection not found");
-        return false;
-    }
-
-    public static String getCurrentVersion() throws IOException {
-        BufferedReader versionFile = new BufferedReader(new InputStreamReader(new URL("https://raw.githubusercontent.com/NicosaurusRex99/DivineRPG/1.12.2/version.txt").openStream()));
-        String curVersion = versionFile.readLine();
-        versionFile.close();
-        return curVersion;
     }
 }
