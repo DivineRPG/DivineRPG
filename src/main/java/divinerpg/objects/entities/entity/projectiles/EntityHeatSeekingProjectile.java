869e79f4890185b8b740398d2c2233cb6dea706e
oshi4 <sheglovaoe@gmail.com>
2019-08-31 21:44:37 +0300

    Fix #64 Searching area only look towards as entity looks

--- src/main/java/divinerpg/objects/entities/entity/projectiles/EntityHeatSeekingProjectile.java
+++ src/main/java/divinerpg/objects/entities/entity/projectiles/EntityHeatSeekingProjectile.java
@@ -1,18 +1,20 @@
 package divinerpg.objects.entities.entity.projectiles;
 
-import java.util.List;
-
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityThrowable;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
+import java.util.List;
+
 public class EntityHeatSeekingProjectile extends EntityThrowable {
 
-    private Entity target = null;
+    private EntityLivingBase target = null;
     private boolean onlyPlayers = false;
 
     public EntityHeatSeekingProjectile(World w) {
@@ -38,27 +40,53 @@ public class EntityHeatSeekingProjectile extends EntityThrowable {
         if (this.world.isRemote)
             return;
         List<EntityLivingBase> mobs = this.world.getEntitiesWithinAABB(EntityLivingBase.class,
-                this.getEntityBoundingBox().grow(30, 30, 30));
-        boolean findNewTarget = this.target == null || (this.target != null && this.target.isDead);
+                getTowards());
+        // remove thrower
+        mobs.remove(getThrower());
+
+        boolean findNewTarget = this.target == null || this.target.isDead;
+
         for (EntityLivingBase e : mobs) {
-            if (e != this.getThrower() && (!this.onlyPlayers || (this.onlyPlayers && e instanceof EntityPlayer))) {
-                float targetDist = target == null ? 0 : this.getDistance(target);
-                float compareDist = this.getDistance(e);
-                if (findNewTarget
-                        && (target == null || (target != null && this.getDistance(e) < this.getDistance(target))))
+            if (!this.onlyPlayers || e instanceof EntityPlayer) {
+                if (findNewTarget && (target == null || this.getDistance(e) < this.getDistance(target)))
                     target = e;
             }
         }
         if (target != null) {
             Vec3d dir = new Vec3d(target.posX - this.posX, (target.posY + target.getEyeHeight()) - this.posY,
-                    target.posZ - this.posZ).normalize();
-            this.motionX = dir.x / 1.5;
-            this.motionY = dir.y / 1.5;
-            this.motionZ = dir.z / 1.5;
+                    target.posZ - this.posZ).normalize().scale(1 / 1.5);
+            setVelocity(dir.x, dir.y, dir.z);
         }
 
         if (this.ticksExisted > 50)
             this.setDead();
+    }
+
+    /**
+     * Get correct towards area when entity can find enemy to attack
+     * Expanding only towards where entity looks
+     */
+    private AxisAlignedBB getTowards() {
+        AxisAlignedBB box = this.getEntityBoundingBox();
+        int expand = 30;
+        EnumFacing horizontalFacing = getHorizontalFacing();
+
+        BlockPos min = new BlockPos(box.minX, box.minY - expand, box.minZ);
+        BlockPos max = new BlockPos(box.maxX, box.maxY + expand, box.maxZ);
+
+        // expanding towards
+        max = max.offset(horizontalFacing, expand)
+                // expanding to sides
+                .offset(horizontalFacing.rotateY(), expand)
+                .offset(horizontalFacing.rotateY().getOpposite(), expand);
+
+        // expanding to sides
+        min = min.offset(horizontalFacing.rotateY(), expand)
+                .offset(horizontalFacing.rotateY().getOpposite(), expand);
+
+
+        box = new AxisAlignedBB(min, max);
+        return box;
     }
 
     @Override
