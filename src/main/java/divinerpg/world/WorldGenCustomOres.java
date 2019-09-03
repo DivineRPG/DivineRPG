4942f475ffd0aa812d54ef26a2a6f637054ae78e
oshi4 <sheglovaoe@gmail.com>
2019-09-04 00:16:48 +0300

    Improve World gen performance

--- src/main/java/divinerpg/world/WorldGenCustomOres.java
+++ src/main/java/divinerpg/world/WorldGenCustomOres.java
@@ -1,12 +1,10 @@
 package divinerpg.world;
 
-import java.util.Random;
-
 import com.google.common.base.Predicate;
-
 import divinerpg.Config;
 import divinerpg.registry.ModBlocks;
 import divinerpg.registry.ModDimensions;
+import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.block.state.pattern.BlockMatcher;
 import net.minecraft.init.Blocks;
@@ -16,24 +14,35 @@ import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraft.world.gen.feature.WorldGenLakes;
 import net.minecraft.world.gen.feature.WorldGenMinable;
+import net.minecraft.world.gen.feature.WorldGenerator;
 import net.minecraftforge.fml.common.IWorldGenerator;
+
+import java.util.Random;
 
 public class WorldGenCustomOres implements IWorldGenerator {
 
     public static WorldGenCustomOres instance = new WorldGenCustomOres();
 
+    // declaring predicats to prevent constantly creating
+    private Predicate<IBlockState> stonePredicat = BlockMatcher.forBlock(Blocks.STONE);
+    private Predicate<IBlockState> netherPredicat = BlockMatcher.forBlock(Blocks.NETHERRACK);
+    private Predicate<IBlockState> twilightPredicat = BlockMatcher.forBlock(ModBlocks.twilightStone);
+
+    private WorldGenerator waterLake = new WorldGenLakes(Blocks.WATER);
+    private WorldGenerator tarLake = new WorldGenLakes(ModBlocks.tar);
+
     @Override
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator,
-            IChunkProvider chunkProvider) {
+                         IChunkProvider chunkProvider) {
         switch (world.provider.getDimension()) {
-        case -1:
-            genNether(world, random, chunkX, chunkZ);
-            break;
-        case 0:
-            genSurface(world, random, chunkX, chunkZ);
-            break;
-        case 1:
-            break;
+            case -1:
+                genNether(world, random, chunkX, chunkZ);
+                break;
+            case 0:
+                genOverworld(world, random, chunkX, chunkZ);
+                break;
+            case 1:
+                break;
         }
 
         if (world.provider.getDimension() == ModDimensions.edenDimension.getId()) {
@@ -54,94 +63,114 @@ public class WorldGenCustomOres implements IWorldGenerator {
 
     }
 
-    private void addOreSpawn(IBlockState block, World world, Random random, int blockXPos, int blockZPos, int maxX,
-            int maxZ, int maxVeinSize, int chance, int minY, int maxY, Predicate<IBlockState> blockToSpawnIn) {
-        int diffMinMaxY = maxY - minY;
-        for (int x = 0; x < chance; x++) {
-            int posX = blockXPos + random.nextInt(maxX);
-            int posY = minY + random.nextInt(diffMinMaxY);
-            if (diffMinMaxY < minY) {
-                diffMinMaxY = maxY;
+    ///////////////////////////
+    // HELPING METHODS
+    ///////////////////////////
+
+    /**
+     * @param chance - we try to spawn ore chance² times
+     */
+    private void spawnOre(World world, Random random, IBlockState ore, Predicate<IBlockState> replacing,
+                          int chunkX, int chunkZ, int maxVeinSize, int chance, int minY, int maxY) {
+
+        int height = getHeightOrThrow(minY, maxY);
+        WorldGenMinable gen = new WorldGenMinable(ore, maxVeinSize, replacing);
+
+        // Originally we passed throw 2 loops, Why????
+        for (int i = 0; i < chance; i++) {
+            for (int j = 0; j < chance; j++) {
+                BlockPos pos = new BlockPos(
+                        chunkX * 16 + random.nextInt(16),
+                        minY + random.nextInt(height + 1),
+                        chunkZ * 16 + random.nextInt(16));
+
+                gen.generate(world, random, pos);
             }
-            int posZ = blockZPos + random.nextInt(maxZ);
-
-            WorldGenMinable gen = new WorldGenMinable(block, maxVeinSize, blockToSpawnIn);
-
-            if (minY > maxY || minY < 0 || maxY > 256)
-                throw new IllegalArgumentException("Ore Generated Out of Bounds");
-            int heighDiff = maxY - minY + 1;
-
-            for (int i = 0; i < chance; i++) {
-                int b = blockXPos * 16 + random.nextInt(16);
-                int y = minY + random.nextInt(heighDiff);
-                int z = blockZPos * 16 + random.nextInt(16);
-                BlockPos pos2 = new BlockPos(b, y, z);
-                gen.generate(world, random, pos2);
-
-            }
-
         }
     }
 
-    //block, worldgen, random, blockXPos, blockZPos, maxX, maxZ, maxVeinSize, chance, minY, maxY, blockToSpawnIn
-    private void genSurface(World world, Random random, int chunkX, int chunkZ) {
-        addOreSpawn(ModBlocks.realmiteOre.getDefaultState(), world, random, chunkX, chunkZ, 16, 16, Config.realmiteVein,
-                Config.realmiteTries, Config.realmiteMin, Config.realmiteMax, BlockMatcher.forBlock(Blocks.STONE));
-        addOreSpawn(ModBlocks.rupeeOre.getDefaultState(), world, random, chunkX, chunkZ, 16, 16, Config.rupeeVein,
-                Config.rupeeTries, Config.rupeeMin, Config.rupeeMax, BlockMatcher.forBlock(Blocks.STONE));
-        addOreSpawn(ModBlocks.arlemiteOre.getDefaultState(), world, random, chunkX, chunkZ, 16, 16, Config.arlemiteVein,
-                Config.arlemiteTries, Config.arlemiteMin, Config.arlemiteMax, BlockMatcher.forBlock(Blocks.STONE));
+    private void spawnTwilightOre(World world, Random random, Block ore, int chunkX, int chunkZ) {
+        spawnOre(world, random, ore.getDefaultState(), twilightPredicat, chunkX, chunkZ,
+                4, 5, 15, 100);
+    }
 
-        if (random.nextInt(8) == 0) {
-            int posX = chunkX + random.nextInt(16) + 8;
-            int posY = random.nextInt(150);
-            int posZ = chunkZ + random.nextInt(16) + 8;
-            BlockPos pos = new BlockPos(posX, posY, posZ);
-            if (Config.generateTar && (random.nextInt(10) == 0 || posY < 60)) {
-                (new WorldGenLakes(ModBlocks.tar)).generate(world, random, pos);
-            }
+    /**
+     * @param chance - in one of passed chanses will spawn lake
+     */
+    private void generateLake(World world, Random random, WorldGenerator generator, int chunkX, int chunkZ,
+                              int minY, int maxY, int chance) {
+
+        // we are not lucky
+        if (random.nextInt(chance) != 0)
+            return;
+
+        BlockPos pos = new BlockPos(chunkX * 16 + random.nextInt(16),
+                minY + random.nextInt(getHeightOrThrow(minY, maxY) + 1),
+                chunkZ * 16 + random.nextInt(16));
+
+        // But here offset is needed. It's a magic, isn't it?
+        generator.generate(world, random, pos.add(8, 0, 8));
+    }
+
+    /**
+     * Gets height difference and throw in case of errors
+     */
+    private int getHeightOrThrow(int minY, int maxY) {
+        int height = maxY - minY;
+        if (height <= 0) {
+            throw new IllegalArgumentException("Max Y equals or less min Y");
+        }
+        if (minY < 0 || maxY > 256)
+            throw new IllegalArgumentException("Ore Generated Out of Bounds");
+
+        return height;
+    }
+
+
+    /////////////////////////////////
+    // DIMENSION ORE SPAWNING
+    /////////////////////////////////
+    private void genOverworld(World world, Random random, int chunkX, int chunkZ) {
+        spawnOre(world, random, ModBlocks.realmiteOre.getDefaultState(), stonePredicat, chunkX, chunkZ,
+                Config.realmiteVein, Config.realmiteTries, Config.realmiteMin, Config.realmiteMax);
+        spawnOre(world, random, ModBlocks.rupeeOre.getDefaultState(), stonePredicat, chunkX, chunkZ,
+                Config.rupeeVein, Config.rupeeTries, Config.rupeeMin, Config.rupeeMax);
+        spawnOre(world, random, ModBlocks.arlemiteOre.getDefaultState(), stonePredicat, chunkX, chunkZ,
+                Config.arlemiteVein, Config.arlemiteTries, Config.arlemiteMin, Config.arlemiteMax);
+
+        if (Config.generateTar) {
+            // bottom layer, more frequently
+            generateLake(world, random, tarLake, chunkX, chunkZ, 0, 60, 8);
+            // top layer, 10 times less
+            generateLake(world, random, tarLake, chunkX, chunkZ, 61, 150, 80);
         }
     }
 
     private void genNether(World world, Random random, int chunkX, int chunkZ) {
-        addOreSpawn(ModBlocks.netheriteOre.getDefaultState(), world, random, chunkX, chunkZ, 16, 16, 4, 5, 1,
-                world.getHeight(), BlockMatcher.forBlock(Blocks.NETHERRACK));
-        addOreSpawn(ModBlocks.bloodgemOre.getDefaultState(), world, random, chunkX, chunkZ, 16, 16, 4, 5, 1,
-                world.getHeight(), BlockMatcher.forBlock(Blocks.NETHERRACK));
-
+        spawnOre(world, random, ModBlocks.netheriteOre.getDefaultState(), netherPredicat, chunkX, chunkZ,
+                4, 5, 1, world.getHeight());
+        spawnOre(world, random, ModBlocks.bloodgemOre.getDefaultState(), netherPredicat, chunkX, chunkZ,
+                4, 5, 1, world.getHeight());
     }
 
     private void genEden(World world, Random random, int chunkX, int chunkZ) {
-        addOreSpawn(ModBlocks.edenOre.getDefaultState(), world, random, chunkX, chunkZ, 16, 16, 4, 5, 15, 100,
-                BlockMatcher.forBlock(ModBlocks.twilightStone));
-
+        spawnTwilightOre(world, random, ModBlocks.edenOre, chunkX, chunkZ);
     }
 
     private void genWild(World world, Random random, int chunkX, int chunkZ) {
-        addOreSpawn(ModBlocks.wildwoodOre.getDefaultState(), world, random, chunkX, chunkZ, 16, 16, 4, 5, 15, 100,
-                BlockMatcher.forBlock(ModBlocks.twilightStone));
+        spawnTwilightOre(world, random, ModBlocks.wildwoodOre, chunkX, chunkZ);
+        generateLake(world, random, waterLake, chunkX, chunkZ, 0, 150, 16);
     }
 
     private void genApalachia(World world, Random random, int chunkX, int chunkZ) {
-        addOreSpawn(ModBlocks.apalachiaOre.getDefaultState(), world, random, chunkX, chunkZ, 16, 16, 4, 5, 15, 100,
-                BlockMatcher.forBlock(ModBlocks.twilightStone));
-        if (random.nextInt(16) == 0) {
-            int posX = chunkX + random.nextInt(16) + 8;
-            int posY = random.nextInt(150);
-            int posZ = chunkZ + random.nextInt(16) + 8;
-            BlockPos pos = new BlockPos(posX, posY, posZ);
-            (new WorldGenLakes(Blocks.WATER)).generate(world, random, pos);
-        }
+        spawnTwilightOre(world, random, ModBlocks.apalachiaOre, chunkX, chunkZ);
     }
 
     private void genSkythern(World world, Random random, int chunkX, int chunkZ) {
-        addOreSpawn(ModBlocks.skythernOre.getDefaultState(), world, random, chunkX, chunkZ, 16, 16, 4, 5, 15, 100,
-                BlockMatcher.forBlock(ModBlocks.twilightStone));
+        spawnTwilightOre(world, random, ModBlocks.skythernOre, chunkX, chunkZ);
     }
 
     private void genMortum(World world, Random random, int chunkX, int chunkZ) {
-        addOreSpawn(ModBlocks.mortumOre.getDefaultState(), world, random, chunkX, chunkZ, 16, 16, 4, 5, 15, 100,
-                BlockMatcher.forBlock(ModBlocks.twilightStone));
+        spawnTwilightOre(world, random, ModBlocks.mortumOre, chunkX, chunkZ);
     }
-
 }
