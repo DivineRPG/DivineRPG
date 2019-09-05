7b8c592e3e236d1e5e4951eaba228223e7f18345
Аркадий <sheglovaoe@gmail.com>
2019-09-05 09:51:59 +0300

    Removed truffle idea from Skythern

--- src/main/java/divinerpg/dimensions/skythern/BiomeSkythern.java
+++ src/main/java/divinerpg/dimensions/skythern/BiomeSkythern.java
@@ -4,25 +4,18 @@ import divinerpg.dimensions.TwilightBiomeBase;
 import divinerpg.dimensions.eden.WorldGenConeUp;
 import divinerpg.registry.ModBlocks;
 import divinerpg.world.features.WorldGenTwilightDoublePlants;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import net.minecraft.world.gen.feature.WorldGenerator;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.awt.*;
-import java.util.Random;
 
 public class BiomeSkythern extends TwilightBiomeBase {
-
-    protected WorldGenerator genLargeTreeTruffle;
 
     public BiomeSkythern() {
         super(new BiomeProperties("Skythern"), "skythern");
 
         this.genTree = new SkythernTree(false, 5);
         this.genLargeTree = new SkythernTreeLarge(true, 5);
-        this.genLargeTreeTruffle = new SkythernTreeLargeTruffle(true, 5);
         this.genConeUp = new WorldGenConeUp(ModBlocks.divineMossystone);
         this.brush = new WorldGenSkythernPlants(ModBlocks.skythernBrush);
         this.bloom = new WorldGenSkythernPlants(ModBlocks.dustLily);
@@ -44,12 +37,5 @@ public class BiomeSkythern extends TwilightBiomeBase {
     @SideOnly(Side.CLIENT)
     public int getSkyColorByTemp(float par1) {
         return Color.getHSBColor(0.1361F, 0.95F, 1.0F).getRGB();
-    }
-
-    @Override
-    public void decorate(World worldIn, Random rand, BlockPos pos) {
-        super.decorate(worldIn, rand, pos);
-
-        generate(worldIn, rand, this.chunk.x, this.chunk.z, 5, 13, 13 + 80, genLargeTreeTruffle);
     }
 }
