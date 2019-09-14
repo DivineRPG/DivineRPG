130d4132f87170c05d6e3e987802e88a29945fc4
Immortallis1 <shieldfury555@gmail.com>
2019-09-13 22:55:09 -0400

    Make BlockModPowered abstract and remove some duplicate lang entries

--- src/main/java/divinerpg/objects/blocks/BlockModPowered.java
+++ src/main/java/divinerpg/objects/blocks/BlockModPowered.java
@@ -2,20 +2,16 @@ package divinerpg.objects.blocks;
 
 import java.util.Random;
 
-import javax.annotation.Nullable;
-
 import divinerpg.enums.EnumBlockType;
 import net.minecraft.block.Block;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
-public class BlockModPowered extends BlockMod {
+public abstract class BlockModPowered extends BlockMod {
     public static final PropertyBool POWERED = PropertyBool.create("powered");
 
     public BlockModPowered(EnumBlockType blockType, String name, float hardness) {
