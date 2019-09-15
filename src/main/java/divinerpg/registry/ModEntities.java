7433a49e3ec60aab90c8b5b843848832f2b57553
Immortallis1 <shieldfury555@gmail.com>
2019-09-15 17:23:40 -0400

    Vethea cleanup: remove bouncing projectiles

--- src/main/java/divinerpg/registry/ModEntities.java
+++ src/main/java/divinerpg/registry/ModEntities.java
@@ -233,8 +233,6 @@ public class ModEntities {
                 buildProjectileEntry(EntityAttractor.class, "attractor"),
                 buildProjectileEntry(EntityGeneralsStaff.class, "generals_staff"),
                 buildProjectileEntry(EntityMeteor.class, "meteor"), buildProjectileEntry(EntityStar.class, "star"),
-                buildProjectileEntry(EntityBouncingProjectile.class, "bouncing"),
-                buildProjectileEntry(EntityEvernightProjectile.class, "evernight"),
                 buildProjectileEntry(EntityLamona.class, "lamona"),
                 buildProjectileEntry(EntityFirefly.class, "firefly"),
                 buildProjectileEntry(EntityReflector.class, "reflector"),
@@ -254,7 +252,6 @@ public class ModEntities {
 
         // Projectiles
         registerRender(EntityAttractor.class, manager -> new RenderAttractor(manager));
-        registerRender(EntityBouncingProjectile.class, manager -> new RenderBouncingProjectile(manager));
         registerRender(EntityCaveRock.class, manager -> new RenderCaveRock(manager, 1f));
         registerRender(EntityCoriShot.class, manager -> new RenderCoriShot(manager, 1f));
         registerRender(EntityCorruptedBullet.class, manager -> new RenderCorruptedBullet(manager, 1f));
@@ -285,7 +282,6 @@ public class ModEntities {
         registerRender(EntityVileStorm.class, manager -> new RenderVileStorm(manager, 1f));
         registerRender(EntityWatcherShot.class, manager -> new RenderWatcherShot(manager, 1f));
 
-        // registerRender(EntityEvernightProjectile.class, manager -> new RenderEvernightProjectile(manager));
         registerRender(EntityFrostCloud.class, manager -> new RenderFrostCloud(manager));
 
         // Vanilla
