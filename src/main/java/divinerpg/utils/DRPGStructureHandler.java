4305a97b7dcc3a8804c610e89c8f34473a2f0d0d
Аркадий <sheglovaoe@gmail.com>
2019-09-26 11:41:21 +0300

    Fixed wrong placement settings for template and now we are getting correct WorldServer for current World dimention

--- src/main/java/divinerpg/utils/DRPGStructureHandler.java
+++ src/main/java/divinerpg/utils/DRPGStructureHandler.java
@@ -1,22 +1,27 @@
 package divinerpg.utils;
 
+import divinerpg.api.Reference;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityLockableLoot;
+import net.minecraft.util.Mirror;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
 import net.minecraft.world.gen.feature.WorldGenerator;
+import net.minecraft.world.gen.structure.template.PlacementSettings;
 import net.minecraft.world.gen.structure.template.Template;
 import net.minecraft.world.gen.structure.template.TemplateManager;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 
 import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Random;
 import java.util.function.Function;
-
-import divinerpg.api.Reference;
 
 /**
  * Created by LiteWolf101 on Jan
@@ -50,19 +55,19 @@ public class DRPGStructureHandler extends WorldGenerator implements IStructure {
 
     public void generateStructure(World world, BlockPos pos){
         MinecraftServer mcServer = world.getMinecraftServer();
-        TemplateManager manager = worldServer.getStructureTemplateManager();
+        TemplateManager manager = getWorldServer(world).getStructureTemplateManager();
         ResourceLocation location = new ResourceLocation(Reference.MODID, structureName);
         Template template = manager.get(mcServer, location);
         if (template != null){
             IBlockState state = world.getBlockState(pos);
             world.notifyBlockUpdate(pos, state, state, 3);
-            template.addBlocksToWorldChunk(world, pos, settings);
+            template.addBlocksToWorldChunk(world, pos, getSettings(pos));
 
-            generateLoot(template, pos);
+            generateLoot(world, template, pos);
         }
     }
 
-    public void generateLoot(Template template, BlockPos pos) {
+    public void generateLoot(World world, Template template, BlockPos pos) {
         if (lootTableFunc == null)
             return;
 
@@ -90,6 +95,7 @@ public class DRPGStructureHandler extends WorldGenerator implements IStructure {
         if (allBlocks == null)
             return;
 
+
         allBlocks
                 .forEach(blockInfo -> {
                     if (blockInfo.tileentityData == null)
@@ -98,20 +104,31 @@ public class DRPGStructureHandler extends WorldGenerator implements IStructure {
                     // Get correct blockpos from seed-relative
                     BlockPos correct = blockInfo.pos.add(pos.getX(), pos.getY(), pos.getZ());
 
-                    TileEntity tileEntity = worldServer.getTileEntity(correct);
+                    TileEntity tileEntity = world.getTileEntity(correct);
 
                     if (tileEntity instanceof TileEntityLockableLoot){
                         // Getting loot table from current chest
-                        ResourceLocation lootTable = lootTableFunc.apply(new TileEntityChestArgs(worldServer, (TileEntityLockableLoot)tileEntity, correct));
+                        ResourceLocation lootTable = lootTableFunc.apply(new TileEntityChestArgs(world, (TileEntityLockableLoot)tileEntity, correct));
 
                         if (lootTable != null)
-                            ((TileEntityLockableLoot)tileEntity).setLootTable(lootTable, worldServer.rand.nextLong());
+                            ((TileEntityLockableLoot)tileEntity).setLootTable(lootTable, world.rand.nextLong());
                         else {
                             System.out.println("Can't find loot table for tile entity :" + correct.toString());
                         }
                     }
                 });
 
+    }
+
+    @Override
+    public WorldServer getWorldServer(World world) {
+        return FMLCommonHandler.instance().getMinecraftServerInstance().getWorld(world.provider.getDimension());
+    }
+
+    @Override
+    public PlacementSettings getSettings(BlockPos pos) {
+        return new PlacementSettings().setChunk(new ChunkPos(pos)).setIgnoreEntities(false)
+                .setIgnoreStructureBlock(false).setMirror(Mirror.NONE).setRotation(Rotation.NONE);
     }
 
     public class TileEntityChestArgs{
