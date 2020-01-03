26b0fb32c06b1c03d636ef808df4b637f47ee5a6
NicosaurusRex99 <pusspot1999@gmail.com>
2020-01-03 19:29:22 +1030

    Scaled zoragon

--- src/main/java/divinerpg/objects/entities/assets/model/vethea/ModelZoragon.java
+++ src/main/java/divinerpg/objects/entities/assets/model/vethea/ModelZoragon.java
@@ -1,5 +1,7 @@
 package divinerpg.objects.entities.assets.model.vethea;
 
+import org.lwjgl.opengl.GL11;
+
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
@@ -166,6 +168,8 @@ public class ModelZoragon extends ModelBase
     public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
     {
         super.render(entity, f, f1, f2, f3, f4, f5);
+        GL11.glScaled(5, 5, 5);
+        GL11.glTranslated(0, -.5, 0);
         setRotationAngles(f, f1, f2, f3, f4, f5);
         Shape1.render(f5);
         Shape2.render(f5);
