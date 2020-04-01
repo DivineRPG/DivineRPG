c17689fa300bbd95e7643ddf0271aebdf5d1e25b
oshi4 <sheglovaoe@gmail.com>
2020-04-02 00:35:14 +0300

    Fix #100 Apotheosis replaces minecraft:mob_spawner block with Tile Entity inside. So I getting "minecraft:mob_spawner" reference from registry and creating 'patched' Tile Entity

--- src/main/java/divinerpg/objects/blocks/BlockModSpawner.java
+++ src/main/java/divinerpg/objects/blocks/BlockModSpawner.java
@@ -2,14 +2,22 @@ package divinerpg.objects.blocks;
 
 import divinerpg.api.Reference;
 import divinerpg.registry.DivineRPGTabs;
+import divinerpg.utils.Lazy;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockMobSpawner;
+import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.tileentity.TileEntityMobSpawner;
+import net.minecraft.util.LazyLoadBase;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
-public class BlockModSpawner extends BlockMobSpawner  {
+import javax.annotation.Nonnull;
+
+public class BlockModSpawner extends BlockMobSpawner {
     protected String mobName;
+    private final LazyLoadBase<ITileEntityProvider> spawnerReference;
 
     public BlockModSpawner(String name, String mobName) {
         this.mobName = mobName;
@@ -18,20 +26,51 @@ public class BlockModSpawner extends BlockMobSpawner  {
         this.setCreativeTab(DivineRPGTabs.BlocksTab);
         setHardness(5.0F);
         this.setTickRandomly(true);
+
+        // Some mods (Apotheosis especially, that guy seriously need to fix that)
+        // can replace vanilla mob spawner block.
+        // Getting this reference to create tile entity below
+        spawnerReference = new Lazy<>(() -> {
+            Block spawner = ForgeRegistries.BLOCKS.getValue(new ResourceLocation("mob_spawner"));
+            if (spawner instanceof ITileEntityProvider) {
+                return ((ITileEntityProvider) spawner);
+            }
+
+            return null;
+        });
     }
 
     @Override
     public TileEntity createNewTileEntity(World world, int par1) {
-        TileEntityMobSpawner spawner = new TileEntityMobSpawner();
-        NBTTagCompound compound = new NBTTagCompound();
-        NBTTagCompound compound2 = new NBTTagCompound();
-        spawner.writeToNBT(compound);
-        compound2.setString("id", Reference.MODID + ":" + mobName);
-        compound.setTag("SpawnData", compound2);
-        compound.setString("SpawnPotentials", mobName);
-        spawner.readFromNBT(compound);
-        spawner.readFromNBT(compound2);
-        spawner.markDirty();
+        TileEntity spawner = null;
+
+        ITileEntityProvider entityProvider = spawnerReference.getValue();
+        if (entityProvider != null) {
+            spawner = entityProvider.createNewTileEntity(world, par1);
+
+            if (spawner != null) {
+                insertModData(spawner);
+            }
+        }
+
         return spawner;
+    }
+
+    /**
+     * Inserting spawner data as reading from NBT
+     *
+     * @param spawner - spawner tile entity (take a look into TileEntityMobSpawner.class)
+     */
+    private void insertModData(@Nonnull TileEntity spawner) {
+        NBTTagCompound spawnerNBT = spawner.writeToNBT(new NBTTagCompound());
+        spawnerNBT.setString("SpawnPotentials", mobName);
+
+        NBTTagCompound compound2 = new NBTTagCompound();
+        compound2.setString("id", Reference.MODID + ":" + mobName);
+
+        spawnerNBT.setTag("SpawnData", compound2);
+
+        spawner.readFromNBT(spawnerNBT);
+        spawner.markDirty();
     }
 }
