ab921bf4f721fa46e064f0fa295d2150aba53009
NicosaurusRex99 <pusspot1999@gmail.com>
2020-05-05 13:26:23 +0930

    versioned

--- src/main/java/divinerpg/api/Reference.java
+++ src/main/java/divinerpg/api/Reference.java
@@ -8,6 +8,6 @@ public class Reference {
     public static final String API_VERSION = "1.0";
     public static final String MODID = "divinerpg";
     public static final String NAME = "DivineRPG";
-    public static final String VERSION = "1.12.2-1.6.2.1";
+    public static final String VERSION = "1.12.2-1.6.3";
     public static final String UPDATE_URL = "https://raw.githubusercontent.com/NicosaurusRex99/DivineRPG/1.12.2/divinerpg_update.json";
 }
