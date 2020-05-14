83f8655e3f10c554ccb5c177f21f79ae8f5bec34
AstralScourge <shieldfury555@gmail.com>
2020-05-14 18:27:41 -0400

    Removed dev team log message on boot, it's old and it's accomplished by the credits anyway

--- src/main/java/divinerpg/utils/log/PreInitLog.java
+++ src/main/java/divinerpg/utils/log/PreInitLog.java
@@ -11,12 +11,10 @@ public class PreInitLog {
 
     @SideOnly(Side.CLIENT)
 	public static void init() {
-		DivineRPG.logger.info("RadioactiveStud, Eternaldoom1, BossLetsPlays, Xolova, sheenrox82, The_SlayerMC, insanity414all, deathman12e3, Fire_Sight, LiteWolf101, krwminer, NicosaurusRex99 and Dash are the DivineRPG dev team!");
-		
 		if(Config.debug == true) {
 	        DivineRPG.logger.info(Reference.MODID + " is now loading");}
 	    if(Loader.isModLoaded("p455w0rdslib")) {
 	    	DivineRPG.logger.info("O.o its TheRealP455w0rd's mods");
-	        }
+	    }
 	}
 }
