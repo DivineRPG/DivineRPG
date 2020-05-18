e0a276bb698ec5f8710fcda3efd2ca8127bfef25
AstralScourge <shieldfury555@gmail.com>
2020-05-18 17:45:45 -0400

    change version number to 1.6.3

--- src/main/java/divinerpg/api/Reference.java
+++ src/main/java/divinerpg/api/Reference.java
@@ -1,13 +1,9 @@
 package divinerpg.api;
 
-/**
- * Created by LiteWolf101 on Feb
- * /21/2019
- */
 public class Reference {
     public static final String API_VERSION = "1.0";
     public static final String MODID = "divinerpg";
     public static final String NAME = "DivineRPG";
-    public static final String VERSION = "1.12.2-1.6.3";
+    public static final String VERSION = "1.6.3";
     public static final String UPDATE_URL = "https://raw.githubusercontent.com/NicosaurusRex99/DivineRPG/1.12.2/divinerpg_update.json";
 }
