d2d584b8f3ae406f1259bca43b29e2664ab7fe92
AstralScourge <shieldfury555@gmail.com>
2020-05-29 12:57:03 -0400

    Created helper methods to streamline vethea data marker handling code

--- src/main/java/divinerpg/structure/template/DivineStructureComponentTemplate.java
+++ src/main/java/divinerpg/structure/template/DivineStructureComponentTemplate.java
@@ -1,10 +1,11 @@
 package divinerpg.structure.template;
 
+import divinerpg.DivineRPG;
 import divinerpg.objects.entities.entity.vethea.EntityCryptKeeper;
 import divinerpg.objects.entities.entity.vethea.EntityTempleGuardian;
 import divinerpg.objects.entities.entity.vethea.EntityTheHunger;
 import divinerpg.registry.LootTableRegistry;
-import net.minecraft.init.Blocks;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityChest;
@@ -17,7 +18,6 @@ import net.minecraft.world.gen.structure.template.PlacementSettings;
 import net.minecraft.world.gen.structure.template.Template;
 import net.minecraft.world.gen.structure.template.TemplateManager;
 
-import javax.annotation.Nullable;
 import java.util.Random;
 
 public class DivineStructureComponentTemplate extends StructureComponentTemplate {
@@ -40,7 +40,7 @@ public class DivineStructureComponentTemplate extends StructureComponentTemplate
         super(0);
         this.location = location;
         Template template = manager.getTemplate(null, location);
-        setup(template, start, settings);
+        this.setup(template, start, settings);
     }
 
     @Override
@@ -57,91 +57,59 @@ public class DivineStructureComponentTemplate extends StructureComponentTemplate
         tagCompound.setString("TmpRes", location.toString());
     }
 
+    @Override
     protected void handleDataMarker(String function, BlockPos pos, World worldIn, Random rand, StructureBoundingBox sbb) {
-
-        //Hunger House data markers
-         if (function.equals("TheHunger")) {
-            EntityTheHunger theHunger = new EntityTheHunger(worldIn);
-            theHunger.enablePersistence();
-            theHunger.moveToBlockPosAndAngles(pos, 0.0F, 0.0F);
-            worldIn.spawnEntity(theHunger);
+        switch (function) {
+            case "TheHunger":
+                spawnPersistentEntity(worldIn, pos, new EntityTheHunger(worldIn));
+                break;
+            case "CryptLoot":
+                populateLootChestBelow(worldIn, pos, rand, LootTableRegistry.CRYPT_LOOT);
+                break;
+            case "CryptKeeper":
+                spawnPersistentEntity(worldIn, pos, new EntityCryptKeeper(worldIn));
+                break;
+            case "TempleLootBottom":
+                populateLootChestBelow(worldIn, pos, rand, LootTableRegistry.TEMPLE_LOOT_BOTTOM);
+                break;
+            case "TempleLootMiddle":
+                populateLootChestBelow(worldIn, pos, rand, LootTableRegistry.TEMPLE_LOOT_MIDDLE);
+                break;
+            case "TempleLootTop":
+                populateLootChestBelow(worldIn, pos, rand, LootTableRegistry.TEMPLE_LOOT_TOP);
+                break;
+            case "TempleGuardian":
+                spawnPersistentEntity(worldIn, pos, new EntityTempleGuardian(worldIn));
+                break;
+            case "QuadroticPostLoot":
+                populateLootChestBelow(worldIn, pos, rand, LootTableRegistry.QUADROTIC_POST_LOOT);
+                break;
+            case "KarosMadhouseLoot":
+                populateLootChestBelow(worldIn, pos, rand, LootTableRegistry.KAROS_MADHOUSE_LOOT);
+                break;
+            case "RaglokChamberLoot":
+                populateLootChestBelow(worldIn, pos, rand, LootTableRegistry.RAGLOK_CHAMBER_LOOT);
+                break;
+            case "WreckHallLoot":
+                populateLootChestBelow(worldIn, pos, rand, LootTableRegistry.WRECK_HALL_LOOT);
+                break;
+            default:
+                DivineRPG.logger.warn("Unknown data marker: " + function + ", please report this.");
+                break;
         }
+    }
 
-        //Crypt data markers
-        if (function.equals("CryptLoot")) {
-            BlockPos blockpos = pos.down();
-            TileEntity tileentity = worldIn.getTileEntity(blockpos);
-            if (tileentity instanceof TileEntityChest) {
-                ((TileEntityChest) tileentity).setLootTable(LootTableRegistry.CRYPT_LOOT, rand.nextLong());
-            }
-        }
-        else if (function.equals("CryptKeeper")) {
-            EntityCryptKeeper cryptKeeper = new EntityCryptKeeper(worldIn);
-            cryptKeeper.enablePersistence();
-            cryptKeeper.moveToBlockPosAndAngles(pos, 0.0F, 0.0F);
-            worldIn.spawnEntity(cryptKeeper);
+    private void spawnPersistentEntity(World world, BlockPos pos, EntityLiving entity) {
+        entity.enablePersistence();
+        entity.moveToBlockPosAndAngles(pos, 0.0F, 0.0F);
+        world.spawnEntity(entity);
+    }
 
-            //Temporary workaround to stop crypt keeper from suffocating
-            worldIn.setBlockToAir(pos.add(0, 1, 0));
-            worldIn.setBlockToAir(pos);
-        }
-
-        //Temple data markers
-        if (function.startsWith("TempleLoot")) {
-            BlockPos blockpos = pos.down();
-            if (sbb.isVecInside(blockpos)) {
-                TileEntity tileentity = worldIn.getTileEntity(blockpos);
-                if (tileentity instanceof TileEntityChest) {
-                    if (function.equals("TempleLootBottom")) {
-                        ((TileEntityChest) tileentity).setLootTable(LootTableRegistry.TEMPLE_LOOT_BOTTOM, rand.nextLong());
-                    } else if (function.equals("TempleLootMiddle")) {
-                        ((TileEntityChest) tileentity).setLootTable(LootTableRegistry.TEMPLE_LOOT_MIDDLE, rand.nextLong());
-                    } else if (function.equals("TempleLootTop")) {
-                        ((TileEntityChest) tileentity).setLootTable(LootTableRegistry.TEMPLE_LOOT_TOP, rand.nextLong());
-                    }
-                }
-            }
-        } else if (function.equals("TempleGuardian")) {
-            EntityTempleGuardian templeGuardian = new EntityTempleGuardian(worldIn);
-            templeGuardian.enablePersistence();
-            templeGuardian.moveToBlockPosAndAngles(pos, 0.0F, 0.0F);
-            worldIn.spawnEntity(templeGuardian);
-        }
-
-        //Quadrotic Post data markers
-        if (function.equals("QuadroticPostLoot")) {
-            BlockPos blockpos = pos.down();
-            TileEntity tileentity = worldIn.getTileEntity(blockpos);
-            if (tileentity instanceof TileEntityChest) {
-                ((TileEntityChest) tileentity).setLootTable(LootTableRegistry.QUADROTIC_POST_LOOT, rand.nextLong());
-            }
-        }
-
-        //Karos Madhouse data markers
-        if (function.equals("KarosMadhouseLoot")) {
-            BlockPos blockpos = pos.down();
-            TileEntity tileentity = worldIn.getTileEntity(blockpos);
-            if (tileentity instanceof TileEntityChest) {
-                ((TileEntityChest) tileentity).setLootTable(LootTableRegistry.KAROS_MADHOUSE_LOOT, rand.nextLong());
-            }
-        }
-
-        //Raglok Chamber data markers
-        if (function.equals("RaglokChamberLoot")) {
-            BlockPos blockpos = pos.down();
-            TileEntity tileentity = worldIn.getTileEntity(blockpos);
-            if (tileentity instanceof TileEntityChest) {
-                ((TileEntityChest) tileentity).setLootTable(LootTableRegistry.RAGLOK_CHAMBER_LOOT, rand.nextLong());
-            }
-        }
-
-        //Wreck Hall data markers
-        if (function.equals("WreckHallLoot")) {
-            BlockPos blockpos = pos.down();
-            TileEntity tileentity = worldIn.getTileEntity(blockpos);
-            if (tileentity instanceof TileEntityChest) {
-                ((TileEntityChest) tileentity).setLootTable(LootTableRegistry.WRECK_HALL_LOOT, rand.nextLong());
-            }
+    private void populateLootChestBelow(World world, BlockPos pos, Random rand, ResourceLocation lootTable) {
+        BlockPos chestPosition = pos.down();
+        TileEntity tileEntity = world.getTileEntity(chestPosition);
+        if(tileEntity instanceof TileEntityChest) {
+            ((TileEntityChest)tileEntity).setLootTable(lootTable, rand.nextLong());
         }
     }
 }
