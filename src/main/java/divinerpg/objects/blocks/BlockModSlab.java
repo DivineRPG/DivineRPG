0c11ae77f5c11d09e90cbbf1705d6745bb7d78e8
AstralScourge <shieldfury555@gmail.com>
2020-07-06 11:24:19 -0400

    cleanup

--- src/main/java/divinerpg/objects/blocks/BlockModSlab.java
+++ src/main/java/divinerpg/objects/blocks/BlockModSlab.java
@@ -1,26 +1,17 @@
 package divinerpg.objects.blocks;
 
 import divinerpg.DivineRPG;
-import divinerpg.objects.items.base.ItemModSlab;
 import divinerpg.registry.DivineRPGTabs;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockPlanks;
 import net.minecraft.block.BlockSlab;
-import net.minecraft.block.material.MapColor;
-import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IStringSerializable;
-import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.RayTraceResult;
-import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 import java.util.Locale;
@@ -54,10 +45,6 @@ public class BlockModSlab extends BlockSlab {
 
         this.setDefaultState(iblockstate.withProperty(VARIANT, ModSlabVariant.BASE));
         this.setCreativeTab(DivineRPGTabs.BLOCKS);
-    }
-
-    public BlockModSlab getSingle() {
-        return this.singleSlab;
     }
 
     @Override
