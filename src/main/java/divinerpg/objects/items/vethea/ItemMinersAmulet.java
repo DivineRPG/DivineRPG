868b75191f98751739e5de4b9baa8c9678f930ab
NicosaurusRex99 <pusspot1999@gmail.com>
2020-07-14 13:09:29 +0930

    we also want the amulet to work without baubles mod

--- src/main/java/divinerpg/objects/items/vethea/ItemMinersAmulet.java
+++ src/main/java/divinerpg/objects/items/vethea/ItemMinersAmulet.java
@@ -4,10 +4,12 @@ import baubles.api.BaubleType;
 import baubles.api.IBauble;
 import divinerpg.objects.items.base.ItemMod;
 import divinerpg.registry.DivineRPGTabs;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.init.MobEffects;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.common.Optional;
 
 @Optional.Interface(iface="baubles.api.IBauble",modid="baubles")
@@ -22,7 +24,13 @@ public class ItemMinersAmulet extends ItemMod implements IBauble {
     }
 
     @Override
-    public void onWornTick(ItemStack itemstack, EntityLivingBase player) {
-        player.addPotionEffect(new PotionEffect(MobEffects.HASTE,40,1,true,true));
+    public void onWornTick(ItemStack stack, EntityLivingBase player) {
+        player.addPotionEffect(new PotionEffect(MobEffects.HASTE, 40, 1, false, false));
+    }
+    
+    @Override
+    public void onUpdate(ItemStack stack, World world, Entity player, int itemSlot, boolean isSelected)
+    {
+        ((EntityLivingBase) player).addPotionEffect(new PotionEffect(MobEffects.HASTE, 40, 1, false, false));
     }
 }
