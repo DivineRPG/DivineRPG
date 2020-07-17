4beb2b05197ebe173bf529ac6fd58a2ff91424cd
NicosaurusRex99 <pusspot1999@gmail.com>
2020-07-17 14:15:40 +0930

    Added sit to tamed mobs

--- src/main/java/divinerpg/objects/entities/entity/EntityDivineTameable.java
+++ src/main/java/divinerpg/objects/entities/entity/EntityDivineTameable.java
@@ -1,28 +1,26 @@
 package divinerpg.objects.entities.entity;
 
 import divinerpg.config.Config;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLiving;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.*;
 import net.minecraft.entity.ai.*;
 import net.minecraft.entity.ai.attributes.IAttributeInstance;
 import net.minecraft.entity.passive.EntityTameable;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.DamageSource;
-import net.minecraft.util.EnumParticleTypes;
-import net.minecraft.world.EnumDifficulty;
-import net.minecraft.world.World;
+import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.init.Items;
+import net.minecraft.item.*;
+import net.minecraft.util.*;
+import net.minecraft.world.*;
 
 public abstract class EntityDivineTameable extends EntityTameable {
-    public EntityDivineTameable(World world) {
+	public EntityDivineTameable(World world) {
         super(world);
         setTamed(false);
     }
 
     @Override
     protected void initEntityAI() {
-        this.aiSit = new EntityAISit(this);
+    	this.aiSit = new EntityAISit(this);
         this.tasks.addTask(1, new EntityAISwimming(this));
         this.tasks.addTask(2, this.aiSit);
         this.tasks.addTask(3, new EntityAILeapAtTarget(this, 0.4F));
@@ -61,6 +59,31 @@ public abstract class EntityDivineTameable extends EntityTameable {
     }
 
     @Override
+    public boolean attackEntityFrom(DamageSource source, float amount)
+    {
+        if (this.isEntityInvulnerable(source))
+        {
+            return false;
+        }
+        else
+        {
+            Entity entity = source.getTrueSource();
+
+            if (this.aiSit != null)
+            {
+                this.aiSit.setSitting(false);
+            }
+
+            if (entity != null && !(entity instanceof EntityPlayer) && !(entity instanceof EntityArrow))
+            {
+                amount = (amount + 1.0F) / 2.0F;
+            }
+
+            return super.attackEntityFrom(source, amount);
+        }
+    }
+    
+    @Override
     public boolean attackEntityAsMob(Entity e) {
         boolean attack = super.attackEntityAsMob(e);
         if (attack && e instanceof EntityLiving) {
@@ -95,5 +118,44 @@ public abstract class EntityDivineTameable extends EntityTameable {
             double d2 = this.rand.nextGaussian() * 0.02D;
             this.world.spawnParticle(enumparticletypes, this.posX + (double) (this.rand.nextFloat() * this.width * 2.0F) - (double) this.width, this.posY + 0.5D + (double) (this.rand.nextFloat() * this.height), this.posZ + (double) (this.rand.nextFloat() * this.width * 2.0F) - (double) this.width, d0, d1, d2);
         }
+    }
+    
+    @Override
+    public boolean processInteract(EntityPlayer player, EnumHand hand)
+    {
+        ItemStack itemstack = player.getHeldItem(hand);
+
+        if (this.isTamed())
+        {
+            if (this.isOwner(player) && !this.world.isRemote && !this.isBreedingItem(itemstack))
+            {
+                this.aiSit.setSitting(!this.isSitting());
+                this.isJumping = false;
+                this.navigator.clearPath();
+                this.setAttackTarget((EntityLivingBase)null);
+            }
+        }
+        else if (!this.isAngry() && !this.world.isRemote)
+            {
+                if (this.rand.nextInt(3) == 0 && !net.minecraftforge.event.ForgeEventFactory.onAnimalTame(this, player))
+                {
+                    this.setTamedBy(player);
+                    this.navigator.clearPath();
+                    this.setAttackTarget((EntityLivingBase)null);
+                    this.aiSit.setSitting(true);
+                    this.setHealth(20.0F);
+                    this.playTameEffect(true);
+                    this.world.setEntityState(this, (byte)7);
+                }
+                else
+                {
+                    this.playTameEffect(false);
+                    this.world.setEntityState(this, (byte)6);
+                }
+                return true;
+            }
+
+
+        return super.processInteract(player, hand);
     }
 }
