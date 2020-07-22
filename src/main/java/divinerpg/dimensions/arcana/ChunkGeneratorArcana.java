ef31e051017485e4fb83fd98abb8322df6b38d38
AstralScourge <shieldfury555@gmail.com>
2020-07-21 21:08:06 -0400

    Trim some fat from the arcana chunk generator

--- src/main/java/divinerpg/dimensions/arcana/ChunkGeneratorArcana.java
+++ src/main/java/divinerpg/dimensions/arcana/ChunkGeneratorArcana.java
@@ -1,44 +1,14 @@
 package divinerpg.dimensions.arcana;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 
-import divinerpg.dimensions.arcana.components.DungeonCeiling;
-import divinerpg.dimensions.arcana.components.DungeonComponent00;
-import divinerpg.dimensions.arcana.components.DungeonComponent01;
-import divinerpg.dimensions.arcana.components.DungeonComponent02;
-import divinerpg.dimensions.arcana.components.DungeonComponent03;
-import divinerpg.dimensions.arcana.components.DungeonComponent04;
-import divinerpg.dimensions.arcana.components.DungeonComponent05;
-import divinerpg.dimensions.arcana.components.DungeonComponent06;
-import divinerpg.dimensions.arcana.components.DungeonComponent07;
-import divinerpg.dimensions.arcana.components.DungeonComponent08;
-import divinerpg.dimensions.arcana.components.DungeonComponent09;
-import divinerpg.dimensions.arcana.components.DungeonComponent10;
-import divinerpg.dimensions.arcana.components.DungeonComponent11;
-import divinerpg.dimensions.arcana.components.DungeonComponent12;
-import divinerpg.dimensions.arcana.components.DungeonComponent13;
-import divinerpg.dimensions.arcana.components.DungeonComponent14;
-import divinerpg.dimensions.arcana.components.DungeonComponent15;
-import divinerpg.dimensions.arcana.components.DungeonComponent16;
-import divinerpg.dimensions.arcana.components.DungeonComponent17;
-import divinerpg.dimensions.arcana.components.DungeonComponent18;
-import divinerpg.dimensions.arcana.components.DungeonComponent19;
-import divinerpg.dimensions.arcana.components.DungeonComponent20;
-import divinerpg.dimensions.arcana.components.DungeonComponentBase;
-import divinerpg.dimensions.arcana.components.DungeonComponentDramix;
-import divinerpg.dimensions.arcana.components.DungeonComponentParasecta;
 import divinerpg.dimensions.arcana.mazegen.ArcanaMazeGenerator;
 import divinerpg.dimensions.arcana.mazegen.Cell;
 import divinerpg.dimensions.arcana.mazegen.MazeMapMemoryStorage;
 import divinerpg.registry.StructureRegistry;
 import divinerpg.structure.arcana.ArcanaStructureHandler;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EnumCreatureType;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
@@ -51,57 +21,20 @@ import net.minecraft.world.chunk.ChunkPrimer;
 import net.minecraft.world.gen.IChunkGenerator;
 
 public class ChunkGeneratorArcana implements IChunkGenerator {
-    private ArrayList<DungeonComponentBase> Rooms = new ArrayList<DungeonComponentBase>();
-    private DungeonComponentBase doubleHeightComponent = new DungeonComponent08();
-    private DungeonCeiling Ceiling;
     private final Random rand;
     private final World world;
-    private double[] buffer;
     private Biome[] biomesForGeneration;
-    private int chunkX = 0;
-    private int chunkZ = 0;
-    private Map chunkTileEntityMap;
 
     public ChunkGeneratorArcana(World world, long seed) {
         this.world = world;
         this.rand = new Random(seed);
-
-        Rooms.add(new DungeonComponent00());
-        Rooms.add(new DungeonComponent01());
-        Rooms.add(new DungeonComponent02());
-        Rooms.add(new DungeonComponent03());
-        Rooms.add(new DungeonComponent04());
-        Rooms.add(new DungeonComponent05());
-        Rooms.add(new DungeonComponent06());
-        Rooms.add(new DungeonComponent07());
-        //Component 8 is the 2-high one, skipping that
-        Rooms.add(new DungeonComponent09());
-        Rooms.add(new DungeonComponent10());
-        Rooms.add(new DungeonComponent11());
-        Rooms.add(new DungeonComponent12());
-        Rooms.add(new DungeonComponent13());
-        Rooms.add(new DungeonComponent14());
-        Rooms.add(new DungeonComponent15());
-        Rooms.add(new DungeonComponent16());
-        Rooms.add(new DungeonComponent17());
-        Rooms.add(new DungeonComponent18());
-        Rooms.add(new DungeonComponent19());
-        Rooms.add(new DungeonComponent20());
-        Rooms.add(new DungeonComponentDramix());
-        Rooms.add(new DungeonComponentParasecta());
-
-        Ceiling = new DungeonCeiling();
-        this.chunkTileEntityMap = new HashMap();
     }
 
+    @Override
     public Chunk generateChunk(int x, int z) {
-        this.chunkX = x;
-        this.chunkZ = z;
-        //this.rand.setSeed((long) x * 341873128712L + (long) z * 132897987541L);
+        this.rand.setSeed((long) x * 341873128712L + (long) z * 132897987541L);
         this.biomesForGeneration = this.world.getBiomeProvider().getBiomes(this.biomesForGeneration, x * 16, z * 16, 16,
                 16);
-
-
 
         Chunk chunk = new Chunk(this.world, new ChunkPrimer(), x, z);
         byte[] abyte = chunk.getBiomeArray();
@@ -145,26 +78,24 @@ public class ChunkGeneratorArcana implements IChunkGenerator {
     public void populate(int chunkX, int chunkZ) {
         int x = chunkX * 16;
         int z = chunkZ * 16;
-
         BlockPos pos = new BlockPos(x, 0, z);
-        ChunkPos chunkpos = new ChunkPos(chunkX, chunkZ);
-
         Biome biome = this.world.getBiome(pos.add(16, 0, 16));
 
         this.rand.setSeed(this.world.getSeed());
         long k = this.rand.nextLong() / 2L * 2L + 1L;
         long l = this.rand.nextLong() / 2L * 2L + 1L;
         this.rand.setSeed((long) chunkX * k + (long) chunkZ * l ^ this.world.getSeed());
+
         //this.rand is not used for overall maze generation which is specific to the region root instead of the specific chunk
-        //however, it will be used to determine per-chunk things like the specific room variant picked, as well as randomize specific things within a chunk
+        //However, it will be used to determine per-chunk things like the specific room variant picked, as well as randomize specific things within a chunk
 
         //MAZE GENERATION
         Cell[][] mazeMap;
 
         int regionRootX, regionRootZ;
         int mapCoordinateX, mapCoordinateZ;
-        regionRootX = roundUp(chunkX, ArcanaMazeGenerator.MAZE_SIZE);
-        regionRootZ = roundUp(chunkZ, ArcanaMazeGenerator.MAZE_SIZE);
+        regionRootX = roundUpToMultiple(chunkX, ArcanaMazeGenerator.MAZE_SIZE);
+        regionRootZ = roundUpToMultiple(chunkZ, ArcanaMazeGenerator.MAZE_SIZE);
         
         ChunkPos regionRoot = new ChunkPos(regionRootX, regionRootZ);
         Cell[][] storedGrid = MazeMapMemoryStorage.getMapForChunkPos(regionRoot);
@@ -220,34 +151,25 @@ public class ChunkGeneratorArcana implements IChunkGenerator {
 
         //biome.decorate(this.world, this.rand, pos);
 
-        List<BlockPos> chunkTileEntityPositions = (List<BlockPos>) chunkTileEntityMap.get(chunkpos);
-        if (chunkTileEntityPositions != null) {
-            Chunk chunk = this.world.getChunkFromChunkCoords(chunkX, chunkZ);
-            for (int i = 0; i < chunkTileEntityPositions.size(); i++) {
-                BlockPos chunkPosition = chunkTileEntityPositions.get(i);
-                IBlockState state = chunk.getBlockState(chunkPosition.getX(), chunkPosition.getY(),
-                        chunkPosition.getZ());
-                TileEntity te = state.getBlock().createTileEntity(this.world, state);
-                this.world.setTileEntity(chunkPosition.add(x, 0, z), te);
-            }
-            chunkTileEntityMap.remove(chunkpos);
-        }
-
         WorldEntitySpawner.performWorldGenSpawning(this.world, biome, x + 8, z + 8, 16, 16, this.rand);
     }
 
-    private static int roundUp(int numToRound, int multiple)
+    private static int roundUpToMultiple(int numToRound, int multiple)
     {
-        if (multiple == 0)
+        if (multiple == 0) {
             return numToRound;
+        }
 
         int remainder = Math.abs(numToRound) % multiple;
-        if (remainder == 0)
+        if (remainder == 0) {
             return numToRound;
+        }
 
-        if (numToRound < 0)
+        if (numToRound < 0) {
             return -1 * (Math.abs(numToRound) - remainder);
-        else
+        }
+        else {
             return numToRound + multiple - remainder;
+        }
     }
 }
