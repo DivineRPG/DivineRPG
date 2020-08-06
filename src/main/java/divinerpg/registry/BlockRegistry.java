1bb399c18ef7686c057a7c2c8e9e190a09f4ce55
AstralScourge <pledgedtoaoa@gmail.com>
2020-08-06 11:22:33 -0400

    Add blast resistance to arcanium metal

--- src/main/java/divinerpg/registry/BlockRegistry.java
+++ src/main/java/divinerpg/registry/BlockRegistry.java
@@ -1504,7 +1504,7 @@ public class BlockRegistry {
         register(registry, new BlockModUnbreakable("ancient_stone"));
         register(registry, new BlockModUnbreakable("ancient_tile"));
         register(registry, new BlockModLadder("arcanite_tubes"));
-        register(registry, new BlockModPillar("arcanium_metal", Material.IRON, -1F));
+        register(registry, new BlockModPillar("arcanium_metal", Material.IRON, -1F).setResistance(6000000F));
         register(registry, new BlockModUnbreakable("arcanium_power"));
         register(registry, new BlockModUnbreakable("dark_degraded_brick"));
 
