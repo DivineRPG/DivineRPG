9e279a89242e1b9835a57e5cafcfd23f2dc48648
AstralScourge <pledgedtoaoa@gmail.com>
2020-08-10 00:23:20 -0400

    Change lord vatticus trades

--- src/main/java/divinerpg/objects/entities/entity/npc/EntityLordVatticus.java
+++ src/main/java/divinerpg/objects/entities/entity/npc/EntityLordVatticus.java
@@ -4,6 +4,9 @@ import divinerpg.objects.entities.entity.EntityDivineVillager;
 import divinerpg.proxy.GUIHandler;
 import divinerpg.registry.ItemRegistry;
 import divinerpg.registry.ArmorRegistry;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.village.MerchantRecipe;
 import net.minecraft.village.MerchantRecipeList;
@@ -30,7 +33,13 @@ public class EntityLordVatticus extends EntityDivineVillager {
 
     public MerchantRecipeList getRecipeList() {
         MerchantRecipeList list = new MerchantRecipeList();
-        list.add(new MerchantRecipe(new ItemStack(ItemRegistry.collectorFragments, 16), new ItemStack(ItemRegistry.collector)));
+        list.add(new MerchantRecipe(new ItemStack(ItemRegistry.collector, 1), new ItemStack(Items.BOOK, 3)));
+        list.add(new MerchantRecipe(new ItemStack(ItemRegistry.collector, 2), new ItemStack(Items.DYE, 16, EnumDyeColor.BLUE.getDyeDamage())));
+        list.add(new MerchantRecipe(new ItemStack(ItemRegistry.collector, 2), new ItemStack(Items.EXPERIENCE_BOTTLE, 12)));
+        list.add(new MerchantRecipe(new ItemStack(ItemRegistry.collector, 2), new ItemStack(ItemRegistry.weakArcanaPotion, 2)));
+        list.add(new MerchantRecipe(new ItemStack(ItemRegistry.collector, 4), new ItemStack(ItemRegistry.strongArcanaPotion, 2)));
+        list.add(new MerchantRecipe(new ItemStack(ItemRegistry.arcanium, 2), new ItemStack(ItemRegistry.wizardsBook)));
+        list.add(new MerchantRecipe(new ItemStack(ItemRegistry.arcanium, 3), new ItemStack(Blocks.ENCHANTING_TABLE)));
         list.add(new MerchantRecipe(new ItemStack(ItemRegistry.arcanium, 8), new ItemStack(ArmorRegistry.kormaHelmet)));
         list.add(new MerchantRecipe(new ItemStack(ItemRegistry.arcanium, 8), new ItemStack(ArmorRegistry.kormaChestplate)));
         list.add(new MerchantRecipe(new ItemStack(ItemRegistry.arcanium, 8), new ItemStack(ArmorRegistry.kormaLeggings)));
@@ -39,11 +48,6 @@ public class EntityLordVatticus extends EntityDivineVillager {
         list.add(new MerchantRecipe(new ItemStack(ItemRegistry.arcanium, 10), new ItemStack(ArmorRegistry.vemosChestplate)));
         list.add(new MerchantRecipe(new ItemStack(ItemRegistry.arcanium, 10), new ItemStack(ArmorRegistry.vemosLeggings)));
         list.add(new MerchantRecipe(new ItemStack(ItemRegistry.arcanium, 10), new ItemStack(ArmorRegistry.vemosBoots)));
-        list.add(new MerchantRecipe(new ItemStack(ItemRegistry.arcanium, 2), new ItemStack(ItemRegistry.staffOfEnrichment)));
-        list.add(new MerchantRecipe(new ItemStack(ItemRegistry.arcanium, 2), new ItemStack(ItemRegistry.wizardsBook)));
-        list.add(new MerchantRecipe(new ItemStack(ItemRegistry.arcanium, 1), new ItemStack(ItemRegistry.weakArcanaPotion, 4)));
-        list.add(new MerchantRecipe(new ItemStack(ItemRegistry.arcanium, 2), new ItemStack(ItemRegistry.strongArcanaPotion, 4)));
-        list.add(new MerchantRecipe(new ItemStack(ItemRegistry.arcanium, 20), new ItemStack(ItemRegistry.orbOfLight)));
         return list;
     }
 }
