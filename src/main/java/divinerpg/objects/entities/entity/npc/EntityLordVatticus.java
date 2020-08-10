808e960da116f99b8613c06d1f19e860f47dcc1f
AstralScourge <pledgedtoaoa@gmail.com>
2020-08-10 11:14:08 -0400

    Tweak vatticus trades a bit

--- src/main/java/divinerpg/objects/entities/entity/npc/EntityLordVatticus.java
+++ src/main/java/divinerpg/objects/entities/entity/npc/EntityLordVatticus.java
@@ -38,16 +38,10 @@ public class EntityLordVatticus extends EntityDivineVillager {
         list.add(new MerchantRecipe(new ItemStack(ItemRegistry.collector, 2), new ItemStack(Items.EXPERIENCE_BOTTLE, 12)));
         list.add(new MerchantRecipe(new ItemStack(ItemRegistry.collector, 2), new ItemStack(ItemRegistry.weakArcanaPotion, 2)));
         list.add(new MerchantRecipe(new ItemStack(ItemRegistry.collector, 4), new ItemStack(ItemRegistry.strongArcanaPotion, 2)));
+        list.add(new MerchantRecipe(new ItemStack(ItemRegistry.arcanium, 1), new ItemStack(Items.BLAZE_ROD)));
+        list.add(new MerchantRecipe(new ItemStack(ItemRegistry.arcanium, 2), new ItemStack(Items.NETHER_WART, 8)));
         list.add(new MerchantRecipe(new ItemStack(ItemRegistry.arcanium, 2), new ItemStack(ItemRegistry.wizardsBook)));
         list.add(new MerchantRecipe(new ItemStack(ItemRegistry.arcanium, 3), new ItemStack(Blocks.ENCHANTING_TABLE)));
-        list.add(new MerchantRecipe(new ItemStack(ItemRegistry.arcanium, 8), new ItemStack(ArmorRegistry.kormaHelmet)));
-        list.add(new MerchantRecipe(new ItemStack(ItemRegistry.arcanium, 8), new ItemStack(ArmorRegistry.kormaChestplate)));
-        list.add(new MerchantRecipe(new ItemStack(ItemRegistry.arcanium, 8), new ItemStack(ArmorRegistry.kormaLeggings)));
-        list.add(new MerchantRecipe(new ItemStack(ItemRegistry.arcanium, 8), new ItemStack(ArmorRegistry.kormaBoots)));
-        list.add(new MerchantRecipe(new ItemStack(ItemRegistry.arcanium, 10), new ItemStack(ArmorRegistry.vemosHelmet)));
-        list.add(new MerchantRecipe(new ItemStack(ItemRegistry.arcanium, 10), new ItemStack(ArmorRegistry.vemosChestplate)));
-        list.add(new MerchantRecipe(new ItemStack(ItemRegistry.arcanium, 10), new ItemStack(ArmorRegistry.vemosLeggings)));
-        list.add(new MerchantRecipe(new ItemStack(ItemRegistry.arcanium, 10), new ItemStack(ArmorRegistry.vemosBoots)));
         return list;
     }
 }
