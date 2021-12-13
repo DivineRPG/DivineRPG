15ec1a423d9627a8b720bc5fb4120da9aa673fa3
Naturix <nicosaurusrex99@gmail.com>
2021-12-13 14:25:46 +1030

    Update VetheaInventorySwapEvent.java

--- src/main/java/divinerpg/events/VetheaInventorySwapEvent.java
+++ src/main/java/divinerpg/events/VetheaInventorySwapEvent.java
@@ -15,29 +15,26 @@ public class VetheaInventorySwapEvent {
         if (event.isCanceled() || !(event.getEntity() instanceof PlayerEntity))
             return;
 
-        RegistryKey<World> destinationDimension = event.getTo();
 
         RegistryKey<World> vetheaID = KeyRegistry.VETHEA_WORLD;
         PlayerEntity playerIn = event.getPlayer();
         CompoundNBT persistentData = playerIn.getPersistentData().getCompound(PlayerEntity.PERSISTED_NBT_TAG);
 
-                if(destinationDimension != vetheaID) {
+                if(event.getFrom() == vetheaID) {
                     persistentData.put("VetheaInv", playerIn.inventory.save(new ListNBT()));
                     playerIn.getPersistentData().put("PlayerPersisted", persistentData);
                     playerIn.inventory.clearContent();
                     ListNBT inv = persistentData.getList("OverworldInv", 10);
                     playerIn.inventory.load(inv);
                     playerIn.inventoryMenu.broadcastChanges();
-
-                } else {
+                } if(event.getTo() == vetheaID) {
                     persistentData.put("OverworldInv", playerIn.inventory.save(new ListNBT()));
                     playerIn.getPersistentData().put("PlayerPersisted", persistentData);
                     playerIn.inventory.clearContent();
                     ListNBT inv = persistentData.getList("VetheaInv", 10);
                     playerIn.inventory.load(inv);
                     playerIn.inventoryMenu.broadcastChanges();
-
-                }
+        }
 
     }
 
