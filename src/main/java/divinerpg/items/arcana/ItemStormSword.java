4076d31d61afa1ff6dc4fe9de8225235b71f6ce5
Naturix <nicosaurusrex99@gmail.com>
2021-12-27 02:42:35 +1030

    Fixed #457

--- src/main/java/divinerpg/items/arcana/ItemStormSword.java
+++ src/main/java/divinerpg/items/arcana/ItemStormSword.java
@@ -8,6 +8,8 @@ import net.minecraft.entity.effect.*;
 import net.minecraft.entity.player.*;
 import net.minecraft.item.*;
 import net.minecraft.util.*;
+import net.minecraft.util.math.*;
+import net.minecraft.util.math.vector.*;
 import net.minecraft.world.*;
 
 public class ItemStormSword extends ItemModSword {
@@ -19,24 +21,26 @@ public class ItemStormSword extends ItemModSword {
     @Override
     public ActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
         Arcana arcana = player.getCapability(ArcanaCapability.CAPABILITY_ARCANA).orElse(null);
-        if (arcana.getArcana() >= 20) {
-            arcana.consume(player, 20);
-            for (int i = 2; i < 5; i += 2) {
-                double angle = 0;
-                while (angle < 2 * Math.PI) {
-                    int xOffset = (int) Math.round(Math.sin(angle) * i);
-                    int zOffset = (int) Math.round(Math.cos(angle) * i);
-                    if (Math.sqrt(xOffset * xOffset + zOffset * zOffset) > 3) {
-                        LightningBoltEntity bolt = new LightningBoltEntity(EntityType.LIGHTNING_BOLT, world);
-                        bolt.moveTo(player.xo + xOffset, player.yo, player.zo + zOffset);
-                        world.addFreshEntity(bolt);
-                        angle += Math.PI / 8.0D;
+        if (!world.isClientSide && arcana.getArcana() >= 75) {
+            Vector3d start  = player.getEyePosition(1);
+            Vector3d vec31 = player.getViewVector(1);
+            Vector3d end = start.add(vec31.x * 32, vec31.y * 32, vec31.z * 32);
+            RayTraceContext pos = new RayTraceContext(start, end, RayTraceContext.BlockMode.OUTLINE, RayTraceContext.FluidMode.ANY, player);
+
+            if (arcana.getArcana() >= 20) {
+                arcana.consume(player, 20);
+                for (int i = 2; i < 5; i += 2) {
+                    double angle = 0;
+                    while (angle < 2 * Math.PI) {
+                            LightningBoltEntity bolt = new LightningBoltEntity(EntityType.LIGHTNING_BOLT, world);
+                            bolt.moveTo(pos.getTo());
+                            world.addFreshEntity(bolt);
+                            angle += Math.PI / 8.0D;
                     }
                 }
             }
+            return super.use(world, player, hand);
         }
-
         return super.use(world, player, hand);
     }
-
 }
