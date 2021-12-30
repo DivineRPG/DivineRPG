4f3cf1692012e9b24e8757d1b7fb8a0f803b15e4
Naturix <nicosaurusrex99@gmail.com>
2021-12-30 14:14:32 +1030

    Fixed starlight stuff

--- src/main/java/divinerpg/items/arcana/ItemStaffStarlight.java
+++ src/main/java/divinerpg/items/arcana/ItemStaffStarlight.java
@@ -1,26 +1,22 @@
 package divinerpg.items.arcana;
 
-import divinerpg.entities.projectile.EntityStar;
-import divinerpg.enums.BulletType;
-import divinerpg.items.base.ItemModRanged;
-import divinerpg.registries.EntityRegistry;
-import divinerpg.registries.SoundRegistry;
-import divinerpg.util.LocalizeUtils;
-import divinerpg.util.RarityList;
-import net.minecraft.block.Block;
-import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.RayTraceContext;
-import net.minecraft.util.math.vector.Vector3d;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.world.World;
+import divinerpg.entities.projectile.*;
+import divinerpg.enums.*;
+import divinerpg.items.base.*;
+import divinerpg.registries.*;
+import divinerpg.util.*;
+import net.minecraft.client.util.*;
+import net.minecraft.entity.*;
+import net.minecraft.entity.player.*;
+import net.minecraft.item.*;
+import net.minecraft.util.*;
+import net.minecraft.util.math.*;
+import net.minecraft.util.math.vector.*;
+import net.minecraft.util.text.*;
+import net.minecraft.world.*;
 
-import javax.annotation.Nullable;
-import java.util.List;
+import javax.annotation.*;
+import java.util.*;
 
 public class ItemStaffStarlight extends ItemModRanged {
 
@@ -44,34 +40,19 @@ public class ItemStaffStarlight extends ItemModRanged {
     }
 
     @Override
-    protected boolean canUseRangedWeapon(PlayerEntity player, ItemStack stack) {
-        if (!player.level.isClientSide) {
-            Vector3d start  = player.getEyePosition(1);
-            Vector3d vec31 = player.getViewVector(1);
-            Vector3d end = start.add(vec31.x * 32, vec31.y * 32, vec31.z * 32);
-            RayTraceContext pos = new RayTraceContext(start, end, RayTraceContext.BlockMode.OUTLINE, RayTraceContext.FluidMode.ANY, player);
-            if(player.level.getBlockState(new BlockPos(pos.getTo().x, pos.getTo().y, pos.getTo().z)).getBlock() instanceof Block){
-                return false;
-            }
-        }
-        return super.canUseRangedWeapon(player, stack);
-    }
-
-    @Override
     protected void spawnEntity(World world, PlayerEntity player, ItemStack stack, BulletType bulletType, EntityType entityType) {
-        Vector3d start  = player.getEyePosition(1);
-        Vector3d vec31 = player.getViewVector(1);
-        Vector3d end = start.add(vec31.x * 32, vec31.y * 32, vec31.z * 32);
-        RayTraceContext pos = new RayTraceContext(start, end, RayTraceContext.BlockMode.OUTLINE, RayTraceContext.FluidMode.ANY, player);
-
-        if(player.level.getBlockState(new BlockPos(pos.getTo().x, pos.getTo().y, pos.getTo().z)).getBlock() instanceof Block){
+        int blockReachDistance = 32;
+        Vector3d vec3d = player.getEyePosition(1);
+        Vector3d vec3d1 = player.getViewVector(1);
+        Vector3d vec3d2 = vec3d.add(vec3d1.x * blockReachDistance, vec3d1.y * blockReachDistance, vec3d1.z * blockReachDistance);
+        BlockRayTraceResult pos = player.level.clip(new RayTraceContext(vec3d, vec3d2, RayTraceContext.BlockMode.COLLIDER, RayTraceContext.FluidMode.NONE, player));
 
             if (!world.isClientSide) {
                 for (int i = 0; i < count; i++) {
-                    world.addFreshEntity(new EntityStar(EntityRegistry.STARLIGHT, (double) pos.getTo().x + 0.5D, (double) pos.getTo().y + 25D,
-                            (double) pos.getTo().z + 0.5D, world));
+                    EntityStar star = new EntityStar(EntityRegistry.STARLIGHT, (double) pos.getBlockPos().getX() + 0.5D, (double) pos.getBlockPos().getY() + 25D, (double) pos.getBlockPos().getZ() + 0.5D, world);
+                    star.moveTo(pos.getBlockPos().getX(), pos.getBlockPos().getY() + 20, pos.getBlockPos().getZ());
+                    world.addFreshEntity(star);
                 }
-            }
         }
     }
 
