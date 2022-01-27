8daefae9ed3e19cf015bfb7ffb044aa6a7dbefba
Naturix <nicosaurusrex99@gmail.com>
2022-01-28 03:27:18 +1030

    ayeraco works-ish

--- src/main/java/divinerpg/entities/boss/ayeraco/EntityAyeraco.java
+++ src/main/java/divinerpg/entities/boss/ayeraco/EntityAyeraco.java
@@ -1,12 +1,10 @@
 package divinerpg.entities.boss.ayeraco;
 
 import divinerpg.entities.base.*;
+import divinerpg.entities.projectile.*;
 import divinerpg.registries.*;
-import divinerpg.util.*;
 import net.minecraft.block.*;
 import net.minecraft.entity.*;
-import net.minecraft.entity.ai.attributes.*;
-import net.minecraft.entity.monster.*;
 import net.minecraft.entity.player.*;
 import net.minecraft.nbt.*;
 import net.minecraft.util.*;
@@ -30,16 +28,11 @@ public class EntityAyeraco extends EntityDivineBoss {
     private AyeracoGroup group;
 
     public EntityAyeraco(EntityType<? extends Entity> type, World worldIn) {
-        super((EntityType<? extends MobEntity>) type, worldIn);
+        this(type, worldIn, new BlockPos(0, 100, 0));
     }
-    /**
-     //     * Main constructor
-     //     *
-     //     * @param world
-     //     * @param beam  - beam location
-     //     */
-    public EntityAyeraco(EntityType<? extends MobEntity> type, World world, BlockPos beam) {
-        super(type, world);
+
+    public EntityAyeraco(EntityType<? extends Entity> type, World world, BlockPos beam) {
+        super((EntityType<? extends MobEntity>) type, world);
         beamLocation = beam;
         this.waitTick = 0;
         this.attacks = false;
@@ -62,20 +55,17 @@ public class EntityAyeraco extends EntityDivineBoss {
         super.registerGoals();
         addAttackingAI();
     }
-    public static AttributeModifierMap.MutableAttribute attributes() {
-        return MonsterEntity.createMonsterAttributes().add(Attributes.MAX_HEALTH, EntityStats.ayeracoHealth).add(Attributes.ATTACK_DAMAGE, EntityStats.ayeracoDamage).add(Attributes.MOVEMENT_SPEED, EntityStats.ayeracoSpeed).add(Attributes.FOLLOW_RANGE, EntityStats.ayeracoFollowRange);
-    }
+
     @Override
     public void tick() {
         super.tick();
-        setDeltaMovement(getDeltaMovement().x, getDeltaMovement().y * 0.6000000238418579, getDeltaMovement().z);
+        setDeltaMovement(getDeltaMovement().x, getDeltaMovement().y * 0.6000000238418579D, getDeltaMovement().z);
     }
 
     @Override
-    protected void customServerAiStep() {
+    public void customServerAiStep() {
         super.customServerAiStep();
 
-        if(group != null)
         group.tick(this);
 
         if (getHealth() * 2 < getMaxHealth()) {
@@ -95,10 +85,10 @@ public class EntityAyeraco extends EntityDivineBoss {
                 int var1 = (int) this.getTarget().getX();
                 int var2 = (int) this.getTarget().getY();
                 int var3 = (int) this.getTarget().getZ();
-                this.currentFlightTarget = this.getTarget().blockPosition();
+                this.currentFlightTarget = new BlockPos(var1, var2, var3);
             }
 
-            this.setDeltaMovement(getDeltaMovement().x, 0, getDeltaMovement().z);
+            setDeltaMovement(getDeltaMovement().x, 0, getDeltaMovement().z);
 
             if (this.currentFlightTarget != null) {
                 double var1 = this.currentFlightTarget.getX() - this.getX();
@@ -107,22 +97,21 @@ public class EntityAyeraco extends EntityDivineBoss {
 
                 if (Math.signum(var1) != 0 || Math.signum(var3) != 0 || Math.signum(var5) != 0) {
                     setDeltaMovement(Math.signum(var1) * moveSpeed, getDeltaMovement().y + (Math.signum(var3) * 5.699999988079071D - this.getDeltaMovement().y) * 0.10000000149011612D, Math.signum(var5) * moveSpeed);
-                    float var7 = (float) (Math.atan2(this.getDeltaMovement().z, this.getDeltaMovement().x) * 180.0D / Math.PI) - 90.0F;
-                    float var8 = MathHelper.wrapDegrees(var7 - this.xRot);
-                    moveSpeed=0.5F;
-                    this.xRot += var8;
+                    float var7 = (float) (Math.atan2(this.zo, this.xo) * 180.0D / Math.PI) - 90.0F;
+                    float var8 = MathHelper.wrapDegrees(var7 - this.yRot);
+                    this.moveDist = 0.5F;
+                    this.yRot += var8;
                 }
             }
         } else {
             this.waitTick--;
-            setDeltaMovement(moveX, 0.3, moveZ);
+            setDeltaMovement(getDeltaMovement().x, 0.3, getDeltaMovement().z);
         }
 
         if (!this.attacks) {
             this.waitTick = 80;
             this.attacks = true;
 
-            if(group != null)
             if (group.canTeleport(this)) {
                 this.waitTick = 0;
                 this.teleportRandomUp();
@@ -130,29 +119,28 @@ public class EntityAyeraco extends EntityDivineBoss {
         }
     }
 
-    protected boolean isMovementNoisy() {
+    @Override
+    public boolean causeFallDamage(float distance, float damageMultiplier) {
         return false;
     }
 
-    public boolean causeFallDamage(float p_225503_1_, float p_225503_2_) {
-        return false;
-    }
-
+    @Override
     protected void checkFallDamage(double p_184231_1_, boolean p_184231_3_, BlockState p_184231_4_, BlockPos p_184231_5_) {
     }
 
     @Override
     public boolean hurt(DamageSource damageSource, float par2) {
-        if(group != null) {
-            if (isInvulnerableTo(damageSource)
-                    || (damageSource.isProjectile() && group.projectileProtected(this))) {
-                return false;
-            }
-
-            if (damageSource.getDirectEntity() instanceof PlayerEntity) {
-                attacks = true;
-            }
+        if (isInvulnerableTo(damageSource)
+                // ignoring local mobs damage. Maybe check for Ender Dragon?
+                || damageSource.getDirectEntity() instanceof EntityEnderTripletsFireball
+                || (damageSource.isProjectile() && group.projectileProtected(this))) {
+            return false;
         }
+
+        if (damageSource.getDirectEntity() instanceof PlayerEntity) {
+            attacks = true;
+        }
+
         return super.hurt(damageSource, par2);
     }
 
@@ -160,11 +148,6 @@ public class EntityAyeraco extends EntityDivineBoss {
     public boolean doHurtTarget(Entity entity) {
         this.attacks = false;
         return super.doHurtTarget(entity);
-    }
-
-    @Override
-    public int getMaxSpawnClusterSize() {
-        return 1;
     }
 
     @Override
@@ -183,24 +166,24 @@ public class EntityAyeraco extends EntityDivineBoss {
     }
 
     @Override
-    public void addAdditionalSaveData(CompoundNBT compound) {
+    public boolean save(CompoundNBT compound) {
         compound.put("group", group.serializeNBT());
         compound.putLong("beamPos", beamLocation.asLong());
-        super.addAdditionalSaveData(compound);
+        return super.save(compound);
     }
 
     @Override
-    public void readAdditionalSaveData(CompoundNBT compound) {
-        super.readAdditionalSaveData(compound);
+    public void load(CompoundNBT compound) {
+        super.load(compound);
         group.deserializeNBT(compound.getCompound("group"));
         beamLocation = BlockPos.of(compound.getLong("beamPos"));
     }
 
     @Override
-    public void die(DamageSource cause) {
-        super.die(cause);
+    protected void tickDeath() {
+        super.tickDeath();
         if (level.isLoaded(beamLocation)) {
-            level.setBlock(beamLocation, Blocks.AIR.defaultBlockState(), 1);
+            level.setBlock(beamLocation, Blocks.AIR.defaultBlockState(), 0);
         }
     }
 
@@ -213,8 +196,16 @@ public class EntityAyeraco extends EntityDivineBoss {
         this.group = group;
     }
 
+
+    @Override
+    public int getMaxSpawnClusterSize() {
+        return 1;
+    }
+
     protected void teleportRandomUp() {
         this.playSound(SoundRegistry.AYERACO_TELEPORT, 2.0F, 0.4F / (this.random.nextFloat() * 0.4F + 0.8F));
-        setDeltaMovement(random.nextInt(5), 20, random.nextInt(5));
+        this.yo = 20;
+        this.xo = this.random.nextInt(5);
+        this.zo = this.random.nextInt(5);
     }
 }
