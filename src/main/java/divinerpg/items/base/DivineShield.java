c0f25fbf14e13fc96a331bf3286ea0e87f623185
Locomen_ <webmaster@locomen.ga>
2022-02-05 17:25:28 +0300

    fixed shield infinity durability

--- src/main/java/divinerpg/items/base/DivineShield.java
+++ src/main/java/divinerpg/items/base/DivineShield.java
@@ -3,6 +3,7 @@ package divinerpg.items.base;
 import divinerpg.DivineRPG;
 import divinerpg.client.renders.item.DivineShieldRenderer;
 import net.minecraft.block.DispenserBlock;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.*;
 import net.minecraft.util.ResourceLocation;
 
@@ -10,12 +11,17 @@ public class DivineShield extends ShieldItem {
     public ResourceLocation resource = new ResourceLocation("");
 
     public DivineShield(String name, Rarity rarity, ItemGroup group, Item repairMaterial, int damage) {
-        super(new Item.Properties().tab(group).defaultDurability(damage).setISTER(() -> new DivineShieldRenderer()).rarity(rarity));
+        super(new Item.Properties().tab(group).durability(damage).setISTER(() -> new DivineShieldRenderer()).rarity(rarity));
         setRegistryName(name);
         this.isRepairable(new ItemStack(repairMaterial));
         DispenserBlock.registerBehavior(this, ArmorItem.DISPENSE_ITEM_BEHAVIOR);
 
         resource = new ResourceLocation(DivineRPG.MODID, "textures/shield/" + name + ".png");
+    }
+
+    @Override
+    public boolean isShield(ItemStack stack, LivingEntity entity) {
+        return true;
     }
 
 }
