0983ede6e1e49a7618d0945b957f777d0bc4ad5d
Locomen_ <webmaster@locomen.ga>
2022-02-08 23:24:07 +0300

    added check when using teleportation crystal

--- src/main/java/divinerpg/items/vanilla/ItemTeleportationCrystal.java
+++ src/main/java/divinerpg/items/vanilla/ItemTeleportationCrystal.java
@@ -12,7 +12,9 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.*;
 import net.minecraft.world.World;
+import net.minecraftforge.server.command.TextComponentHelper;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -35,21 +37,26 @@ public class ItemTeleportationCrystal extends ItemMod {
     }
 
 
-
     @Override
     public ActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
         if (!world.isClientSide) {
             if (player instanceof ServerPlayerEntity) {
-                player.changeDimension(world.getServer().getLevel(World.OVERWORLD), new SecondaryTeleporter(world.getServer().getLevel(World.OVERWORLD)));
-                if (!player.isCreative()) {
-                    ItemStack stack = player.getItemInHand(hand);
-                    stack.hurtAndBreak(1, player, (p_220009_1_) -> {
-                        p_220009_1_.broadcastBreakEvent(player.getUsedItemHand());
-                    });
+                if (((ServerPlayerEntity) player).getRespawnPosition() != null) {
+                    player.changeDimension(world.getServer().getLevel(World.OVERWORLD), new SecondaryTeleporter(world.getServer().getLevel(World.OVERWORLD)));
+                    if (!player.isCreative()) {
+                        ItemStack stack = player.getItemInHand(hand);
+                        stack.hurtAndBreak(1, player, (p_220009_1_) -> {
+                            p_220009_1_.broadcastBreakEvent(player.getUsedItemHand());
+                        });
+                    }
+                    return ActionResult.success(player.getItemInHand(hand));
+                } else {
+                    TextComponent message = TextComponentHelper.createComponentTranslation(player, "message.teleportation_crystal_no_respawn");
+                    message.withStyle(TextFormatting.RED);
+                    player.sendMessage(message, player.getUUID());
                 }
-                return ActionResult.success(player.getItemInHand(hand));
             }
         }
-            return ActionResult.fail(player.getItemInHand(hand));
+        return ActionResult.fail(player.getItemInHand(hand));
     }
 }
