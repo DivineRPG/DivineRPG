2aa06aff4d3ac0a99bce9f04c1cbe0174db3a23e
Locomen_ <webmaster@locomen.ga>
2022-02-10 17:07:31 +0300

    restoring inventory after death in vethea

--- src/main/java/divinerpg/events/VetheaInventorySwapEvent.java
+++ src/main/java/divinerpg/events/VetheaInventorySwapEvent.java
@@ -9,33 +9,48 @@ import net.minecraftforge.event.entity.player.*;
 import net.minecraftforge.eventbus.api.*;
 
 public class VetheaInventorySwapEvent {
+    private CompoundNBT persistentData;
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public void onRespawn(PlayerEvent.PlayerRespawnEvent event) {
+        if (event.isCanceled() || !(event.getEntity() instanceof PlayerEntity)) return;
+
+        PlayerEntity playerIn = event.getPlayer();
+        if (!persistentData.getList("OverworldInv", 10).isEmpty() && persistentData.getList("OverworldInv", 10) != null) {
+            ListNBT inv = persistentData.getList("OverworldInv", 10);
+            playerIn.inventory.load(inv);
+            playerIn.inventoryMenu.broadcastChanges();
+            persistentData.getList("OverworldInv", 10).clear();
+        }
+    }
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public void onDimensionChange(PlayerEvent.PlayerChangedDimensionEvent event) {
         if (event.isCanceled() || !(event.getEntity() instanceof PlayerEntity))
             return;
 
-
         RegistryKey<World> vetheaID = KeyRegistry.VETHEA_WORLD;
         PlayerEntity playerIn = event.getPlayer();
-        CompoundNBT persistentData = playerIn.getPersistentData().getCompound(PlayerEntity.PERSISTED_NBT_TAG);
+        persistentData = playerIn.getPersistentData().getCompound(PlayerEntity.PERSISTED_NBT_TAG);
 
-                if(event.getFrom() == vetheaID) {
-                    persistentData.put("VetheaInv", playerIn.inventory.save(new ListNBT()));
-                    playerIn.getPersistentData().put("PlayerPersisted", persistentData);
-                    playerIn.inventory.clearContent();
-                    ListNBT inv = persistentData.getList("OverworldInv", 10);
-                    playerIn.inventory.load(inv);
-                    playerIn.inventoryMenu.broadcastChanges();
-                    playerIn.removeAllEffects();
-                } if(event.getTo() == vetheaID) {
-                    persistentData.put("OverworldInv", playerIn.inventory.save(new ListNBT()));
-                    playerIn.getPersistentData().put("PlayerPersisted", persistentData);
-                    playerIn.inventory.clearContent();
-                    ListNBT inv = persistentData.getList("VetheaInv", 10);
-                    playerIn.inventory.load(inv);
-                    playerIn.inventoryMenu.broadcastChanges();
-                    playerIn.removeAllEffects();
+        if (event.getFrom() == vetheaID) {
+            persistentData.put("VetheaInv", playerIn.inventory.save(new ListNBT()));
+            playerIn.getPersistentData().put("PlayerPersisted", persistentData);
+            playerIn.inventory.clearContent();
+            ListNBT inv = persistentData.getList("OverworldInv", 10);
+            playerIn.inventory.load(inv);
+            playerIn.inventoryMenu.broadcastChanges();
+            persistentData.getList("OverworldInv", 10).clear();
+            playerIn.removeAllEffects();
+        }
+        if (event.getTo() == vetheaID) {
+            persistentData.put("OverworldInv", playerIn.inventory.save(new ListNBT()));
+            playerIn.getPersistentData().put("PlayerPersisted", persistentData);
+            playerIn.inventory.clearContent();
+            ListNBT inv = persistentData.getList("VetheaInv", 10);
+            playerIn.inventory.load(inv);
+            playerIn.inventoryMenu.broadcastChanges();
+            playerIn.removeAllEffects();
         }
 
     }
