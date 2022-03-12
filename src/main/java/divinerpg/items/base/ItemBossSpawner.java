e632f20f258982e31f0714f0202b7b785be1cf1e
Locomen_ <webmaster@locomen.ga>
2022-03-12 22:16:33 +0300

    fixed infinite use of summoning items

--- src/main/java/divinerpg/items/base/ItemBossSpawner.java
+++ src/main/java/divinerpg/items/base/ItemBossSpawner.java
@@ -1,20 +1,15 @@
 package divinerpg.items.base;
 
-import divinerpg.DivineRPG;
-import divinerpg.registries.KeyRegistry;
-import divinerpg.util.RarityList;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.RegistryKey;
-import net.minecraft.util.text.TextComponent;
-import net.minecraft.util.text.TextFormatting;
-import net.minecraft.world.World;
-import net.minecraftforge.server.command.TextComponentHelper;
+import divinerpg.*;
+import divinerpg.registries.*;
+import divinerpg.util.*;
+import net.minecraft.entity.*;
+import net.minecraft.entity.player.*;
+import net.minecraft.item.*;
+import net.minecraft.util.*;
+import net.minecraft.util.text.*;
+import net.minecraft.world.*;
+import net.minecraftforge.server.command.*;
 
 public class ItemBossSpawner extends ItemMod {
 
@@ -32,24 +27,28 @@ public class ItemBossSpawner extends ItemMod {
     @Override
     public ActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
         if (!world.isClientSide) {
-            if(dimensionID == null){
-                dimensionID = KeyRegistry.MORTUM_WORLD;
-            }
-            if (world.dimension() != dimensionID) {
-                TextComponent message = TextComponentHelper.createComponentTranslation(player, langKey);
-                message.withStyle(TextFormatting.AQUA);
-                player.sendMessage(message, player.getUUID());
-                return ActionResult.fail(player.getItemInHand(hand));
-            } else {
-                for (EntityType<?> entType : ents) {
-                    Entity entity = entType.create(world);
-                    entity.moveTo(player.getX(), player.getY() + 1, player.getZ());
-                    world.addFreshEntity(entity);
-                    DivineRPG.LOGGER.info("spawned " + entType.getDescriptionId() + " at " + entity.blockPosition());
-                    if(!player.isCreative())
-                        player.getMainHandItem().shrink(1);
-                    return ActionResult.success(player.getItemInHand(hand));
+            if (hand == Hand.MAIN_HAND) {
+                if (dimensionID == null) {
+                    dimensionID = KeyRegistry.MORTUM_WORLD;
                 }
+                if (world.dimension() != dimensionID) {
+                    TextComponent message = TextComponentHelper.createComponentTranslation(player, langKey);
+                    message.withStyle(TextFormatting.AQUA);
+                    player.sendMessage(message, player.getUUID());
+                    return ActionResult.fail(player.getItemInHand(hand));
+                } else {
+                    for (EntityType<?> entType : ents) {
+                        Entity entity = entType.create(world);
+                        entity.moveTo(player.getX(), player.getY() + 1, player.getZ());
+                        world.addFreshEntity(entity);
+                        DivineRPG.LOGGER.info("spawned " + entType.getDescriptionId() + " at " + entity.blockPosition());
+                        if (!player.isCreative())
+                            player.getMainHandItem().shrink(1);
+                        return ActionResult.success(player.getItemInHand(hand));
+                    }
+                }
+            } else {
+                return ActionResult.fail(player.getItemInHand(hand));
             }
         }
 
