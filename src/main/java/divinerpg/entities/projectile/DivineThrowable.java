97eeafca6199f6163dd86c3d52435537d03fdd39
Nicusha <nicosaurusrex99@gmail.com>
2022-04-11 01:09:40 +0930

    Fixed #487

--- src/main/java/divinerpg/entities/projectile/DivineThrowable.java
+++ src/main/java/divinerpg/entities/projectile/DivineThrowable.java
@@ -3,6 +3,7 @@ package divinerpg.entities.projectile;
 import net.minecraft.entity.*;
 import net.minecraft.entity.projectile.*;
 import net.minecraft.network.*;
+import net.minecraft.util.math.*;
 import net.minecraft.world.*;
 import net.minecraftforge.fml.network.*;
 
@@ -22,6 +23,24 @@ public class DivineThrowable extends ThrowableEntity {
     @Override
     protected void defineSynchedData() {
 
+    }
+
+    @Override
+    protected void onHit(RayTraceResult result) {
+        super.onHit(result);
+        this.kill();
+    }
+
+    @Override
+    protected void onHitEntity(EntityRayTraceResult result) {
+        super.onHitEntity(result);
+        this.kill();
+    }
+
+    @Override
+    protected void onHitBlock(BlockRayTraceResult result) {
+        super.onHitBlock(result);
+        this.kill();
     }
 
     @Override
