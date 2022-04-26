8bf2105f037fbf3f0ed6c7fa59b976667d616ce2
Nicusha <nicosaurusrex99@gmail.com>
2022-04-26 13:14:21 +0930

    Update RenderKobblin.java

--- src/main/java/divinerpg/client/renders/entity/vanilla/RenderKobblin.java
+++ src/main/java/divinerpg/client/renders/entity/vanilla/RenderKobblin.java
@@ -1,12 +1,12 @@
 package divinerpg.client.renders.entity.vanilla;
 
 
-import com.mojang.blaze3d.matrix.MatrixStack;
-import divinerpg.DivineRPG;
+import divinerpg.*;
 import divinerpg.client.models.vanilla.*;
-import divinerpg.entities.vanilla.overworld.EntityKobblin;
+import divinerpg.entities.vanilla.overworld.*;
+import net.minecraft.client.renderer.culling.*;
 import net.minecraft.client.renderer.entity.*;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.*;
 import net.minecraftforge.api.distmarker.*;
 
 @OnlyIn(Dist.CLIENT)
@@ -17,9 +17,12 @@ public class RenderKobblin extends MobRenderer<EntityKobblin, ModelKobblin<Entit
         super(renderManagerIn, new ModelKobblin<>(), 0.8F);
     }
 
-    protected void scale(EntityKobblin entity, MatrixStack matrixStackIn, float partialTickTime) {
-        if(entity instanceof EntityKobblin && !((EntityKobblin) entity).getProvoked()){
-            matrixStackIn.translate(0, 1.125F, 0);
+    @Override
+    public boolean shouldRender(EntityKobblin entity, ClippingHelper p_225626_2_, double p_225626_3_, double p_225626_5_, double p_225626_7_) {
+        if(!entity.getProvoked()) {
+        return false;
+        }else{
+            return super.shouldRender(entity, p_225626_2_, p_225626_3_, p_225626_5_, p_225626_7_);
         }
     }
 
