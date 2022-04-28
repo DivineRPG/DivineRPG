acd81ce91021c0f2af2430c84a779ca9d4bb862a
Nicusha <nicosaurusrex99@gmail.com>
2022-04-28 14:21:27 +0930

    fixed sunstorm model

--- src/main/java/divinerpg/client/models/boss/ModelSunstorm.java
+++ src/main/java/divinerpg/client/models/boss/ModelSunstorm.java
@@ -5,7 +5,8 @@ import net.minecraft.client.renderer.entity.model.SegmentedModel;
 import net.minecraft.client.renderer.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.MathHelper;
-import net.minecraftforge.api.distmarker.*;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
 public class ModelSunstorm<T extends Entity> extends SegmentedModel<T> {
@@ -77,28 +78,28 @@ public class ModelSunstorm<T extends Entity> extends SegmentedModel<T> {
       setRotation(armR, -1.047198F, 0F, 0F);
       staffPoleL = new ModelRenderer(this, 0, 104);
       staffPoleL.addBox(-1F, -9F, -17F, 2, 22, 2);
-      staffPoleL.setPos(6F, -10F, 0F);
+      staffPoleL.setPos(0F, 0.0F, 3.0F);
       staffPoleL.setTexSize(128, 128);
       staffPoleL.mirror = true;
-      setRotation(staffPoleL, 0.5235988F, 0F, 0F);
+      setRotation(staffPoleL, 1.5235988F, 0F, 0F);
       staffCapL = new ModelRenderer(this, 8, 121);
       staffCapL.addBox(-3F, -7F, -19F, 6, 1, 6);
-      staffCapL.setPos(6F, -10F, 0F);
+      staffCapL.setPos(0F, 0.0F, 3.0F);
       staffCapL.setTexSize(128, 128);
       staffCapL.mirror = true;
-      setRotation(staffCapL, 0.5235988F, 0F, 0F);
+      setRotation(staffCapL, 1.5235988F, 0F, 0F);
       staffPoleR = new ModelRenderer(this, 0, 104);
       staffPoleR.addBox(-1F, -9F, -17F, 2, 22, 2);
-      staffPoleR.setPos(-6F, -10F, 0F);
+      staffPoleR.setPos(0F, 0.0F, 3.0F);
       staffPoleR.setTexSize(128, 128);
       staffPoleR.mirror = true;
-      setRotation(staffPoleR, 0.5235988F, 0F, 0F);
+      setRotation(staffPoleR, 1.5235988F, 0F, 0F);
       staffCapR = new ModelRenderer(this, 8, 121);
       staffCapR.addBox(-3F, -7F, -19F, 6, 1, 6);
-      staffCapR.setPos(-6F, -10F, 0F);
+      staffCapR.setPos(0F, 0.0F, 3.0F);
       staffCapR.setTexSize(128, 128);
       staffCapR.mirror = true;
-      setRotation(staffCapR, 0.5235988F, 0F, 0F);
+      setRotation(staffCapR, 1.5235988F, 0F, 0F);
       neck = new ModelRenderer(this, 60, 0);
       neck.addBox(-3F, 0F, -3F, 6, 4, 6);
       neck.setPos(0F, -16F, 0F);
@@ -113,19 +114,19 @@ public class ModelSunstorm<T extends Entity> extends SegmentedModel<T> {
       setRotation(head, 0F, 0F, 0F);
       rayMid = new ModelRenderer(this, 84, 0);
       rayMid.addBox(-2.5F, -15F, 0F, 5, 8, 1);
-      rayMid.setPos(0F, -20F, 0F);
+      rayMid.setPos(0F, -3F, 0F);
       rayMid.setTexSize(128, 128);
       rayMid.mirror = true;
       setRotation(rayMid, 0F, 0F, 0F);
       rayL = new ModelRenderer(this, 84, 0);
       rayL.addBox(-2.5F, -15F, 0F, 5, 8, 1);
-      rayL.setPos(0F, -20F, 0F);
+      rayL.setPos(0F, -3F, 0F);
       rayL.setTexSize(128, 128);
       rayL.mirror = true;
       setRotation(rayL, 0F, 0F, 0.7853982F);
       rayR = new ModelRenderer(this, 84, 0);
       rayR.addBox(-2.5F, -15F, 0F, 5, 8, 1);
-      rayR.setPos(0F, -20F, 0F);
+      rayR.setPos(0F, -3F, 0F);
       rayR.setTexSize(128, 128);
       rayR.mirror = true;
       setRotation(rayR, 0F, 0F, -0.7853982F);
@@ -143,61 +144,61 @@ public class ModelSunstorm<T extends Entity> extends SegmentedModel<T> {
       setRotation(headSideR, 0F, 0F, 0F);
       staffProng1L = new ModelRenderer(this, 32, 123);
       staffProng1L.addBox(2F, -11F, -14F, 1, 4, 1);
-      staffProng1L.setPos(6F, -10F, 0F);
+      staffProng1L.setPos(0F, 0.0F, 3.0F);
       staffProng1L.setTexSize(128, 128);
       staffProng1L.mirror = true;
-      setRotation(staffProng1L, 0.5235988F, 0F, 0F);
+      setRotation(staffProng1L, 1.5235988F, 0F, 0F);
       staffProng2L = new ModelRenderer(this, 32, 123);
       staffProng2L.addBox(2F, -11F, -19F, 1, 4, 1);
-      staffProng2L.setPos(6F, -10F, 0F);
+      staffProng2L.setPos(0F, 0.0F, 3.0F);
       staffProng2L.setTexSize(128, 128);
       staffProng2L.mirror = true;
-      setRotation(staffProng2L, 0.5235988F, 0F, 0F);
+      setRotation(staffProng2L, 1.5235988F, 0F, 0F);
       staffProng3L = new ModelRenderer(this, 32, 123);
       staffProng3L.addBox(-3F, -11F, -19F, 1, 4, 1);
-      staffProng3L.setPos(6F, -10F, 0F);
+      staffProng3L.setPos(0F, 0.0F, 3.0F);
       staffProng3L.setTexSize(128, 128);
       staffProng3L.mirror = true;
-      setRotation(staffProng3L, 0.5235988F, 0F, 0F);
+      setRotation(staffProng3L, 1.5235988F, 0F, 0F);
       staffProng4L = new ModelRenderer(this, 32, 123);
       staffProng4L.addBox(-3F, -11F, -14F, 1, 4, 1);
-      staffProng4L.setPos(6F, -10F, 0F);
+      staffProng4L.setPos(0F, 0.0F, 3.0F);
       staffProng4L.setTexSize(128, 128);
       staffProng4L.mirror = true;
-      setRotation(staffProng4L, 0.5235988F, 0F, 0F);
+      setRotation(staffProng4L, 1.5235988F, 0F, 0F);
       staffProng1R = new ModelRenderer(this, 32, 123);
       staffProng1R.addBox(2F, -11F, -14F, 1, 4, 1);
-      staffProng1R.setPos(-6F, -10F, 0F);
+      staffProng1R.setPos(0F, 0.0F, 3.0F);
       staffProng1R.setTexSize(128, 128);
       staffProng1R.mirror = true;
-      setRotation(staffProng1R, 0.5235988F, 0F, 0F);
+      setRotation(staffProng1R, 1.5235988F, 0F, 0F);
       staffProng2R = new ModelRenderer(this, 32, 123);
       staffProng2R.addBox(2F, -11F, -19F, 1, 4, 1);
-      staffProng2R.setPos(-6F, -10F, 0F);
+      staffProng2R.setPos(0F, 0.0F, 3.0F);
       staffProng2R.setTexSize(128, 128);
       staffProng2R.mirror = true;
-      setRotation(staffProng2R, 0.5235988F, 0F, 0F);
+      setRotation(staffProng2R, 1.5235988F, 0F, 0F);
       staffProng3R = new ModelRenderer(this, 32, 123);
       staffProng3R.addBox(-3F, -11F, -19F, 1, 4, 1);
-      staffProng3R.setPos(-6F, -10F, 0F);
+      staffProng3R.setPos(0F, 0.0F, 3.0F);
       staffProng3R.setTexSize(128, 128);
       staffProng3R.mirror = true;
-      setRotation(staffProng3R, 0.5235988F, 0F, 0F);
+      setRotation(staffProng3R, 1.5235988F, 0F, 0F);
       staffProng4R = new ModelRenderer(this, 32, 123);
       staffProng4R.addBox(-3F, -11F, -14F, 1, 4, 1);
-      staffProng4R.setPos(-6F, -10F, 0F);
+      staffProng4R.setPos(0F, 0.0F, 3.0F);
       staffProng4R.setTexSize(128, 128);
       staffProng4R.mirror = true;
-      setRotation(staffProng4R, 0.5235988F, 0F, 0F);
+      setRotation(staffProng4R, 1.5235988F, 0F, 0F);
       armSpikeL = new ModelRenderer(this, 0, 62);
       armSpikeL.addBox(2F, 6F, -1F, 6, 3, 2);
-      armSpikeL.setPos(6F, -10F, 0F);
+      armSpikeL.setPos(-1F, 1F, 6.5F);
       armSpikeL.setTexSize(128, 128);
       armSpikeL.mirror = true;
       setRotation(armSpikeL, -1.047198F, 0F, 0F);
       armSpikeR = new ModelRenderer(this, 0, 62);
       armSpikeR.addBox(-8F, 6F, -1F, 6, 3, 2);
-      armSpikeR.setPos(-6F, -10F, 0F);
+      armSpikeR.setPos(-0F, 1F, 6.5F);
       armSpikeR.setTexSize(128, 128);
       armSpikeR.mirror = true;
       setRotation(armSpikeR, -1.047198F, 0F, 0F);
@@ -219,12 +220,30 @@ public class ModelSunstorm<T extends Entity> extends SegmentedModel<T> {
       backSpine3.setTexSize(128, 128);
       backSpine3.mirror = true;
       setRotation(backSpine3, -0.3839724F, 0F, 0F);
+
+      //Child
+      head.addChild(rayR);
+      head.addChild(rayMid);
+      head.addChild(rayL);
+      armR.addChild(armSpikeR);
+      armL.addChild(armSpikeL);
+      armL.addChild(staffPoleL);
+      armR.addChild(staffPoleR);
+      armL.addChild(staffCapL);
+      armL.addChild(staffProng1L);
+      armL.addChild(staffProng2L);
+      armL.addChild(staffProng3L);
+      armL.addChild(staffProng4L);
+      armR.addChild(staffCapR);
+      armR.addChild(staffProng1R);
+      armR.addChild(staffProng2R);
+      armR.addChild(staffProng3R);
+      armR.addChild(staffProng4R);
   }
 
     @Override
     public Iterable<ModelRenderer> parts() {
-        return ImmutableList.of(legR, legL, body, armL, armR, staffPoleL, staffCapL, staffPoleR, staffCapR, neck, head, rayMid, rayL, rayR, headSideL, headSideR, staffProng1L, staffProng2L, staffProng3L,
-        staffProng4L, staffProng1R, staffProng2R, staffProng3R, staffProng4R, armSpikeL, armSpikeR, backSpine1, backSpine2, backSpine3);
+        return ImmutableList.of(legR, legL, body, armL, armR, neck, head, headSideL, headSideR, backSpine1, backSpine2, backSpine3);
     }
 
     private void setRotation(ModelRenderer model, float x, float y, float z) {
@@ -233,15 +252,22 @@ public class ModelSunstorm<T extends Entity> extends SegmentedModel<T> {
         model.zRot = z;
     }
 
-    @Override
-    public void setupAnim(T e, float f, float f1, float f2, float f3, float f4)
-  {
-    this.armR.xRot = MathHelper.cos(f * 0.6662F + (float)Math.PI) * 2.0F * f1 * 0.5F / f;
-    this.armL.xRot = MathHelper.cos(f * 0.6662F) * 2.0F * f1 * 0.5F / f;
+  @Override
+  public void setupAnim(T entity, float limbSwing, float limbSwingAmount, float p_225597_4_, float p_225597_5_, float headSpin) {
+    this.body.yRot = 0.0F;
+    this.armR.z = 0.0F;
+    this.armR.x = -5.0F;
+    this.armL.z = 0.0F;
+    this.armL.x = 5.0F;
+    this.armR.xRot = -32 + MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 2.0F * limbSwingAmount * 0.5F;
+    this.armL.xRot = -32 + MathHelper.cos(limbSwing * 0.6662F) * 2.0F * limbSwingAmount * 0.5F;
     this.armR.zRot = 0.0F;
     this.armL.zRot = 0.0F;
-	this.legR.xRot = (MathHelper.cos(f * 0.6662F) * 1.4F * f1);
-	this.legL.xRot = (MathHelper.cos(f * 0.6662F + 3.141593F) * 1.4F * f1);
+    this.legR.xRot = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
+    this.legL.xRot = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
+    this.legR.yRot = 0.0F;
+    this.legL.yRot = 0.0F;
+    this.legR.zRot = 0.0F;
+    this.legL.zRot = 0.0F;
   }
-
 }
