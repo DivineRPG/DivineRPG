f9c755fa34f28856900f1a8f2e6ad209264eadfa
Nicusha <nicosaurusrex99@gmail.com>
2022-05-03 02:27:43 +0930

    Fixed king of scorchers AI

--- src/main/java/divinerpg/entities/boss/EntityKingOfScorchers.java
+++ src/main/java/divinerpg/entities/boss/EntityKingOfScorchers.java
@@ -1,24 +1,26 @@
 package divinerpg.entities.boss;
 
-import divinerpg.entities.ai.*;
-import divinerpg.entities.base.*;
-import divinerpg.entities.projectile.*;
-import divinerpg.enums.*;
-import divinerpg.registries.*;
-import divinerpg.util.*;
+import divinerpg.entities.base.EntityDivineBoss;
+import divinerpg.entities.projectile.EntityKingOfScorchersMeteor;
+import divinerpg.entities.projectile.EntityKingOfScorchersShot;
+import divinerpg.registries.EntityRegistry;
+import divinerpg.registries.SoundRegistry;
+import divinerpg.util.EntityStats;
 import net.minecraft.entity.*;
-import net.minecraft.entity.ai.attributes.*;
-import net.minecraft.entity.ai.goal.*;
-import net.minecraft.entity.monster.*;
-import net.minecraft.entity.player.*;
-import net.minecraft.entity.projectile.*;
-import net.minecraft.util.*;
-import net.minecraft.world.BossInfo.*;
-import net.minecraft.world.*;
+import net.minecraft.entity.ai.attributes.AttributeModifierMap;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
+import net.minecraft.entity.ai.goal.RangedAttackGoal;
+import net.minecraft.entity.monster.MonsterEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.BossInfo.Color;
+import net.minecraft.world.World;
 
-import java.util.*;
-
-public class EntityKingOfScorchers extends EntityDivineBoss implements ILaunchThrowable {
+public class EntityKingOfScorchers extends EntityDivineBoss implements IRangedAttackMob {
     public EntityKingOfScorchers(EntityType<? extends MobEntity> type, World worldIn) {
         super(type, worldIn);
     }
@@ -35,99 +37,18 @@ public class EntityKingOfScorchers extends EntityDivineBoss implements ILaunchTh
     protected void registerGoals() {
         super.registerGoals();
 
+        this.goalSelector.addGoal(2, new RangedAttackGoal(this, 1.0D, 40, 32.0F));
         this.goalSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, true));
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, 10, true, false, (entity) -> {
+            return Math.abs(entity.getY() - this.getY()) <= 4.0D;
+        }));
 
-        // regular shoots
-        this.goalSelector.addGoal(7, new AIDivineFireballAttack(
-                this,
-                this::createFireball,
-                15,
-                64,
-                null,
-                SoundEvents.BLAZE_SHOOT));
-//
-        this.goalSelector.addGoal(8, new AIDivineFireballAttack(
-                this,
-                this::createMeteors,
-                20 * 60,
-                64,
-                null,
-                SoundEvents.BLAZE_SHOOT));
     }
 
         protected float getStandingEyeHeight(Pose poseIn, EntitySize sizeIn) {
             return 1F;
         }
 
-    public Entity createMeteors(LivingEntity parent, Entity victim) {
-        if (!(victim instanceof LivingEntity))
-            return null;
-
-        List<Entity> meteors = new ArrayList<>();
-
-
-        for (int i = 0; i < 4; i++) {
-            EntityKingOfScorchersMeteor meteor = new EntityKingOfScorchersMeteor(EntityRegistry.KING_OF_SCORCHERS_METEOR, ((LivingEntity) victim), level);
-
-            Random rand = meteor.level.random;
-
-            meteor.xo = rand.nextDouble() - rand.nextDouble() * 2;
-            meteor.xo += 10;
-            meteor.zo = rand.nextDouble() - rand.nextDouble() * 2;
-
-            meteor.setDeltaMovement((rand.nextDouble() - rand.nextDouble()) / 5, -0.7, (rand.nextDouble() - rand.nextDouble()) / 5);
-
-            meteors.add(meteor);
-        }
-
-        while (meteors.size() > 1) {
-            level.addFreshEntity(meteors.get(0));
-            meteors.remove(0);
-        }
-
-
-        return meteors.get(0);
-    }
-
-    @Override
-    public ThrowableEntity createThowable(World world, LivingEntity parent, double x, double y, double z) {
-        return new EntityKingOfScorchersShot(EntityRegistry.KING_OF_SCORCHERS_SHOT, parent, world);
-    }
-
-    @Override
-    public float getInaccuracy(World world) {
-        return 1;
-    }
-
-    public Entity createFireball(LivingEntity parent, Entity victim) {
-        if (!(victim instanceof LivingEntity))
-            return null;
-
-        List<Entity> meteors = new ArrayList<>();
-
-
-        for (int i = 0; i < 4; i++) {
-            EntityKingOfScorchersShot meteor = new EntityKingOfScorchersShot(EntityRegistry.KING_OF_SCORCHERS_SHOT, ((LivingEntity) victim), level);
-
-            Random rand = meteor.level.random;
-
-            meteor.xo = rand.nextDouble() - rand.nextDouble() * 2;
-            meteor.xo += 10;
-            meteor.zo = rand.nextDouble() - rand.nextDouble() * 2;
-
-            meteor.setDeltaMovement((rand.nextDouble() - rand.nextDouble()) / 5, -0.7, (rand.nextDouble() - rand.nextDouble()) / 5);
-
-            meteors.add(meteor);
-        }
-
-        while (meteors.size() > 1) {
-            level.addFreshEntity(meteors.get(0));
-            meteors.remove(0);
-        }
-
-
-        return meteors.get(0);
-    }
 
 
     @Override
@@ -156,5 +77,28 @@ public class EntityKingOfScorchers extends EntityDivineBoss implements ILaunchTh
     public Color getBarColor() {
         return Color.RED;
     }
-    
+
+    @Override
+    public void performRangedAttack(LivingEntity entity, float range) {
+        ProjectileEntity projectile = new EntityKingOfScorchersShot(EntityRegistry.KING_OF_SCORCHERS_SHOT, this, level);
+        if (level.random.nextInt(10) == 0) {
+            projectile = new EntityKingOfScorchersMeteor(EntityRegistry.KING_OF_SCORCHERS_METEOR, this, level);
+            for (int i = 0; i < 4; i++) {
+                double d0 = getTarget().getX() - this.getX();
+                double d1 = getTarget().getY(0.3333333333333333D) - projectile.getY();
+                double d2 = getTarget().getZ() - this.getZ();
+                double d3 = (double) MathHelper.sqrt(d0 * d0 + d2 * d2);
+                projectile.shoot(d0, d1 + d3 * (double) 0.2F, d2, 1.6F, (float) (14 - this.level.getDifficulty().getId() * 4));
+                this.level.addFreshEntity(projectile);
+            }
+
+        } else {
+            double d0 = getTarget().getX() - this.getX();
+            double d1 = getTarget().getY(0.3333333333333333D) - projectile.getY();
+            double d2 = getTarget().getZ() - this.getZ();
+            double d3 = (double) MathHelper.sqrt(d0 * d0 + d2 * d2);
+            projectile.shoot(d0, d1 + d3 * (double) 0.2F, d2, 1.6F, (float) (14 - this.level.getDifficulty().getId() * 4));
+            this.level.addFreshEntity(projectile);
+        }
+    }
 }
