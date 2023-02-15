3cd67bc4c0509a79ba2061f310c577090fe60019
Nicusha <nicosaurusrex99@gmail.com>
2023-02-16 02:47:32 +1030

    Fixed karos altar

--- src/main/java/divinerpg/blocks/vethea/BlockKarosAltar.java
+++ src/main/java/divinerpg/blocks/vethea/BlockKarosAltar.java
@@ -3,10 +3,15 @@ package divinerpg.blocks.vethea;
 import divinerpg.DivineRPG;
 import divinerpg.entities.boss.*;
 import divinerpg.registries.*;
+import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.*;
 import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.*;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class BlockKarosAltar extends BlockVetheaAltar {
@@ -16,6 +21,23 @@ public class BlockKarosAltar extends BlockVetheaAltar {
 
     protected Item acceptedItem() {
         return ForgeRegistries.ITEMS.getValue(new ResourceLocation(DivineRPG.MODID, "dream_flint"));
+    }
+
+    @Override
+    public InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result) {
+        ItemStack heldItemStack = player.getItemInHand(hand);
+
+        if(!world.isClientSide && heldItemStack != null && heldItemStack.getItem() == acceptedItem()) {
+            if (!player.isCreative()) {
+                heldItemStack.shrink(1);
+            }
+            spawnBoss(world, pos.above());
+        }
+        else {
+            onFailure();
+        }
+
+        return InteractionResult.PASS;
     }
 
     protected LivingEntity getBoss(Level world) {
