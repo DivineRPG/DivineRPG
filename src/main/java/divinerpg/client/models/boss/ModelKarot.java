59c49408ba3e3f4a245f54feb686e2fb31bc988b
Nicusha <nicosaurusrex99@gmail.com>
2023-02-23 02:09:59 +1030

    Fixed Karot model

--- src/main/java/divinerpg/client/models/boss/ModelKarot.java
+++ src/main/java/divinerpg/client/models/boss/ModelKarot.java
@@ -11,17 +11,7 @@ import static divinerpg.util.ClientUtils.createLocation;
 
 public class ModelKarot extends EntityModel<EntityKarot> {
 	public static final ModelLayerLocation LAYER_LOCATION = createLocation("karot");
-	private final ModelPart Head;
-	private final ModelPart Body;
-	private final ModelPart Leg1;
-	private final ModelPart Leg2;
-	private final ModelPart Leg3;
-	private final ModelPart Leg4;
-	private final ModelPart Tail;
-	private final ModelPart Shape1;
-	private final ModelPart Shape2;
-	private final ModelPart Shape3;
-	private final ModelPart Shape4;
+	private final ModelPart Head, Body, Leg1, Leg2, Leg3, Leg4, Ear1, Ear2, Tail, Shape1, Shape2, Shape3, Shape4;
 
 	public ModelKarot(ModelPart root) {
 		this.Head = root.getChild("Head");
@@ -30,6 +20,8 @@ public class ModelKarot extends EntityModel<EntityKarot> {
 		this.Leg2 = root.getChild("Leg2");
 		this.Leg3 = root.getChild("Leg3");
 		this.Leg4 = root.getChild("Leg4");
+		this.Ear1 = root.getChild("Ear1");
+		this.Ear2 = root.getChild("Ear2");
 		this.Tail = root.getChild("Tail");
 		this.Shape1 = root.getChild("Shape1");
 		this.Shape2 = root.getChild("Shape2");
@@ -44,6 +36,8 @@ public class ModelKarot extends EntityModel<EntityKarot> {
 		this.Leg2 = root.getChild("Leg2");
 		this.Leg3 = root.getChild("Leg3");
 		this.Leg4 = root.getChild("Leg4");
+		this.Ear1 = root.getChild("Ear1");
+		this.Ear2 = root.getChild("Ear2");
 		this.Tail = root.getChild("Tail");
 		this.Shape1 = root.getChild("Shape1");
 		this.Shape2 = root.getChild("Shape2");
@@ -54,35 +48,36 @@ public class ModelKarot extends EntityModel<EntityKarot> {
 	public static LayerDefinition createBodyLayer() {
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
-		CubeDeformation cubedef = new CubeDeformation(0.0F);
 
-		partdefinition.addOrReplaceChild("Head", CubeListBuilder.create().texOffs(0, 0).mirror().addBox(-3.0F, -3.0F, -2.0F, 6.0F, 6.0F, 4.0F, cubedef).mirror(false)
-		.texOffs(16, 10).mirror().addBox(-5.0F, -2.5F, -6.0F, 2.0F, 2.0F, 6.0F, cubedef).mirror(false)
-		.texOffs(16, 10).mirror().addBox(3.0F, -2.5F, -6.0F, 2.0F, 2.0F, 6.0F, cubedef).mirror(false)
-		.texOffs(16, 14).mirror().addBox(-3.0F, -5.0F, 0.0F, 2.0F, 2.0F, 1.0F, cubedef).mirror(false)
-		.texOffs(16, 14).mirror().addBox(1.0F, -5.0F, 0.0F, 2.0F, 2.0F, 1.0F, cubedef).mirror(false), PartPose.offset(-1.0F, 13.5F, -7.0F));
+		PartDefinition Head = partdefinition.addOrReplaceChild("Head", CubeListBuilder.create().texOffs(0, 0).mirror().addBox(-3.0F, -3.0F, -2.0F, 6.0F, 6.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false)
+				.texOffs(16, 10).mirror().addBox(-5.0F, -2.5F, -6.0F, 2.0F, 2.0F, 6.0F, new CubeDeformation(0.0F)).mirror(false)
+				.texOffs(16, 10).mirror().addBox(3.0F, -2.5F, -6.0F, 2.0F, 2.0F, 6.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-1.0F, 13.5F, -7.0F));
 
 		PartDefinition Body = partdefinition.addOrReplaceChild("Body", CubeListBuilder.create(), PartPose.offset(0.0F, 14.0F, -3.0F));
 
-		Body.addOrReplaceChild("Body_r1", CubeListBuilder.create().texOffs(18, 14).mirror().addBox(-4.0F, -5.0F, -5.0F, 6.0F, 9.0F, 6.0F, cubedef).mirror(false), PartPose.offsetAndRotation(0.0F, -2.0F, 3.0F, 1.5708F, 0.0F, 0.0F));
+		PartDefinition Body_r1 = Body.addOrReplaceChild("Body_r1", CubeListBuilder.create().texOffs(18, 14).mirror().addBox(-4.0F, -4.0F, -13.0F, 6.0F, 9.0F, 6.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(0.0F, 10.0F, 3.0F, -1.5708F, 0.0F, 0.0F));
 
-		partdefinition.addOrReplaceChild("Leg1", CubeListBuilder.create().texOffs(0, 18).mirror().addBox(-1.0F, 0.0F, -0.9F, 2.0F, 8.0F, 2.0F, cubedef).mirror(false), PartPose.offset(-2.5F, 16.0F, 3.0F));
+		PartDefinition Leg1 = partdefinition.addOrReplaceChild("Leg1", CubeListBuilder.create().texOffs(0, 18).mirror().addBox(-1.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-2.5F, 16.0F, 3.0F));
 
-		partdefinition.addOrReplaceChild("Leg2", CubeListBuilder.create().texOffs(0, 18).mirror().addBox(-1.0F, 0.0F, -0.9F, 2.0F, 8.0F, 2.0F, cubedef).mirror(false), PartPose.offset(0.5F, 16.0F, 3.0F));
+		PartDefinition Leg2 = partdefinition.addOrReplaceChild("Leg2", CubeListBuilder.create().texOffs(0, 18).mirror().addBox(-1.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(0.5F, 16.0F, 3.0F));
 
-		partdefinition.addOrReplaceChild("Leg3", CubeListBuilder.create().texOffs(0, 18).mirror().addBox(-1.0F, 0.0F, -1.1F, 2.0F, 8.0F, 2.0F, cubedef).mirror(false), PartPose.offset(-2.5F, 16.0F, -4.0F));
+		PartDefinition Leg3 = partdefinition.addOrReplaceChild("Leg3", CubeListBuilder.create().texOffs(0, 18).mirror().addBox(-1.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-2.5F, 16.0F, -4.0F));
 
-		partdefinition.addOrReplaceChild("Leg4", CubeListBuilder.create().texOffs(0, 18).mirror().addBox(-1.0F, 0.0F, -1.1F, 2.0F, 8.0F, 2.0F, cubedef).mirror(false), PartPose.offset(0.5F, 16.0F, -4.0F));
+		PartDefinition Leg4 = partdefinition.addOrReplaceChild("Leg4", CubeListBuilder.create().texOffs(0, 18).mirror().addBox(-1.0F, 0.0F, -1.0F, 2.0F, 8.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(0.5F, 16.0F, -4.0F));
 
-		partdefinition.addOrReplaceChild("Tail", CubeListBuilder.create().texOffs(6, 0).mirror().addBox(0.0F, 0.0F, 0.0F, 2.0F, 2.0F, 1.0F, cubedef).mirror(false), PartPose.offset(-2.0F, 13.0F, 4.0F));
+		PartDefinition Ear1 = partdefinition.addOrReplaceChild("Ear1", CubeListBuilder.create().texOffs(16, 14).mirror().addBox(-3.0F, -5.0F, 0.0F, 2.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-1.0F, 13.5F, -7.0F));
 
-		partdefinition.addOrReplaceChild("Shape1", CubeListBuilder.create().texOffs(50, 18).mirror().addBox(0.0F, 0.0F, 0.0F, 2.0F, 2.0F, 2.0F, cubedef).mirror(false), PartPose.offset(0.0F, 9.0F, 0.0F));
+		PartDefinition Ear2 = partdefinition.addOrReplaceChild("Ear2", CubeListBuilder.create().texOffs(16, 14).mirror().addBox(1.0F, -5.0F, 0.0F, 2.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-1.0F, 13.5F, -7.0F));
 
-		partdefinition.addOrReplaceChild("Shape2", CubeListBuilder.create().texOffs(50, 18).mirror().addBox(0.0F, 0.0F, 0.0F, 2.0F, 2.0F, 2.0F, cubedef).mirror(false), PartPose.offset(-4.0F, 9.0F, 0.0F));
+		PartDefinition Tail = partdefinition.addOrReplaceChild("Tail", CubeListBuilder.create().texOffs(6, 0).mirror().addBox(0.0F, 0.0F, 0.0F, 2.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-2.0F, 13.0F, 4.0F));
 
-		partdefinition.addOrReplaceChild("Shape3", CubeListBuilder.create().texOffs(50, 18).mirror().addBox(0.0F, 0.0F, 0.0F, 2.0F, 2.0F, 2.0F, cubedef).mirror(false), PartPose.offset(0.0F, 9.0F, -4.0F));
+		PartDefinition Shape1 = partdefinition.addOrReplaceChild("Shape1", CubeListBuilder.create().texOffs(50, 18).mirror().addBox(0.0F, 0.0F, 0.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(0.0F, 9.0F, 0.0F));
 
-		partdefinition.addOrReplaceChild("Shape4", CubeListBuilder.create().texOffs(50, 18).mirror().addBox(0.0F, 0.0F, 0.0F, 2.0F, 2.0F, 2.0F, cubedef).mirror(false), PartPose.offset(-4.0F, 9.0F, -4.0F));
+		PartDefinition Shape2 = partdefinition.addOrReplaceChild("Shape2", CubeListBuilder.create().texOffs(50, 18).mirror().addBox(0.0F, 0.0F, 0.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-4.0F, 9.0F, 0.0F));
+
+		PartDefinition Shape3 = partdefinition.addOrReplaceChild("Shape3", CubeListBuilder.create().texOffs(50, 18).mirror().addBox(0.0F, 0.0F, 0.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(0.0F, 9.0F, -4.0F));
+
+		PartDefinition Shape4 = partdefinition.addOrReplaceChild("Shape4", CubeListBuilder.create().texOffs(50, 18).mirror().addBox(0.0F, 0.0F, 0.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-4.0F, 9.0F, -4.0F));
 
 		return LayerDefinition.create(meshdefinition, 64, 32);
 	}
@@ -91,7 +86,6 @@ public class ModelKarot extends EntityModel<EntityKarot> {
 	public void setupAnim(EntityKarot entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
 		this.Head.yRot = netHeadYaw / (180F / (float) Math.PI);
         this.Head.xRot = headPitch / (180F / (float) Math.PI);
-		this.Body.xRot = ((float)Math.PI / 2F);
         this.Leg1.xRot = (float) (Math.cos(limbSwing * 0.6662) * 1.4 * limbSwingAmount);
         this.Leg2.xRot = (float) (Math.cos(limbSwing * 0.6662 + Math.PI) * 1.4 * limbSwingAmount);
         this.Leg3.xRot = (float) (Math.cos(limbSwing * 0.6662 + Math.PI) * 1.4 * limbSwingAmount);
@@ -106,6 +100,8 @@ public class ModelKarot extends EntityModel<EntityKarot> {
 		Leg2.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 		Leg3.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 		Leg4.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+		Ear1.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+		Ear2.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 		Tail.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 		Shape1.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 		Shape2.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
