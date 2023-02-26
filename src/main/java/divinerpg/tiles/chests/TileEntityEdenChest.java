0d5a8615164a3c11575d5df13bbaa5c92c8a8440
Nicusha <nicosaurusrex99@gmail.com>
2023-02-27 02:59:17 +1030

    chest fixes

--- src/main/java/divinerpg/tiles/chests/TileEntityEdenChest.java
+++ src/main/java/divinerpg/tiles/chests/TileEntityEdenChest.java
@@ -2,15 +2,19 @@ package divinerpg.tiles.chests;
 
 import divinerpg.DivineRPG;
 import divinerpg.registries.BlockEntityRegistry;
-import net.minecraft.core.BlockPos;
+import net.minecraft.core.*;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.ContainerHelper;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.*;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class TileEntityEdenChest extends TileEntityModChest {
 
+    private NonNullList<ItemStack> items;
     public TileEntityEdenChest(BlockPos p_154992_, BlockState p_154993_) {
         super(BlockEntityRegistry.EDEN_CHEST.get(), p_154992_, p_154993_);
     }
@@ -28,4 +32,28 @@ public class TileEntityEdenChest extends TileEntityModChest {
     protected AbstractContainerMenu createMenu(int i, Inventory inv) {
         return ChestMenu.sixRows(i, inv, this);
     }
+
+    public void load(CompoundTag p_155349_) {
+        super.load(p_155349_);
+        this.items = NonNullList.withSize(this.getContainerSize(), ItemStack.EMPTY);
+        if (!this.tryLoadLootTable(p_155349_)) {
+            ContainerHelper.loadAllItems(p_155349_, this.items);
+        }
+
+    }
+    protected void saveAdditional(CompoundTag p_187489_) {
+        super.saveAdditional(p_187489_);
+        if (!this.trySaveLootTable(p_187489_)) {
+            ContainerHelper.saveAllItems(p_187489_, this.items);
+        }
+
+    }
+    protected NonNullList<ItemStack> getItems() {
+        return this.items;
+    }
+
+    protected void setItems(NonNullList<ItemStack> p_59110_) {
+        this.items = p_59110_;
+    }
+
 }
