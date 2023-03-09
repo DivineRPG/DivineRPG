fb9a3a82556d1d6251f018b204b120b752151d08
Nicusha <nicosaurusrex99@gmail.com>
2023-03-10 02:24:38 +1030

    made infusion table not auto craft the second the ingredients are present

--- src/main/java/divinerpg/client/containers/InfusionTableContainer.java
+++ src/main/java/divinerpg/client/containers/InfusionTableContainer.java
@@ -117,18 +117,26 @@ public class InfusionTableContainer extends AbstractContainerMenu {
     }
 
     protected void slotChangedCraftingGrid(Level world, Player player, InfusionInventory inv, ResultContainer craftResult) {
-        Optional<InfusionTableRecipe> recipes = world.getServer().getRecipeManager().getRecipeFor(InfusionTableRecipe.Type.INSTANCE, inv, world);
-        if(recipes.isPresent()) {
-            InfusionTableRecipe recipe = recipes.get();
-            if (recipe.matches(inv, world) && craftResult.getItem(2).isEmpty()) {
-                craftResult.setItem(1, recipe.template.copy());
-                craftResult.setItem(2, recipe.output.copy());
-                ((ServerPlayer)player).connection.send(new ClientboundContainerSetSlotPacket(this.containerId, incrementStateId(), 1, recipe.template.copy()));
-                ((ServerPlayer)player).connection.send(new ClientboundContainerSetSlotPacket(this.containerId, incrementStateId(), 2, recipe.output.copy()));
-                inv.removeItem(0, recipe.getCount());
+        Optional<InfusionTableRecipe> recipeOptional = world.getServer().getRecipeManager().getRecipeFor(InfusionTableRecipe.Type.INSTANCE, inv, world);
+
+        if (recipeOptional.isPresent()) {
+            InfusionTableRecipe recipe = recipeOptional.get();
+
+            if (recipe.matches(inv, world)) {
+                ItemStack output = recipe.output.copy();
+                ItemStack template = recipe.template.copy();
+
+                if (craftResult.getItem(2).isEmpty()) {
+                    craftResult.setItem(1, template);
+                    craftResult.setItem(2, output);
+                    ((ServerPlayer) player).connection.send(new ClientboundContainerSetSlotPacket(this.containerId, incrementStateId(), 1, template));
+                    ((ServerPlayer) player).connection.send(new ClientboundContainerSetSlotPacket(this.containerId, incrementStateId(), 2, output));
+                } else if (craftResult.getItem(2).getItem() == output.getItem() && ItemStack.tagMatches(craftResult.getItem(2), output)) {
+                    inv.removeItem(0, recipe.getCount());
+                    craftResult.getItem(2).grow(output.getCount());
+                }
             }
         }
-
     }
 
     public static void openContainer(ServerPlayer player, BlockPos pos) {
