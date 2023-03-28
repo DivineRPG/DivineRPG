27b2908892f3d99987a66177051a3ca1da86fe67
Nicusha <nicosaurusrex99@gmail.com>
2023-03-28 21:11:38 +1030

    gave the sabear the ability to break stuff

--- src/main/java/divinerpg/entities/iceika/EntitySabear.java
+++ src/main/java/divinerpg/entities/iceika/EntitySabear.java
@@ -1,9 +1,16 @@
 package divinerpg.entities.iceika;
 
 import divinerpg.entities.base.EntityDivineMonster;
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.*;
 
 public class EntitySabear extends EntityDivineMonster {
 
@@ -15,4 +22,49 @@ public class EntitySabear extends EntityDivineMonster {
     public boolean isAggressive() {
         return true;
     }
+
+
+    @Override
+    public void tick() {
+        super.tick();
+
+        if (this.isAlive()) {
+            boolean playerNearby = this.level.getEntitiesOfClass(Player.class, this.getBoundingBox().inflate(8.0D)).size() > 0;
+            if (playerNearby) {
+                Player nearestPlayer = this.level.getNearestPlayer(this, 8.0D);
+                if (nearestPlayer != null) {
+                    Vec3 playerPos = new Vec3(nearestPlayer.getX(), nearestPlayer.getY(), nearestPlayer.getZ());
+                    Vec3 sabearPos = new Vec3(this.getX(), this.getY(), this.getZ());
+                    AABB box = new AABB(playerPos, sabearPos).inflate(1.0D);
+
+                    BlockPos.betweenClosedStream(
+                            new BlockPos((int) box.minX, (int) box.minY, (int) box.minZ),
+                            new BlockPos((int) box.maxX, (int) box.maxY, (int) box.maxZ)
+                    ).forEach((blockPos) -> {
+                        if (blockPos.getY() < this.blockPosition().getY()) {
+                            return;
+                        }
+                        BlockState state = this.level.getBlockState(blockPos);
+                        if (state.getBlock() != Blocks.AIR && state.getBlock().defaultDestroyTime() < 1.0F) {
+                            if (blockPos.equals(this.blockPosition().below())) {
+                                return;
+                            }
+                            this.level.destroyBlock(blockPos, true);
+                            this.playSound(SoundEvents.SNOW_BREAK, 1.0F, 1.0F);
+                        } else if (state.is(BlockTags.ICE)) {
+                            if (blockPos.getY() == this.level.getSeaLevel()) {
+                                this.level.setBlockAndUpdate(blockPos, Blocks.WATER.defaultBlockState());
+                            } else {
+                                if (state.getBlock() != Blocks.AIR && blockPos.getY() > blockPosition().below().getY()) {
+                                    this.level.destroyBlock(blockPos, true);
+                                }
+                            }
+                            this.playSound(SoundEvents.GLASS_BREAK, 1.0F, 1.0F);
+                        }
+                    });
+                }
+            }
+        }
+    }
+
 }
