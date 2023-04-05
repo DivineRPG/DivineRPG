6409c26d6ef7137f4d4c1171f593f8aaf189f23c
Nicusha <nicosaurusrex99@gmail.com>
2023-04-05 12:56:35 +0930

    fixed hostiles fleeing

--- src/main/java/divinerpg/entities/base/EntityDivineMonster.java
+++ src/main/java/divinerpg/entities/base/EntityDivineMonster.java
@@ -15,7 +15,6 @@ public abstract class EntityDivineMonster extends Monster {
     @Override
     protected void registerGoals() {
         goalSelector.addGoal(0, new FloatGoal(this));
-        goalSelector.addGoal(3, new PanicGoal(this, 1.25D));
         goalSelector.addGoal(6, new WaterAvoidingRandomStrollGoal(this, 1.0D));
         goalSelector.addGoal(7, new LookAtPlayerGoal(this, Player.class, 6.0F));
         goalSelector.addGoal(8, new RandomLookAroundGoal(this));
@@ -23,6 +22,9 @@ public abstract class EntityDivineMonster extends Monster {
         if(isAggressive()) {
             goalSelector.addGoal(0, new MeleeAttackGoal(this, 1, true));
             targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, Player.class, true));
+        }
+        if(!isAggressive()){
+            goalSelector.addGoal(3, new PanicGoal(this, 1.25D));
         }
     }
     @Override
