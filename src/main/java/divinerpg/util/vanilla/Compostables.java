c276cb81b347fb958aca3ed926706dae96bfbb78
Nicusha <nicosaurusrex99@gmail.com>
2023-04-08 03:42:53 +0930

    began compostable changes but still has the air bug

--- src/main/java/divinerpg/util/vanilla/Compostables.java
+++ src/main/java/divinerpg/util/vanilla/Compostables.java
@@ -1,97 +1,104 @@
 package divinerpg.util.vanilla;
 
-import divinerpg.registries.ItemRegistry;
-import net.minecraft.world.level.block.ComposterBlock;
+import divinerpg.DivineRPG;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.*;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-
-import static divinerpg.registries.BlockRegistry.*;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class Compostables {
 
     public static void setup(final FMLCommonSetupEvent event) {
         // Compostable Items (Some of these Floats should be raised in accordance with the Item)
-        ComposterBlock.COMPOSTABLES.put(divineLeaves.get().asItem(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(edenLeaves.get().asItem(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(wildwoodLeaves.get().asItem(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(apalachiaLeaves.get().asItem(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(skythernLeaves.get().asItem(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(mortumLeaves.get().asItem(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(brittleLeaves.get().asItem(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(edenSapling.get().asItem(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(wildwoodSapling.get().asItem(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(apalachiaSapling.get().asItem(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(skythernSapling.get().asItem(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(mortumSapling.get().asItem(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(brittleGrass.get().asItem(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(frozenSapling.get().asItem(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(divineSapling.get().asItem(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(ItemRegistry.aquamarine_seeds.get(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(ItemRegistry.eucalyptus_root_seeds.get(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(ItemRegistry.firestock_seeds.get(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(ItemRegistry.hitchak_seeds.get(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(ItemRegistry.lamona_seeds.get(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(ItemRegistry.marsine_seeds.get(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(ItemRegistry.moonbulb_seeds.get(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(ItemRegistry.pinfly_seeds.get(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(ItemRegistry.pink_glowbone_seeds.get(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(ItemRegistry.purple_glowbone_seeds.get(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(ItemRegistry.sky_plant_seeds.get(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(ItemRegistry.tomato_seeds.get(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(ItemRegistry.veilo_seeds.get(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(ItemRegistry.white_mushroom_seeds.get(), 0.3F);
-        ComposterBlock.COMPOSTABLES.put(ItemRegistry.winterberry.get(), 0.3F);
+        add(0.3F, "divine_leaves");
+        add(0.3F, "eden_leaves");
+        add(0.3F, "wildwood_leaves");
+        add(0.3F, "apalachia_leaves");
+        add(0.3F, "skythern_leaves");
+        add(0.3F, "mortum_leaves");
+        add(0.3F, "brittle_leaves");
+        add(0.3F, "eden_sapling");
+        add(0.3F, "wildwood_sapling");
+        add(0.3F, "apalachia_sapling");
+        add(0.3F, "skythern_sapling");
+        add(0.3F, "mortum_sapling");
+        add(0.3F, "brittle_grass");
+        add(0.3F, "frozen_sapling");
+        add(0.3F, "divine_sapling");
+        add(0.3F, "aquamarine_seeds");
+        add(0.3F, "eucalyptus_root_seeds");
+        add(0.3F, "firestock_seeds");
+        add(0.3F, "hitchak_seeds");
+        add(0.3F, "lamona_seeds");
+        add(0.3F, "marsine_seeds");
+        add(0.3F, "moonbulb_seeds");
+        add(0.3F, "pinfly_seeds");
+        add(0.3F, "pink_glowbone_seeds");
+        add(0.3F, "purple_glowbone_seeds");
+        add(0.3F, "sky_plant_seeds");
+        add(0.3F, "tomato_seeds");
+        add(0.3F, "veilo_seeds");
+        add(0.3F, "white_mushroom_seeds");
+        add(0.3F, "winterberry");
 
-        ComposterBlock.COMPOSTABLES.put(winterberryVinesHead.get().asItem(), 0.5F);
-        ComposterBlock.COMPOSTABLES.put(winterberryVinesBody.get().asItem(), 0.5F);
-        ComposterBlock.COMPOSTABLES.put(arcaniteVinesHead.get().asItem(), 0.5F);
-        ComposterBlock.COMPOSTABLES.put(arcaniteVinesBody.get().asItem(), 0.5F);
-        ComposterBlock.COMPOSTABLES.put(ItemRegistry.dream_melon.get(), 0.5F);
+        add(0.5F, "winterberry_vines_head");
+        add(0.5F, "arcanite_vines_head");
+        add(0.5F, "dream_melon");
 
-        ComposterBlock.COMPOSTABLES.put(blazePumpkin.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(creeperPumpkin.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(cyclopsPumpkin.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(enderPumpkin.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(enderWatcherPumpkin.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(frostPumpkin.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(ghastPumpkin.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(glaconPumpkin.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(hellspiderPumpkin.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(jungleSpiderPumpkin.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(skeletonPumpkin.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(spiderPumpkin.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(zombiePumpkin.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(bulbatobe.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(cracklespike.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(dreamglow.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(fernite.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(greenDulah.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(greenGemtop.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(purpleGemtop.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(shimmer.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(shineGrass.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(yellowDulah.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(arcanaBrush.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(arcanaBush.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(gemOfTheDunes.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(demonBrambles.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(eyePlant.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(mortumBrush.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(dustBrambles.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(duskFlower.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(moonBud.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(moonlightFern.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(sunBlossom.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(sunbloom.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(edenBrush.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(winterberryBush.get().asItem(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(ItemRegistry.dream_carrot.get(), 0.65F);
-        ComposterBlock.COMPOSTABLES.put(ItemRegistry.tomato.get(), 0.65F);
+        add(0.65F, "blaze_pumpkin");
+        add(0.65F, "creeper_pumpkin");
+        add(0.65F, "cyclops_pumpkin");
+        add(0.65F, "ender_pumpkin");
+        add(0.65F, "ender_watcher_pumpkin");
+        add(0.65F, "frost_pumpkin");
+        add(0.65F, "ghast_pumpkin");
+        add(0.65F, "glacon_pumpkin");
+        add(0.65F, "hellspider_pumpkin");
+        add(0.65F, "jungle_spider_pumpkin");
+        add(0.65F, "skeleton_pumpkin");
+        add(0.65F, "spider_pumpkin");
+        add(0.65F, "zombie_pumpkin");
+        add(0.65F, "bulbatobe");
+        add(0.65F, "cracklespike");
+        add(0.65F, "dreamglow");
+        add(0.65F, "fernite");
+        add(0.65F, "green_dulah");
+        add(0.65F, "green_gemtop");
+        add(0.65F, "purple_gemtop");
+        add(0.65F, "shimmer");
+        add(0.65F, "shine_grass");
+        add(0.65F, "yellow_dulah");
+        add(0.65F, "arcana_brush");
+        add(0.65F, "arcana_bush");
+        add(0.65F, "gem_of_the_dunes");
+        add(0.65F, "demon_brambles");
+        add(0.65F, "eye_plant");
+        add(0.65F, "mortum_brush");
+        add(0.65F, "dust_brambles");
+        add(0.65F, "dusk_flower");
+        add(0.65F, "moon_bud");
+        add(0.65F, "moonlight_fern");
+        add(0.65F, "sun_blossom");
+        add(0.65F, "sunbloom");
+        add(0.65F, "eden_brush");
+        add(0.65F, "winterberry_bush");
+        add(0.65F, "dream_carrot");
+        add(0.65F, "tomato");
 
-        ComposterBlock.COMPOSTABLES.put(ItemRegistry.donut.get(), 0.85F);
+        add(0.85F, "donut");
 
-        ComposterBlock.COMPOSTABLES.put(ItemRegistry.dream_cake.get(), 1.0F);
-        ComposterBlock.COMPOSTABLES.put(ItemRegistry.dream_pie.get(), 1.0F);
-        ComposterBlock.COMPOSTABLES.put(ItemRegistry.fruit_cake.get(), 1.0F);
-        ComposterBlock.COMPOSTABLES.put(ItemRegistry.hot_pumpkin_pie.get(), 1.0F);
+        add(1.0F, "dream_cake");
+        add(1.0F, "dream_pie");
+        add(1.0F, "fruit_cake");
+        add(1.0F, "hot_pumpkin_pie");
+
+    }
+
+    public static void add(float quantity, String itemId) {
+        DivineRPG.LOGGER.info("tried to add " + itemId);
+        Item item = ForgeRegistries.ITEMS.getValue(new ResourceLocation(DivineRPG.MODID, itemId));
+        DivineRPG.LOGGER.info("id: " + itemId + ". descriptionID: " + item.getDescriptionId());
+        ComposterBlock.add(quantity, item);
     }
 }
