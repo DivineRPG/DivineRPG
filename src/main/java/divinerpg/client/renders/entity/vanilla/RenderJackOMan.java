9747ca9482cb30761097a4e364ae4dbdb9e7c5e8
Nicusha <nicosaurusrex99@gmail.com>
2023-04-11 23:46:29 +0930

    Fixed Jack O Man model

--- src/main/java/divinerpg/client/renders/entity/vanilla/RenderJackOMan.java
+++ src/main/java/divinerpg/client/renders/entity/vanilla/RenderJackOMan.java
@@ -4,19 +4,18 @@ import divinerpg.DivineRPG;
 import divinerpg.client.renders.layer.*;
 import divinerpg.entities.vanilla.overworld.EntityJackOMan;
 import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.entity.EntityRendererProvider.Context;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.*;
-
-import static divinerpg.util.ClientUtils.layerHumanoid;
 
 @OnlyIn(Dist.CLIENT)
 public class RenderJackOMan extends MobRenderer<EntityJackOMan, HumanoidModel<EntityJackOMan>> {
     private static final ResourceLocation TEXTURE = new ResourceLocation(DivineRPG.MODID, "textures/entity/jack_o_man.png");
 
     public RenderJackOMan(Context context) {
-        super(context, new HumanoidModel<EntityJackOMan>(context.bakeLayer(layerHumanoid)), 0.8F);
+        super(context, new HumanoidModel<EntityJackOMan>(context.bakeLayer(ModelLayers.PLAYER)), 0.8F);
         this.addLayer(new JackOManHeadLayer(this));
         this.addLayer(new JackOManScytheLayer(this));
     }
