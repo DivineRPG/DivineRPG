04382bf00afdceb4da2ac21035a9bf4721241868
PegasDoggeAM <Shaipple@gmail.com>
2023-04-26 11:48:20 +1100

    Some recipe/tag additions

--- src/main/java/divinerpg/blocks/base/BlockModLadder.java
+++ src/main/java/divinerpg/blocks/base/BlockModLadder.java
@@ -6,11 +6,10 @@ import net.minecraft.world.level.material.*;
 public class BlockModLadder extends LadderBlock {
     public BlockModLadder() {
         super(Block.Properties.of(Material.WOOD)
-                .strength(0.4F, 3.0F)
+                .strength(0.4F)
                 .sound(SoundType.LADDER)
                 .dynamicShape()
-                .noOcclusion()
-                .requiresCorrectToolForDrops());
+                .noOcclusion());
     }
     public BlockModLadder(Properties properties) {
         super(properties);
