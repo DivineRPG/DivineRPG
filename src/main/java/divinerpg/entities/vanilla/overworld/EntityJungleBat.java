a9532af6b04491f18353d70ff2a63e0a0c038ae8
Nicusha <nicosaurusrex99@gmail.com>
2023-05-02 01:14:02 +0930

    Fixed lazy bats resting when they shouldnt

--- src/main/java/divinerpg/entities/vanilla/overworld/EntityJungleBat.java
+++ src/main/java/divinerpg/entities/vanilla/overworld/EntityJungleBat.java
@@ -1,34 +1,17 @@
 package divinerpg.entities.vanilla.overworld;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.syncher.*;
 import net.minecraft.sounds.*;
-import net.minecraft.util.Mth;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.*;
-import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import net.minecraft.world.entity.ambient.Bat;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.Vec3;
 
 import javax.annotation.Nullable;
 
 public class EntityJungleBat extends Bat {
-    private static final EntityDataAccessor<Byte> DATA_ID_FLAGS = SynchedEntityData.defineId(EntityJungleBat.class, EntityDataSerializers.BYTE);
-    private static final TargetingConditions BAT_RESTING_TARGETING = TargetingConditions.forCombat().range(4.0D); //TODO .allowSameTeam()
-    private BlockPos targetPosition;
 
-    public EntityJungleBat(EntityType<? extends Bat> p_i50290_1_, Level p_i50290_2_) {
-        super(p_i50290_1_, p_i50290_2_);
-        this.setResting(true);
-    }
-
-    protected void defineSynchedData() {
-        super.defineSynchedData();
-        this.entityData.define(DATA_ID_FLAGS, (byte)0);
+    public EntityJungleBat(EntityType<? extends Bat> type, Level level) {
+        super(type, level);
     }
 
     protected float getSoundVolume() {
@@ -52,126 +35,6 @@ public class EntityJungleBat extends Bat {
         return SoundEvents.BAT_DEATH;
     }
 
-    public boolean isPushable() {
-        return false;
-    }
-
-    protected void doPush(Entity p_82167_1_) {
-    }
-
-    protected void pushEntities() {
-    }
-
-    public boolean isResting() {
-        return (this.entityData.get(DATA_ID_FLAGS) & 1) != 0;
-    }
-
-    public void setResting(boolean p_82236_1_) {
-        byte b0 = this.entityData.get(DATA_ID_FLAGS);
-        if (p_82236_1_) {
-            this.entityData.set(DATA_ID_FLAGS, (byte)(b0 | 1));
-        } else {
-            this.entityData.set(DATA_ID_FLAGS, (byte)(b0 & -2));
-        }
-
-    }
-
-    public void tick() {
-        super.tick();
-        if (this.isResting()) {
-            this.setDeltaMovement(Vec3.ZERO);
-            this.setPosRaw(this.getX(), (double)Mth.floor(this.getY()) + 1.0D - (double)this.getBbHeight(), this.getZ());
-        } else {
-            this.setDeltaMovement(this.getDeltaMovement().multiply(1.0D, 0.6D, 1.0D));
-        }
-
-    }
-
-    protected void customServerAiStep() {
-        super.customServerAiStep();
-        BlockPos blockpos = this.blockPosition();
-        BlockPos blockpos1 = blockpos.above();
-        if (this.isResting()) {
-            boolean flag = this.isSilent();
-            if (this.level.getBlockState(blockpos1).isRedstoneConductor(this.level, blockpos)) {
-                if (this.random.nextInt(200) == 0) {
-                    this.yHeadRot = (float)this.random.nextInt(360);
-                }
-
-                if (this.level.getNearestPlayer(BAT_RESTING_TARGETING, this) != null) {
-                    this.setResting(false);
-                    if (!flag) {
-                        this.level.levelEvent((Player)null, 1025, blockpos, 0);
-                    }
-                }
-            } else {
-                this.setResting(false);
-                if (!flag) {
-                    this.level.levelEvent((Player)null, 1025, blockpos, 0);
-                }
-            }
-        } else {
-            if (this.targetPosition != null && (!this.level.isEmptyBlock(this.targetPosition) || this.targetPosition.getY() < 1)) {
-                this.targetPosition = null;
-            }
-
-            if (this.targetPosition == null || this.random.nextInt(30) == 0 || this.targetPosition.closerToCenterThan(this.position(), 2.0D)) {
-                this.targetPosition = new BlockPos((int) (this.getX() + (double)this.random.nextInt(7) - (double)this.random.nextInt(7)), (int) (this.getY() + (double)this.random.nextInt(6) - 2.0D), (int) (this.getZ() + (double)this.random.nextInt(7) - (double)this.random.nextInt(7)));
-            }
-
-            double d2 = (double)this.targetPosition.getX() + 0.5D - this.getX();
-            double d0 = (double)this.targetPosition.getY() + 0.1D - this.getY();
-            double d1 = (double)this.targetPosition.getZ() + 0.5D - this.getZ();
-            Vec3 vector3d = this.getDeltaMovement();
-            Vec3 vector3d1 = vector3d.add((Math.signum(d2) * 0.5D - vector3d.x) * (double)0.1F, (Math.signum(d0) * (double)0.7F - vector3d.y) * (double)0.1F, (Math.signum(d1) * 0.5D - vector3d.z) * (double)0.1F);
-            this.setDeltaMovement(vector3d1);
-            float f = (float)(Mth.atan2(vector3d1.z, vector3d1.x) * (double)(180F / (float)Math.PI)) - 90.0F;
-            float f1 = Mth.wrapDegrees(f - this.yRot);
-            this.zza = 0.5F;
-            this.yRot += f1;
-            if (this.random.nextInt(100) == 0 && this.level.getBlockState(blockpos1).isRedstoneConductor(this.level, blockpos1)) {
-                this.setResting(true);
-            }
-        }
-
-    }
-
-    protected boolean isMovementNoisy() {
-        return false;
-    }
-
-    public boolean causeFallDamage(float p_225503_1_, float p_225503_2_) {
-        return false;
-    }
-
-    protected void checkFallDamage(double p_184231_1_, boolean p_184231_3_, BlockState p_184231_4_, BlockPos p_184231_5_) {
-    }
-
-    public boolean isIgnoringBlockTriggers() {
-        return true;
-    }
-
-    public boolean hurt(DamageSource p_70097_1_, float p_70097_2_) {
-        if (this.isInvulnerableTo(p_70097_1_)) {
-            return false;
-        } else {
-            if (!this.level.isClientSide && this.isResting()) {
-                this.setResting(false);
-            }
-
-            return super.hurt(p_70097_1_, p_70097_2_);
-        }
-    }
-
-    public void readAdditionalSaveData(CompoundTag p_70037_1_) {
-        super.readAdditionalSaveData(p_70037_1_);
-        this.entityData.set(DATA_ID_FLAGS, p_70037_1_.getByte("BatFlags"));
-    }
-
-    public void addAdditionalSaveData(CompoundTag p_213281_1_) {
-        super.addAdditionalSaveData(p_213281_1_);
-        p_213281_1_.putByte("BatFlags", this.entityData.get(DATA_ID_FLAGS));
-    }
 
     protected float getStandingEyeHeight(Pose poseIn, EntityDimensions sizeIn) {
         return sizeIn.height / 2.0F;
