db0025b3c65f429bb265c26dd6b10b147e17d0cc
Nicusha <nicosaurusrex99@gmail.com>
2023-05-06 17:43:13 +0930

    change to vethea inventory naming

--- src/main/java/divinerpg/events/VetheaInventorySwapEvent.java
+++ src/main/java/divinerpg/events/VetheaInventorySwapEvent.java
@@ -7,7 +7,7 @@ import net.minecraftforge.event.entity.player.*;
 import net.minecraftforge.eventbus.api.*;
 
 public class VetheaInventorySwapEvent {
-	public static final String OVERWORLD_INVENTORY = "divinerpg:OvWorldInv", VETHEA_INVENTORY = "divinerpg:DreamInv";
+	public static final String OVERWORLD_INVENTORY = "OvWorldInv", VETHEA_INVENTORY = "DreamInv", MODID_SEPERATOR = "divinerpg:";
     private CompoundTag persistentData;
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
@@ -15,17 +15,17 @@ public class VetheaInventorySwapEvent {
         if(!event.isCanceled() && event.getEntity() instanceof Player && persistentData != null) {
         	Player playerIn = event.getEntity();
         	if(event.getEntity().level.dimension() == LevelRegistry.VETHEA) {
-        		if(!persistentData.getList(VETHEA_INVENTORY, 10).isEmpty()) {
-                    ListTag inv = persistentData.getList(VETHEA_INVENTORY, 10);
+        		if(!persistentData.getList(MODID_SEPERATOR + playerIn.getStringUUID() + "_" + VETHEA_INVENTORY, 10).isEmpty()) {
+                    ListTag inv = persistentData.getList(MODID_SEPERATOR + playerIn.getStringUUID() + "_" + VETHEA_INVENTORY, 10);
                     playerIn.inventory.load(inv);
                     playerIn.inventoryMenu.broadcastChanges();
-                    persistentData.getList(VETHEA_INVENTORY, 10).clear();
+                    persistentData.getList(MODID_SEPERATOR + playerIn.getStringUUID() + "_" + VETHEA_INVENTORY, 10).clear();
         		}
-        	} else if(!persistentData.getList(OVERWORLD_INVENTORY, 10).isEmpty()) {
-                ListTag inv = persistentData.getList(OVERWORLD_INVENTORY, 10);
+        	} else if(!persistentData.getList(MODID_SEPERATOR + playerIn.getStringUUID() + "_" + OVERWORLD_INVENTORY, 10).isEmpty()) {
+                ListTag inv = persistentData.getList(MODID_SEPERATOR + playerIn.getStringUUID() + "_" + OVERWORLD_INVENTORY, 10);
                 playerIn.inventory.load(inv);
                 playerIn.inventoryMenu.broadcastChanges();
-                persistentData.getList(OVERWORLD_INVENTORY, 10).clear();
+                persistentData.getList(MODID_SEPERATOR + playerIn.getStringUUID() + "_" + OVERWORLD_INVENTORY, 10).clear();
             }
         	ArmorAbilitiesEvent.updateAbilities(playerIn);
         }
@@ -36,22 +36,22 @@ public class VetheaInventorySwapEvent {
 	        Player playerIn = event.getEntity();
 	        persistentData = playerIn.getPersistentData().getCompound(Player.PERSISTED_NBT_TAG);
 	        if(event.getFrom() == LevelRegistry.VETHEA) {
-	            persistentData.put(VETHEA_INVENTORY, playerIn.inventory.save(new ListTag()));
+	            persistentData.put(MODID_SEPERATOR + playerIn.getStringUUID() + "_" + VETHEA_INVENTORY, playerIn.inventory.save(new ListTag()));
 	            playerIn.getPersistentData().put("PlayerPersisted", persistentData);
 	            playerIn.inventory.clearContent();
-	            ListTag inv = persistentData.getList(OVERWORLD_INVENTORY, 10);
+	            ListTag inv = persistentData.getList(MODID_SEPERATOR + playerIn.getStringUUID() + "_" + OVERWORLD_INVENTORY, 10);
 	            playerIn.inventory.load(inv);
 	            playerIn.inventoryMenu.broadcastChanges();
-	            persistentData.getList(OVERWORLD_INVENTORY, 10).clear();
+	            persistentData.getList(MODID_SEPERATOR + playerIn.getStringUUID() + "_" + OVERWORLD_INVENTORY, 10).clear();
 	            playerIn.removeAllEffects();
 	        } else if(event.getTo() == LevelRegistry.VETHEA) {
-	        	persistentData.put(OVERWORLD_INVENTORY, playerIn.inventory.save(new ListTag()));
+	        	persistentData.put(MODID_SEPERATOR + playerIn.getStringUUID() + "_" + OVERWORLD_INVENTORY, playerIn.inventory.save(new ListTag()));
 	            playerIn.getPersistentData().put("PlayerPersisted", persistentData);
 	            playerIn.inventory.clearContent();
-	            ListTag inv = persistentData.getList(VETHEA_INVENTORY, 10);
+	            ListTag inv = persistentData.getList(MODID_SEPERATOR + playerIn.getStringUUID() + "_" + VETHEA_INVENTORY, 10);
 	            playerIn.inventory.load(inv);
 	            playerIn.inventoryMenu.broadcastChanges();
-	            persistentData.getList(VETHEA_INVENTORY, 10).clear();
+	            persistentData.getList(MODID_SEPERATOR + playerIn.getStringUUID() + "_" + VETHEA_INVENTORY, 10).clear();
 	            playerIn.removeAllEffects();
 	        }
 	        ArmorAbilitiesEvent.updateAbilities(playerIn);
