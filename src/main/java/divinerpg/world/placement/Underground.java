14ad63f32ed25f2260225c2f22dacedc51746559
velocityraptor-real <vilim@ziborski.net>
2023-06-16 12:48:07 +0200

    Village improvements
    - improved and fixed issues with gruzzorlug village generation
    - fixed the underground placement modifier

--- src/main/java/divinerpg/world/placement/Underground.java
+++ src/main/java/divinerpg/world/placement/Underground.java
@@ -4,20 +4,28 @@ import com.mojang.serialization.Codec;
 
 import divinerpg.registries.PlacementModifierRegistry;
 import net.minecraft.core.*;
+import net.minecraft.core.BlockPos.MutableBlockPos;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.placement.*;
 
 public class Underground extends PlacementFilter {
 	private static final Underground INSTANCE = new Underground();
-	public static Codec<Underground> CODEC = Codec.unit(() -> INSTANCE);
+	public static final Codec<Underground> CODEC = Codec.unit(() -> INSTANCE);
 	private Underground() {}
 	public static Underground underground() {return INSTANCE;}
 	protected boolean shouldPlace(PlacementContext context, RandomSource source, BlockPos pos) {
 		return isUnderground(context.getLevel(), pos);
 	}
 	public static boolean isUnderground(LevelAccessor level, BlockPos pos) {
-		return !level.canSeeSkyFromBelowWater(pos.above());
+		MutableBlockPos mut = pos.mutable();
+		while(mut.getY() < level.getMaxBuildHeight() - 1) {
+			BlockState state = level.getBlockState(mut.move(Direction.UP));
+			if(state.isCollisionShapeFullBlock(level, mut) && !state.is(BlockTags.LEAVES)) return true;
+		}
+		return false;
 	}
 	public PlacementModifierType<?> type() {
 		return PlacementModifierRegistry.UNDERGROUND;
