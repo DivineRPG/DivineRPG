80bec9870d93feb1273747c0a32c6ffb1ee3c947
Nicusha <nicosaurusrex99@gmail.com>
2023-06-30 16:03:23 +0930

    Update EntityFrosty.java

--- src/main/java/divinerpg/entities/iceika/EntityFrosty.java
+++ src/main/java/divinerpg/entities/iceika/EntityFrosty.java
@@ -1,14 +1,14 @@
 package divinerpg.entities.iceika;
 
 import divinerpg.entities.base.EntityPeacefulUntilAttacked;
-import divinerpg.registries.*;
-
+import divinerpg.registries.SoundRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.util.Mth;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.*;
 import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.*;
 
 public class EntityFrosty extends EntityPeacefulUntilAttacked {
@@ -23,6 +23,11 @@ public class EntityFrosty extends EntityPeacefulUntilAttacked {
     public boolean doHurtTarget(Entity entity) {
         boolean attack = super.doHurtTarget(entity);
         if (attack && entity instanceof LivingEntity) {
+            if(entity instanceof Player player){
+                if(player.isCreative() || player.isSpectator()) {
+                    return false;
+                }
+            }
             ((LivingEntity) entity).addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 100, 2, true, false));
             entity.setDeltaMovement(-Mth.sin(this.xRot * (float) Math.PI / 180.0F) * 2.5 * 0.5F, 0.1D,
                     Mth.cos(this.xRot * (float) Math.PI / 180.0F) * 2.5 * 0.5F);
