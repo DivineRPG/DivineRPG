2558cebc39e5de544559cc092b0b43e25406932c
Nicusha <nicosaurusrex99@gmail.com>
2023-07-17 20:40:45 +0930

    more sir_deccy paintings

--- src/main/java/divinerpg/registries/PaintingRegistry.java
+++ src/main/java/divinerpg/registries/PaintingRegistry.java
@@ -7,10 +7,11 @@ import static divinerpg.DivineRPG.MODID;
 
 public class PaintingRegistry {
     public static final DeferredRegister<PaintingVariant> PAINTING_VARIANTS = DeferredRegister.create(ForgeRegistries.PAINTING_VARIANTS, MODID);
-    public static RegistryObject<PaintingVariant> ISLAND = PAINTING_VARIANTS.register("island",()-> new PaintingVariant(64,64));
+    public static RegistryObject<PaintingVariant> FALL = PAINTING_VARIANTS.register("fall",()-> new PaintingVariant(32,32));
     public static RegistryObject<PaintingVariant> MOONLIGHT_RAVE = PAINTING_VARIANTS.register("moonlight_rave",()-> new PaintingVariant(32,16));
     public static RegistryObject<PaintingVariant> GRAZING = PAINTING_VARIANTS.register("grazing",()-> new PaintingVariant(16,16));
-    public static RegistryObject<PaintingVariant> MONSTER = PAINTING_VARIANTS.register("monster",()-> new PaintingVariant(32,32));
+    public static RegistryObject<PaintingVariant> DISTURBED = PAINTING_VARIANTS.register("disturbed",()-> new PaintingVariant(16,32));
     public static RegistryObject<PaintingVariant> LEVELS = PAINTING_VARIANTS.register("levels",()-> new PaintingVariant(32,32));
-    public static RegistryObject<PaintingVariant> PINK_ELEPHANT = PAINTING_VARIANTS.register("pink_elephant",()-> new PaintingVariant(96,96));
+    public static RegistryObject<PaintingVariant> ICE_AGE = PAINTING_VARIANTS.register("ice_age",()-> new PaintingVariant(64,64));
+    public static RegistryObject<PaintingVariant> CRAWLING = PAINTING_VARIANTS.register("crawling",()-> new PaintingVariant(16,16));
 }
