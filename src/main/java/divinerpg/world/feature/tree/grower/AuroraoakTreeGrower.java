72dc03de859343a4416117676d574eda9b7e581b
velocityraptor-real <vilim@ziborski.net>
2023-07-26 22:33:10 +0200

    auroraoak blocks
    door and trapdoor textures are missing
    auroraoak tree feature not yet coded

---
+++ src/main/java/divinerpg/world/feature/tree/grower/AuroraoakTreeGrower.java
@@ -0,0 +1,16 @@
+package divinerpg.world.feature.tree.grower;
+
+import org.jetbrains.annotations.Nullable;
+
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.grower.AbstractTreeGrower;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+
+public class AuroraoakTreeGrower extends AbstractTreeGrower {
+    @Nullable
+    @Override
+    protected ResourceKey<ConfiguredFeature<?, ?>> getConfiguredFeature(RandomSource p_222910_, boolean p_222911_) {
+        return null;
+    }
+}
