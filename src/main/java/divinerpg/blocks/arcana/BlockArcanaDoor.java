f98f6076916710a2ff9624ee8d25c4ff8aab4eba
PegasDoggeAM <Shaipple@gmail.com>
2023-07-26 18:29:19 +1100

    Fixed some stuff related to Arcana doors

--- src/main/java/divinerpg/blocks/arcana/BlockArcanaDoor.java
+++ src/main/java/divinerpg/blocks/arcana/BlockArcanaDoor.java
@@ -2,6 +2,7 @@ package divinerpg.blocks.arcana;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.*;
 import net.minecraft.world.*;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
@@ -14,7 +15,7 @@ import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class BlockArcanaDoor extends DoorBlock {
-    private ResourceLocation keyItem;
+    private final ResourceLocation keyItem;
 
     public BlockArcanaDoor(MapColor color, ResourceLocation key) {
         super(BlockBehaviour.Properties.of().mapColor(color).strength(-1.0F, 3600000.0F).noOcclusion().instrument(NoteBlockInstrument.BASEDRUM), BlockSetType.STONE);
@@ -43,18 +44,40 @@ public class BlockArcanaDoor extends DoorBlock {
     @Override
     public InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult rayTraceResult) {
         ItemStack itemstack = player.getItemInHand(hand);
+        BlockState iblockstate = pos.equals(pos.below()) ? state : world.getBlockState(pos.below());
         Item key = ForgeRegistries.ITEMS.getValue(keyItem);
 
         if (!player.isCreative() && itemstack.getItem() != key) {
             return InteractionResult.FAIL;
         }
 
+        if (!player.isCreative()) {
+            if (iblockstate.getValue(OPEN).equals(true)) {
+                return InteractionResult.FAIL;
+            }
+
+            if (itemstack.getItem() != key) {
+                return InteractionResult.FAIL;
+            }
+            itemstack.shrink(1);
+        }
+
         world.setBlockAndUpdate(pos, state.cycle(BlockStateProperties.OPEN));
         world.levelEvent(player, state.getValue(BlockStateProperties.OPEN) ? 1005 : 1011, pos, 0);
+        if (state.getValue(OPEN)) {
+            world.playSound(player, pos, SoundEvents.IRON_DOOR_CLOSE, SoundSource.BLOCKS, 1.0F, 0.8F);
+        }
+
+        if (!state.getValue(OPEN)) {
+            world.playSound(player, pos, SoundEvents.IRON_DOOR_OPEN, SoundSource.BLOCKS, 1.0F, 0.8F);
+        }
 
         updateAdjacentDoors(world, pos, player, state);
 
         return InteractionResult.SUCCESS;
     }
 
+    @Override
+    public void neighborChanged(BlockState state, Level level, BlockPos pos, Block neighborBlock, BlockPos neighborPosition, boolean isPowered) {
+    }
 }
