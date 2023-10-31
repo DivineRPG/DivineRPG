f9ed656e0d8f767c974b364d90ee1beb6947c3f0
PegasDoggeAM <Shaipple@gmail.com>
2023-10-31 20:39:08 +1100

    Textures

--- src/main/java/divinerpg/blocks/base/BlockModGate.java
+++ src/main/java/divinerpg/blocks/base/BlockModGate.java
@@ -1,7 +1,9 @@
 package divinerpg.blocks.base;
 
+import net.minecraft.core.*;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.*;
-import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.*;
 import net.minecraft.world.level.block.state.properties.WoodType;
 import net.minecraft.world.level.material.MapColor;
 
@@ -10,4 +12,13 @@ public class BlockModGate extends FenceGateBlock {
         super(BlockBehaviour.Properties.copy(Blocks.OAK_FENCE_GATE).mapColor(color), type);
     }
 
+    @Override
+    public int getFlammability(BlockState state, BlockGetter getter, BlockPos pos, Direction face) {
+        return 20;
+    }
+
+    @Override
+    public int getFireSpreadSpeed(BlockState state, BlockGetter getter, BlockPos pos, Direction face) {
+        return 5;
+    }
 }
