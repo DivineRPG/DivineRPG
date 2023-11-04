1a0e0621592cd0b004075739bc47bfa15bd2939c
PegasDoggeAM <Shaipple@gmail.com>
2023-11-04 22:18:12 +1100

    Melee weapon stats rebalance

--- src/main/java/divinerpg/items/iceika/ItemGlacialBlade.java
+++ src/main/java/divinerpg/items/iceika/ItemGlacialBlade.java
@@ -2,10 +2,14 @@ package divinerpg.items.iceika;
 
 import divinerpg.enums.ToolStats;
 import divinerpg.items.base.ItemModSword;
+import divinerpg.util.LocalizeUtils;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.effect.*;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+
+import java.util.List;
 
 public class ItemGlacialBlade extends ItemModSword {
     public ItemGlacialBlade() {
@@ -21,4 +25,8 @@ public class ItemGlacialBlade extends ItemModSword {
         return super.hurtEnemy(stack, target, attacker);
     }
 
+    @Override
+    protected void addAdditionalInformation(List<Component> list) {
+        list.add(LocalizeUtils.slow(1.6));
+    }
 }
