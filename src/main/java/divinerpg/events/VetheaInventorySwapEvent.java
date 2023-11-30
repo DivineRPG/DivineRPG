bf314aab6d4fd02fe4cf219d23ab54c6c19360be
Nicusha <nicosaurusrex99@gmail.com>
2023-11-30 16:13:19 +1030

    safer vethea inventory

--- src/main/java/divinerpg/events/VetheaInventorySwapEvent.java
+++ src/main/java/divinerpg/events/VetheaInventorySwapEvent.java
@@ -1,58 +1,79 @@
 package divinerpg.events;
 
-import divinerpg.registries.*;
+import divinerpg.registries.LevelRegistry;
 import net.minecraft.nbt.*;
-import net.minecraft.world.entity.player.*;
-import net.minecraftforge.event.entity.player.*;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.entity.EntityTravelToDimensionEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.*;
 
 public class VetheaInventorySwapEvent {
-	public static final String OVERWORLD_INVENTORY = "divinerpg:OvWorldInv", VETHEA_INVENTORY = "divinerpg:DreamInv";
-    private CompoundTag persistentData;
-
-    @SubscribeEvent(priority = EventPriority.LOWEST)
-    public void onRespawn(PlayerEvent.PlayerRespawnEvent event) {
-        if(!event.isCanceled() && event.getEntity() instanceof Player && persistentData != null) {
-        	Player playerIn = event.getEntity();
-        	if(event.getEntity().level.dimension() == LevelRegistry.VETHEA) {
-        		if(!persistentData.getList(VETHEA_INVENTORY, 10).isEmpty()) {
-                    ListTag inv = persistentData.getList(VETHEA_INVENTORY, 10);
-                    playerIn.inventory.load(inv);
-                    playerIn.inventoryMenu.broadcastChanges();
-                    persistentData.getList(VETHEA_INVENTORY, 10).clear();
-        		}
-        	} else if(!persistentData.getList(OVERWORLD_INVENTORY, 10).isEmpty()) {
-                ListTag inv = persistentData.getList(OVERWORLD_INVENTORY, 10);
-                playerIn.inventory.load(inv);
-                playerIn.inventoryMenu.broadcastChanges();
-                persistentData.getList(OVERWORLD_INVENTORY, 10).clear();
-            }
-        }
+    public static final String OVERWORLD_INVENTORY = "OvWorldInv", VETHEA_INVENTORY = "DreamInv", MODID_SEPERATOR = "divinerpg:";
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    public void onDeath(LivingDeathEvent event) {
+//            if (!event.isCanceled() && event.getEntity() instanceof Player player) {
+//                if (player.level.getGameRules().getBoolean(GameRules.RULE_KEEPINVENTORY)) {
+//                    if (player.level.dimension().equals(LevelRegistry.VETHEA)) saveToInv(player, VETHEA_INVENTORY);
+//                    else saveToInv(player, OVERWORLD_INVENTORY);
+//                } else if (player.level.dimension().equals(LevelRegistry.VETHEA)) clearInv(player, VETHEA_INVENTORY);
+//                else clearInv(player, OVERWORLD_INVENTORY);
+//            }
     }
     @SubscribeEvent(priority = EventPriority.HIGH)
-    public void onDimensionChange(PlayerEvent.PlayerChangedDimensionEvent event) {
-        if(!event.isCanceled() && event.getEntity() instanceof Player) {
-	        Player playerIn = event.getEntity();
-	        persistentData = playerIn.getPersistentData().getCompound(Player.PERSISTED_NBT_TAG);
-	        if(event.getFrom() == LevelRegistry.VETHEA) {
-	            persistentData.put(VETHEA_INVENTORY, playerIn.inventory.save(new ListTag()));
-	            playerIn.getPersistentData().put("PlayerPersisted", persistentData);
-	            playerIn.inventory.clearContent();
-	            ListTag inv = persistentData.getList(OVERWORLD_INVENTORY, 10);
-	            playerIn.inventory.load(inv);
-	            playerIn.inventoryMenu.broadcastChanges();
-	            persistentData.getList(OVERWORLD_INVENTORY, 10).clear();
-	            playerIn.removeAllEffects();
-	        } else if(event.getTo() == LevelRegistry.VETHEA) {
-	        	persistentData.put(OVERWORLD_INVENTORY, playerIn.inventory.save(new ListTag()));
-	            playerIn.getPersistentData().put("PlayerPersisted", persistentData);
-	            playerIn.inventory.clearContent();
-	            ListTag inv = persistentData.getList(VETHEA_INVENTORY, 10);
-	            playerIn.inventory.load(inv);
-	            playerIn.inventoryMenu.broadcastChanges();
-	            persistentData.getList(VETHEA_INVENTORY, 10).clear();
-	            playerIn.removeAllEffects();
-	        }
+    public void onRespawn(PlayerEvent.PlayerRespawnEvent event) {
+//            if (!event.isCanceled()) {
+//                Player player = event.getEntity();
+//                if (player.level.dimension().equals(LevelRegistry.VETHEA)) loadInv(player, VETHEA_INVENTORY);
+//                else loadInv(player, OVERWORLD_INVENTORY);
+//                player.inventoryMenu.broadcastChanges();
+//            }
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    public void onDimensionChange(EntityTravelToDimensionEvent event) {
+        if (!event.isCanceled() && event.getEntity() instanceof Player player) {
+            boolean from = player.level.dimension().equals(LevelRegistry.VETHEA);
+            boolean to = event.getDimension().equals(LevelRegistry.VETHEA);
+
+            if ((from && !player.inventory.isEmpty()) || (to && !player.inventory.isEmpty())) {
+                event.setCanceled(true);
+
+                player.displayClientMessage(Component.translatable("teleport.failed.inventory_not_empty"), true);
+                return;
+            }
+
+
+//            if (from ^ to) {
+//                if (from) {
+//                    saveToInv(player, VETHEA_INVENTORY);
+//                    loadInv(player, OVERWORLD_INVENTORY);
+//                } else {
+//                    saveToInv(player, OVERWORLD_INVENTORY);
+//                    loadInv(player, VETHEA_INVENTORY);
+//                }
+//                player.inventoryMenu.broadcastChanges();
+//                player.removeAllEffects();
+//            } else if (from && to) {
+//                saveToInv(player, VETHEA_INVENTORY);
+//            } else {
+//                saveToInv(player, OVERWORLD_INVENTORY);
+//            }
         }
+    }
+    public void saveToInv(Player player, String inv) {
+        CompoundTag persisted = player.getPersistentData().getCompound(Player.PERSISTED_NBT_TAG);
+        persisted.put(MODID_SEPERATOR + player.getStringUUID() + "_" + inv, player.inventory.save(new ListTag()));
+        player.getPersistentData().put(Player.PERSISTED_NBT_TAG, persisted);
+    }
+    public void loadInv(Player player, String inv) {
+        ListTag newInventory = player.getPersistentData().getCompound(Player.PERSISTED_NBT_TAG).getList(MODID_SEPERATOR + player.getStringUUID() + "_" + inv, 10);
+        if(newInventory != null) player.inventory.load(newInventory);
+    }
+    public void clearInv(Player player, String inv) {
+        CompoundTag persisted = player.getPersistentData().getCompound(Player.PERSISTED_NBT_TAG);
+        persisted.put(MODID_SEPERATOR + player.getStringUUID() + "_" + inv, new ListTag());
+        player.getPersistentData().put(Player.PERSISTED_NBT_TAG, persisted);
     }
 }
