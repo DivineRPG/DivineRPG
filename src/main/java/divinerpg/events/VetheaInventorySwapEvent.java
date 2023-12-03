60aa661b82c3b864364a4e6f79a9cf011e648566
Nicusha <nicosaurusrex99@gmail.com>
2023-12-03 13:19:45 +1030

    fixed vethea inventory bug

--- src/main/java/divinerpg/events/VetheaInventorySwapEvent.java
+++ src/main/java/divinerpg/events/VetheaInventorySwapEvent.java
@@ -1,87 +1,47 @@
 package divinerpg.events;
 
+import divinerpg.DivineRPG;
 import divinerpg.compat.CuriosCompat;
 import divinerpg.registries.LevelRegistry;
-import net.minecraft.nbt.*;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.event.entity.EntityTravelToDimensionEvent;
-import net.minecraftforge.event.entity.living.LivingDeathEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.*;
 import net.minecraftforge.fml.ModList;
 
 public class VetheaInventorySwapEvent {
-    public static final String OVERWORLD_INVENTORY = "OvWorldInv", VETHEA_INVENTORY = "DreamInv", MODID_SEPERATOR = "divinerpg:";
-    @SubscribeEvent(priority = EventPriority.HIGH)
-    public void onDeath(LivingDeathEvent event) {
-//            if (!event.isCanceled() && event.getEntity() instanceof Player player) {
-//                if (player.level.getGameRules().getBoolean(GameRules.RULE_KEEPINVENTORY)) {
-//                    if (player.level.dimension().equals(LevelRegistry.VETHEA)) saveToInv(player, VETHEA_INVENTORY);
-//                    else saveToInv(player, OVERWORLD_INVENTORY);
-//                } else if (player.level.dimension().equals(LevelRegistry.VETHEA)) clearInv(player, VETHEA_INVENTORY);
-//                else clearInv(player, OVERWORLD_INVENTORY);
-//            }
-    }
-    @SubscribeEvent(priority = EventPriority.HIGH)
-    public void onRespawn(PlayerEvent.PlayerRespawnEvent event) {
-//            if (!event.isCanceled()) {
-//                Player player = event.getEntity();
-//                if (player.level.dimension().equals(LevelRegistry.VETHEA)) loadInv(player, VETHEA_INVENTORY);
-//                else loadInv(player, OVERWORLD_INVENTORY);
-//                player.inventoryMenu.broadcastChanges();
-//            }
-    }
 
     @SubscribeEvent(priority = EventPriority.HIGH)
     public void onDimensionChange(EntityTravelToDimensionEvent event) {
         if (!event.isCanceled() && event.getEntity() instanceof Player player) {
-            boolean from = player.level.dimension().equals(LevelRegistry.VETHEA);
-            boolean to = event.getDimension().equals(LevelRegistry.VETHEA);
+            boolean isVetheaDimension = player.level.dimension().equals(LevelRegistry.VETHEA);
+            boolean isTargetVetheaDimension = event.getDimension().equals(LevelRegistry.VETHEA);
+            boolean isOverworld = player.level.dimension().equals(Level.OVERWORLD);
+            boolean isTargetOverworld = event.getDimension().equals(Level.OVERWORLD);
 
-            if ((from && !player.inventory.isEmpty()) || (to && !player.inventory.isEmpty())) {
+            if ((isVetheaDimension && isTargetOverworld) || (isOverworld && isTargetVetheaDimension)) {
+                if (!player.inventory.isEmpty()) {
                     event.setCanceled(true);
                     player.displayClientMessage(Component.translatable("teleport.failed.inventory_not_empty"), true);
-                    return;
-            }
-            if (ModList.get().isLoaded("curios")) {
-                if (CuriosCompat.checkCuriosSlots(player)) {
-                    event.setCanceled(true);
-                    player.displayClientMessage(Component.translatable("teleport.failed.curios_slots"), true);
+
+                    DivineRPG.LOGGER.warn("Dimension change canceled for player '{}' from '{}' to '{}' due to non-empty inventory.", player.getDisplayName(), player.level.dimension().location(), event.getDimension().location());
+
+
                     return;
                 }
+                if (ModList.get().isLoaded("curios")) {
+                    if (CuriosCompat.checkCuriosSlots(player)) {
+                        event.setCanceled(true);
+                        player.displayClientMessage(Component.translatable("teleport.failed.curios_slots"), true);
+
+                        DivineRPG.LOGGER.warn("Dimension change canceled for player '{}' from '{}' to '{}' due to insufficient Curios slots.", player.getDisplayName(), player.level.dimension().location(), event.getDimension().location());
+
+
+                        return;
+                    }
+                }
             }
-
-
-//            if (from ^ to) {
-//                if (from) {
-//                    saveToInv(player, VETHEA_INVENTORY);
-//                    loadInv(player, OVERWORLD_INVENTORY);
-//                } else {
-//                    saveToInv(player, OVERWORLD_INVENTORY);
-//                    loadInv(player, VETHEA_INVENTORY);
-//                }
-//                player.inventoryMenu.broadcastChanges();
-//                player.removeAllEffects();
-//            } else if (from && to) {
-//                saveToInv(player, VETHEA_INVENTORY);
-//            } else {
-//                saveToInv(player, OVERWORLD_INVENTORY);
-//            }
         }
-    }
-    public void saveToInv(Player player, String inv) {
-        CompoundTag persisted = player.getPersistentData().getCompound(Player.PERSISTED_NBT_TAG);
-        persisted.put(MODID_SEPERATOR + player.getStringUUID() + "_" + inv, player.inventory.save(new ListTag()));
-        player.getPersistentData().put(Player.PERSISTED_NBT_TAG, persisted);
-    }
-    public void loadInv(Player player, String inv) {
-        ListTag newInventory = player.getPersistentData().getCompound(Player.PERSISTED_NBT_TAG).getList(MODID_SEPERATOR + player.getStringUUID() + "_" + inv, 10);
-        if(newInventory != null) player.inventory.load(newInventory);
-    }
-    public void clearInv(Player player, String inv) {
-        CompoundTag persisted = player.getPersistentData().getCompound(Player.PERSISTED_NBT_TAG);
-        persisted.put(MODID_SEPERATOR + player.getStringUUID() + "_" + inv, new ListTag());
-        player.getPersistentData().put(Player.PERSISTED_NBT_TAG, persisted);
     }
 }
