2d1e1a9d63f3ef88bf6086b84e7b06125d5c0f0f
PegasDoggeAM <Shaipple@gmail.com>
2023-12-11 22:01:23 +1100

    Rive tweaks

--- src/main/java/divinerpg/events/enchant/RiveHandler.java
+++ src/main/java/divinerpg/events/enchant/RiveHandler.java
@@ -2,7 +2,8 @@ package divinerpg.events.enchant;
 
 import divinerpg.registries.EnchantmentRegistry;
 import net.minecraft.core.*;
-import net.minecraft.server.level.*;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
 import net.minecraft.world.item.enchantment.*;
@@ -24,12 +25,17 @@ public class RiveHandler {
         }
 
         Player player = event.getPlayer();
-        if(player.isCrouching()) {
+        if(player.isShiftKeyDown()) {
             return;
         }
 
-        ItemStack itemStack = event.getPlayer().getMainHandItem();
+        ItemStack itemStack = player.getMainHandItem();
         if(!(itemStack.getItem() instanceof DiggerItem)) {
+            return;
+        }
+
+        BlockState blockState = world.getBlockState(event.getPos());
+        if(!itemStack.getItem().isCorrectToolForDrops(itemStack, blockState)) {
             return;
         }
 
@@ -38,71 +44,64 @@ public class RiveHandler {
             return;
         }
 
-        Direction facing = player.getDirection();
+        float pitch = player.getXRot();
+        Direction facing = (pitch > 45) ? Direction.DOWN : (pitch < -45) ? Direction.UP : player.getDirection();
 
         int[] dimensions = getSizeByDirection(facing, level);
+        int totalBlocksBroken = 0;
         for(int x = dimensions[0]; x <= dimensions[3]; x++) {
             for(int y = dimensions[1]; y <= dimensions[4]; y++) {
                 for(int z = dimensions[2]; z <= dimensions[5]; z++) {
                     BlockPos pos = event.getPos().offset(x, y, z);
-                    tryToBreakBlock(world, player, pos, world.getBlockState(pos), itemStack);
+                    if (tryToBreakBlock(world, player, pos, world.getBlockState(pos), itemStack)) {
+                        totalBlocksBroken++;
+                    }
                 }
             }
         }
+        if (blockState.getDestroySpeed(world, event.getPos()) != 0.0F && totalBlocksBroken > 0) {
+            itemStack.hurtAndBreak(totalBlocksBroken - 1, player, (context) -> context.broadcastBreakEvent(EquipmentSlot.MAINHAND));
+        }
     }
 
-    private void tryToBreakBlock(Level world, Player player, BlockPos pos, BlockState blockState, ItemStack tool) {
-        if(blockState.getBlock() == Blocks.AIR) {
-            return;
+    private boolean tryToBreakBlock(Level world, Player player, BlockPos pos, BlockState blockState, ItemStack tool) {
+        if(!tool.getItem().isCorrectToolForDrops(tool, blockState)) {
+            return false;
         }
-
-        if((blockState.getBlock() instanceof TorchBlock)){
-            return;
+        if(blockState.getBlock() instanceof DoorBlock) {
+            return false;
         }
-        if(blockState.getBlock() instanceof DoorBlock){
-            return;
+        if(blockState.hasBlockEntity()) {
+            return false;
         }
-        if(blockState.hasBlockEntity()){
-            return;
+        if(blockState.getBlock().defaultDestroyTime() < 0.0F) {
+            return false;
         }
-
-        if(blockState.getBlock().defaultDestroyTime() < 0) {
-            return;
-        }
-        
 
         Block block = blockState.getBlock();
         if(block.canHarvestBlock(blockState, world, pos, player) && world instanceof ServerLevel) {
-            block.playerDestroy(world, player, pos, blockState, null, tool);
-            world.destroyBlock(pos, false, player);
-            block.popExperience((ServerLevel) world, pos, block.getExpDrop(blockState, world, world.random, pos, EnchantmentHelper.getEnchantmentLevel(Enchantments.BLOCK_FORTUNE, player), EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH, player)));
-            tool.hurtAndBreak(1, player, (context) -> {
-                context.broadcastBreakEvent(player.getUsedItemHand());
-            });
+            if(!player.isCreative()) {
+                block.playerDestroy(world, player, pos, blockState, null, tool);
+                block.popExperience((ServerLevel) world, pos, block.getExpDrop(blockState, world, world.random, pos, EnchantmentHelper.getEnchantmentLevel(Enchantments.BLOCK_FORTUNE, player), EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH, player)));
+            }
+            world.destroyBlock(pos, false);
+            return true;
         }
+        return false;
     }
 
     private int[] getSizeByDirection(Direction facing, int level) {
-
         int depth = level - 1;
 
         //Format: fromX, fromY, fromZ, toX, toY, toZ
         //arr[x] must be greater than or equal to arr[x - 3]
-        switch(facing) {
-            case NORTH:
-                return new int[] {-1, -1, 0 - depth, 1, 1, 0};
-            case EAST:
-                return new int[] {0, -1, -1, 0 + depth, 1, 1};
-            case WEST:
-                return new int[] {0 - depth, -1, -1, 0, 1, 1};
-            case SOUTH:
-                return new int[] {-1, -1, 0, 1, 1, 0 + depth};
-            case UP:
-                return new int[] {-1, 0, -1, 1, 0 + depth, 1};
-            case DOWN:
-                return new int[] {-1, 0 - depth, -1, 1, 0, 1};
-            default:
-                return new int[] {0, 0, 0, 0, 0, 0};
-        }
+        return switch (facing) {
+            case NORTH -> new int[]{-1, -1, -depth, 1, 1, 0};
+            case EAST -> new int[]{0, -1, -1, depth, 1, 1};
+            case WEST -> new int[]{-depth, -1, -1, 0, 1, 1};
+            case SOUTH -> new int[]{-1, -1, 0, 1, 1, depth};
+            case UP -> new int[] {-1, 0, -1, 1, depth, 1};
+            case DOWN -> new int[] {-1, -depth, -1, 1, 0, 1};
+        };
     }
 }
