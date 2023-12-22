adcbca0903915aa4a2c5964f28d9043a6290652e
PegasDoggeAM <Shaipple@gmail.com>
2023-12-22 20:42:07 +1100

    Chest dupe fix for other chests

--- src/main/java/divinerpg/client/menu/FrostedChestMenu.java
+++ src/main/java/divinerpg/client/menu/FrostedChestMenu.java
@@ -3,8 +3,8 @@ package divinerpg.client.menu;
 import divinerpg.registries.MenuTypeRegistry;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.*;
-import net.minecraft.world.entity.player.*;
-import net.minecraft.world.inventory.*;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.ChestMenu;
 
 public class FrostedChestMenu extends ChestMenu {
     public FrostedChestMenu(int i, Inventory playerInventory, FriendlyByteBuf buffer) {
