590daed7dc9f210d1a02835410cd4826adf76d6d
velocityraptor-real <56190563+velocityraptor-real@users.noreply.github.com>
2024-03-23 15:12:28 +0100

    Update CommonConfig.java

--- src/main/java/divinerpg/config/CommonConfig.java
+++ src/main/java/divinerpg/config/CommonConfig.java
@@ -17,7 +17,7 @@ public class CommonConfig {
         maxArcana = BUILDER.comment("Maximum stored arcana").defineInRange("maxArcana", 200, 200, 10000);
         allureMaxMobs = BUILDER.comment("Maximum amount of loaded mobs before the frosted allure stops working").defineInRange("allureMaxMobs", 300, 150, 1000);
         kitraKills = BUILDER.comment("The amount of protected mob kills before the Kitra is spawned").defineInRange("kitraKills", 100, 1, 10000);
-        kitraProtected = BUILDER.comment("Entities that count towards Kitra spawning").define("kitraProtected", ImmutableList.of("minecraft:salmon", "minecraft:cod", "minecraft:pufferfish", "minecraft:squid", "minecraft:tadpole", "minecraft:turtle", "divinerpg:shark", "divinerpg:aequorea", "divinerpg:whale", "divinerpg:liopleurodon", "minecraft:glow_squid", "divinerpg:robbin"));
+        kitraProtected = BUILDER.comment("Entities that count towards Kitra spawning").define("kitraProtected", ImmutableList.of("minecraft:salmon", "minecraft:cod", "minecraft:pufferfish", "minecraft:squid", "minecraft:tadpole", "minecraft:turtle", "divinerpg:shark", "divinerpg:aequorea", "divinerpg:whale", "divinerpg:liopleurodon", "minecraft:glow_squid", "divinerpg:robbin", "divinerpg:pink_ghost_glider"));
         saferVetheanInventory = BUILDER.comment("Safer handling of the Vethean inventory").define("saferVetheanInventory", false);
         BUILDER.pop();
         SPEC = BUILDER.build();
