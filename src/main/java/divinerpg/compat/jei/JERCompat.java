dc053bf3a133beb3925bb8e092a2bac6fbfe6108
PegasDoggeAM <Shaipple@gmail.com>
2024-06-25 22:18:10 +1100

    Textures
    - some small changes for crop/loot chest jer support
    - changed seed recipes
    - fixed some translucent textures having extra colors

--- src/main/java/divinerpg/compat/jei/JERCompat.java
+++ src/main/java/divinerpg/compat/jei/JERCompat.java
@@ -1,77 +1,86 @@
 package divinerpg.compat.jei;
 
 import divinerpg.DivineRPG;
+import divinerpg.registries.*;
 import jeresources.api.*;
 import jeresources.api.drop.PlantDrop;
 import jeresources.compatibility.api.JERAPI;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraft.world.level.block.Blocks;
 
 public class JERCompat {
-
     public static void init() {
         IJERAPI jerApi = JERAPI.getInstance();
-
-
         IDungeonRegistry dungeonRegistry = jerApi.getDungeonRegistry();
-        if (dungeonRegistry != null) {
-            //Arcana
-            dungeonRegistry.registerChest("Banquet Hall Chest", new ResourceLocation(DivineRPG.MODID, "chests/arcana/banquet_hall_loot"));
-            dungeonRegistry.registerChest("Utility Hallway Chest", new ResourceLocation(DivineRPG.MODID, "chests/arcana/utility_hallway_loot"));
-
-            //Iceika
-            dungeonRegistry.registerChest("Iceika Chest", new ResourceLocation(DivineRPG.MODID, "chests/iceika/iceika_chest_dungeon"));
-            dungeonRegistry.registerChest("Iceika Hut Chest", new ResourceLocation(DivineRPG.MODID, "chests/iceika/iceika_chest_hut"));
-
+        if(dungeonRegistry != null) {
+            //TODO: to add lang keys for translation
             //Overworld
             dungeonRegistry.registerChest("Diamond Dave's Chest", new ResourceLocation(DivineRPG.MODID, "chests/overworld/amethyst"));
             dungeonRegistry.registerChest("Ancient Tomb Chest", new ResourceLocation(DivineRPG.MODID, "chests/overworld/ancient_tomb"));
-            dungeonRegistry.registerChest("Livestock Merchant's Chest", new ResourceLocation(DivineRPG.MODID, "chests/overworld/livestock_chest"));
-
+            dungeonRegistry.registerChest("Merchant's Hut Chest", new ResourceLocation(DivineRPG.MODID, "chests/overworld/livestock_chest"));
+            //Iceika
+            dungeonRegistry.registerChest("Iceika Chest", new ResourceLocation(DivineRPG.MODID, "chests/iceika/iceika_chest_dungeon"));
+            dungeonRegistry.registerChest("Iceika Hut Chest", new ResourceLocation(DivineRPG.MODID, "chests/iceika/iceika_chest_hut"));
+            dungeonRegistry.registerChest("Groglin Chest", new ResourceLocation(DivineRPG.MODID, "chests/iceika/groglin_chest"));
+            dungeonRegistry.registerChest("Gruzzorlug Chest", new ResourceLocation(DivineRPG.MODID, "chests/iceika/gruzzorlug_chest"));
+            //Arcana
+            dungeonRegistry.registerChest("Banquet Hall Chest", new ResourceLocation(DivineRPG.MODID, "chests/arcana/banquet_hall_loot"));
+            dungeonRegistry.registerChest("Utility Hallway Chest", new ResourceLocation(DivineRPG.MODID, "chests/arcana/utility_hallway_loot"));
             //Twilight
             dungeonRegistry.registerChest("Eden Hut Chest", new ResourceLocation(DivineRPG.MODID, "chests/twilight/eden/hut_chest"));
             dungeonRegistry.registerChest("Skythern Chest", new ResourceLocation(DivineRPG.MODID, "chests/twilight/skythern/chest"));
             dungeonRegistry.registerChest("Wildwood Chest", new ResourceLocation(DivineRPG.MODID, "chests/twilight/wildwood/chest"));
-
             //Vethea
-            dungeonRegistry.registerChest("Crypt Loot Chest", new ResourceLocation(DivineRPG.MODID, "chests/vethea/crypt_loot"));
-            dungeonRegistry.registerChest("Karos Madhouse Loot Chest", new ResourceLocation(DivineRPG.MODID, "chests/vethea/karos_madhouse_loot"));
-            dungeonRegistry.registerChest("Quadrotic Post Loot Chest", new ResourceLocation(DivineRPG.MODID, "chests/vethea/quadrotic_post_loot"));
-            dungeonRegistry.registerChest("Raglok Chamber Loot Chest", new ResourceLocation(DivineRPG.MODID, "chests/vethea/raglok_chamber_loot"));
-            dungeonRegistry.registerChest("Temple Bottom Loot Chest", new ResourceLocation(DivineRPG.MODID, "chests/vethea/temple_loot_bottom"));
-            dungeonRegistry.registerChest("Temple Middle Loot Chest", new ResourceLocation(DivineRPG.MODID, "chests/vethea/temple_loot_middle"));
-            dungeonRegistry.registerChest("Temple Top Loot Chest", new ResourceLocation(DivineRPG.MODID, "chests/vethea/temple_loot_top"));
-            dungeonRegistry.registerChest("Wreck Hall Loot Chest", new ResourceLocation(DivineRPG.MODID, "chests/vethea/wreck_hall_loot"));
+            dungeonRegistry.registerChest("Crypt Chest", new ResourceLocation(DivineRPG.MODID, "chests/vethea/crypt_loot"));
+            dungeonRegistry.registerChest("Karos Madhouse Chest", new ResourceLocation(DivineRPG.MODID, "chests/vethea/karos_madhouse_loot"));
+            dungeonRegistry.registerChest("Quadrotic Post Chest", new ResourceLocation(DivineRPG.MODID, "chests/vethea/quadrotic_post_loot"));
+            dungeonRegistry.registerChest("Raglok Chamber Chest", new ResourceLocation(DivineRPG.MODID, "chests/vethea/raglok_chamber_loot"));
+            dungeonRegistry.registerChest("Temple Bottom Chest", new ResourceLocation(DivineRPG.MODID, "chests/vethea/temple_loot_bottom"));
+            dungeonRegistry.registerChest("Temple Middle Chest", new ResourceLocation(DivineRPG.MODID, "chests/vethea/temple_loot_middle"));
+            dungeonRegistry.registerChest("Temple Top Chest", new ResourceLocation(DivineRPG.MODID, "chests/vethea/temple_loot_top"));
+            dungeonRegistry.registerChest("Wreck Hall Chest", new ResourceLocation(DivineRPG.MODID, "chests/vethea/wreck_hall_loot"));
         }
-
 //        IWorldGenRegistry worldGenRegistry = jerApi.getWorldGenRegistry();
-//        if (worldGenRegistry != null) {
-//            worldGenRegistry.register(new ItemStack(ForgeRegistries.BLOCKS.getValue(new ResourceLocation(DivineRPG.MODID, "arlemite_ore"))), new DistributionSquare(2, 8, 5, 70));
-//            worldGenRegistry.register(new ItemStack(ForgeRegistries.BLOCKS.getValue(new ResourceLocation(DivineRPG.MODID, "realmite_ore"))), new DistributionSquare(30, 8, 18, 52));
-//            worldGenRegistry.register(new ItemStack(ForgeRegistries.BLOCKS.getValue(new ResourceLocation(DivineRPG.MODID, "rupee_ore"))), new DistributionSquare(3, 8, -64, 64));
-//            worldGenRegistry.register(new ItemStack(ForgeRegistries.BLOCKS.getValue(new ResourceLocation(DivineRPG.MODID, "torridite_ore"))), new DistributionSquare(5, 4, 0, 256), new Restriction(new DimensionRestriction(Level.NETHER)));
-//            worldGenRegistry.register(new ItemStack(ForgeRegistries.BLOCKS.getValue(new ResourceLocation(DivineRPG.MODID, "bloodgem_ore"))), new DistributionSquare(5, 4, 0, 256), new Restriction(new DimensionRestriction(Level.NETHER)));
-//            worldGenRegistry.register(new ItemStack(ForgeRegistries.BLOCKS.getValue(new ResourceLocation(DivineRPG.MODID, "eden_ore"))), new DistributionSquare(6, 5, -64, 256), new Restriction(new DimensionRestriction(LevelRegistry.EDEN)));
-//            worldGenRegistry.register(new ItemStack(ForgeRegistries.BLOCKS.getValue(new ResourceLocation(DivineRPG.MODID, "wildwood_ore"))), new DistributionSquare(6, 8, -64, 256), new Restriction(new DimensionRestriction(LevelRegistry.WILDWOOD)));
-//            worldGenRegistry.register(new ItemStack(ForgeRegistries.BLOCKS.getValue(new ResourceLocation(DivineRPG.MODID, "apalachia_ore"))), new DistributionSquare(12, 12, -64, 256), new Restriction(new DimensionRestriction(LevelRegistry.APALACHIA)));
-//            worldGenRegistry.register(new ItemStack(ForgeRegistries.BLOCKS.getValue(new ResourceLocation(DivineRPG.MODID, "skythern_ore"))), new DistributionSquare(7, 6, -64, 256), new Restriction(new DimensionRestriction(LevelRegistry.SKYTHERN)));
-//            worldGenRegistry.register(new ItemStack(ForgeRegistries.BLOCKS.getValue(new ResourceLocation(DivineRPG.MODID, "mortum_ore"))), new DistributionSquare(5, 5, -64, 256), new Restriction(new DimensionRestriction(LevelRegistry.MORTUM)));
+//        if(worldGenRegistry != null) {
+//            //Overworld
+//            worldGenRegistry.register(new ItemStack(BlockRegistry.arlemiteOre.get()), new DistributionSquare(2, 8, 5, 70));
+//            worldGenRegistry.register(new ItemStack(BlockRegistry.realmiteOre.get()), new DistributionSquare(30, 8, 18, 52));
+//            worldGenRegistry.register(new ItemStack(BlockRegistry.rupeeOre.get()), new DistributionSquare(3, 8, -64, 64));
+//            //Nether
+//            worldGenRegistry.register(new ItemStack(BlockRegistry.torriditeOre.get()), new DistributionSquare(5, 4, 0, 256), new Restriction(new DimensionRestriction(Level.NETHER)));
+//            worldGenRegistry.register(new ItemStack(BlockRegistry.bloodgemOre.get()), new DistributionSquare(5, 4, 0, 256), new Restriction(new DimensionRestriction(Level.NETHER)));
+//            //Iceika
+//            worldGenRegistry.register(new ItemStack(BlockRegistry.anthraciteOre.get()), new DistributionSquare(x, x, x, x), new Restriction(new DimensionRestriction(LevelRegistry.ICEIKA)));
+//            worldGenRegistry.register(new ItemStack(BlockRegistry.oxdriteOre.get()), new DistributionSquare(x, x, x, x), new Restriction(new DimensionRestriction(LevelRegistry.ICEIKA)));
+//            //Twilight
+//            worldGenRegistry.register(new ItemStack(BlockRegistry.edenOre.get()), new DistributionSquare(6, 5, -64, 256), new Restriction(new DimensionRestriction(LevelRegistry.EDEN)));
+//            worldGenRegistry.register(new ItemStack(BlockRegistry.wildwoodOre.get()), new DistributionSquare(6, 8, -64, 256), new Restriction(new DimensionRestriction(LevelRegistry.WILDWOOD)));
+//            worldGenRegistry.register(new ItemStack(BlockRegistry.apalachiaOre.get()), new DistributionSquare(12, 12, -64, 256), new Restriction(new DimensionRestriction(LevelRegistry.APALACHIA)));
+//            worldGenRegistry.register(new ItemStack(BlockRegistry.skythernOre.get()), new DistributionSquare(7, 6, -64, 256), new Restriction(new DimensionRestriction(LevelRegistry.SKYTHERN)));
+//            worldGenRegistry.register(new ItemStack(BlockRegistry.mortumOre.get()), new DistributionSquare(5, 5, -64, 256), new Restriction(new DimensionRestriction(LevelRegistry.MORTUM)));
 //        }
-
         IPlantRegistry plantRegistry = jerApi.getPlantRegistry();
         if(plantRegistry != null) {
-            plantRegistry.register(new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(DivineRPG.MODID, "moonbulb_seeds"))), ForgeRegistries.BLOCKS.getValue(new ResourceLocation(DivineRPG.MODID, "moonbulb_plant")).defaultBlockState(), new PlantDrop(new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(DivineRPG.MODID, "moonbulb"))), 1, 1));
-            plantRegistry.register(new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(DivineRPG.MODID, "tomato_seeds"))), ForgeRegistries.BLOCKS.getValue(new ResourceLocation(DivineRPG.MODID, "tomato_plant")).defaultBlockState(), new PlantDrop(new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(DivineRPG.MODID, "tomato"))), 1, 1));
-            plantRegistry.register(new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(DivineRPG.MODID, "white_mushroom_seeds"))), ForgeRegistries.BLOCKS.getValue(new ResourceLocation(DivineRPG.MODID, "white_mushroom_plant")).defaultBlockState(), new PlantDrop(new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(DivineRPG.MODID, "white_mushroom"))), 1, 1));
-            plantRegistry.register(new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(DivineRPG.MODID, "aquamarine_seeds"))), ForgeRegistries.BLOCKS.getValue(new ResourceLocation(DivineRPG.MODID, "aquamarine_plant")).defaultBlockState(), new PlantDrop(new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(DivineRPG.MODID, "aquamarine"))), 1, 1));
-            plantRegistry.register(new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(DivineRPG.MODID, "eucalyptus_root_seeds"))), ForgeRegistries.BLOCKS.getValue(new ResourceLocation(DivineRPG.MODID, "eucalyptus_plant")).defaultBlockState(), new PlantDrop(new ItemStack(ForgeRegistries.BLOCKS.getValue(new ResourceLocation(DivineRPG.MODID, "eucalyptus_log"))), 1, 1));
-            plantRegistry.register(new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(DivineRPG.MODID, "firestock_seeds"))), ForgeRegistries.BLOCKS.getValue(new ResourceLocation(DivineRPG.MODID, "firestock_plant")).defaultBlockState(), new PlantDrop(new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(DivineRPG.MODID, "firestock"))), 1, 1));
-            plantRegistry.register(new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(DivineRPG.MODID, "hitchak_seeds"))), ForgeRegistries.BLOCKS.getValue(new ResourceLocation(DivineRPG.MODID, "hitchak_plant")).defaultBlockState(), new PlantDrop(new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(DivineRPG.MODID, "hitchak"))), 1, 1));
-            plantRegistry.register(new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(DivineRPG.MODID, "lamona_seeds"))), ForgeRegistries.BLOCKS.getValue(new ResourceLocation(DivineRPG.MODID, "lamona_plant")).defaultBlockState(), new PlantDrop(new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(DivineRPG.MODID, "lamona"))), 1, 1));
-            plantRegistry.register(new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(DivineRPG.MODID, "marsine_seeds"))), ForgeRegistries.BLOCKS.getValue(new ResourceLocation(DivineRPG.MODID, "marsine_plant")).defaultBlockState(), new PlantDrop(new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(DivineRPG.MODID, "marsine"))), 1, 1));
-            plantRegistry.register(new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(DivineRPG.MODID, "pinfly_seeds"))), ForgeRegistries.BLOCKS.getValue(new ResourceLocation(DivineRPG.MODID, "pinfly_plant")).defaultBlockState(), new PlantDrop(new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(DivineRPG.MODID, "pinfly"))), 1, 1));
-            plantRegistry.register(new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(DivineRPG.MODID, "veilo_seeds"))), ForgeRegistries.BLOCKS.getValue(new ResourceLocation(DivineRPG.MODID, "veilo_plant")).defaultBlockState(), new PlantDrop(new ItemStack(ForgeRegistries.ITEMS.getValue(new ResourceLocation(DivineRPG.MODID, "veilo"))), 1, 1));
+            //Overworld
+            plantRegistry.register(new ItemStack(ItemRegistry.tomato_seeds.get()), BlockRegistry.tomatoPlant.get().defaultBlockState(), new PlantDrop(new ItemStack(ItemRegistry.tomato.get()), 2, 5));
+            plantRegistry.register(new ItemStack(ItemRegistry.white_mushroom_seeds.get()), BlockRegistry.whiteMushroomPlant.get().defaultBlockState(), new PlantDrop(new ItemStack(ItemRegistry.white_mushroom.get()), 2, 5));
+            //Iceika
+            plantRegistry.registerWithSoil(new ItemStack(ItemRegistry.winterberry.get()), BlockRegistry.winterberryBush.get().defaultBlockState(), BlockRegistry.brittleMoss.get().defaultBlockState(), new PlantDrop(new ItemStack(ItemRegistry.winterberry.get()), 1, 1));
+            //Arcana
+            plantRegistry.register(new ItemStack(ItemRegistry.aquamarine_seeds.get()), BlockRegistry.aquamarinePlant.get().defaultBlockState(), new PlantDrop(new ItemStack(ItemRegistry.aquamarine.get()), 2, 5));
+            plantRegistry.register(new ItemStack(ItemRegistry.eucalyptus_root_seeds.get()), BlockRegistry.eucalyptusPlant.get().defaultBlockState(), new PlantDrop(new ItemStack(BlockRegistry.eucalyptusLog.get()), 1, 1), new PlantDrop(new ItemStack(ItemRegistry.eucalyptus_root_seeds.get()), 1, 4));
+            plantRegistry.register(new ItemStack(ItemRegistry.hitchak_seeds.get()), BlockRegistry.hitchakPlant.get().defaultBlockState(), new PlantDrop(new ItemStack(ItemRegistry.hitchak.get()), 2, 5));
+            plantRegistry.register(new ItemStack(ItemRegistry.lamona_seeds.get()), BlockRegistry.lamonaPlant.get().defaultBlockState(), new PlantDrop(new ItemStack(ItemRegistry.lamona.get()), 2, 5));
+            plantRegistry.register(new ItemStack(ItemRegistry.marsine_seeds.get()), BlockRegistry.marsinePlant.get().defaultBlockState(), new PlantDrop(new ItemStack(ItemRegistry.marsine.get()), 2, 5));
+            plantRegistry.register(new ItemStack(ItemRegistry.veilo_seeds.get()), BlockRegistry.veiloPlant.get().defaultBlockState(), new PlantDrop(new ItemStack(ItemRegistry.veilo.get()), 2, 5));
+            plantRegistry.registerWithSoil(new ItemStack(ItemRegistry.firestock_seeds.get()), BlockRegistry.firestockPlant.get().defaultBlockState(), BlockRegistry.arcaniteGrass.get().defaultBlockState(), new PlantDrop(new ItemStack(ItemRegistry.firestock.get()), 2, 5));
+            plantRegistry.registerWithSoil(new ItemStack(ItemRegistry.pinfly_seeds.get()), BlockRegistry.pinflyPlant.get().defaultBlockState(), BlockRegistry.arcaniteGrass.get().defaultBlockState(), new PlantDrop(new ItemStack(ItemRegistry.pinfly.get()), 2, 5));
+            //Twilight
+            plantRegistry.registerWithSoil(new ItemStack(ItemRegistry.moonbulb_seeds.get()), BlockRegistry.moonbulbPlant.get().defaultBlockState(), Blocks.GRASS_BLOCK.defaultBlockState(), new PlantDrop(new ItemStack(ItemRegistry.moonbulb.get()), 2, 5));
+            plantRegistry.registerWithSoil(new ItemStack(ItemRegistry.purple_glowbone_seeds.get()), BlockRegistry.purpleGlowbonePlant.get().defaultBlockState(), Blocks.GRASS_BLOCK.defaultBlockState(), new PlantDrop(new ItemStack(ItemRegistry.purple_glowbone.get()), 2, 5));
+            plantRegistry.registerWithSoil(new ItemStack(ItemRegistry.pink_glowbone_seeds.get()), BlockRegistry.pinkGlowbonePlant.get().defaultBlockState(), Blocks.GRASS_BLOCK.defaultBlockState(), new PlantDrop(new ItemStack(ItemRegistry.pink_glowbone.get()), 2, 5));
+            plantRegistry.register(new ItemStack(ItemRegistry.sky_plant_seeds.get()), BlockRegistry.skyPlant.get().defaultBlockState(), new PlantDrop(new ItemStack(ItemRegistry.sky_flower.get()), 1, 1), new PlantDrop(new ItemStack(ItemRegistry.sky_plant_seeds.get()), 1, 4));
         }
     }
 }
