ba4dd52bdfd75fca40e5603d814f0828810a342d
Charlie Angstrom <charlieangstrom@protonmail.com>
2024-10-13 21:14:40 -0700

    Update GuiLeorna.java

--- java/net/divinerpg/client/render/gui/GuiLeorna.java
+++ java/net/divinerpg/client/render/gui/GuiLeorna.java
@@ -2,10 +2,11 @@ package net.divinerpg.client.render.gui;
 
 import net.divinerpg.blocks.base.tileentity.container.ContainerDivineMerchant;
 import net.minecraft.entity.IMerchant;
+import net.divinerpg.utils.MessageLocalizer;
 
 public class GuiLeorna extends GuiDivineMerchant{
 
 	public GuiLeorna(ContainerDivineMerchant container, IMerchant mer) {
-		super(container, mer, "Leorna", "leorna");
+		super(container, mer, (MessageLocalizer.normal("entity.DRPGLeorna.name")), "leorna");
 	}
 }
