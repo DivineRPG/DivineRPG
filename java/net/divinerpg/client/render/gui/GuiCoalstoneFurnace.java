72b7dd3b31a612367117c7f84d4453f541fdecf6
Charlie Angstrom <charlieangstrom@protonmail.com>
2024-10-13 20:59:04 -0700

    Update GuiCoalstoneFurnace.java

--- java/net/divinerpg/client/render/gui/GuiCoalstoneFurnace.java
+++ java/net/divinerpg/client/render/gui/GuiCoalstoneFurnace.java
@@ -2,10 +2,11 @@ package net.divinerpg.client.render.gui;
 
 import net.divinerpg.blocks.base.tileentity.TileEntityInfiniteFurnace;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.divinerpg.utils.MessageLocalizer;
 
 public class GuiCoalstoneFurnace extends GuiInfiniteFurnace {
 
 	public GuiCoalstoneFurnace(InventoryPlayer par1InventoryPlayer, TileEntityInfiniteFurnace par2TileEntityFurnace) {
-		super(par1InventoryPlayer, par2TileEntityFurnace, "Coalstone Furnace", "noFuel");
+		super(par1InventoryPlayer, par2TileEntityFurnace, (MessageLocalizer.normal("gui.coalstonefurnace")), "noFuel");
 	}
 }
