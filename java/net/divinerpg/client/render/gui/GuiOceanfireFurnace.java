9a6cfe6d616d48f3dfbdf9a2c75f52a108c24a4c
Charlie Angstrom <charlieangstrom@protonmail.com>
2024-10-13 21:16:25 -0700

    Update GuiOceanfireFurnace.java

--- java/net/divinerpg/client/render/gui/GuiOceanfireFurnace.java
+++ java/net/divinerpg/client/render/gui/GuiOceanfireFurnace.java
@@ -2,10 +2,11 @@ package net.divinerpg.client.render.gui;
 
 import net.divinerpg.blocks.base.tileentity.TileEntityInfiniteFurnace;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.divinerpg.utils.MessageLocalizer;
 
 public class GuiOceanfireFurnace extends GuiInfiniteFurnace {
 
 	public GuiOceanfireFurnace(InventoryPlayer par1InventoryPlayer, TileEntityInfiniteFurnace entity) {
-		super(par1InventoryPlayer, entity, "Oceanfire Furnace", "oceanfireFurnace");
+		super(par1InventoryPlayer, entity, (MessageLocalizer.normal("tile.oceanfireFurnace.name")), "oceanfireFurnace");
 	}
 }
