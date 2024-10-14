071f71054b3230c08102a7130b048b9ceb9457ef
Charlie Angstrom <charlieangstrom@protonmail.com>
2024-10-13 21:15:54 -0700

    Update GuiMoltenFurnace.java

--- java/net/divinerpg/client/render/gui/GuiMoltenFurnace.java
+++ java/net/divinerpg/client/render/gui/GuiMoltenFurnace.java
@@ -2,10 +2,11 @@ package net.divinerpg.client.render.gui;
 
 import net.divinerpg.blocks.base.tileentity.TileEntityInfiniteFurnace;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.divinerpg.utils.MessageLocalizer;
 
 public class GuiMoltenFurnace extends GuiInfiniteFurnace {
 
 	public GuiMoltenFurnace(InventoryPlayer par1InventoryPlayer, TileEntityInfiniteFurnace par2TileEntityFurnace) {
-		super(par1InventoryPlayer, par2TileEntityFurnace, "Molten Furnace", "moltenFurnace");
+		super(par1InventoryPlayer, par2TileEntityFurnace, (MessageLocalizer.normal("tile.moltenFurnace.name")), "moltenFurnace");
 	}
 }
