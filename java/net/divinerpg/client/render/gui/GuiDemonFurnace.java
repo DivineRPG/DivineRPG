6ab4564372951869c49cc136ab6413c7e8a78e60
Charlie Angstrom <charlieangstrom@protonmail.com>
2024-10-13 21:05:17 -0700

    Update GuiDemonFurnace.java

--- java/net/divinerpg/client/render/gui/GuiDemonFurnace.java
+++ java/net/divinerpg/client/render/gui/GuiDemonFurnace.java
@@ -2,10 +2,11 @@ package net.divinerpg.client.render.gui;
 
 import net.divinerpg.blocks.base.tileentity.TileEntityInfiniteFurnace;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.divinerpg.utils.MessageLocalizer;
 
 public class GuiDemonFurnace extends GuiInfiniteFurnace {
 
 	public GuiDemonFurnace(InventoryPlayer par1InventoryPlayer, TileEntityInfiniteFurnace par2TileEntityFurnace) {
-		super(par1InventoryPlayer, par2TileEntityFurnace, "Demon Furnace", "demonFurnace");
+		super(par1InventoryPlayer, par2TileEntityFurnace, (MessageLocalizer.normal("gui.demonfurnace")), "demonFurnace");
 	}
 }
