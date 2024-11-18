9b292a0352a806e19af8fc3ee404fddb942d446a
Charlie Angstrom <charlieangstrom@protonmail.com>
2024-11-17 21:27:38 -0800

    more item updates + orb of light fix

--- src/main/java/net/divinerpg/items/arcana/ItemOrbOfLight.java
+++ src/main/java/net/divinerpg/items/arcana/ItemOrbOfLight.java
@@ -19,7 +19,7 @@ public class ItemOrbOfLight extends ItemMod {
 	
 	@Override
 	public void onUpdate(ItemStack stack, World world, Entity entity, int var4, boolean var5) {
-		if(Ticker.tick%2 == 0) {
+		if(Ticker.tick%1 == 0) {
 		world.setLightValue(EnumSkyBlock.Block, (int)entity.posX, (int)entity.posY, (int)entity.posZ, 15);
         world.markBlockRangeForRenderUpdate((int)entity.posX, (int)entity.posY, (int)entity.posX, 12, 12, 12);
         world.markBlockForUpdate((int)entity.posX, (int)entity.posY, (int)entity.posZ);
