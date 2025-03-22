5df41909b80fdde50b2467f8ffd9fa537712bf42
velocityraptor-real <56190563+velocityraptor-real@users.noreply.github.com>
2025-03-22 10:28:35 +0100

    Update FireStock.java

--- src/main/java/divinerpg/items/arcana/FireStock.java
+++ src/main/java/divinerpg/items/arcana/FireStock.java
@@ -1,6 +1,7 @@
 package divinerpg.items.arcana;
 
 import divinerpg.items.base.ItemMod;
+import net.minecraft.core.*;
 import net.minecraft.sounds.*;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.damagesource.*;
@@ -11,8 +12,13 @@ public class FireStock extends ItemMod {
     @Override
     public void onDestroyed(ItemEntity itemEntity, DamageSource damageSource) {
         BlockState state;
-        if((damageSource.is(DamageTypes.IN_FIRE) || damageSource.is(DamageTypes.ON_FIRE)) && (state = itemEntity.getInBlockState()).is(BlockTags.FIRE))
-            itemEntity.level().setBlock(itemEntity.blockPosition().offset(itemEntity.getRandom().nextInt(3) - 1, itemEntity.getRandom().nextInt(2) - 1, itemEntity.getRandom().nextInt(3) - 1), state, 3);
-        itemEntity.level().playSound(null, itemEntity.getX(), itemEntity.getY(), itemEntity.getZ(), SoundEvents.FIRECHARGE_USE, SoundSource.NEUTRAL);
+        if((damageSource.is(DamageTypes.IN_FIRE) || damageSource.is(DamageTypes.ON_FIRE)) && (state = itemEntity.getInBlockState()).is(BlockTags.FIRE)) {
+            BlockPos pos = itemEntity.blockPosition().offset(itemEntity.getRandom().nextInt(3) - 1, itemEntity.getRandom().nextInt(2) - 1, itemEntity.getRandom().nextInt(3) - 1);
+            if(itemEntity.level().getBlockState(pos).isAir() && itemEntity.level().getBlockState(pos = pos.below()).isFaceSturdy(itemEntity.level(), pos, Direction.UP)) {
+                itemEntity.level().playSound(null, itemEntity.getX(), itemEntity.getY(), itemEntity.getZ(), SoundEvents.FIRECHARGE_USE, SoundSource.NEUTRAL);
+                itemEntity.level().setBlock(pos.above(), state, 3);
+                return;
+            }
+        } itemEntity.level().playSound(null, itemEntity.getX(), itemEntity.getY(), itemEntity.getZ(), SoundEvents.LAVA_EXTINGUISH, SoundSource.NEUTRAL);
     }
 }
