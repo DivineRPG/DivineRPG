8db629d4ac4bb45d733cc30424e740a5abd11a94
PegasDoggeAM <Shaipple@gmail.com>
2025-06-14 22:52:06 +1100

    TwEaKs

--- src/main/java/divinerpg/client/models/iceika/ModelRobbin.java
+++ src/main/java/divinerpg/client/models/iceika/ModelRobbin.java
@@ -27,33 +27,39 @@ public class ModelRobbin extends EntityModel<EntityRobbin> {
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
 		CubeDeformation cubeDef = CubeDeformation.NONE;
-		partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-2, -4, -4, 4, 4, 4, cubeDef), PartPose.offsetAndRotation(0, 22, 1, -.2618F, 0, 0));
+		partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-2, -4, -4, 4, 4, 4, cubeDef), PartPose.offset(0, 22, 1));
 		partdefinition.addOrReplaceChild("tail", CubeListBuilder.create().texOffs(0, 17).addBox(-1, -2, 0, 2, 2, 3, cubeDef), PartPose.offset(0, 18.3F, 1.6F));
-		partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 8).addBox(-2, 5.5F, .5F, 1, 1, 2, cubeDef)
-		.texOffs(11, 14).addBox(-1.5F, 6.5F, -1.5F, 3, 3, 3, cubeDef)
-		.texOffs(8, 11).addBox(-1, 8, -2.5F, 2, 1, 2, cubeDef)
-		.texOffs(0, 11).addBox(1, 5.5F, .5F, 1, 1, 2, cubeDef), PartPose.offset(0, 8.5F, -2.5F));
+		partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(11, 14).addBox(-1.5F, 6.5F, -1.5F, 3, 3, 3, cubeDef) //Head
+		.texOffs(8, 11).addBox(-1, 8, -2.5F, 2, 1, 2, cubeDef) //Beak
+		.texOffs(0, 8).addBox(-2, 5.51F, .5F, 1, 1, 2, cubeDef) //Right Horn
+		.texOffs(0, 11).addBox(1, 5.51F, .5F, 1, 1, 2, cubeDef), PartPose.offset(0, 8.5F, -2.5F)); //Left Horn
 		partdefinition.addOrReplaceChild("right_wing", CubeListBuilder.create().texOffs(0, 24).addBox(-1, 0, 0, 1, 3, 5, cubeDef), PartPose.offset(-2, 18, -2));
 		partdefinition.addOrReplaceChild("left_wing", CubeListBuilder.create().texOffs(20, 24).addBox(0, 0, 0, 1, 3, 5, cubeDef), PartPose.offset(2, 18, -2));
-		partdefinition.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(18, 0).addBox(-2, 0, -2, 2, 3, 2, cubeDef), PartPose.offset(0, 21, 0));
-		partdefinition.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(8, 20).addBox(0, 0, -2, 2, 3, 2, cubeDef), PartPose.offset(0, 21, 0));
+		partdefinition.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(18, 0).addBox(-2, 0, -2, 2, 3, 2, cubeDef), PartPose.ZERO);
+		partdefinition.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(8, 20).addBox(0, 0, -2, 2, 3, 2, cubeDef), PartPose.ZERO);
 		return LayerDefinition.create(meshdefinition, 32, 32);
 	}
 	@Override public void setupAnim(EntityRobbin entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
 		head.xRot = headPitch * Mth.DEG_TO_RAD / 2;
 		head.yRot = netHeadYaw * Mth.DEG_TO_RAD;
+		float r = body.xRot = - Mth.PI / 12;
 		float f = Mth.cos(limbSwing * .6662F) * 1.4F * limbSwingAmount;
-		tail.xRot = f * .35F + .2618F;
+		tail.xRot = f * .35F - r;
 		if(!entity.onGround()) {
 			float f1 = Mth.cos(ageInTicks * 80 * Mth.DEG_TO_RAD) / 1.5F - Mth.PI / 6;
+			tail.yRot = 0;
 			rightWing.yRot = f1;
 			leftWing.yRot = -f1;
-			rightLeg.xRot = leftLeg.xRot = tail.yRot = 0;
+			rightLeg.xRot = leftLeg.xRot = - Mth.PI / 5;
+			rightLeg.y = leftLeg.y = 20.5F;
+			rightLeg.z = leftLeg.z = .95F;
 		} else {
 			tail.yRot = f * .4F;
 			rightWing.yRot = leftWing.yRot = 0;
 			rightLeg.xRot = f;
 			leftLeg.xRot = Mth.cos(limbSwing * .6662F + Mth.PI) * 1.4F * limbSwingAmount;
+			rightLeg.y = leftLeg.y = 21;
+			rightLeg.z = leftLeg.z = 0;
 		}
 	}
 	@Override public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, int color) {
