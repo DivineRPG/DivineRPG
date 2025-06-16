8953edd64ffbea812b39b0b77006904cb3713251
PegasDoggeAM <Shaipple@gmail.com>
2025-06-16 11:19:40 +1100

    Update ItemAnchor.java

--- src/main/java/divinerpg/items/vanilla/ItemAnchor.java
+++ src/main/java/divinerpg/items/vanilla/ItemAnchor.java
@@ -1,21 +1,23 @@
 package divinerpg.items.vanilla;
 
 import divinerpg.entities.projectile.DivineThrowableProjectile;
-import divinerpg.enums.*;
+import divinerpg.enums.ToolStats;
 import divinerpg.items.base.ItemModSword;
-import divinerpg.registries.*;
 import divinerpg.util.LocalizeUtils;
 import net.minecraft.network.chat.Component;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.*;
-import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import net.neoforged.api.distmarker.*;
+
 import java.util.List;
 import java.util.function.Supplier;
+
+import static divinerpg.registries.SoundRegistry.BLITZ;
 
 public class ItemAnchor extends ItemModSword {
     protected Supplier<EntityType<? extends DivineThrowableProjectile>> projectileType;
@@ -25,17 +27,17 @@ public class ItemAnchor extends ItemModSword {
         this.projectileType = projectileType;
         this.baseDamageTooltip = baseDamageTooltip;
     }
-    //TODO: it doesn't loose durability, for some reason
     @Override public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
         ItemStack stack = player.getItemInHand(hand);
-        player.playSound(SoundRegistry.BLITZ.get(), 1, 1);
+        player.playSound(BLITZ.get(), 1, 1);
         if(!world.isClientSide) {
             Projectile bullet = projectileType.get().create(world);
             bullet.setOwner(player);
             bullet.setPos(player.getEyePosition().add(0, -.15, 0));
             bullet.shootFromRotation(player, player.getXRot(), player.getYRot(), 0, 1.5F, .5F);
             world.addFreshEntity(bullet);
-        } player.awardStat(Stats.ITEM_USED.get(this));
+        } if(!player.isCreative()) stack.hurtAndBreak(1, player, LivingEntity.getSlotForHand(hand));
+        player.awardStat(Stats.ITEM_USED.get(this));
         return InteractionResultHolder.consume(stack);
     }
     @OnlyIn(Dist.CLIENT)
