1735ce0adc39914699baeb62e220e01540a3dcd3
brian-catcow-b <brianjbillman@gmail.com>
2025-06-26 22:21:20 -0400

    switch ENDER_SPIDER register arguments to specify MONSTER, not MONSTER_DARKNESS_AGILE

--- src/main/java/divinerpg/events/SpawnEvents.java
+++ src/main/java/divinerpg/events/SpawnEvents.java
@@ -112,7 +112,7 @@ public class SpawnEvents {
 		register(e, SCORCHER.get(), SpawnType.FLY, MONSTER);
 		register(e, WILDFIRE.get(), SpawnType.GROUND, MONSTER);
     	//End
-		register(e, ENDER_SPIDER.get(), SpawnType.AGILE, MONSTER_DARKNESS_AGILE);
+		register(e, ENDER_SPIDER.get(), SpawnType.AGILE, MONSTER);
 		register(e, ENDER_TRIPLETS.get(), SpawnType.GROUND, (en, s, t, p, r) -> difficultyFilter(en, s, t, p, r) && p.getY() > 60);
 		register(e, ENDER_WATCHER.get(), SpawnType.GROUND, MONSTER);
 		register(e, ENDER_SCROUNGE.get(), SpawnType.GROUND, DARKNESS);
