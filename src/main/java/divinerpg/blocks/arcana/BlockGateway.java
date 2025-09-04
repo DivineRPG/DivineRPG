1172b28c00b59d6f456c69e80ae06405b6abde3e
PegasDoggeAM <Shaipple@gmail.com>
2025-09-05 00:38:27 +1100

    How did tweaks get here
    - Added cooldown check when interacting with the gateway
    - Little improve for Brittle Grass placement
    - Returned arcana_shot.png (oops)

--- src/main/java/divinerpg/blocks/arcana/BlockGateway.java
+++ src/main/java/divinerpg/blocks/arcana/BlockGateway.java
@@ -2,10 +2,9 @@ package divinerpg.blocks.arcana;
 
 import com.mojang.serialization.MapCodec;
 import divinerpg.block_entities.block.PortalBlockEntity;
-import divinerpg.registries.*;
+import divinerpg.registries.DataComponentRegistry;
 import divinerpg.util.UniversalPosition;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.*;
@@ -22,22 +21,19 @@ import net.minecraft.world.level.portal.DimensionTransition;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.Nullable;
 
+import static divinerpg.registries.BlockEntityRegistry.PORTAL;
+import static divinerpg.registries.ItemRegistry.teleportation_star;
+import static divinerpg.registries.TagRegistry.GATEWAY_RECEIVER;
+import static net.minecraft.core.registries.Registries.DIMENSION;
+import static net.minecraft.world.level.block.RenderShape.MODEL;
+
 public class BlockGateway extends BaseEntityBlock implements Portal {
     public static final MapCodec<BlockGateway> CODEC = simpleCodec(BlockGateway::new);
     @Override public MapCodec<BlockGateway> codec() {return CODEC;}
-    public BlockGateway(Properties properties) {
-        super(properties.instrument(NoteBlockInstrument.BASS));
-    }
-    @Override
-    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
-        return BlockEntityRegistry.PORTAL.get().create(pos, state);
-    }
-    @Override
-    public RenderShape getRenderShape(BlockState state) {
-        return RenderShape.MODEL;
-    }
-    @Override
-    public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
+    public BlockGateway(Properties properties) {super(properties.instrument(NoteBlockInstrument.BASS));}
+    @Override public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {return PORTAL.get().create(pos, state);}
+    @Override public RenderShape getRenderShape(BlockState state) {return MODEL;}
+    @Override public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
         if(!entity.isOnPortalCooldown() && level instanceof ServerLevel serverLevel) {
             DimensionTransition d = getPortalDestination(serverLevel, entity, pos);
             if(d != null) {
@@ -47,21 +43,20 @@ public class BlockGateway extends BaseEntityBlock implements Portal {
         }
     }
     @Override public ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result) {
-        if(stack.is(ItemRegistry.teleportation_star) && level.getBlockEntity(pos) instanceof PortalBlockEntity portal) {
+        if(stack.is(teleportation_star) && !player.getCooldowns().isOnCooldown(teleportation_star.get()) && level.getBlockEntity(pos) instanceof PortalBlockEntity portal) {
             if(stack.has(DataComponentRegistry.dimension) && stack.has(DataComponentRegistry.position)) {
-                portal.targetPosition = new UniversalPosition(ResourceKey.create(Registries.DIMENSION, stack.get(DataComponentRegistry.dimension)), stack.get(DataComponentRegistry.position));
+                portal.targetPosition = new UniversalPosition(ResourceKey.create(DIMENSION, stack.get(DataComponentRegistry.dimension)), stack.get(DataComponentRegistry.position));
                 level.playSound(null, pos, SoundEvents.BEACON_ACTIVATE, SoundSource.BLOCKS);
                 player.getCooldowns().addCooldown(stack.getItem(), 40);
                 return ItemInteractionResult.SUCCESS;
             }
         } return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
     }
-    @Override
-    public @Nullable DimensionTransition getPortalDestination(ServerLevel serverLevel, Entity entity, BlockPos blockPos) {
+    @Override public @Nullable DimensionTransition getPortalDestination(ServerLevel serverLevel, Entity entity, BlockPos blockPos) {
         if(serverLevel.getBlockEntity(blockPos) instanceof PortalBlockEntity portal && portal.hasTargetPos()) {
             ServerLevel targetLevel = portal.targetPosition.level(serverLevel.getServer());
-            if(targetLevel.getBlockState(portal.targetPosition.blockPos().below()).is(TagRegistry.GATEWAY_RECEIVER))
-                return new DimensionTransition(targetLevel, portal.targetPosition.blockPosVec().add(.5, 0, .5), entity.getKnownMovement(), entity.getYRot(), entity.getXRot(), false, DimensionTransition.DO_NOTHING.then(e -> e.level().playSound(null, e.getX(), e.getY(), e.getZ(), SoundEvents.ENDERMAN_TELEPORT, SoundSource.NEUTRAL, .7F, 1F)).then(e -> e.setPortalCooldown(40)));
+            if(targetLevel.getBlockState(portal.targetPosition.blockPos().below()).is(GATEWAY_RECEIVER))
+                return new DimensionTransition(targetLevel, portal.targetPosition.blockPosVec().add(.5, 0, .5), entity.getKnownMovement(), entity.getYRot(), entity.getXRot(), false, DimensionTransition.DO_NOTHING.then(e -> e.level().playSound(null, e.getX(), e.getY(), e.getZ(), SoundEvents.ENDERMAN_TELEPORT, SoundSource.NEUTRAL, .7F, 1)).then(e -> e.setPortalCooldown(40)));
             //DivineRPG.LOGGER.info("invalid target: " + portal.targetPosition);
         } return null;
     }
--- src/main/java/divinerpg/blocks/iceika/BlockBrittleGrass.java
+++ src/main/java/divinerpg/blocks/iceika/BlockBrittleGrass.java
@@ -7,50 +7,44 @@ import net.minecraft.world.level.*;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.*;
 import net.minecraft.world.level.block.state.properties.*;
-import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.phys.shapes.*;
+
+import static net.minecraft.world.level.block.SoundType.MOSS_CARPET;
+import static net.minecraft.world.level.material.MapColor.GLOW_LICHEN;
 
 public class BlockBrittleGrass extends BushBlock {
 	public static final MapCodec<BlockBrittleGrass> CODEC = simpleCodec(BlockBrittleGrass::new);
 	public static final BooleanProperty HANGING = BlockStateProperties.HANGING;
-	protected static final VoxelShape SHAPE = Block.box(2, 0, 2, 14, 12, 14), HANGING_SHAPE = Block.box(2, 4, 2, 14, 16, 14);
+	protected static final VoxelShape SHAPE = box(2, 0, 2, 14, 12, 14), HANGING_SHAPE = box(2, 4, 2, 14, 16, 14);
 	@Override public MapCodec<BlockBrittleGrass> codec() {return CODEC;}
 	public BlockBrittleGrass(Properties properties) {
-		super(properties.mapColor(MapColor.GLOW_LICHEN).sound(SoundType.MOSS_CARPET).dynamicShape().offsetType(BlockBehaviour.OffsetType.XZ));
-		registerDefaultState(this.stateDefinition.any().setValue(HANGING, false));
+		super(properties.mapColor(GLOW_LICHEN).sound(MOSS_CARPET).dynamicShape().offsetType(OffsetType.XZ));
+		registerDefaultState(stateDefinition.any().setValue(HANGING, false));
 	}
-	@Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(HANGING);
-    }
-	@Override
-	public VoxelShape getShape(BlockState state, BlockGetter p_154526_, BlockPos p_154527_, CollisionContext p_154528_) {
+	@Override protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {builder.add(HANGING);}
+	@Override public VoxelShape getShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context) {
 		return state.getValue(HANGING) ? HANGING_SHAPE : SHAPE;
 	}
-	@Override
-	protected boolean mayPlaceOn(BlockState state, BlockGetter getter, BlockPos pos) {
-		return getter.getBlockState(pos.below()).isFaceSturdy(getter, pos.below(), Direction.UP) || getter.getBlockState(pos.above()).isFaceSturdy(getter, pos.above(), Direction.DOWN);
+	@Override protected boolean mayPlaceOn(BlockState state, BlockGetter getter, BlockPos pos) {
+		BlockState belowState = getter.getBlockState(pos.below());
+		BlockState aboveState = getter.getBlockState(pos.above());
+		boolean sturdyFace = belowState.isFaceSturdy(getter, pos.below(), Direction.UP)
+				|| aboveState.isFaceSturdy(getter, pos.above(), Direction.DOWN);
+		return sturdyFace || belowState.getBlock() instanceof FarmBlock;}
+	@Override public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor level, BlockPos pos, BlockPos neighborPos) {
+		return getConnectedDirection(state).getOpposite() == direction && !state.canSurvive(level, pos) ? Blocks.AIR.defaultBlockState() : super.updateShape(state, direction, neighborState, level, pos, neighborPos);
 	}
-	@Override
-	public BlockState updateShape(BlockState state, Direction direction, BlockState s, LevelAccessor level, BlockPos pos, BlockPos p) {
-		return level.getBlockState(pos.above()).isFaceSturdy(level, pos.above(), Direction.DOWN) ? defaultBlockState().setValue(HANGING, true) : level.getBlockState(pos.below()).isFaceSturdy(level, pos.below(), Direction.UP) ? defaultBlockState() : Blocks.AIR.defaultBlockState();
+	@Override public boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {return mayPlaceOn(state, level, pos);}
+	protected static Direction getConnectedDirection(BlockState state) {return state.getValue(HANGING) ? Direction.DOWN : Direction.UP;}
+	@Override public BlockState getStateForPlacement(BlockPlaceContext context) {
+		Direction[] var3 = context.getNearestLookingDirections();
+		for(Direction direction : var3) {
+			if(direction.getAxis() == Direction.Axis.Y) {
+				BlockState blockstate = defaultBlockState().setValue(HANGING, direction == Direction.UP);
+				if(blockstate.canSurvive(context.getLevel(), context.getClickedPos())) return blockstate;
+			}
+		} return null;
 	}
-	@Override
-	public boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
-		return mayPlaceOn(null, level, pos);
-	}
-	@Override
-	public BlockState getStateForPlacement(BlockPlaceContext context) {
-		return context.getLevel().getBlockState(context.getClickedPos().above()).isFaceSturdy(context.getLevel(), context.getClickedPos().above(), Direction.DOWN) ? defaultBlockState().setValue(HANGING, true) : context.getLevel().getBlockState(context.getClickedPos().below()).isFaceSturdy(context.getLevel(), context.getClickedPos().below(), Direction.UP) ? defaultBlockState() : Blocks.AIR.defaultBlockState();
-	}
-
-        @Override
-        public int getFlammability(BlockState state, BlockGetter getter, BlockPos pos, Direction face) {
-            return 100;
-        }
-
-        @Override
-        public int getFireSpreadSpeed(BlockState state, BlockGetter getter, BlockPos pos, Direction face) {
-            return 60;
-    }
+    @Override public int getFlammability(BlockState state, BlockGetter getter, BlockPos pos, Direction face) {return 100;}
+    @Override public int getFireSpreadSpeed(BlockState state, BlockGetter getter, BlockPos pos, Direction face) {return 60;}
 }
