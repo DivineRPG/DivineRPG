6d5fa0a96264ce26f80281d7ec36c68d4222f53c
PegasDoggeAM <Shaipple@gmail.com>
2025-10-07 23:37:07 +1100

    Firevantium
    - Mostly fixed the sound spam for Sky Fire.
    - Fixed multiple Sky Fires causing the player to fly really high.
    - Sky Fire and Elevantium no longer negate fall damage and holding Shift down prevents the bounce.

--- src/main/java/divinerpg/blocks/arcana/BlockElevantium.java
+++ src/main/java/divinerpg/blocks/arcana/BlockElevantium.java
@@ -1,41 +1,36 @@
 package divinerpg.blocks.arcana;
 
 import divinerpg.blocks.base.BlockMod;
-import divinerpg.registries.SoundRegistry;
 import net.minecraft.core.*;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.*;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.material.*;
-import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.*;
 
 import javax.annotation.Nullable;
 
+import static divinerpg.registries.SoundRegistry.CONSTRUCTOR_PUNCH;
+import static net.minecraft.sounds.SoundSource.BLOCKS;
+import static net.minecraft.world.level.block.Blocks.DIAMOND_BLOCK;
+import static net.minecraft.world.level.material.MapColor.COLOR_LIGHT_BLUE;
+import static net.minecraft.world.level.material.PushReaction.DESTROY;
+
 public class BlockElevantium extends BlockMod {
     protected static final VoxelShape ELEVANTIUM = Shapes.or(box(1, 0, 1, 15, 1, 15),
                                                              box(4.5, 1, 4.5, 11.5, 2, 11.5));
-    public BlockElevantium() {
-        super(Properties.ofFullCopy(Blocks.DIAMOND_BLOCK).mapColor(MapColor.COLOR_LIGHT_BLUE).pushReaction(PushReaction.DESTROY));
-    }
-    @Override public void stepOn(Level world, BlockPos pos, BlockState state, Entity entity) {
-        int signal = world.getBestNeighborSignal(pos);
-        if(signal > 0) {
-            entity.addDeltaMovement(new Vec3(0, signal * .25, 0));
-            entity.hasImpulse = true;
-            world.playSound(null, pos, SoundRegistry.CONSTRUCTOR_PUNCH.get(), SoundSource.BLOCKS, .75F, .9F);
+    public BlockElevantium() {super(Properties.ofFullCopy(DIAMOND_BLOCK).mapColor(COLOR_LIGHT_BLUE).pushReaction(DESTROY));}
+    @Override public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
+        int signal = level.getBestNeighborSignal(pos);
+        boolean canJump = !level.getBlockCollisions(entity, entity.getBoundingBox().expandTowards(0, 2, 0)).iterator().hasNext();
+        if(signal > 0 && !entity.isSuppressingBounce() && canJump) {
+            //TODO: holding the jump button as well as pressing the shift down at the right timing causes the sound to play, but ignore the launch (seems like the reason is that setDeltaMovement doesn't apply immediately)
+            entity.push(0, signal * .25, 0);
+            level.playSound(null, pos, CONSTRUCTOR_PUNCH.get(), BLOCKS, .75F, .9F);
         }
     }
-    @Override public VoxelShape getShape(BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
-        return ELEVANTIUM;
-    }
+    @Override public VoxelShape getShape(BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {return ELEVANTIUM;}
     @Override public VoxelShape getCollisionShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context) {
         return Shapes.create(ELEVANTIUM.bounds().inflate(.0625, .125, .0625));
     }
-    @Override
-    public boolean canConnectRedstone(BlockState state, BlockGetter level, BlockPos pos, @Nullable Direction direction) {
-        return true;
-    }
+    @Override public boolean canConnectRedstone(BlockState state, BlockGetter level, BlockPos pos, @Nullable Direction direction) {return true;}
 }
