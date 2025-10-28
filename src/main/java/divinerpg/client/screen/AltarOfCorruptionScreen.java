bf80b12cd7a278509627156212772d568b165a01
PegasDoggeAM <Shaipple@gmail.com>
2025-10-29 00:24:43 +1100

    Corruption
    - Fixed Altar of Corruption screen

--- src/main/java/divinerpg/client/screen/AltarOfCorruptionScreen.java
+++ src/main/java/divinerpg/client/screen/AltarOfCorruptionScreen.java
@@ -2,15 +2,14 @@ package divinerpg.client.screen;
 
 import com.google.common.collect.Lists;
 import com.mojang.blaze3d.platform.Lighting;
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
-import divinerpg.DivineRPG;
 import divinerpg.client.menu.AltarOfCorruptionMenu;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.*;
 import net.minecraft.client.model.BookModel;
-import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.core.Holder;
 import net.minecraft.core.IdMap;
@@ -24,201 +23,162 @@ import net.minecraft.world.item.enchantment.Enchantment;
 import net.neoforged.api.distmarker.*;
 
 import java.util.List;
+import java.util.Optional;
+
+import static divinerpg.DivineRPG.MODID;
+import static net.minecraft.client.model.geom.ModelLayers.BOOK;
+import static net.minecraft.core.registries.Registries.ENCHANTMENT;
 
 @OnlyIn(Dist.CLIENT)
 public class AltarOfCorruptionScreen extends AbstractContainerScreen<AltarOfCorruptionMenu> {
-    private static final ResourceLocation ENCHANTING_TABLE_LOCATION = ResourceLocation.fromNamespaceAndPath(DivineRPG.MODID, "textures/gui/altar_of_corruption.png");
-    private static final ResourceLocation ENCHANTING_BOOK_LOCATION = ResourceLocation.fromNamespaceAndPath(DivineRPG.MODID, "textures/model/altar_book.png");
+    //TODO: to split sprites just like vanilla
+    private static final ResourceLocation ENCHANTING_TABLE_LOCATION = ResourceLocation.fromNamespaceAndPath(MODID, "textures/gui/altar_of_corruption.png");
+    private static final ResourceLocation ENCHANTING_BOOK_LOCATION = ResourceLocation.fromNamespaceAndPath(MODID, "textures/model/altar_book.png");
     private final RandomSource random = RandomSource.create();
     private BookModel bookModel;
     public int time;
-    public float flip;
-    public float oFlip;
-    public float flipT;
-    public float flipA;
-    public float open;
-    public float oOpen;
+    public float flip, oFlip, flipT, flipA, open, oOpen;
     private ItemStack last = ItemStack.EMPTY;
-
-    public AltarOfCorruptionScreen(AltarOfCorruptionMenu p_98754_, Inventory p_98755_, Component p_98756_) {
-        super(p_98754_, p_98755_, p_98756_);
-    }
-
-    protected void init() {
+    public AltarOfCorruptionScreen(AltarOfCorruptionMenu menu, Inventory inventory, Component component) {super(menu, inventory, component);}
+    @Override protected void init() {
         super.init();
-        this.bookModel = new BookModel(this.minecraft.getEntityModels().bakeLayer(ModelLayers.BOOK));
+        bookModel = new BookModel(minecraft.getEntityModels().bakeLayer(BOOK));
     }
-
-    public void containerTick() {
+    @Override public void containerTick() {
         super.containerTick();
-        this.tickBook();
+        tickBook();
     }
-
-    public boolean mouseClicked(double p_98758_, double p_98759_, int p_98760_) {
-        int i = (this.width - this.imageWidth) / 2;
-        int j = (this.height - this.imageHeight) / 2;
-
-        for(int k = 0; k < 3; ++k) {
-            double d0 = p_98758_ - (double)(i + 60);
-            double d1 = p_98759_ - (double)(j + 14 + 19 * k);
-            if (d0 >= 0.0D && d1 >= 0.0D && d0 < 108.0D && d1 < 19.0D && this.menu.clickMenuButton(this.minecraft.player, k)) {
-                this.minecraft.gameMode.handleInventoryButtonClick((this.menu).containerId, k);
+    @Override public boolean mouseClicked(double mouseX, double mouseY, int mouseZ) {
+        int i = (width - imageWidth) / 2;
+        int j = (height - imageHeight) / 2;
+        for(int k = 0; k < 3; k++) {
+            double d0 = mouseX - (double)(i + 60);
+            double d1 = mouseY - (double)(j + 14 + 19 * k);
+            if(d0 >= 0 && d1 >= 0 && d0 < 108 && d1 < 19 && menu.clickMenuButton(minecraft.player, k)) {
+                minecraft.gameMode.handleInventoryButtonClick(menu.containerId, k);
                 return true;
             }
-        }
-
-        return super.mouseClicked(p_98758_, p_98759_, p_98760_);
+        } return super.mouseClicked(mouseX, mouseY, mouseZ);
     }
-
-    protected void renderBg(GuiGraphics p_282430_, float p_282530_, int p_281621_, int p_283333_) {
-        int i = (this.width - this.imageWidth) / 2;
-        int j = (this.height - this.imageHeight) / 2;
-        p_282430_.blit(ENCHANTING_TABLE_LOCATION, i, j, 0, 0, this.imageWidth, this.imageHeight);
-        this.renderBook(p_282430_, i, j, p_282530_);
-        EnchantmentNames.getInstance().initSeed((long)this.menu.getEnchantmentSeed());
-        int k = this.menu.getGoldCount();
-
-        for(int l = 0; l < 3; ++l) {
+    @Override protected void renderBg(GuiGraphics guiGraphics, float partialTick, int mouseX, int mouseY) {
+        int i = (width - imageWidth) / 2;
+        int j = (height - imageHeight) / 2;
+        guiGraphics.blit(ENCHANTING_TABLE_LOCATION, i, j, 0, 0, imageWidth, imageHeight);
+        renderBook(guiGraphics, i, j, partialTick);
+        EnchantmentNames.getInstance().initSeed(menu.getEnchantmentSeed());
+        int k = menu.getGoldCount();
+        for(int l = 0; l < 3; l++) {
             int i1 = i + 60;
             int j1 = i1 + 20;
-            int k1 = (this.menu).costs[l];
-            if (k1 == 0) {
-                p_282430_.blit(ENCHANTING_TABLE_LOCATION, i1, j + 14 + 19 * l, 0, 185, 108, 19);
+            int k1 = menu.costs[l];if (k1 == 0) {
+                RenderSystem.enableBlend();
+                guiGraphics.blit(ENCHANTING_TABLE_LOCATION, i1, j + 14 + 19 * l, 0, 185, 108, 19);
+                RenderSystem.disableBlend();
             } else {
-                String s = "" + k1;
-                int l1 = 86 - this.font.width(s);
-                FormattedText formattedtext = EnchantmentNames.getInstance().getRandomName(this.font, l1);
+                String s = String.valueOf(k1);
+                int l1 = 86 - font.width(s);
+                FormattedText formattedtext = EnchantmentNames.getInstance().getRandomName(font, l1);
                 int i2 = 6839882;
-                if (((k < l + 1 || this.minecraft.player.experienceLevel < k1) && !this.minecraft.player.getAbilities().instabuild) || this.menu.enchantClue[l] == -1) { // Forge: render buttons as disabled when enchantable but enchantability not met on lower levels
-                    p_282430_.blit(ENCHANTING_TABLE_LOCATION, i1, j + 14 + 19 * l, 0, 185, 108, 19);
-                    p_282430_.blit(ENCHANTING_TABLE_LOCATION, i1 + 1, j + 15 + 19 * l, 16 * l, 239, 16, 16);
-                    p_282430_.drawWordWrap(this.font, formattedtext, j1, j + 16 + 19 * l, l1, (i2 & 16711422) >> 1);
+                if(((k < l + 1 || minecraft.player.experienceLevel < k1) && !minecraft.player.getAbilities().instabuild) || menu.enchantClue[l] == -1) {
+                    RenderSystem.enableBlend();
+                    guiGraphics.blit(ENCHANTING_TABLE_LOCATION, i1, j + 14 + 19 * l, 0, 185, 108, 19);
+                    guiGraphics.blit(ENCHANTING_TABLE_LOCATION, i1 + 1, j + 15 + 19 * l, 16 * l, 239, 16, 16);
+                    RenderSystem.disableBlend();
+                    guiGraphics.drawWordWrap(font, formattedtext, j1, j + 16 + 19 * l, l1, (i2 & 16711422) >> 1);
                     i2 = 4226832;
                 } else {
-                    int j2 = p_281621_ - (i + 60);
-                    int k2 = p_283333_ - (j + 14 + 19 * l);
-                    if (j2 >= 0 && k2 >= 0 && j2 < 108 && k2 < 19) {
-                        p_282430_.blit(ENCHANTING_TABLE_LOCATION, i1, j + 14 + 19 * l, 0, 204, 108, 19);
+                    int j2 = mouseX - (i + 60);
+                    int k2 = mouseY - (j + 14 + 19 * l);
+                    if(j2 >= 0 && k2 >= 0 && j2 < 108 && k2 < 19) {
+                        guiGraphics.blit(ENCHANTING_TABLE_LOCATION, i1, j + 14 + 19 * l, 0, 204, 108, 19);
                         i2 = 16777088;
-                    } else {
-                        p_282430_.blit(ENCHANTING_TABLE_LOCATION, i1, j + 14 + 19 * l, 0, 166, 108, 19);
-                    }
-
-                    p_282430_.blit(ENCHANTING_TABLE_LOCATION, i1 + 1, j + 15 + 19 * l, 16 * l, 223, 16, 16);
-                    p_282430_.drawWordWrap(this.font, formattedtext, j1, j + 16 + 19 * l, l1, i2);
+                    } else guiGraphics.blit(ENCHANTING_TABLE_LOCATION, i1, j + 14 + 19 * l, 0, 166, 108, 19);
+                    guiGraphics.blit(ENCHANTING_TABLE_LOCATION, i1 + 1, j + 15 + 19 * l, 16 * l, 223, 16, 16);
+                    RenderSystem.disableBlend();
+                    guiGraphics.drawWordWrap(font, formattedtext, j1, j + 16 + 19 * l, l1, i2);
                     i2 = 8453920;
-                }
-
-                p_282430_.drawString(this.font, s, j1 + 86 - this.font.width(s), j + 16 + 19 * l + 7, i2);
+                } guiGraphics.drawString(font, s, j1 + 86 - font.width(s), j + 16 + 19 * l + 7, i2);
             }
         }
-
     }
-
-    private void renderBook(GuiGraphics p_289697_, int p_289667_, int p_289669_, float p_289670_) {
-        float f = Mth.lerp(p_289670_, this.oOpen, this.open);
-        float f1 = Mth.lerp(p_289670_, this.oFlip, this.flip);
+    private void renderBook(GuiGraphics guiGraphics, int x, int y, float partialTick) {
+        float f = Mth.lerp(partialTick, oOpen, open);
+        float f1 = Mth.lerp(partialTick, oFlip, flip);
         Lighting.setupForEntityInInventory();
-        p_289697_.pose().pushPose();
-        p_289697_.pose().translate((float)p_289667_ + 33.0F, (float)p_289669_ + 31.0F, 100.0F);
-        p_289697_.pose().scale(-40.0F, 40.0F, 40.0F);
-        p_289697_.pose().mulPose(Axis.XP.rotationDegrees(25.0F));
-        p_289697_.pose().translate((1.0F - f) * 0.2F, (1.0F - f) * 0.1F, (1.0F - f) * 0.25F);
-        float f3 = -(1.0F - f) * 90.0F - 90.0F;
-        p_289697_.pose().mulPose(Axis.YP.rotationDegrees(f3));
-        p_289697_.pose().mulPose(Axis.XP.rotationDegrees(180.0F));
-        float f4 = Mth.clamp(Mth.frac(f1 + 0.25F) * 1.6F - 0.3F, 0.0F, 1.0F);
-        float f5 = Mth.clamp(Mth.frac(f1 + 0.75F) * 1.6F - 0.3F, 0.0F, 1.0F);
-        this.bookModel.setupAnim(0.0F, f4, f5, f);
-        VertexConsumer vertexconsumer = p_289697_.bufferSource().getBuffer(this.bookModel.renderType(ENCHANTING_BOOK_LOCATION));
-        this.bookModel.renderToBuffer(p_289697_.pose(), vertexconsumer, 15728880, OverlayTexture.NO_OVERLAY);
-        p_289697_.flush();
-        p_289697_.pose().popPose();
+        guiGraphics.pose().pushPose();
+        guiGraphics.pose().translate((float)x + 33, (float)y + 31, 100);
+        guiGraphics.pose().scale(-40, 40, 40);
+        guiGraphics.pose().mulPose(Axis.XP.rotationDegrees(25));
+        guiGraphics.pose().translate((1 - f) * .2F, (1 - f) * .1F, (1 - f) * .25F);
+        float f3 = -(1 - f) * 90 - 90;
+        guiGraphics.pose().mulPose(Axis.YP.rotationDegrees(f3));
+        guiGraphics.pose().mulPose(Axis.XP.rotationDegrees(180));
+        float f4 = Mth.clamp(Mth.frac(f1 + .25F) * 1.6F - .3F, 0, 1);
+        float f5 = Mth.clamp(Mth.frac(f1 + .75F) * 1.6F - .3F, 0, 1);
+        bookModel.setupAnim(0, f4, f5, f);
+        VertexConsumer vertexconsumer = guiGraphics.bufferSource().getBuffer(bookModel.renderType(ENCHANTING_BOOK_LOCATION));
+        bookModel.renderToBuffer(guiGraphics.pose(), vertexconsumer, 15728880, OverlayTexture.NO_OVERLAY);
+        guiGraphics.flush();
+        guiGraphics.pose().popPose();
         Lighting.setupFor3DItems();
     }
-
-    @Override public void render(GuiGraphics matrixStack, int mouseX, int mouseY, float partialTicks) {
-        partialTicks = this.minecraft.getFrameTimeNs();
-        this.renderBackground(matrixStack, mouseX, mouseY, partialTicks);
-        super.render(matrixStack, mouseX, mouseY, partialTicks);
-        this.renderTooltip(matrixStack, mouseX, mouseY);
-        boolean flag = this.minecraft.player.getAbilities().instabuild;
-        int i = this.menu.getGoldCount();
-
-        for(int j = 0; j < 3; ++j) {
-            int k = (this.menu).costs[j];
-            IdMap<Holder<Enchantment>> idmap = minecraft.level.registryAccess().registryOrThrow(Registries.ENCHANTMENT).asHolderIdMap();
-            Holder<Enchantment> enchantment = idmap.byId((this.menu).enchantClue[j]);
-            int l = (this.menu).levelClue[j];
+    @Override public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
+        super.render(guiGraphics, mouseX, mouseY, partialTick);
+        renderTooltip(guiGraphics, mouseX, mouseY);
+        boolean flag = minecraft.player.getAbilities().instabuild;
+        int i = menu.getGoldCount();
+        for(int j = 0; j < 3; j++) {
+            int k = menu.costs[j];
+            Optional<Holder.Reference<Enchantment>> optional = minecraft.level.registryAccess().registryOrThrow(ENCHANTMENT).getHolder(menu.enchantClue[j]);
+            int l = menu.levelClue[j];
             int i1 = j + 1;
-            if (this.isHovering(60, 14 + 19 * j, 108, 17, (double)mouseX, (double)mouseY) && k > 0) {
+            if (isHovering(60, 14 + 19 * j, 108, 17, mouseX, mouseY) && k > 0) {
                 List<Component> list = Lists.newArrayList();
-                list.add((Component.translatable("container.enchant.clue", enchantment == null ? "" : Enchantment.getFullname(enchantment, l))).withStyle(ChatFormatting.WHITE));
-                if (enchantment == null) {
+                list.add(Component.translatable("container.enchant.clue", optional.isEmpty() ? "" : Enchantment.getFullname(optional.get(), l)).withStyle(ChatFormatting.WHITE));
+                if(optional.isEmpty()) {
                     list.add(Component.literal(""));
                     list.add(Component.translatable("neoforge.container.enchant.limitedEnchantability").withStyle(ChatFormatting.RED));
                 } else if (!flag) {
                     list.add(CommonComponents.EMPTY);
-                    if (this.minecraft.player.experienceLevel < k) {
-                        list.add(Component.translatable("container.enchant.level.requirement", (this.menu).costs[j]).withStyle(ChatFormatting.RED));
-                    } else {
+                    if(minecraft.player.experienceLevel < k) list.add(Component.translatable("container.enchant.level.requirement", menu.costs[j]).withStyle(ChatFormatting.RED));
+                    else {
                         MutableComponent mutablecomponent;
-                        if (i1 == 1) {
-                            mutablecomponent = Component.translatable("container.enchant.lapis.one");
-                        } else {
-                            mutablecomponent = Component.translatable("container.enchant.lapis.many", i1);
-                        }
-
+                        if (i1 == 1) mutablecomponent = Component.translatable("container.enchant.lapis.one");
+                        else mutablecomponent = Component.translatable("container.enchant.lapis.many", i1);
                         list.add(mutablecomponent.withStyle(i >= i1 ? ChatFormatting.GRAY : ChatFormatting.RED));
                         MutableComponent mutablecomponent1;
-                        if (i1 == 1) {
-                            mutablecomponent1 = Component.translatable("container.enchant.level.one");
-                        } else {
-                            mutablecomponent1 = Component.translatable("container.enchant.level.many", i1);
-                        }
-
+                        if(i1 == 1) mutablecomponent1 = Component.translatable("container.enchant.level.one");
+                        else mutablecomponent1 = Component.translatable("container.enchant.level.many", i1);
                         list.add(mutablecomponent1.withStyle(ChatFormatting.GRAY));
                     }
-                }
-
-                matrixStack.renderComponentTooltip(this.font, list, mouseX, mouseY);
+                } guiGraphics.renderComponentTooltip(font, list, mouseX, mouseY);
                 break;
             }
         }
-
     }
-
     public void tickBook() {
-        ItemStack itemstack = this.menu.getSlot(0).getItem();
-        if (!ItemStack.matches(itemstack, this.last)) {
-            this.last = itemstack;
-
+        ItemStack itemstack = menu.getSlot(0).getItem();
+        if(!ItemStack.matches(itemstack, last)) {
+            last = itemstack;
             do {
-                this.flipT += (float)(this.random.nextInt(4) - this.random.nextInt(4));
-            } while(this.flip <= this.flipT + 1.0F && this.flip >= this.flipT - 1.0F);
-        }
-
-        ++this.time;
-        this.oFlip = this.flip;
-        this.oOpen = this.open;
+                flipT = flipT + (float)(random.nextInt(4) - random.nextInt(4));
+            } while (flip <= flipT + 1 && flip >= flipT - 1);
+        } time++;
+        oFlip = flip;
+        oOpen = open;
         boolean flag = false;
 
-        for(int i = 0; i < 3; ++i) {
-            if ((this.menu).costs[i] != 0) {
+        for(int i = 0; i < 3; i++) {
+            if(menu.costs[i] != 0) {
                 flag = true;
+                break;
             }
-        }
-
-        if (flag) {
-            this.open += 0.2F;
-        } else {
-            this.open -= 0.2F;
-        }
-
-        this.open = Mth.clamp(this.open, 0.0F, 1.0F);
-        float f1 = (this.flipT - this.flip) * 0.4F;
-        f1 = Mth.clamp(f1, -0.2F, 0.2F);
-        this.flipA += (f1 - this.flipA) * 0.9F;
-        this.flip += this.flipA;
+        } if(flag) open += .2F;
+        else open -= .2F;
+        open = Mth.clamp(open, 0, 1);
+        float f1 = (flipT - flip) * .4F;
+        f1 = Mth.clamp(f1, -.2F, .2F);
+        flipA = flipA + (f1 - flipA) * .9F;
+        flip = flip + flipA;
     }
 }
