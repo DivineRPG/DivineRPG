ff0d6b7d710033f8d2e6923deb42a8a8aad8d630
PegasDoggeAM <Shaipple@gmail.com>
2025-11-15 16:57:36 +1100

    Update Tooltips.java

--- src/main/java/divinerpg/events/Tooltips.java
+++ src/main/java/divinerpg/events/Tooltips.java
@@ -10,13 +10,13 @@ import net.minecraft.world.item.*;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.neoforge.common.Tags;
 import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
 
 import java.util.List;
 
 import static net.minecraft.ChatFormatting.*;
 import static net.neoforged.api.distmarker.Dist.CLIENT;
+import static net.neoforged.neoforge.common.Tags.Blocks.NEEDS_NETHERITE_TOOL;
 
 @OnlyIn(CLIENT)
 public class Tooltips {
@@ -24,21 +24,15 @@ public class Tooltips {
     public void onItemTooltip(final ItemTooltipEvent event) {
         List<Component> tooltip = event.getToolTip();
         ItemStack stack = event.getItemStack();
-        if(stack.getItem() instanceof TieredItem item) {
-            if(ClientConfig.Values.TOOL_TOOLTIPIFICATION
-                    && (stack.is(ItemTags.SHOVELS) || stack.is(ItemTags.PICKAXES) || stack.is(Tags.Items.MINING_TOOL_TOOLS)
-                    || stack.is(ItemTags.AXES) || stack.is(ItemTags.HOES) || stack.is(ItemTags.MINING_ENCHANTABLE)
-                    || stack.is(Tags.Items.TOOLS) && !stack.is(Tags.Items.MELEE_WEAPON_TOOLS) && !stack.is(ItemTags.SWORDS))) {
-                tooltip.add(1, LocalizeUtils.efficiency((int)item.getTier().getSpeed()));
-                TagKey<Block> tagKey = item.getTier().getIncorrectBlocksForDrops();
-                HolderSet<Block> holderSet = BuiltInRegistries.BLOCK.getOrCreateTag(tagKey);
-                if(holderSet.stream().allMatch(holder -> holder.is(BlockTags.INCORRECT_FOR_NETHERITE_TOOL))) tooltip.add(2, LocalizeUtils.harvestLevel(Component.literal(Items.OBSIDIAN.getName(stack).getString() + "+").withStyle(DARK_RED)));
-                else if(holderSet.stream().allMatch(holder -> holder.is(BlockTags.INCORRECT_FOR_DIAMOND_TOOL))) tooltip.add(2, LocalizeUtils.harvestLevel(Component.literal(Items.OBSIDIAN.getName(stack).getString()).withColor(7096985)));
-                else if(holderSet.stream().allMatch(holder -> holder.is(BlockTags.INCORRECT_FOR_IRON_TOOL))) tooltip.add(2, LocalizeUtils.harvestLevel(Component.literal(Items.DIAMOND.getName(stack).getString()).withColor(2289878)));
-                else if(holderSet.stream().allMatch(holder -> holder.is(BlockTags.INCORRECT_FOR_STONE_TOOL))) tooltip.add(2, LocalizeUtils.harvestLevel(Component.literal(Items.LAPIS_LAZULI.getName(stack).getString()).withColor(5931746)));
-                else if(holderSet.stream().allMatch(holder -> holder.is(BlockTags.INCORRECT_FOR_GOLD_TOOL)) || holderSet.stream().allMatch(holder -> holder.is(BlockTags.INCORRECT_FOR_WOODEN_TOOL))) tooltip.add(2, LocalizeUtils.harvestLevel(Component.literal(Items.COAL.getName(stack).getString()).withStyle(DARK_GRAY)));
-                else tooltip.add(2, LocalizeUtils.harvestLevel(Component.translatable("selectWorld.versionUnknown").withStyle(WHITE, OBFUSCATED)));
-            }
+        if(stack.getItem() instanceof DiggerItem item && ClientConfig.Values.TOOL_TOOLTIPIFICATION) {
+            tooltip.add(1, LocalizeUtils.efficiency((int)item.getTier().getSpeed()));
+            TagKey<Block> tagKey = item.getTier().getIncorrectBlocksForDrops();
+            HolderSet<Block> holderSet = BuiltInRegistries.BLOCK.getOrCreateTag(tagKey);
+            if(holderSet.stream().anyMatch(holder -> holder.is(BlockTags.NEEDS_STONE_TOOL))) tooltip.add(2, LocalizeUtils.harvestLevel(Component.literal(Items.COAL.getName(stack).getString()).withStyle(DARK_GRAY)));
+            else if(holderSet.stream().anyMatch(holder -> holder.is(BlockTags.NEEDS_IRON_TOOL))) tooltip.add(2, LocalizeUtils.harvestLevel(Component.literal(Items.LAPIS_LAZULI.getName(stack).getString()).withColor(5931746)));
+            else if(holderSet.stream().anyMatch(holder -> holder.is(BlockTags.NEEDS_DIAMOND_TOOL))) tooltip.add(2, LocalizeUtils.harvestLevel(Component.literal(Items.DIAMOND.getName(stack).getString()).withColor(2293477)));
+            else if(holderSet.stream().anyMatch(holder -> holder.is(NEEDS_NETHERITE_TOOL))) tooltip.add(2, LocalizeUtils.harvestLevel(Component.literal(Items.OBSIDIAN.getName(stack).getString()).withColor(7096985)));
+            else tooltip.add(2, LocalizeUtils.harvestLevel(Component.literal(Items.OBSIDIAN.getName(stack).getString() + "+").withStyle(DARK_RED)));
         }
     }
 }
