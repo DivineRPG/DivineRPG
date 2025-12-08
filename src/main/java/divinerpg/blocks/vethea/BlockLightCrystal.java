e80e89d41aed3f690f05e0ac432dee08f8a1a9ba
PegasDoggeAM <Shaipple@gmail.com>
2025-12-09 00:48:36 +1100

    Shiverthern sign textures

--- src/main/java/divinerpg/blocks/vethea/BlockLightCrystal.java
+++ src/main/java/divinerpg/blocks/vethea/BlockLightCrystal.java
@@ -1,13 +1,10 @@
 package divinerpg.blocks.vethea;
 
 import divinerpg.blocks.base.BlockMod;
-import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.state.*;
-import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
-import net.minecraft.world.level.material.*;
+import net.minecraft.world.level.material.MapColor;
+
+import static net.minecraft.world.level.block.Blocks.GLOWSTONE;
 
 public class BlockLightCrystal extends BlockMod {
-    public BlockLightCrystal(MapColor color) {
-        super(BlockBehaviour.Properties.of().mapColor(color).strength(0.3F).sound(SoundType.GLASS).instrument(NoteBlockInstrument.PLING).lightLevel((state) -> 15));
-    }
+    public BlockLightCrystal(MapColor color) {super(Properties.ofFullCopy(GLOWSTONE).mapColor(color));}
 }
