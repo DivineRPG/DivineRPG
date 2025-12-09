e38853587eebb164f4c281a003ec5bf8df1504d6
PegasDoggeAM <Shaipple@gmail.com>
2025-12-09 16:29:11 +1100

    eucozyvine
    Two more sign textures left

--- src/main/java/divinerpg/blocks/base/BlockModCrop.java
+++ src/main/java/divinerpg/blocks/base/BlockModCrop.java
@@ -1,5 +1,6 @@
 package divinerpg.blocks.base;
 
+import divinerpg.registries.BlockRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.*;
 import net.minecraft.world.level.block.CropBlock;
@@ -10,20 +11,23 @@ import static net.minecraft.world.level.block.Blocks.WHEAT;
 
 public class BlockModCrop extends CropBlock {
     private final ItemLike seed;
-    private static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[]{
-            box(0, 0, 0, 16, 3, 16),
-            box(0, 0, 0, 16, 6, 16),
-            box(0, 0, 0, 16, 9, 16),
-            box(0, 0, 0, 16, 11, 16),
-            box(0, 0, 0, 16, 11, 16),
-            box(0, 0, 0, 16, 11, 16),
-            box(0, 0, 0, 16, 11, 16),
-            box(0, 0, 0, 16, 11, 16)
-    };
+    private static VoxelShape[] createHeightByAge(int[] heights) {
+        VoxelShape[] shapes = new VoxelShape[heights.length];
+        for(int i = 0; i < heights.length; i++) shapes[i] = box(0, 0, 0, 16, heights[i], 16);
+        return shapes;
+    }
+    private static final VoxelShape[]
+            DEFAULT = createHeightByAge(new int[]{3, 6, 9, 11, 11, 11, 11, 11}),
+            EUCALYPTUS = createHeightByAge(new int[]{3, 5, 5, 7, 8, 10, 12, 14});
     public BlockModCrop(ItemLike seed) {
         super(Properties.ofFullCopy(WHEAT));
         this.seed = seed;
     }
-    @Override public VoxelShape getShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context) {return SHAPE_BY_AGE[state.getValue(getAgeProperty())];}
+    @Override public VoxelShape getShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context) {
+        VoxelShape[] shapes;
+        if(this == BlockRegistry.eucalyptusPlant.get()) shapes = EUCALYPTUS;
+        else shapes = DEFAULT;
+        return shapes[state.getValue(getAgeProperty())];
+    }
     @Override protected ItemLike getBaseSeedId() {return seed;}
 }
