9c4decdeb52d1909bd890b317f2c280c5e3caa2b
PegasDoggeAM <Shaipple@gmail.com>
2026-07-08 20:57:23 +1100

    Reverted the change

--- src/main/java/divinerpg/entities/vanilla/end/EntityEnderWatcher.java
+++ src/main/java/divinerpg/entities/vanilla/end/EntityEnderWatcher.java
@@ -4,7 +4,16 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.monster.EnderMan;
 import net.minecraft.world.level.Level;
 
+import static net.minecraft.sounds.SoundEvents.ENDERMAN_SCREAM;
+
 public class EntityEnderWatcher extends EnderMan {
     //TODO: they still take effects from arrows for some reason
     public EntityEnderWatcher(EntityType<? extends EntityEnderWatcher> type, Level worldIn) {super(type, worldIn);}
+    int lastStareSound;
+    @Override public void playStareSound() {
+        if(tickCount >= lastStareSound + 400) {
+            lastStareSound = tickCount;
+            if(!isSilent()) level().playLocalSound(getX(), getEyeY(), getZ(), ENDERMAN_SCREAM, getSoundSource(), 2.5F, 1, false);
+        }
+    }
 }
