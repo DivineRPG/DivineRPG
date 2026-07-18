9890ae0a2cb5d5eb301d398698c81603a7f0b03f
velocityraptor-real <Velocityraptor_real@proton.me>
2026-07-18 23:59:18 +0200

    made sparkles passify glinthops

--- src/main/java/divinerpg/entities/projectile/throwable/EdenSparkles.java
+++ src/main/java/divinerpg/entities/projectile/throwable/EdenSparkles.java
@@ -1,7 +1,9 @@
 package divinerpg.entities.projectile.throwable;
 
+import divinerpg.entities.eden.EntityGlinthop;
 import divinerpg.entities.projectile.DivineThrownItem;
 import it.unimi.dsi.fastutil.ints.IntList;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
 import net.minecraft.world.item.Item;
@@ -16,13 +18,24 @@ import static divinerpg.registries.ItemRegistry.eden_sparkles;
 import static net.minecraft.sounds.SoundEvents.FIREWORK_ROCKET_BLAST;
 
 public class EdenSparkles extends DivineThrownItem {
+    private boolean flame = true;
     public EdenSparkles(EntityType<? extends ThrowableItemProjectile> entityType, Level level) {super(entityType, level);}
     @Override protected void onHit(HitResult result) {
         super.onHit(result);
         drop();
     }
+
+    @Override
+    protected void onHitEntity(EntityHitResult result) {
+        if(result.getEntity() instanceof EntityGlinthop hop) {
+            if(level() instanceof ServerLevel) hop.setTarget(null);
+            flame = false;
+            drop();
+        } else super.onHitEntity(result);
+    }
+
     @Override public void drop() {
-        level().setBlock(blockPosition(), Blocks.FIRE.defaultBlockState(), 3);
+        if(flame) level().setBlock(blockPosition(), Blocks.FIRE.defaultBlockState(), 3);
         playSound(FIREWORK_ROCKET_BLAST);
         level().broadcastEntityEvent(this, (byte)3);
         discard();
